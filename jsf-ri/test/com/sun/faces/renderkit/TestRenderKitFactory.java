d5962c6d4f611978abf6c0003438fc1e567ac70b
rlubke <rlubke@java.net>
2008-04-01 22:02:58 +0000

    [project @ 2002-05-29 20:46:48 by rkitain] test module
    Original author: rkitain
    Date: 2002-05-29 20:46:48+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@352 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ jsf-ri/test/com/sun/faces/renderkit/TestRenderKitFactory.java
@@ -0,0 +1,145 @@
+/*
+ * $Id: TestRenderKitFactory.java,v 1.1 2002/05/29 20:46:48 rkitain Exp $
+ */
+
+/*
+ * Copyright 2002 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+// TestRenderKitFactory.java
+
+package com.sun.faces.renderkit;
+
+import com.sun.faces.renderkit.RenderKitFactoryImpl;
+
+import java.util.Iterator;
+
+import javax.faces.render.RenderKit;
+import javax.faces.render.RenderKitFactory;
+import javax.faces.FacesException;
+
+import org.mozilla.util.Assert;
+import org.mozilla.util.Debug;
+import org.mozilla.util.ParameterCheck;
+import org.apache.cactus.ServletTestCase;
+
+/**
+ *
+ *  <B>TestRenderKitFactory</B> is a class ...
+ *
+ * <B>Lifetime And Scope</B> <P>
+ *
+ * @version $Id: TestRenderKitFactory.java,v 1.1 2002/05/29 20:46:48 rkitain Exp $
+ * 
+ * @see	Blah
+ * @see	Bloo
+ *
+ */
+
+public class TestRenderKitFactory extends ServletTestCase {
+//
+// Protected Constants
+//
+
+//
+// Class Variables
+//
+
+//
+// Instance Variables
+//
+    private RenderKitFactoryImpl renderKitFactory = null;
+
+// Attribute Instance Variables
+
+// Relationship Instance Variables
+
+//
+// Constructors and Initializers    
+//
+
+    public TestRenderKitFactory() {super("TestRenderKitFactory");}
+    public TestRenderKitFactory(String name) {super(name);}
+//
+// Class methods
+//
+
+//
+// General Methods
+//
+
+    public void testFactory() {
+        renderKitFactory = new RenderKitFactoryImpl();
+
+        // 1. Verify "createRenderKit" returns the same RenderKit instance
+        //    if called multiple times with the same identifier.
+        //  
+        RenderKit renderKit1 = renderKitFactory.createRenderKit("DEFAULT");
+        RenderKit renderKit2 = renderKitFactory.createRenderKit("DEFAULT");
+        assertTrue(renderKit1 == renderKit2);
+
+        // 2. Verify "addRenderKit" adds instances.. /
+        //      "getRenderKitIds returns iteration..
+        //    Should be iteration of "3" because default was added
+        //    via RenderKitFactoryImpl constructor call (above).
+        //
+        renderKitFactory.addRenderKit("Foo", renderKit1);
+        renderKitFactory.addRenderKit("Bar", renderKit2);
+        Iterator iter = renderKitFactory.getRenderKitIds();
+        int i = 0;
+        while (iter.hasNext()) {
+            iter.next();
+            i++;
+        }
+        assertTrue(i == 3);
+        
+    }
+
+    public void testDefaultExists() {
+        renderKitFactory = new RenderKitFactoryImpl();
+    
+        // 1. Verify constructor created "default" renderkit..
+        //
+        RenderKit renderKit;
+        String id = null;
+        Iterator iter = renderKitFactory.getRenderKitIds();
+        while (iter.hasNext()) {
+            id = (String)iter.next();
+        }
+        assertTrue(id.equals(RenderKitFactory.DEFAULT_RENDER_KIT));
+    }
+            
+    public void testExceptions() {
+        renderKitFactory = new RenderKitFactoryImpl();
+        RenderKit rKit = null;
+
+        // 1. Verify "IllegalArg" exception which occurs when attempting
+        //    to add the default (which already exists...
+        //
+        boolean thrown = false;
+        try {
+            rKit = renderKitFactory.createRenderKit(
+                RenderKitFactory.DEFAULT_RENDER_KIT);
+            renderKitFactory.addRenderKit(RenderKitFactory.DEFAULT_RENDER_KIT,
+                rKit);
+        } catch (IllegalArgumentException ia) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+
+        // 2. Verify "IllegalArg exception which occurs when attempting
+        //    to add the same renderkit id.
+        //
+        thrown = false;
+        try {
+            renderKitFactory.addRenderKit("foo", rKit); 
+            renderKitFactory.addRenderKit("foo", rKit); 
+        } catch (IllegalArgumentException ia) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+    }
+            
+            
+} // end of class TestRenderKitFactory
