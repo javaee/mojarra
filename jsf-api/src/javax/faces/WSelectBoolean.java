a9810e3f1e7aa75bf48c95cddba5a5f9b8b51148
rlubke <rlubke@java.net>
2008-04-01 20:28:22 +0000

    [project @ 2001-11-08 02:27:18 by rogerk] added implementation details for WSelectBoolean component.
    Original author: rogerk
    Date: 2001-11-08 02:27:18+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@28 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/javax/faces/WSelectBoolean.java
+++ jsf-api/src/javax/faces/WSelectBoolean.java
@@ -1,11 +1,21 @@
 package javax.faces;
 
+import java.util.Hashtable;
+
 /**
  * Class for representing a user-interface component which allows
  * the user to select a boolean value.
  */
 public class WSelectBoolean extends WComponent {
     private static String TYPE = "SelectBoolean";
+
+//RWK:11-4-2001-kludge using temporarily for storing attributes...
+//setAttribute/getAttribute should work differently...
+    private Hashtable ht;
+
+    public WSelectBoolean() {
+        ht = new Hashtable();
+    }
 
     /** 
      * Returns a String representing the select-boolean type.  
@@ -41,7 +51,7 @@ public class WSelectBoolean extends WComponent {
      * <li>Boolean
      * </ul>
      *
-     * @param value the Boolean object containing the true/valse value
+     * @param value the Boolean object containing the true/false value
      * @throws NullPointerException if value is null
      * @throws FacesException if model is not one of the valid instances
      */
@@ -58,5 +68,33 @@ public class WSelectBoolean extends WComponent {
 	return false;
     }
 
+    /**
+     * Returns the component attribute with the given name
+     * within the specified render context or null if there is the
+     * specified attribute is not set on this component.
+     *
+     * @param rc the render context used to render this component
+     * @param attributeName a String specifying the name of the attribute
+     * @return the Object bound to the attribute name, or null if the
+     *          attribute does not exist.
+     */
+    public Object getAttribute(RenderContext rc, String attributeName) {
+        return ht.get(attributeName);
+    }
+
+    /**
+     * Binds an object to the specified attribute name for this component
+     * within the specified render context.
+     *
+     * @param rc the render context used to render this component
+     * @param attributeName a String specifying the name of the attribute
+     * @param value an Object representing the value of the attribute
+     */
+    public void setAttribute(RenderContext rc, String attributeName,
+        Object value) {
+        if (attributeName != null && value != null) {
+            ht.put(attributeName,value);
+        }
+    }
 }
 
