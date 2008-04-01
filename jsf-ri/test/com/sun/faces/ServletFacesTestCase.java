96c75c392babe86d1ab4755164a2fc2fbef6970a
rlubke <rlubke@java.net>
2008-04-01 22:32:31 +0000

    [project @ 2002-06-19 18:59:06 by eburns]  *  <B>ServletFacesTestCase</B> is a base class that leverages  *  FacesTestCaseService to add Faces specific behavior to that provided  *  by cactus.  This class just delegates all method calls to  *  facesService.
    Original author: eburns
    Date: 2002-06-19 18:59:06+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@457 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ jsf-ri/test/com/sun/faces/ServletFacesTestCase.java
@@ -0,0 +1,156 @@
+/*
+ * $Id: ServletFacesTestCase.java,v 1.1 2002/06/19 18:59:06 eburns Exp $
+ */
+
+/*
+ * Copyright 2002 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+// ServletFacesTestCase.java
+
+package com.sun.faces;
+
+import org.apache.cactus.ServletTestCase;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletConfig;
+
+import javax.servlet.jsp.PageContext;
+
+import javax.faces.context.FacesContext;
+
+/**
+ *
+
+ *  <B>ServletFacesTestCase</B> is a base class that leverages
+ *  FacesTestCaseService to add Faces specific behavior to that provided
+ *  by cactus.  This class just delegates all method calls to
+ *  facesService.
+ *
+ * @version $Id: ServletFacesTestCase.java,v 1.1 2002/06/19 18:59:06 eburns Exp $
+ * 
+ * @see	#facesService
+ *
+ */
+
+public abstract class ServletFacesTestCase extends ServletTestCase implements FacesTestCase
+{
+//
+// Protected Constants
+//
+
+public static final String ENTER_CALLED = FacesTestCaseService.ENTER_CALLED;
+public static final String EXIT_CALLED = FacesTestCaseService.EXIT_CALLED;
+public static final String EMPTY = FacesTestCaseService.EMPTY;
+
+
+//
+// Class Variables
+//
+
+//
+// Instance Variables
+//
+
+// Attribute Instance Variables
+
+// Relationship Instance Variables
+
+/*
+
+* This is the thing you use to get the facesContext and other
+* Faces Objects.
+
+*/
+
+protected FacesTestCaseService facesService = null;
+
+//
+// Constructors and Initializers    
+//
+
+    public ServletFacesTestCase() { 
+	super("ServletFacesTestCase");
+	init();
+    }
+
+    public ServletFacesTestCase(String name) { 
+	super(name);
+	init();
+    }
+
+    protected void init() {
+	facesService = new FacesTestCaseService(this);
+    }
+
+//
+// Class methods
+//
+
+//
+// Methods from FacesTestCase
+//
+
+public ServletConfig getConfig() 
+{
+    return config;
+}
+
+public HttpServletRequest getRequest() 
+{
+    return request;
+}
+
+public HttpServletResponse getResponse()
+{
+    return response;
+}
+
+public PageContext getPageContext()
+{
+    return null;
+}
+
+public boolean sendResponseToFile()
+{
+    return false;
+}
+
+public String getExpectedOutputFilename()
+{
+    return null;
+}
+
+public String [] getLinesToIgnore()
+{
+    return null;
+}
+
+//
+// General Methods
+//
+
+
+public void setUp()
+{
+    facesService.setUp();
+}
+
+public void tearDown()
+{
+    facesService.tearDown();
+}
+
+public FacesContext getFacesContext()
+{
+    return facesService.getFacesContext();
+}
+
+public boolean verifyExpectedOutput()
+{
+    return facesService.verifyExpectedOutput();
+}
+
+} // end of class ServletFacesTestCase
