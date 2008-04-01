c2db579c1baaa1d9af639c013105725265b3f3ba
rlubke <rlubke@java.net>
2008-04-01 22:49:57 +0000

    [project @ 2002-07-16 19:27:51 by craigmcc] [5.5][ResponseWriter] In addition to the low-level methods inherited from java.io.Writer, implement output methods (based on Oracle's OutputMethod contribution) that facilitate creating elements and attributes with appropriate escaping and encoding.
    Original author: craigmcc
    Date: 2002-07-16 19:27:51+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@516 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/javax/faces/context/ResponseWriter.java
+++ jsf-api/src/javax/faces/context/ResponseWriter.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ResponseWriter.java,v 1.1 2002/06/05 03:01:55 craigmcc Exp $
+ * $Id: ResponseWriter.java,v 1.2 2002/07/16 19:27:51 craigmcc Exp $
  */
 
 /*
@@ -10,18 +10,200 @@
 package javax.faces.context;
 
 
+import java.io.IOException;
 import java.io.Writer;
 
 
 /**
- * <p><strong>ResponseWriter</strong> is an interface describing an adapter
- * to an underlying output mechanism for character-based output.</p>
- *
- * <p><strong>FIXME</strong> - Consider adding back some of the convenience
- * mechanisms from <code>javax.faces.OutputMethod</code> in the .03 version
- * of the JSF specification.</p>
+ * <p><strong>ResponseWriter</strong> is an abstract class describing an
+ * adapter to an underlying output mechanism for character-based output.
+ * In addition to the low-level <code>write()</code> methods inherited from
+ * <code>java.io>Writer</code>, this class provides utility methods
+ * that are useful in producing elements and attributes for markup languages
+ * like HTML and XML.</p>
  */
 
 public abstract class ResponseWriter extends Writer {
+
+
+    /**
+     * <p>Flush any text buffered within this object to the underlying
+     * <code>Writer</code>, without flushing that <code>Writer</code> itself.
+     * </p>
+     *
+     * @exception IOException if an input/output error occurs
+     */
+    public abstract void flushBuffer() throws IOException;
+
+
+    /**
+     * <p>Write whatever text should begin a response.</p>
+     *
+     * @exception IOException if an input/output error occurs
+     */
+    public abstract void startDocument() throws IOException;
+
+
+    /**
+     * <p>Write whatever text should end a response.  If there is an open
+     * element that has been created by a call to <code>startElement()</code>,
+     * that element will be closed first.</p>
+     *
+     * @exception IOException if an input/output error occurs
+     */
+    public abstract void endDocument() throws IOException;
+
+
+    /**
+     * <p>Write the start of an element, up to and including the
+     * element name.  Once this method has been called, clients can
+     * call <code>writeAttribute()</code> or <code>writeURIAttribute()</code>
+     * method to add attributes and corresponding values.  The starting
+     * element will be closed (that is, the trailing '>' character added)
+     * on any subsequent call to <code>startElement()</code>,
+     * <code>writeComment()</code>,
+     * <code>writeText()</code>, <code>endElement()</code>, or
+     * <code>endDocument()</code>.</p>
+     *
+     * @param name Name of the element to be started
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception NullPointerException if <code>name</code>
+     *  is <code>null</code>
+     */
+    public abstract void startElement(String name) throws IOException;
+
+
+    /**
+     * <p>Write the end of an element, after closing any open element
+     * created by a call to <code>startElement()</code>.
+     *
+     * @param name Name of the element to be ended
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception NullPointerException if <code>name</code>
+     *  is <code>null</code>
+     */
+    public abstract void endElement(String name) throws IOException;
+
+
+    /**
+     * <p>Write an attribute name and corresponding value (after converting
+     * that text to a String if necessary), after escaping it properly.
+     * This method may only be called after a call to
+     * <code>startElement()</code>, and before the opened element has been
+     * closed.</p>
+     *
+     * @param name Attribute name to be added
+     * @param value Attribute value to be added
+     *
+     * @exception IllegalStateException if this method is called when there
+     *  is no currently open element
+     * @exception IOException if an input/output error occurs
+     * @exception NullPointerException if <code>name</code> or
+     *  <code>value</code> is <code>null</code>
+     */
+    public abstract void writeAttribute(String name, Object value)
+        throws IOException;
+
+
+    /**
+     * <p>Write a URI attribute name and corresponding value (after converting
+     * that text to a String if necessary), after encoding it properly
+     * (for example, '%' encoded for HTML).
+     * This method may only be called after a call to
+     * <code>startElement()</code>, and before the opened element has been
+     * closed.</p>
+     *
+     * @param name Attribute name to be added
+     * @param value Attribute value to be added
+     *
+     * @exception IllegalStateException if this method is called when there
+     *  is no currently open element
+     * @exception IOException if an input/output error occurs
+     * @exception NullPointerException if <code>name</code> or
+     *  <code>value</code> is <code>null</code>
+     */
+    public abstract void writeURIAttribute(String name, Object value)
+        throws IOException;
+
+
+    /**
+     * <p>Write a comment containing the specified text, after converting
+     * that text to a String if necessary.  If there is an open element
+     * that has been created by a call to <code>startElement()</code>,
+     * that element will be closed first.</p>
+     *
+     * @param comment Text content of the comment
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception NullPointerException if <code>comment</code>
+     *  is <code>null</code>
+     */
+    public abstract void writeComment(Object comment) throws IOException;
+
+
+    /**
+     * <p>Write an object (after converting it to a String, if necessary),
+     * after escaping it properly.  If there is an open element
+     * that has been created by a call to <code>startElement()</code>,
+     * that element will be closed first.</p>
+     *
+     * @param text Text to be written
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception NullPointerException if <code>text</code>
+     *  is <code>null</code>
+     */
+    public abstract void writeText(Object text) throws IOException;
+
+
+    /**
+     * <p>Write a single character, after escaping it properly.  If there
+     * is an open element that has been created by a call to
+     * <code>startElement()</code>, that element will be closed first.</p>
+     *
+     * @param text Text to be written
+     *
+     * @exception IOException if an input/output error occurs
+     */
+    public abstract void writeText(char text) throws IOException;
+
+
+    /**
+     * <p>Write text from a character array, after escaping it properly.
+     * This is equivalent to calling <code>writeText(c, 0, c.length)</code>.
+     * If there is an open element that has been created by a call to
+     * <code>startElement()</code>, that element will be closed first.</p>
+     * </p>
+     *
+     * @param text Text to be written
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception NullPointerException if <code>text</code>
+     *  is <code>null</code>
+     */
+    public abstract void writeText(char text[]) throws IOException;
+
+
+    /**
+     * <p>Write text from a character array, after escaping it properly
+     * for this method.  If there is an open element that has been
+     * created by a call to <code>startElement()</code>, that element
+     * will be closed first.</p>
+     *
+     * @param text Text to be written
+     * @param off Starting offset (zero-relative)
+     * @param len Number of characters to be written
+     *
+     * @exception IndexOutOfBoundsException if the calculated starting or
+     *  ending position is outside the bounds of the character array
+     * @exception IOException if an input/output error occurs
+     * @exception NullPointerException if <code>text</code>
+     *  is <code>null</code>
+     */
+    public abstract void writeText(char text[], int off, int len)
+        throws IOException;
+
 
 }
