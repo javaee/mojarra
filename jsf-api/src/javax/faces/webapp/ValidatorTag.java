df406d400aba14f2c7904b556cef8d14e4aeee2c
rlubke <rlubke@java.net>
2008-04-01 23:43:45 +0000

    [project @ 2002-08-29 16:24:38 by craigmcc] Make ValidatorTag an abstract base class for Validator tags that will have custom configuration properties.  JSF implementations will be required to provide ValidatorTag subclasses for each of the standard Validators, which must extend this class.
    Original author: craigmcc
    Date: 2002-08-29 16:24:38+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@740 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/javax/faces/webapp/ValidatorTag.java
+++ jsf-api/src/javax/faces/webapp/ValidatorTag.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ValidatorTag.java,v 1.2 2002/07/31 00:27:47 craigmcc Exp $
+ * $Id: ValidatorTag.java,v 1.3 2002/08/29 16:24:38 craigmcc Exp $
  */
 
 /*
@@ -18,54 +18,28 @@ import javax.servlet.jsp.tagext.TagSupport;
 
 
 /**
- * <p>Tag implementation that creates an {@link Validator} instance
- * and registers it on the {@link UIComponent} associated with our most
- * immediate surrounding instance of a tag whose implementation class
- * is a subclass of {@link FacesTag}.  This tag creates no output to the
- * page currently being created.</p>
+ * <p><strong>ValidatorTag</strong> is a base class for all JSP custom actions
+ * that create and register a <code>Validator</code> instance on the
+ * {@link UIComponent} associated with our most immediate surrounding instance
+ * of a tag whose implementation class is a subclass of {@link FacesTag}.
+ * To avoid creating duplicate instances when a page is redisplayed,
+ * creation and registration of a {@link Validator} occurs
+ * <strong>only</strong> if the corresponding {@link UIComponent} was
+ * created (by the owning {@link FacesTag}) during the execution of the
+ * current page.</p>
  *
- * <p>FIXME - should this class be in jsf-api, or just a spec requirement
- * to provide such a tag with a well known name?</p>
+ * <p>Subclasses of this class must implement the
+ * <code>createValidator()</code> method, which creates and returns a
+ * {@link Validator} instance.  Any configuration properties that specify
+ * the limits to be enforced by this {@link Validator} must have been
+ * set by the <code>createValidator()</code> method.  Generally, this occurs
+ * by copying corresponding attribute values on the tag instance.</p>
+ *
+ * <p>This tag creates no output to the page currently being created.  It
+ * is used solely for the side effect of {@link Validator} creation.</p>
  */
 
-public final class ValidatorTag extends TagSupport {
-
-
-    // ----------------------------------------------------- Instance Variables
-
-
-    /**
-     * <p>The fully qualified class name of the {@link Validator}
-     * instance to be created.</p>
-     */
-    private String className = null;
-
-
-    // ------------------------------------------------------------- Properties
-
-
-    /**
-     * <p>Return the fully qualified class name of the
-     * {@link Validator} instance to be created.
-     */
-    public String getClassName() {
-
-        return (this.className);
-
-    }
-
-
-    /**
-     * <p>Set the fully qualified class name of the
-     * {@link Validator} instance to be created.
-     *
-     * @param className The new class name
-     */
-    public void setClassName(String className) {
-
-        this.className = className;
-
-    }
+public abstract class ValidatorTag extends TagSupport {
 
 
     // --------------------------------------------------------- Public Methods
@@ -74,59 +48,48 @@ public final class ValidatorTag extends TagSupport {
     /**
      * <p>Create a new instance of the specified {@link Validator}
      * class, and register it with the {@link UIComponent} instance associated
-     * with our most immediately surrounding {@link FacesTag} instance.
+     * with our most immediately surrounding {@link FacesTag} instance, if
+     * the {@link UIComponent} instance was created by this execution of the
+     * containing JSP page.</p>
      *
      * @exception JspException if a JSP error occurs
      */
     public int doStartTag() throws JspException {
 
-        try {
+        // Locate our parent FacesTag
+        Tag tag = getParent();
+        while ((tag != null) && !(tag instanceof FacesTag)) {
+            tag = tag.getParent();
+        }
+        if (tag == null) { // FIXME - i18n
+            throw new JspException("Not nested in a FacesTag");
+        }
+        FacesTag facesTag = (FacesTag) tag;
 
-            // Locate our parent FacesTag
-            Tag tag = getParent();
-            while ((tag != null) && !(tag instanceof FacesTag)) {
-                tag = tag.getParent();
-            }
-            if (tag == null) { // FIXME - i18n
-                throw new JspException("Not nested in a FacesTag");
-            }
-            FacesTag facesTag = (FacesTag) tag;
-
-            // Nothing to do unless this tag created a component
-            if (!facesTag.getCreated()) {
-                return (SKIP_BODY);
-            }
-
-            // Create a new instance of the specified class
-            ClassLoader classLoader =
-                Thread.currentThread().getContextClassLoader();
-            if (classLoader == null) {
-                classLoader = this.getClass().getClassLoader();
-            }
-            Class clazz = classLoader.loadClass(className);
-            Validator validator = (Validator) clazz.newInstance();
-
-            // Register this instance with the appropriate component
-            facesTag.getComponent().addValidator(validator);
+        // Nothing to do unless this tag created a component
+        if (!facesTag.getCreated()) {
             return (SKIP_BODY);
-
-        } catch (Exception e) {
-
-            throw new JspException(e);
-
         }
 
+        // Create a new instance of the specified Validator
+        Validator validator = createValidator();
+
+        // Register this instance with the appropriate component
+        facesTag.getComponent().addValidator(validator);
+        return (SKIP_BODY);
+
     }
+
+
+    // ------------------------------------------------------ Protected Methods
 
 
     /**
-     * <p>Release references to any acquired resources.
+     * <p>Create and return a {@link Validator} instance, to be registered
+     * with our surrounding {@link UIComponent}, that has been configured
+     * based on the attributes used on this tag instance.</p>
      */
-    public void release() {
-
-        this.className = null;
-
-    }
+    protected abstract Validator createValidator();
 
 
 }
