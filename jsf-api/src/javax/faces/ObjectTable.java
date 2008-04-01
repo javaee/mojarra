2f9ca8770de037edca922b92fea9c7b7107f250f
rlubke <rlubke@java.net>
2008-04-01 20:32:44 +0000

    [project @ 2001-11-13 02:09:13 by edburns]
    Modified Files:
     	src/javax/faces/RenderContext.java
    Added way to get ObjectTable from RenderContext.
    
    Added Files:
     	src/javax/faces/ObjectTable.java
    Hans's ObjectTable, with some mods.
    
    Original author: edburns
    Date: 2001-11-13 02:09:13+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@48 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ jsf-api/src/javax/faces/ObjectTable.java
@@ -0,0 +1,148 @@
+/*
+ * $Id: ObjectTable.java,v 1.1 2001/11/13 02:09:13 edburns Exp $
+ *
+ * Copyright 2000-2001 by Sun Microsystems, Inc.,
+ * 901 San Antonio Road, Palo Alto, California, 94303, U.S.A.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information
+ * of Sun Microsystems, Inc. ("Confidential Information").  You
+ * shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with Sun.
+ */
+
+// ObjectTable.java
+
+package javax.faces;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ *  <B>ObjectTable</B> is a class ...
+ *
+ * <B>Lifetime And Scope</B> <P>
+ *
+ * @version $Id: ObjectTable.java,v 1.1 2001/11/13 02:09:13 edburns Exp $
+ * 
+ * @see	Blah
+ * @see	Bloo
+ *
+ */
+
+public abstract class ObjectTable
+{
+    public interface ActiveValue {
+        Object getValue(Scope scope, Object scopeKey, Object name);
+    }
+
+
+    public interface LazyValue {
+        Object getValue(Scope scope, Object scopeKey, Object name);
+    }
+
+
+    public interface ManagedValue {
+        void willRemove(Scope scope, Object scopeKey, Object name);
+        void didRemove(Scope scope, Object scopeKey, Object name);
+    }
+
+
+    public static interface Scope {
+        public boolean isA(Object scopeKey);
+
+	public void put(Object name, Object value);
+
+	public Object get(Object scopeKey, Object name);
+
+	public void enter(Object scopeKey);
+
+	public void exit(Object scopeKey);
+    }
+
+
+    public static Scope RequestScope;
+
+    public static Scope SessionScope;
+
+    public static Scope GlobalScope;
+
+private static Scope keyToScope(Object scopeKey) {
+    Scope result = null;
+    if (scopeKey instanceof HttpServletRequest) {
+	result = RequestScope;
+    }
+    else if (scopeKey instanceof HttpSession) {
+	result = SessionScope;
+    }
+    else {
+	result = GlobalScope;
+    }
+    return result;
+}
+
+    public static void put(Scope scope, Object name, Object value) {
+	scope.put(name, value);
+    }
+
+
+    public static void put(Scope scope, Object name, Class value) {
+	final Class finalValue = value;
+	Object createValue = new LazyValue() {
+	    public Object getValue(Scope scope, Object scopeKey, Object name) {
+		Object result = null;
+		try {
+		    result = finalValue.newInstance();
+		}
+		catch (Exception e) {
+		    System.out.println(e.getMessage());
+		}
+		return result;
+	    }
+        };
+	scope.put(name, createValue);
+    }
+
+
+    public static Object get(Object scopeKey, Object name) {
+	Scope scope = keyToScope(scopeKey);
+	return scope.get(scopeKey, name);
+    }
+
+
+    public static Object get(Object name) {
+	return GlobalScope.get("global", name);
+    }
+
+    // PENDING(edburns): why does ObjectTable itself have enter and exit?
+    public static void enter(Object scopeKey) {
+	Scope scope = keyToScope(scopeKey);
+	scope.enter(scopeKey);
+    }
+
+
+    public static void exit(Object scopeKey) {
+	Scope scope = keyToScope(scopeKey);
+	scope.exit(scopeKey);
+    }
+
+
+    public static List getScopes() {
+	// return a copy of the internal list
+	return null;
+    }
+
+
+    public static void setScopes(List scopes) {
+	// make a copy of the list parameter
+    }
+}
+
+				   
+
