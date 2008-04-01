16b67d9b1cea4865643a70a687e1fe5aa10ed03a
rlubke <rlubke@java.net>
2008-04-01 20:49:15 +0000

    [project @ 2001-12-06 23:30:45 by visvan]
    Original author: visvan
    Date: 2001-12-06 23:30:45+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@125 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ jsf-api/src/javax/faces/AbstractCommand.java
@@ -0,0 +1,62 @@
+package javax.faces;
+
+/**
+ * An abstract implementation of a command object which stores
+ * the completionPath and errorPath property values.
+ */
+public abstract class AbstractCommand implements Command {
+
+    private String completionPath;
+    private String errorPath;
+
+    protected AbstractCommand() {
+    }
+
+    /**
+     * Constructs a command object with the specified completionPath
+     * and errorPath.
+     * @param completionPath String containing the completion redirection 
+     *        path, or null if no completion path is specified for this command
+     * @param errorPath String containing the error redirection path, or
+     *        null if no error path is specified for this command
+     */
+    protected AbstractCommand(String completionPath, String errorPath) {
+	this.completionPath = completionPath;
+	this.errorPath = errorPath;
+    }
+
+    /**
+     * Invoked in response to a command event.  Subclasses
+     * should override this method to provide the application
+     * specific command behavior.
+     * @param event the CommandEvent object which invoked this command listener
+     * @throws CommandFailedException if the command could not
+     *         be performed successfully
+     */
+    public abstract void doCommand(CommandEvent event) 
+	                           throws CommandFailedException; 
+    
+
+    /**
+     * The completion-path redirection property.  
+     * @see Command#getCompletionPath
+     * @param event the CommandEvent object which invoked this command listener
+     * @return String containing the completion redirection path, 
+     *         or null if no completion path is specified for this command
+     */
+    public String getCompletionPath(CommandEvent event) {
+	return completionPath;
+    }
+
+    /**
+     * The error-path redirection property.  
+     * @see Command#getErrorPath
+     * @param event the CommandEvent object which invoked this command listener
+     * @return String containing the error-redirection path, 
+     *         or null if no error-path is specified for this command
+     */
+    public String getErrorPath(CommandEvent event) {
+	return errorPath;
+    }
+ 
+}
