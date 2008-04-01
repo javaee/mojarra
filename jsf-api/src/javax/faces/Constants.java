dfc54811315c56b6965640e1225e720219ab0512
rlubke <rlubke@java.net>
2008-04-01 20:42:17 +0000

    [project @ 2001-11-29 01:28:19 by rogerk] Faces constants for string literals.
    Original author: rogerk
    Date: 2001-11-29 01:28:19+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@91 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ jsf-api/src/javax/faces/Constants.java
@@ -0,0 +1,76 @@
+/*
+ * $Id: Constants.java,v 1.1 2001/11/29 01:28:19 rogerk Exp $
+ *
+ * Copyright 2000-2001 by Sun Microsystems, Inc.,
+ * 901 San Antonio Road, Palo Alto, California, 94303, U.S.A.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information
+ * of Sun Microsystems, Inc. ("Confidential Information").  You
+ * shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with Sun.
+ */
+
+package javax.faces;
+
+/**
+ * This class contains literal strings used throughout Faces.
+ */
+public class Constants {
+
+    /**
+     * The Faces data structure used to store Faces objects in
+     * specified scopes. 
+     */
+    public static final String REF_OBJECTTABLE = "faces.ObjectTable";
+
+    /**
+     * The event queue used to queue Faces events.
+     */
+    public static final String REF_EVENTQUEUE = "faces.EventQueue";
+
+    /**
+     * The factory used to create the event queue.
+     */
+    public static final String REF_EVENTQUEUEFACTORY = 
+        "faces.EventQueueFactory";
+
+    /**
+     * The Faces object which contains resources used for rendering 
+     * user interface components.
+     */ 
+    public static final String REF_RENDERCONTEXT = "faces.RenderContext";
+
+    /**
+     * The factory used to create the render context.
+     */
+    public static final String REF_RENDERCONTEXTFACTORY = 
+        "faces.RenderContextFactory";
+
+    /**
+     * The component type name for 'WCommand' components.
+     */
+    public static final String REF_WCOMMAND = "WCommand";
+
+    /**
+     * The component type name for 'WForm' components.
+     */
+    public static final String REF_WFORM = "WForm";
+
+    /**
+     * The component type name for 'WOutput' components.
+     */
+    public static final String REF_WOUTPUT = "WOutput";
+
+    /**
+     * The component type name for 'WSelectBoolean' components.
+     */
+    public static final String REF_WSELECTBOOLEAN = "WSelectBoolean";
+
+    /**
+     * The component type name for 'WTextEntry' components.
+     */
+    public static final String REF_WTEXTENTRY = "WTextEntry";
+    
+}
