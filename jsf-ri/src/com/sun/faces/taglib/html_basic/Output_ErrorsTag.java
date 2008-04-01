12cbf812da19472603990777b4d481fee2179b16
rlubke <rlubke@java.net>
2008-04-01 23:32:00 +0000

    [project @ 2002-08-19 21:37:01 by rkitain] new tag for displaying errors
    Original author: rkitain
    Date: 2002-08-19 21:37:01+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@701 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ jsf-ri/src/com/sun/faces/taglib/html_basic/Output_ErrorsTag.java
@@ -0,0 +1,118 @@
+/*
+ * $Id: Output_ErrorsTag.java,v 1.1 2002/08/19 21:37:01 rkitain Exp $
+ */
+
+/*
+ * Copyright 2002 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+// Output_ErrorsTag.java
+
+package com.sun.faces.taglib.html_basic;
+
+import com.sun.faces.taglib.FacesTag;
+
+import org.mozilla.util.Assert;
+import org.mozilla.util.ParameterCheck;
+
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIOutput;
+
+/**
+ *
+
+ *  <B>Output_ErrorsTag</B> is a convenience tag allowing content
+ *  authors flexible error reporting.  See the tld for
+ *  attribute description.
+
+ *
+
+ * @version $Id: Output_ErrorsTag.java,v 1.1 2002/08/19 21:37:01 rkitain Exp $
+ * 
+ * @see	Blah
+ * @see	Bloo
+ *
+ */
+
+public class Output_ErrorsTag extends FacesTag {
+
+//
+// Protected Constants
+//
+
+//
+// Class Variables
+//
+
+//
+// Instance Variables
+//
+
+// Attribute Instance Variables
+
+    protected String compoundId = null;
+    protected String color = null;
+
+// Relationship Instance Variables
+
+//
+// Constructors and Initializers    
+//
+
+    public Output_ErrorsTag() {
+        super();
+    }
+
+//
+// Class methods
+//
+
+//
+// Accessors
+//
+
+    public String getCompoundId() {
+        return compoundId;
+    }
+
+    public void setCompoundId(String newCompoundId) {
+        compoundId = newCompoundId;
+    }
+
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String newColor) {
+        color = newColor;
+    }
+
+//
+// General Methods
+//
+
+    public String getLocalRendererType() {return "ErrorsRenderer"; }
+
+    public UIComponent createComponent() {
+        return (new UIOutput());
+    }
+
+    protected void overrideProperties(UIComponent component) {
+        super.overrideProperties(component);
+        UIOutput output = (UIOutput) component;
+
+        if (null == component.getAttribute("compId")) {
+            component.setAttribute("compId", getCompoundId());
+        }
+        if (null == component.getAttribute("color")) {
+            component.setAttribute("color", getColor());
+        }
+    }
+//
+// Methods from TagSupport
+//
+
+
+} // end of class Output_ErrorsTag
