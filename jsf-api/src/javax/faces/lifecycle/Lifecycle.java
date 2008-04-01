60f794d2300a702412fc86003d21ba133e81ffa5
rlubke <rlubke@java.net>
2008-04-01 22:17:41 +0000

    [project @ 2002-06-07 22:43:22 by craigmcc] Remove detailed description of each phase that is now in the spec.
    Original author: craigmcc
    Date: 2002-06-07 22:43:22+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@407 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/javax/faces/lifecycle/Lifecycle.java
+++ jsf-api/src/javax/faces/lifecycle/Lifecycle.java
@@ -1,5 +1,5 @@
 /*
- * $Id: Lifecycle.java,v 1.7 2002/05/17 23:38:03 craigmcc Exp $
+ * $Id: Lifecycle.java,v 1.8 2002/06/07 22:43:22 craigmcc Exp $
  */
 
 /*
@@ -63,134 +63,49 @@ public abstract class Lifecycle {
 
 
     /**
-     * <p>Phase identifier for <em>Create Request Tree</em>.  This
-     * {@link Phase} must perform the following actions:</p>
-     * <ul>
-     * <li>Calculate the logical page identifer of the previous
-     *     JavaServer Faces response that created this page (typically
-     *     extracted from the request URI and/or request parameters),
-     *     and save this value by calling the <code>setRequestPageId()</code>
-     *     method of {@link FacesContext}.</p>
-     * <li>Look up component tree metadata the corresponds to the request
-     *     page identifier, construct a corresponding component tree, and
-     *     save this value by calling the <code>setRequestTree()</code>
-     *     method of {@link FacesContext}.</p>
-     * </ul>
+     * <p>Phase identifier for <em>Create Request Tree</em>.</p>
      */
     public static final int CREATE_REQUEST_TREE_PHASE = 0;
 
 
     /**
-     * <p>Phase identifier for <em>Reconstitute Request Tree</em>.  This
-     * {@link Phase} must ensure that all components of the request tree
-     * reflect the render-independent properties, and render-dependent
-     * attributes, that were current as of the previous response.  If
-     * the previous response caused state information to be serialized into
-     * the response page (and included in this subsequent request), it will
-     * typically be deserialized here and used to restore state, along with
-     * any state information saved in the user's session.</p>
+     * <p>Phase identifier for <em>Reconstitute Request Tree</em>.</p>
      */
     public static final int RECONSTITUTE_REQUEST_TREE_PHASE = 1;
 
 
     /**
-     * <p>Phase identifier for <em>Apply Request Values</em>.  This
-     * {@link Phase} must notify each {@link UIComponent} in the request tree
-     * (in top-down, left-to-right order) via a call to the
-     * <code>applyRequestValues()</code> method of that component.</p>
+     * <p>Phase identifier for <em>Apply Request Values</em>.</p>
      */
     public static final int APPLY_REQUEST_VALUES_PHASE = 2;
 
 
     /**
-     * <p>Phase identifier for <em>Handle Request Events</em>.  This
-     * {@link Phase} must notify each {@link UIComponent} in the request tree
-     * (in top-down, left-to-right order), via a call to the
-     * <code>handleRequestEvents()</code> method of each component,
-     * that it has the opportunity to
-     * handle any events that were queued for that component.</p>
-     *
-     * <p>If one or more event handlers determines that the sole purpose of
-     * this request to the server was to modify the render-dependent state
-     * of a particular component (such as expanding or contracting a node
-     * in a tree control), they will typically cause this {@link Phase} to
-     * return the value <code>Lifecycle.RENDER_RESPONSE_PHASE</code>.
-     * (<strong>FIXME</strong> how to make this easy?)  Otherwise, this
-     * {@link Phase} should return <code>Lifecycle.NEXT_PHASE</code> in the
-     * usual way.</p>
+     * <p>Phase identifier for <em>Handle Request Events</em>.</p>
      */
     public static final int HANDLE_REQUEST_EVENTS_PHASE = 3;
 
 
     /**
-     * <p>Phase identifier for <em>Process Validations</em>.  This
-     * {@link Phase} must request each {@link UIComponent} in the request tree
-     * (in top-down, left-to-right order), via a call to the
-     * <code>processValidations()</code> method, to perform all validation
-     * checks associated with that component (<strong>FIXME</strong> - specify
-     * the mechanism by which validations are registered).</p>
-     *
-     * <p>If one or more validation errors have been accumulated, this
-     * {@link Phase} will typically return
-     * <code>Lifecycle.RENDER_RESPONSE_PHASE</code>, typically to the original
-     * input page but optionally elsewhere.</p>
+     * <p>Phase identifier for <em>Process Validations</em>.</p>
      */
     public static final int PROCESS_VALIDATIONS_PHASE = 4;
 
 
     /**
-     * <p>Phase identifier for <em>Update Model Values</em>.  This
-     * {@link Phase} must request each {@link UIComponent} in the request tree
-     * (in top-down, left-to-right order), via a call to the
-     * <code>updateModelValues</code> method, to update any corresponding
-     * model data that is associated with this component.</p>
-     *
-     * <p><strong>FIXME</strong> - Specify handling of conversion exceptions
-     * that are encountered here (most of them <em>should</em> have been
-     * caught by the conversion attempt during validations, but ...).</p>
+     * <p>Phase identifier for <em>Update Model Values</em>.</p>
      */
     public static final int UPDATE_MODEL_VALUES_PHASE = 5;
 
 
     /**
-     * <p>Phase identifier for <em>Invoke Application</em>.  This
-     * {@link Phase} is responsible for invoking event handler(s) in the
-     * web application that is utilizing JavaServer Faces, for the purpose
-     * of performing the requested transactions.  An application will
-     * typically perform one or more of the followin actions:</p>
-     * <ul>
-     * <li>Perform the business logic associated with the incoming request
-     *     (typically based on some sort of mapping or dispatch action).</li>
-     * <li>Select the page identifier of the response page to be created
-     *     (defaults to the same as the request page identifier.</li>
-     * <li>If the response page identifier is different from the request
-     *     page identifier, construct a new response component tree by using
-     *     the response page identifier to look up the corresponding metadata
-     *     and construct a tree with default values.</li>
-     * <li>Customize the response tree as desired.</li>
-     * </ul>
-     *
-     * <p><strong>FIXME</strong> - Is there one application event handler,
-     * or are there many (i.e. dispatch off request URI or a request parameter
-     * value)?  Is there at most one application "event" per request, or do we
-     * allow components to queue up several of them?  Is this really just a
-     * matter of processing any <code>UICommand</code> event(s)?</p>
+     * <p>Phase identifier for <em>Invoke Application</em>.</p>
      */
     public static final int INVOKE_APPLICATION_PHASE = 6;
 
 
     /**
-     * <p>Phase identifier for <em>Render Response</em>.  This
-     * {@link Phase} will utilize the selected response page identifier to
-     * invoke the appropriate rendering technology (for example, if JSP
-     * pages are utilized, it will perform a
-     * <code>RequestDispatcher.forward()</code> to the appropriate JSP page).
-     * Typically, this {@link Phase} will return
-     * <code>Lifecycle.EXIT_PHASE</code> to signal completion of the request
-     * processing lifecycle.</p>
-     *
-     * <p><strong>FIXME</strong> - Where does serializing the current state
-     * information fit in?  Is it a separate phase prior to this one?</p>
+     * <p>Phase identifier for <em>Render Response</em>.</p>
      */
     public static final int RENDER_RESPONSE_PHASE = 7;
 
