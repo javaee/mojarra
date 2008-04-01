059e8c350ea105aae6560215842992e0f00a5f26
rlubke <rlubke@java.net>
2008-04-01 20:27:20 +0000

    [project @ 2001-11-08 00:10:53 by visvan] M build.xml removed references to manifest M src/javax/faces/WForm.java added Hashtable M src/javax/faces/WOutput.java added Hashtable added setValue and getValue methods.
    Original author: visvan
    Date: 2001-11-08 00:10:54+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@23 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/javax/faces/WForm.java
+++ jsf-api/src/javax/faces/WForm.java
@@ -1,5 +1,7 @@
 package javax.faces;
 
+import java.util.Hashtable;
+
 /**
  * Class for representing a form user-interface component. 
  * A form encapsulates the process of taking a collection of
@@ -10,6 +12,13 @@ public class WForm extends WComponent {
 
     private static String TYPE = "Form";
 
+    // JV revisit later
+    private Hashtable ht = null;
+
+    public WForm() {
+        ht = new Hashtable();
+    }
+
     /** 
      * Returns a String representing the form's type.  
      *
@@ -18,6 +27,35 @@ public class WForm extends WComponent {
      */
     public String getType() {
 	return TYPE;
+    }
+
+    /**
+     * Returns the component attribute with the given name
+     * within the specified render context or null if there is the
+     * specified attribute is not set on this component.
+     *
+     * @param rc the render context used to render this component
+     * @param attributeName a String specifying the name of the attribute
+     * @return the Object bound to the attribute name, or null if the
+     *          attribute does not exist.
+     */
+    public Object getAttribute(RenderContext rc, String attributeName) {
+        return ht.get(attributeName);
+    }
+
+    /**
+     * Binds an object to the specified attribute name for this component
+     * within the specified render context.
+     *
+     * @param rc the render context used to render this component
+     * @param attributeName a String specifying the name of the attribute
+     * @param value an Object representing the value of the attribute
+     */
+    public void setAttribute(RenderContext rc, String attributeName,
+        Object value) {
+        if (attributeName != null && value != null) {
+            ht.put(attributeName,value);
+        }
     }
 
 }
--- jsf-api/src/javax/faces/WOutput.java
+++ jsf-api/src/javax/faces/WOutput.java
@@ -1,12 +1,23 @@
 package javax.faces;
 
+import java.util.Hashtable;
+
 /**
  * Class for representing a user-interface component which displays
  * output to the user.  This component type is not interactive -
  * a user cannot directly manipulate this component.
  */
 public class WOutput extends WComponent {
+
     private static String TYPE = "Output";
+    private Object value = null;
+
+    // JV revisit later.
+    private Hashtable ht = null;
+
+    public WOutput() {
+        ht = new Hashtable();
+    }
 
     /** 
      * Returns a String representing the this component type.  
@@ -19,10 +30,40 @@ public class WOutput extends WComponent {
     }
 
     public Object getValue() {
-	return null;
+	return value;
     }
 
     public void setValue(Object value) {
+        this.value = value;
+    }
+
+    /**
+     * Returns the component attribute with the given name
+     * within the specified render context or null if there is the
+     * specified attribute is not set on this component.
+     *
+     * @param rc the render context used to render this component
+     * @param attributeName a String specifying the name of the attribute
+     * @return the Object bound to the attribute name, or null if the
+     *          attribute does not exist.
+     */
+    public Object getAttribute(RenderContext rc, String attributeName) {
+        return ht.get(attributeName);
+    }
+
+    /**
+     * Binds an object to the specified attribute name for this component
+     * within the specified render context.
+     *
+     * @param rc the render context used to render this component
+     * @param attributeName a String specifying the name of the attribute
+     * @param value an Object representing the value of the attribute
+     */
+    public void setAttribute(RenderContext rc, String attributeName,
+        Object value) {
+        if (attributeName != null && value != null) {
+            ht.put(attributeName,value);
+        }
     }
 
 }
