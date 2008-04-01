b3b7acd731096c16364d4644f872a7e9223c6c2f
rlubke <rlubke@java.net>
2008-04-01 23:31:50 +0000

    [project @ 2002-08-19 21:36:21 by rkitain] new Renderer for output_errors tag;
    Original author: rkitain
    Date: 2002-08-19 21:36:21+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@700 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ jsf-ri/src/com/sun/faces/renderkit/html_basic/ErrorsRenderer.java
@@ -0,0 +1,178 @@
+/*
+ * $Id: ErrorsRenderer.java,v 1.1 2002/08/19 21:36:21 rkitain Exp $
+ */
+
+/*
+ * Copyright 2002 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+// ErrorsRenderer.java
+
+package com.sun.faces.renderkit.html_basic;
+
+import com.sun.faces.util.Util;
+
+import java.util.Iterator;
+
+import javax.faces.component.AttributeDescriptor;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIOutput;
+import javax.faces.context.FacesContext;
+import javax.faces.context.Message;
+import javax.faces.context.ResponseWriter;
+
+import org.mozilla.util.Assert;
+import org.mozilla.util.Debug;
+import org.mozilla.util.Log;
+import org.mozilla.util.ParameterCheck;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ *  <B>ErrorsRenderer</B> is a class ...
+ *
+ * <B>Lifetime And Scope</B> <P>
+ *
+ * @version $Id: ErrorsRenderer.java,v 1.1 2002/08/19 21:36:21 rkitain Exp $
+ * 
+ * @see	Blah
+ * @see	Bloo
+ *
+ */
+
+public class ErrorsRenderer extends HtmlBasicRenderer {
+    //
+    // Protected Constants
+    //
+
+    //
+    // Class Variables
+    //
+
+    //
+    // Instance Variables
+    //
+
+    // Attribute Instance Variables
+
+
+    // Relationship Instance Variables
+
+    //
+    // Constructors and Initializers    
+    //
+
+    public ErrorsRenderer() {
+        super();
+    }
+
+    //
+    // Class methods
+    //
+
+    //
+    // General Methods
+    //
+
+    //
+    // Methods From Renderer
+    //
+    public boolean supportsComponentType(String componentType) {
+        if ( componentType == null ) {
+            throw new NullPointerException(Util.getExceptionMessage(Util.NULL_PARAMETERS_ERROR_MESSAGE_ID));
+        }    
+        return (componentType.equals(UIOutput.TYPE));
+    }
+
+    public void decode(FacesContext context, UIComponent component) 
+            throws IOException {
+        return;
+    }
+
+    public void encodeBegin(FacesContext context, UIComponent component) 
+            throws IOException {
+        if (context == null || component == null) {
+            throw new NullPointerException(Util.getExceptionMessage(Util.NULL_PARAMETERS_ERROR_MESSAGE_ID));
+        }
+    }
+
+    public void encodeChildren(FacesContext context, UIComponent component) {
+        if (context == null || component == null) {
+            throw new NullPointerException(Util.getExceptionMessage(Util.NULL_PARAMETERS_ERROR_MESSAGE_ID));
+        }
+    }
+
+    public void encodeEnd(FacesContext context, UIComponent component) 
+            throws IOException {
+        Iterator messageIter = null;
+        String curColor = null;
+        Message curMessage = null;
+        ResponseWriter writer = null;
+        
+        if (context == null || component == null) {
+            throw new NullPointerException(Util.getExceptionMessage(Util.NULL_PARAMETERS_ERROR_MESSAGE_ID));
+        }
+       
+        writer = context.getResponseWriter();
+        Assert.assert_it(writer != null );
+        
+        // Attempt to use the compoundId expression to locate 
+        // messages.  Threee possible scenarios here:
+        // 1. valid compoundId expression - messages returned
+        //    for valid component identified by compoundId expression.
+        // 2. zero length compoundId expression - global errors
+        //    not associated with any component returned
+        // 3. no compoundId expression - all messages returned.
+        // 
+        String compoundId = (String)component.getAttribute("compId");
+        if (null != compoundId) {
+            if (compoundId.length() == 0) {
+                messageIter = context.getMessages(null);
+            } else {
+                UIComponent root = context.getResponseTree().getRoot();
+                Assert.assert_it(null != root);
+                UIComponent comp = null;
+                try {
+                    comp = root.findComponent(compoundId);
+                } catch (Throwable t) {
+                    Object[] params = {compoundId};
+                    throw new RuntimeException(Util.getExceptionMessage(
+                        Util.COMPONENT_NOT_FOUND_ERROR_MESSAGE_ID, params));
+                }
+                messageIter = context.getMessages(comp);
+            }
+        } else {
+            messageIter = context.getMessages();
+        }
+        Assert.assert_it(null != messageIter);
+
+        String color = (String)component.getAttribute("color");
+        if (null == color) {
+            color = "RED";
+        }
+
+        boolean wroteIt = false;
+        if (messageIter.hasNext()) {
+            writer.write("\n<FONT COLOR=\"" + color + "\">");
+            wroteIt = true;
+        }
+        while (messageIter.hasNext()) {
+            curMessage = (Message) messageIter.next();
+            writer.write("\t" + curMessage.getSummary() + "<BR>");
+            writer.write("\tSeverity: " + curMessage.getSeverity() + "<BR>");
+            writer.write("\t" + curMessage.getDetail());
+        }
+        if (wroteIt) {
+            writer.write("</FONT>");
+        }
+    }
+    
+    // The testcase for this class is TestRenderers_2.java 
+
+} // end of class ErrorsRenderer
+
+
