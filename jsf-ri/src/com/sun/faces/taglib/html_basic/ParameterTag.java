91f94153107f612550948e959b648fd2a7f56dfa
rlubke <rlubke@java.net>
2008-04-01 23:45:15 +0000

    [project @ 2002-08-30 17:52:56 by rkitain] tags to support output messaging
    Original author: rkitain
    Date: 2002-08-30 17:52:56+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@746 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ jsf-ri/src/com/sun/faces/taglib/html_basic/ParameterTag.java
@@ -0,0 +1,83 @@
+/*
+ * $Id: ParameterTag.java,v 1.1 2002/08/30 17:52:56 rkitain Exp $
+ */
+
+/*
+ * Copyright 2002 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+// ParameterTag.java
+
+package com.sun.faces.taglib.html_basic;
+
+import com.sun.faces.taglib.FacesTag;
+
+import java.io.IOException;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIParameter;
+import javax.servlet.jsp.JspException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.mozilla.util.Assert;
+import org.mozilla.util.ParameterCheck;
+
+
+public class ParameterTag extends FacesTag {
+
+//
+// Protected Constants
+//
+
+//
+// Class Variables
+//
+
+//
+// Instance Variables
+//
+
+// Attribute Instance Variables
+
+
+// Relationship Instance Variables
+
+//
+// Constructors and Initializers
+//
+
+    public ParameterTag() {
+        super();
+    }
+
+//
+// Class methods
+//
+
+//
+// Accessors
+//
+
+//
+// General Methods
+//
+
+    public String getLocalRendererType() { return null; }
+
+    public UIComponent createComponent() {
+        return (new UIParameter());
+    }
+
+    protected void overrideProperties(UIComponent component) {
+        super.overrideProperties(component);
+        UIParameter parameter = (UIParameter)component;
+
+        if ((getName() != null) && (parameter.getName() == null)) {
+            parameter.setName(name);
+        }
+        if ((getValue() != null) && (parameter.getValue() == null)) {
+            parameter.setValue(value);
+        }
+    }
+}
