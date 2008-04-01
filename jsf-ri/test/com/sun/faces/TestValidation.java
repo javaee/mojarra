61468793993062aab460fad07f7bc5a5db17e915
rlubke <rlubke@java.net>
2008-04-01 21:34:33 +0000

    [project @ 2002-03-19 22:19:05 by jvisvanathan] M build-tests.xml   added TestValidation under suite of tests.
    A test/com/sun/faces/TestValidation.java
      unit test converters and validators
    
    Original author: jvisvanathan
    Date: 2002-03-19 22:19:05+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@245 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ jsf-ri/test/com/sun/faces/TestValidation.java
@@ -0,0 +1,180 @@
+/*
+ * $Id: TestValidation.java,v 1.1 2002/03/19 22:19:05 jvisvanathan Exp $
+ */
+
+/*
+ * Copyright 2002 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+// TestValidation.java
+
+package com.sun.faces;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.ServletException;
+import javax.servlet.ServletResponse;
+import javax.servlet.ServletRequest;
+
+import javax.faces.ObjectManager;
+import javax.faces.UITextEntry;
+import javax.faces.ObjectAccessor;
+import javax.faces.FormatValidator;
+import javax.faces.RangeValidator;
+import javax.faces.LengthValidator;
+import javax.faces.RequiredValidator;
+import javax.faces.Validatible;
+import com.sun.faces.util.Util;
+
+/**
+ *
+ *  <B>TestValidation</B> is a class ...
+ *
+ * <B>Lifetime And Scope</B> <P>
+ *
+ * @version $Id: TestValidation.java,v 1.1 2002/03/19 22:19:05 jvisvanathan Exp $
+ * 
+ * @see	Blah
+ * @see	Bloo
+ *
+ */
+
+public class TestValidation extends FacesTestCase
+{
+//
+// Protected Constants
+//
+
+//
+// Class Variables
+//
+
+//
+// Instance Variables
+//
+
+// Attribute Instance Variables
+
+// Relationship Instance Variables
+
+//
+// Constructors and Initializers    
+//
+
+    public TestValidation() {super("TestValidation");}
+    public TestValidation(String name) {super(name);}
+//
+// Class methods
+//
+
+//
+// Methods from TestCase
+//
+
+//
+// General Methods
+//
+
+public void testValidators() 
+{
+    int state = 13;
+
+    UITextEntry input = new UITextEntry();
+    input.setId("source");
+
+    // Test BooleanConverter
+    System.out.println("Testing BooleanConverter");
+    input.setValue("test");
+    input.setAttribute("modelType", "java.lang.Boolean");
+    input.doValidate(eventContext);
+    state = input.getValidState();
+    assertTrue(state == Validatible.VALID);
+ 
+    // Test DateConverter
+    System.out.println("Testing DateConverter");
+    input.setValue("06/27/1990");
+    input.setAttribute("modelType", "java.util.Date");
+    input.doValidate(eventContext);
+    state = input.getValidState();
+    assertTrue(state == Validatible.VALID);
+    
+
+    System.out.println("Testing DateConverter");
+    input.setValue("test");
+    input.setAttribute("modelType", "java.util.Date");
+    input.doValidate(eventContext);
+    state = input.getValidState();
+    assertTrue(state == Validatible.INVALID);
+
+    // Test RequiredValidator.
+    System.out.println("Testing RequiredValidator");
+    input.setValue(null);
+    RequiredValidator reqValidator = new RequiredValidator();
+    String reqValidatorId = Util.generateId();
+    objectManager.put( session, reqValidatorId, reqValidator);
+    input.addValidator(reqValidatorId);
+    input.doValidate(eventContext);
+    state = input.getValidState();
+    assertTrue(state == Validatible.INVALID);
+
+    // Test IntegerConverter
+    System.out.println("Testing IntegerConverter");
+    input.setValue("test");
+    input.setAttribute("modelType", "java.lang.Integer");
+    input.doValidate(eventContext);
+    state = input.getValidState();
+    assertTrue(state == Validatible.INVALID);
+
+    // Test IntegerConverter 
+    System.out.println("Testing IntegerConverter");
+    input.setValue("999");
+    input.setAttribute("modelType", "java.lang.Integer");
+    input.doValidate(eventContext);
+    state = input.getValidState();
+    assertTrue(state == Validatible.VALID);
+
+    // Test RangeValidator
+    System.out.println("Testing RangeValidator");
+    input.setValue("999");
+    input.setAttribute("rangeMinimum", "1");
+    input.setAttribute("rangeMaximum", "1000");
+    RangeValidator rangeValidator = new RangeValidator();
+    String rangeValidatorId = Util.generateId();
+    objectManager.put( session, rangeValidatorId, rangeValidator);
+    input.addValidator(rangeValidatorId);
+    input.doValidate(eventContext);
+    state = input.getValidState();
+    assertTrue(state == Validatible.VALID);
+
+    // Test RangeValidator
+    System.out.println("Testing RangeValidator");
+    input.setValue("1001");
+    input.doValidate(eventContext);
+    state = input.getValidState();
+    assertTrue(state == Validatible.INVALID);
+
+    input.removeValidator(rangeValidatorId);
+
+    // Test LengthValidator.
+    System.out.println("Testing LengthValidator");
+    input.setAttribute("modelType", "java.lang.String");
+    input.setValue("Test LengthValidation");
+    input.setAttribute("lengthMinimum", "1");
+    input.setAttribute("lengthMaximum", "10");
+    LengthValidator lengthValidator = new LengthValidator();
+    String lengthValidatorId = Util.generateId();
+    objectManager.put( session, lengthValidatorId, lengthValidator);
+    input.addValidator(lengthValidatorId);
+    input.doValidate(eventContext);
+    state = input.getValidState();
+    assertTrue(state == Validatible.INVALID);
+
+    // Test LengthValidator
+    System.out.println("Testing LengthValidator");
+    input.setValue("Test");
+    input.doValidate(eventContext);
+    state = input.getValidState();
+    assertTrue(state == Validatible.VALID);
+}
+
+} // end of class TestValidation.
