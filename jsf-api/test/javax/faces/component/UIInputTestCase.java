5e472e6f0442711f1d63734417a6948b80a0ffaa
rlubke <rlubke@java.net>
2008-04-01 23:11:09 +0000

    [project @ 2002-08-04 23:19:08 by craigmcc] (Unit Tests only)
    Add unit test for UIInput component class.
    
    Original author: craigmcc
    Date: 2002-08-04 23:19:09+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@612 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ jsf-api/test/javax/faces/component/UIInputTestCase.java
@@ -0,0 +1,105 @@
+/*
+ * $Id: UIInputTestCase.java,v 1.1 2002/08/04 23:19:09 craigmcc Exp $
+ */
+
+/*
+ * Copyright 2002 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package javax.faces.component;
+
+
+import java.util.Iterator;
+import javax.faces.event.FacesEvent;
+import javax.faces.validator.Validator;
+import junit.framework.TestCase;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+
+/**
+ * <p>Test case for the <strong>javax.faces.UIInput</strong>
+ * concrete class.</p>
+ */
+
+public class UIInputTestCase extends UIComponentTestCase {
+
+
+    // ----------------------------------------------------- Instance Variables
+
+
+    // ---------------------------------------------------------- Constructors
+
+
+    /**
+     * Construct a new instance of this test case.
+     *
+     * @param name Name of the test case
+     */
+    public UIInputTestCase(String name) {
+        super(name);
+    }
+
+
+    // -------------------------------------------------- Overall Test Methods
+
+
+    /**
+     * Set up instance variables required by this test case.
+     */
+    public void setUp() {
+
+        component = new UIInput();
+        component.setComponentId("test");
+        attributes = new String[]
+            { "componentId", "rendersChildren" };
+
+    }
+
+
+    /**
+     * Return the tests included in this test suite.
+     */
+    public static Test suite() {
+
+        return (new TestSuite(UIInputTestCase.class));
+
+    }
+
+    /**
+     * Tear down instance variables required by this test case.
+     */
+    public void tearDown() {
+
+        component = null;
+
+    }
+
+
+    // ------------------------------------------------ Individual Test Methods
+
+
+    /**
+     * [3.1.1] Component Type.
+     */
+    public void testComponentType() {
+
+        assertEquals("componentType", UIInput.TYPE,
+                     component.getComponentType());
+
+    }
+
+
+    /**
+     * [3.1.7] Attribute/Property Transparency
+     */
+    public void testAttributePropertyTransparency() {
+
+        super.testAttributePropertyTransparency();
+        UIInput input = (UIInput) component;
+
+    }
+
+
+}
