0e5fec1a200b44e4425bea1027d24c3cbfaea3fe
rlubke <rlubke@java.net>
2008-04-02 09:58:27 +0000

    [project @ 2004-01-15 21:51:21 by eburns] At Adam's request, remove impl specific details about using UIForm.setSubmitted().
    Original author: eburns
    Date: 2004-01-15 21:51:21+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@2496 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/javax/faces/component/UIForm.java
+++ jsf-api/src/javax/faces/component/UIForm.java
@@ -1,5 +1,5 @@
 /*
- * $Id: UIForm.java,v 1.36 2003/10/09 15:41:51 eburns Exp $
+ * $Id: UIForm.java,v 1.37 2004/01/15 21:51:21 eburns Exp $
  */
 
 /*
@@ -68,20 +68,11 @@ public class UIForm extends UIComponentBase implements NamingContainer {
 
 
     /**
-     * <p>If this <code>UIForm</code> instance is experiencing a submit
+     * <p>If <strong>this<strong> <code>UIForm</code> instance (as
+     * opposed to other forms in the page) is experiencing a submit
      * during this request processing lifecycle, this method must be
      * called, passing <code>true</code>, during the {@link
      * UIComponent#decode} for this <code>UIForm</code> instance.</p>
-     *
-     * <p>The standard implementation delegates the responsibility for
-     * calling this method to the {@link javax.faces.render.Renderer}
-     * for this <code>UIForm</code> instance.  The standard form
-     * renderer includes a hidden field in the form, named by the
-     * <code>clientId</code> of this <code>UIForm</code> instance.  The
-     * renderer checks for the presence of a request parameter named the
-     * by the <code>clientId</code> of this <code>UIForm</code>
-     * instance.  If present, it calls <code>setSubmitted(true)</code>.
-     * If not present, it may call <code>setSubmitted(false)</code>.</p>
      *
      * <p>The value of a <code>UIForm</code>'s submitted property must
      * not be saved as part of its state.</p>
