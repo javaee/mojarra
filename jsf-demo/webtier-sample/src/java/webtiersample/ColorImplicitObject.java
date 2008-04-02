e426a27ae84fca9629b2b41725a550a0fa70c29f
rlubke <rlubke@java.net>
2008-04-02 18:35:25 +0000

    [project @ 2005-08-29 18:41:16 by rlubke] Removed setters.
    Original author: rlubke
    Date: 2005-08-29 18:41:16+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@3381 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-demo/webtier-sample/src/java/webtiersample/ColorImplicitObject.java
+++ jsf-demo/webtier-sample/src/java/webtiersample/ColorImplicitObject.java
@@ -143,27 +143,15 @@ public class ColorImplicitObject {
         public int getRed() {
             return red;
         }
-
-        public void setRed(int red) {
-            this.red = red;
-        }
-
+       
         public int getGreen() {
             return green;
         }
-
-        public void setGreen(int green) {
-            this.green = green;
-        }
-
+       
         public int getBlue() {
             return blue;
         }
-
-        public void setBlue(int blue) {
-            this.blue = blue;
-        }
-
+       
         public java.awt.Color getColor() {
             return new java.awt.Color(red, green, blue);
         }
