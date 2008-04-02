c86e18057fe9181d9f580bf7890f6ba36b7b7693
rlubke <rlubke@java.net>
2008-04-02 04:20:48 +0000

    [project @ 2003-08-18 18:24:58 by rlubke]  A test/com/sun/faces/demotest/HtmlUnitTestCase.java      - initial HtmlUnit testing base class.  Not much        in here at the moment.
      A test/com/sun/faces/demotest/guessnumber/TestGuessNumber.java
         - Unit tests for guessNumber including:
            - Validate that a single correct guess is returned
              when 'clicking' back and forth between the input
              and result pages
            - Validate the proper response is returned if no
              guess is provided when submitting a guess
            - Validate the proper response is returned when
              an input guess is less than or greater than the
              valid input range.
    
    Original author: rlubke
    Date: 2003-08-18 18:24:59+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@1660 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ jsf-demo/test/tests/com/sun/faces/demotest/HtmlUnitTestCase.java
@@ -0,0 +1,55 @@
+/*
+ * $Id: HtmlUnitTestCase.java,v 1.1 2003/08/18 18:24:58 rlubke Exp $
+ */
+
+/*
+ * Copyright 2003 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.sun.faces.demotest;
+
+import junit.framework.TestCase;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.Page;
+
+import java.net.URL;
+
+
+public class HtmlUnitTestCase extends TestCase {
+    // target host
+    protected String host;
+    
+    // target port
+    protected String port;
+    
+    // target context
+    protected String context;
+
+    //
+    // Test Methods
+    //
+    protected void setUp() throws Exception {
+        host = System.getProperty("host");
+        port = System.getProperty("port");
+        context = System.getProperty("context.path");
+        super.setUp();    
+    }
+
+    // Utility method to strip jsessionid information from values.
+    // Servlet specification doesn't disallow a path from being
+    // encoded even if the client is using cookies.
+    protected String stripJsessionInfo(String value) {
+        int idx = value.indexOf(";jsessionid");
+        if (idx != -1) {
+            return value.substring(0, idx);
+        }
+        return value;
+    }
+    
+    // Get the initial page of the target application
+    protected Page getInitialPage() throws Exception {
+        WebClient client = new WebClient();
+        return client.getPage(new URL("http://" + host + ":" + port + context));
+    }
+}
---
+++ jsf-demo/test/tests/com/sun/faces/demotest/guessNumber/TestGuessNumber.java
@@ -0,0 +1,188 @@
+/*
+ * $Id: TestGuessNumber.java,v 1.1 2003/08/18 18:24:59 rlubke Exp $
+ */
+
+/*
+ * Copyright 2003 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.sun.faces.demotest.guessNumber;
+
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+import com.sun.faces.demotest.HtmlUnitTestCase;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class TestGuessNumber extends HtmlUnitTestCase {
+
+
+    /*
+     * Click back and forth between greeting.jsp and response.jsp providing
+     * guesses that are within 0 - 10.  
+     */
+    public void testGuessNumber() throws Exception {
+               
+        HtmlPage greetingPage = accessAppAndGetGreetingJSP();
+        
+        int numberFound = 0;
+        
+        // loop through the range of valid guesses
+        for (int i = 0; i < 11; i++) {
+          
+            assertTrue(greetingPage.getTitleText().equals("Hello"));
+            for (Iterator iter = greetingPage.getAllHtmlChildElements(); iter.hasNext();) {
+                HtmlElement element = (HtmlElement) iter.next();
+                if (element.getTagName().equalsIgnoreCase("img")) {
+                    assertTrue(element.getAttributeValue("id").equals("wave_img"));                    
+                    assertTrue(stripJsessionInfo(element.getAttributeValue("src")).equals("/jsf-guessNumber/wave.med.gif"));
+                }
+            }    
+            
+            List forms = greetingPage.getAllForms();
+            assertTrue(forms != null);
+            assertTrue(forms.size() == 1);
+
+            HtmlForm form = (HtmlForm) forms.get(0);
+            assertTrue(form != null);
+            assertTrue(form.getIdAttribute().equals("helloForm"));
+            assertTrue(stripJsessionInfo(form.getActionAttribute()).equals("/jsf-guessNumber/faces/greeting.jsp"));
+            
+            HtmlTextInput input = (HtmlTextInput) form.getInputByName("userNo");
+            assertTrue(input != null);
+            
+            input.setValueAttribute(Integer.toString(i));
+            
+            // "click" the submit button to send our guess
+            HtmlPage resultPage = (HtmlPage) form.submit("submit");
+            assertTrue(resultPage != null); 
+            
+            assertTrue(resultPage.getTitleText().equals("Guess The Number"));
+
+            for (Iterator iter = resultPage.getAllHtmlChildElements(); iter.hasNext();) {
+                HtmlElement element = (HtmlElement) iter.next();
+                
+                // check to see if we guessed correctly or not
+                if (element.getTagName().equalsIgnoreCase("h2")) {
+                    if (element.asText().trim().equals("Yay! You got it!")) {
+                        numberFound++;
+                        System.out.println("Correct number was '" + i + "'");
+                        break;
+                    } else if (element.asText().trim().equals("Sorry, " + i + " is incorrect.")) {
+                        System.out.println("Incorrect guess for '" + i + "', going back to guess again.");
+                        break;
+                    }
+                } else if (element.getTagName().equalsIgnoreCase("img")) {
+                    assertTrue(element.getAttributeValue("id").equals("wave_img"));
+                    assertTrue(stripJsessionInfo(element.getAttributeValue("src")).equals("/jsf-guessNumber/wave.med.gif"));
+                }
+            } 
+            
+            
+                // "click" the back button and submit a new guess
+            List forms1 = resultPage.getAllForms();
+            assertTrue(forms1 != null);
+            assertTrue(forms1.size() == 1);
+
+            HtmlForm back = (HtmlForm) forms1.get(0);
+            assertTrue(back != null);
+            assertTrue(back.getIdAttribute().equals("responseForm"));
+            assertTrue(stripJsessionInfo(back.getActionAttribute()).equals("/jsf-guessNumber/faces/response.jsp"));
+
+            greetingPage = (HtmlPage) back.submit("back");
+            assertTrue(greetingPage != null);            
+        }
+        // ensure that there was only one correct guess through the progession 
+        // of valid values
+        assertTrue(numberFound == 1);
+    }
+    
+    /*
+     * Validate the proper response is returned when no input is provided
+     * to the form for guessing duke's number
+     */
+    public void testGuessNumberNullInput() throws Exception {
+        HtmlPage greetingPage = accessAppAndGetGreetingJSP();
+        HtmlForm guessForm = (HtmlForm) ((List) greetingPage.getAllForms()).get(0);
+        assertTrue(guessForm != null);              
+        
+        HtmlPage resultPage = (HtmlPage) guessForm.submit("submit");
+        assertTrue(resultPage != null);
+        
+        for (Iterator iter = resultPage.getAllHtmlChildElements(); iter.hasNext(); ) {
+            HtmlElement element = (HtmlElement) iter.next();
+            if (element.getTagName().equalsIgnoreCase("h2")) {
+                assertTrue(element.asText().trim().equals("Sorry, null is incorrect."));
+            }
+        }        
+    }
+    
+    /* 
+     * Confirm a validation error is returned when the input value is outside
+     * the specified range of 0 and 10.
+     */ 
+    public void testGuessNumberInvalidInputRange() throws Exception {
+        HtmlPage greetingPage = accessAppAndGetGreetingJSP();
+        HtmlForm guessForm = (HtmlForm) ((List) greetingPage.getAllForms()).get(0);
+        assertTrue(guessForm != null);
+        
+        HtmlTextInput input = (HtmlTextInput) guessForm.getInputByName("userNo");
+        assertTrue(input != null);
+
+        input.setValueAttribute(Integer.toString(-1));
+        
+        HtmlPage failed = (HtmlPage) guessForm.submit("submit");
+        assertTrue(failed != null);
+        assertTrue(failed.getTitleText().equals("Hello"));
+        for (Iterator iter = failed.getAllHtmlChildElements(); iter.hasNext();) {
+            HtmlElement element = (HtmlElement) iter.next();
+            if (element.getTagName().equalsIgnoreCase("font")) {
+                assertTrue(element.getAttributeValue("color").equals("RED"));
+                assertTrue(element.asText().trim().equals("Validation Error: Value is less than allowable minimum of '0'."));
+            }
+        }  
+        
+        guessForm = (HtmlForm) ((List) failed.getAllForms()).get(0);
+        assertTrue(guessForm != null);
+        
+        input = (HtmlTextInput) guessForm.getInputByName("userNo");
+        assertTrue(input != null);
+
+        input.setValueAttribute(Integer.toString(11));
+        
+        failed = (HtmlPage) guessForm.submit("submit");
+        assertTrue(failed != null);
+        assertTrue(failed.getTitleText().equals("Hello"));
+        for (Iterator iter = failed.getAllHtmlChildElements(); iter.hasNext();) {
+            HtmlElement element = (HtmlElement) iter.next();
+            if (element.getTagName().equalsIgnoreCase("font")) {
+                assertTrue(element.getAttributeValue("color").equals("RED"));
+                assertTrue(element.asText().trim().equals("Validation Error: Value is greater than allowable maximum of '10'."));
+            }
+        }  
+        
+    }
+    
+    private HtmlPage accessAppAndGetGreetingJSP() throws Exception {
+        
+        HtmlPage page = (HtmlPage) getInitialPage();
+        
+        assertTrue(page != null);
+        assertTrue(page.getTitleText().equals("index"));
+
+        List anchorList = page.getAnchors();
+        assertTrue(anchorList.size() == 1);
+
+        HtmlAnchor link = (HtmlAnchor) anchorList.get(0);
+        HtmlPage greetingPage = (HtmlPage) link.click();
+        assertTrue(greetingPage != null);
+        return greetingPage;
+    }
+
+} // end of class DemoTest01
+    
