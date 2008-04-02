7846b1302819c21407cd1ce63d53d1b6b42114c2
rlubke <rlubke@java.net>
2008-04-02 05:19:11 +0000

    [project @ 2003-09-05 01:01:21 by craigmcc] Port a fix forward from the datagrid branch, to properly save and restore the "id" property.  PENDING - investigate why this wasn't being caught in the existing unit tests.
    We'll probably want an 0903C tag to pick up this change, or it's
    likely that state saving based on component ids will never work.
    
    Original author: craigmcc
    Date: 2003-09-05 01:01:21+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@1811 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/javax/faces/component/base/UIComponentBase.java
+++ jsf-api/src/javax/faces/component/base/UIComponentBase.java
@@ -1,5 +1,5 @@
 /*
- * $Id: UIComponentBase.java,v 1.9 2003/09/04 03:52:49 eburns Exp $
+ * $Id: UIComponentBase.java,v 1.10 2003/09/05 01:01:21 craigmcc Exp $
  */
 
 /*
@@ -1217,16 +1217,17 @@ public abstract class UIComponentBase implements UIComponent {
 
     public Object getState(FacesContext context) {
 
-        Object values[] = new Object[7];
+        Object values[] = new Object[8];
         values[0] = attributes;
         values[1] = getClientId(context);
         values[2] = componentRef;
-        values[3] = rendered ? Boolean.TRUE : Boolean.FALSE;
-        values[4] = rendererType;
-        values[5] =
+        values[3] = id;
+        values[4] = rendered ? Boolean.TRUE : Boolean.FALSE;
+        values[5] = rendererType;
+        values[6] =
             context.getApplication().getViewHandler().getStateManager().
             getAttachedObjectState(context, this, null, listeners);
-        values[6] = transientFlag ? Boolean.TRUE : Boolean.FALSE;
+        values[7] = transientFlag ? Boolean.TRUE : Boolean.FALSE;
         return (values);
 
     }
@@ -1239,12 +1240,13 @@ public abstract class UIComponentBase implements UIComponent {
         attributes = (HashMap) values[0];
         clientId = (String) values[1];
         componentRef = (String) values[2];
-        rendered = ((Boolean) values[3]).booleanValue();
-        rendererType = (String) values[4];
+        id = (String) values[3];
+        rendered = ((Boolean) values[4]).booleanValue();
+        rendererType = (String) values[5];
         listeners = (List[])
             context.getApplication().getViewHandler().getStateManager().
-            restoreAttachedObjectState(context, values[5]);
-        transientFlag = ((Boolean) values[6]).booleanValue();
+            restoreAttachedObjectState(context, values[6]);
+        transientFlag = ((Boolean) values[7]).booleanValue();
 
     }
 
