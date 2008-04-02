313ee6d64b56b7f664ecbdc53848d8bc7133632f
rlubke <rlubke@java.net>
2008-04-02 12:04:46 +0000

    [project @ 2004-05-05 16:18:35 by eburns] bugtraq 5042162
    Part 1.
    
    This check-in gives us the capability to have per-webapp systests.  This
    is the way the TCK works, and we need this flexibility to exercise
    things that can only be safely configured on a per-webapp basis.
    
    Original author: eburns
    Date: 2004-05-05 16:18:38+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@2813 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ jsf-ri/systest-per-webapp/replace-application/src/java/com/sun/faces/systest/NewApplication.java
@@ -0,0 +1,238 @@
+/*
+ * $Id: NewApplication.java,v 1.1 2004/05/05 16:18:36 eburns Exp $
+ */
+
+/*
+ * Copyright 2004 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+
+
+package com.sun.faces.systest;
+
+import java.util.Iterator;
+import java.util.Collection;
+import java.util.Locale;
+import javax.faces.FacesException;
+import javax.faces.FactoryFinder;
+import javax.faces.application.Application;
+import javax.faces.application.NavigationHandler;
+import javax.faces.application.ViewHandler;
+import javax.faces.application.StateManager;
+import javax.faces.component.ActionSource;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.el.MethodBinding;
+import javax.faces.el.PropertyResolver;
+import javax.faces.el.ReferenceSyntaxException;
+import javax.faces.el.ValueBinding;
+import javax.faces.el.VariableResolver;
+import javax.faces.event.ActionListener;
+import javax.faces.render.RenderKit;
+import javax.faces.validator.Validator;
+
+
+public class NewApplication extends Application {
+    
+    private Application oldApp = null;
+
+    public NewApplication(Application oldApp) {
+	this.oldApp = oldApp;
+    }
+
+    public ActionListener getActionListener() {
+	return oldApp.getActionListener();
+    }
+
+
+    public void setActionListener(ActionListener listener) {
+	oldApp.setActionListener(listener);
+    }
+
+
+    public Locale getDefaultLocale() {
+	return oldApp.getDefaultLocale();
+    }
+
+
+    public void setDefaultLocale(Locale locale) {
+	oldApp.setDefaultLocale(locale);
+    }
+
+
+    public String getDefaultRenderKitId() {
+	return oldApp.getDefaultRenderKitId();
+    }
+
+
+    public void setDefaultRenderKitId(String renderKitId) {
+	oldApp.setDefaultRenderKitId(renderKitId);
+    }
+	
+
+
+    public String getMessageBundle() {
+	return oldApp.getMessageBundle();
+    }
+
+
+    public void setMessageBundle(String bundle) {
+	oldApp.setMessageBundle(bundle);
+    }
+
+
+    public NavigationHandler getNavigationHandler() {
+	return oldApp.getNavigationHandler();
+    }
+
+
+    public void setNavigationHandler(NavigationHandler handler) {
+	oldApp.setNavigationHandler(handler);
+    }
+
+
+    public PropertyResolver getPropertyResolver() {
+	return oldApp.getPropertyResolver();
+    }
+
+
+    public void setPropertyResolver(PropertyResolver resolver) {
+	oldApp.setPropertyResolver(resolver);
+    }
+
+
+    public VariableResolver getVariableResolver() {
+	return oldApp.getVariableResolver();
+    }
+
+
+    public void setVariableResolver(VariableResolver resolver) {
+	oldApp.setVariableResolver(resolver);
+    }
+
+
+    public ViewHandler getViewHandler() {
+	return oldApp.getViewHandler();
+    }
+
+
+    public void setViewHandler(ViewHandler handler) {
+	oldApp.setViewHandler(handler);
+    }
+
+
+
+    public StateManager getStateManager() {
+	return oldApp.getStateManager();
+    }
+
+
+    public void setStateManager(StateManager manager) {
+	oldApp.setStateManager(manager);
+    }
+
+
+    // ------------------------------------------------------- Object Factories
+
+
+    public void addComponent(String componentType,
+			     String componentClass) {
+	oldApp.addComponent(componentType, componentClass);
+    }
+
+
+    public UIComponent createComponent(String componentType)
+        throws FacesException {
+	return oldApp.createComponent(componentType);
+    }
+
+
+    public UIComponent createComponent(ValueBinding componentBinding,
+                                                FacesContext context,
+                                                String componentType)
+	throws FacesException {
+	return oldApp.createComponent(componentBinding, context, 
+				      componentType);
+    }
+
+
+    public Iterator getComponentTypes() {
+	return oldApp.getComponentTypes();
+    }
+
+
+    public void addConverter(String converterId, 
+			     String converterClass) {
+	oldApp.addConverter(converterId, converterClass);
+    }
+
+
+    public void addConverter(Class targetClass,
+			     String converterClass) {
+	oldApp.addConverter(targetClass, converterClass);
+    }
+
+
+    public Converter createConverter(String converterId) {
+	return oldApp.createConverter(converterId);
+    }
+
+
+    public Converter createConverter(Class targetClass) {
+	return oldApp.createConverter(targetClass);
+    }
+
+
+    public Iterator getConverterIds() {
+	return oldApp.getConverterIds();
+    }
+
+    
+    public Iterator getConverterTypes() {
+	return oldApp.getConverterTypes();
+    }
+
+
+    public MethodBinding createMethodBinding(String ref,
+                                                      Class params[])
+        throws ReferenceSyntaxException {
+	return oldApp.createMethodBinding(ref, params);
+    }
+
+
+    public Iterator getSupportedLocales() {
+	return oldApp.getSupportedLocales();
+    }
+
+
+    public void setSupportedLocales(Collection locales) {
+	oldApp.setSupportedLocales(locales);
+    }
+
+
+    public void addValidator(String validatorId, 
+			     String validatorClass) {
+	oldApp.addValidator(validatorId, validatorClass);
+    }
+
+
+    public Validator createValidator(String validatorId)
+        throws FacesException {
+	return oldApp.createValidator(validatorId);
+    }
+
+
+    public Iterator getValidatorIds() {
+	return oldApp.getValidatorIds();
+    }
+
+
+    public ValueBinding createValueBinding(String ref)
+        throws ReferenceSyntaxException {
+	return oldApp.createValueBinding(ref);
+    }
+
+
+}
