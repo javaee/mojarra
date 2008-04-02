832d3452ff59f37208115b7d2a1a2f376a15b39a
rlubke <rlubke@java.net>
2008-04-02 03:41:05 +0000

    [project @ 2003-07-15 23:59:20 by eburns] Testcase to exercise PhaseListener.
    Original author: eburns
    Date: 2003-07-15 23:59:22+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@1538 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/test/com/sun/faces/lifecycle/TestLifecycleImpl.java
+++ jsf-ri/test/com/sun/faces/lifecycle/TestLifecycleImpl.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestLifecycleImpl.java,v 1.15 2003/04/03 18:39:05 rkitain Exp $
+ * $Id: TestLifecycleImpl.java,v 1.16 2003/07/15 23:59:21 eburns Exp $
  */
 
 /*
@@ -18,6 +18,10 @@ import org.mozilla.util.ParameterCheck;
 
 import javax.faces.lifecycle.LifecycleFactory;
 import javax.faces.lifecycle.Lifecycle;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
 import javax.faces.FacesException;
 import javax.faces.context.FacesContext;
 import javax.faces.event.FacesEvent;
@@ -32,7 +36,7 @@ import com.sun.faces.JspFacesTestCase;
  *
  * <B>Lifetime And Scope</B> <P>
  *
- * @version $Id: TestLifecycleImpl.java,v 1.15 2003/04/03 18:39:05 rkitain Exp $
+ * @version $Id: TestLifecycleImpl.java,v 1.16 2003/07/15 23:59:21 eburns Exp $
  * 
  * @see	Blah
  * @see	Bloo
@@ -45,7 +49,7 @@ public class TestLifecycleImpl extends JspFacesTestCase
 // Protected Constants
 //
 
-public static final String TEST_URI = "/components.jsp";
+public static final String TEST_URI = "/TestLifecycleImpl.html";
 
 //
 // Class Variables
@@ -59,7 +63,8 @@ public static final String TEST_URI = "/components.jsp";
 
 // Relationship Instance Variables
 
-protected LifecycleImpl sharedLifecycleImpl = null;
+protected static LifecycleImpl sharedLifecycleImpl = null;
+protected static PhaseListenerImpl sharedListener = null;
 
 //
 // Constructors and Initializers    
@@ -84,14 +89,171 @@ protected LifecycleImpl getSharedLifecycleImpl()
     return sharedLifecycleImpl;
 }
 
-protected void setSharedLifecycleImpl(LifecycleImpl newLife)
-{
-    sharedLifecycleImpl = newLife;
+protected PhaseListenerImpl getSharedPhaseListenerImpl() {
+    return sharedListener;
 }
     
 protected void initWebRequest(WebRequest theRequest)
 {
     theRequest.setURL("localhost:8080", null, null, TEST_URI, null);
+}
+
+public void setUp() {
+    RIConstants.IS_UNIT_TEST_MODE = true;
+    super.setUp();
+}
+
+public void beginAnyPhaseWithListener(WebRequest theRequest) {
+    initWebRequest(theRequest);
+}
+
+public void testAnyPhaseWithListener() {
+    LifecycleImpl life = getSharedLifecycleImpl();
+    final int [] phaseCalled = new 
+	int[LifecycleFactoryImpl.LAST_PHASE + 1];
+    int i;
+    for (i = 1; i < phaseCalled.length; i++) {
+	phaseCalled[i] = 0;
+    }
+
+    sharedListener = new PhaseListenerImpl(phaseCalled,  PhaseId.ANY_PHASE);
+    life.addPhaseListener(sharedListener);
+
+    try {
+	life.execute(getFacesContext());
+    }
+    catch (Throwable e) {
+	e.printStackTrace();
+	assertTrue(e.getMessage(), false);
+    }
+    
+    for (i = 1; i < phaseCalled.length; i++) {
+	assertTrue(phaseCalled[i] == 2);
+    }
+}
+
+public void beginAnyPhaseWithoutListener(WebRequest theRequest) {
+    initWebRequest(theRequest);
+}
+
+public void testAnyPhaseWithoutListener() {
+    assertTrue(null != sharedListener);
+
+    LifecycleImpl life = getSharedLifecycleImpl();
+    final int [] phaseCalled = sharedListener.getPhaseCalled();
+    int i;
+
+    life.removePhaseListener(sharedListener);
+
+    try {
+	life.execute(getFacesContext());
+    }
+    catch (Throwable e) {
+	e.printStackTrace();
+	assertTrue(e.getMessage(), false);
+    }
+    
+    // make sure the listener wasn't called
+    for (i = 1; i < phaseCalled.length; i++) {
+	assertTrue(phaseCalled[i] == 2);
+    }
+
+}
+
+public void beginValidateWithListener(WebRequest theRequest) {
+    initWebRequest(theRequest);
+}
+
+public void testValidateWithListener() {
+    LifecycleImpl life = getSharedLifecycleImpl();
+    final int [] phaseCalled = new 
+	int[LifecycleFactoryImpl.LAST_PHASE + 1];
+    int i;
+    for (i = 1; i < phaseCalled.length; i++) {
+	phaseCalled[i] = 0;
+    }
+
+    sharedListener = new PhaseListenerImpl(phaseCalled,  PhaseId.PROCESS_VALIDATIONS);
+    life.addPhaseListener(sharedListener);
+
+    try {
+	life.execute(getFacesContext());
+    }
+    catch (Throwable e) {
+	e.printStackTrace();
+	assertTrue(e.getMessage(), false);
+    }
+    
+    for (i = 1; i < phaseCalled.length; i++) {
+	if (PhaseId.PROCESS_VALIDATIONS.getOrdinal() == i) {
+	    assertTrue(phaseCalled[i] == 2);
+	}
+	else {
+	    assertTrue(phaseCalled[i] == 0);
+	}
+    }
+}
+
+public void beginValidateWithoutListener(WebRequest theRequest) {
+    initWebRequest(theRequest);
+}
+
+public void testValidateWithoutListener() {
+    assertTrue(null != sharedListener);
+
+    LifecycleImpl life = getSharedLifecycleImpl();
+    final int [] phaseCalled = sharedListener.getPhaseCalled();
+    int i;
+
+    life.removePhaseListener(sharedListener);
+
+    try {
+	life.execute(getFacesContext());
+    }
+    catch (Throwable e) {
+	e.printStackTrace();
+	assertTrue(e.getMessage(), false);
+    }
+    
+    // make sure the listener wasn't called
+    for (i = 1; i < phaseCalled.length; i++) {
+	if (PhaseId.PROCESS_VALIDATIONS.getOrdinal() == i) {
+	    assertTrue(phaseCalled[i] == 2);
+	}
+	else {
+	    assertTrue(phaseCalled[i] == 0);
+	}
+    }
+
+}
+
+class PhaseListenerImpl implements PhaseListener {
+    int [] phaseCalled = null;
+    PhaseId phaseId = null;
+
+    public int [] getPhaseCalled() {
+	return phaseCalled;
+    }
+
+    public PhaseListenerImpl(int [] newPhaseCalled, PhaseId newPhaseId) {
+	phaseCalled = newPhaseCalled;
+	phaseId = newPhaseId;
+    }
+
+    public void afterPhase(PhaseEvent event) {
+	phaseCalled[event.getPhaseId().getOrdinal()] = 
+	    phaseCalled[event.getPhaseId().getOrdinal()] + 1;
+    }
+    
+    public void beforePhase(PhaseEvent event) {
+	phaseCalled[event.getPhaseId().getOrdinal()] = 
+	    phaseCalled[event.getPhaseId().getOrdinal()] + 1;
+    }
+    
+    public PhaseId getPhaseId() {
+	return phaseId;
+    }
+    
 }
 
 } // end of class TestLifecycleImpl
