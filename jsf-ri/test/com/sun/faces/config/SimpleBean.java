3666ad13b79c759a9bcaa1fc9957c97d34492c59
rlubke <rlubke@java.net>
2008-04-02 02:42:37 +0000

    [project @ 2003-04-29 22:23:52 by rkitain] simple bean used for config/managed bean factory testing.
    Original author: rkitain
    Date: 2003-04-29 22:23:52+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@1319 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ jsf-ri/test/com/sun/faces/config/SimpleBean.java
@@ -0,0 +1,16 @@
+package com.sun.faces.config;
+
+public class SimpleBean {
+
+    private String simpleProperty;
+
+    public SimpleBean() {
+    }
+
+    public String getSimpleProperty() {
+        return simpleProperty;
+    }
+    public void setSimpleProperty(String simpleProperty) {
+        this.simpleProperty = simpleProperty;
+    }
+}
