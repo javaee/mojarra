0c6a96a0bd5717313ed87544b9ebd6c0ae7016cc
rlubke <rlubke@java.net>
2008-04-02 05:51:24 +0000

    [project @ 2003-09-18 22:27:46 by craigmcc] [JSFA115] Switching to behavioral interfaces like ValueHolder has introduced some duplicate logic in the component classes that used to extend UIOutput. It makes sense to create a support class to which these component classes can delegate the required behavior, in the same way that components implementing NamingContainer can delegate to NamingContainerSupport.
    PENDING(craigmcc) - This class cannot be used yet, because there is still
    no way to restore the "component" instance variable.
    
    Original author: craigmcc
    Date: 2003-09-18 22:27:46+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@1913 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ jsf-api/src/javax/faces/component/ValueHolderSupport.java
@@ -0,0 +1,241 @@
+/*
+ * $Id: ValueHolderSupport.java,v 1.1 2003/09/18 22:27:46 craigmcc Exp $
+ */
+
+/*
+ * Copyright 2003 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package javax.faces.component;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.application.Application;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.el.ValueBinding;
+
+
+/**
+ * <p><strong>ValueHolderSupport</strong> is a utility class that may be
+ * utilized by {@link UIComponent}s that implement {@link ValueHolder} to
+ * delegate value holder management methods.</p>
+ *
+ * <p>Typical use in a {@link UIComponent} implementation class would be:</p>
+ * <pre>
+ *   public class MyComponent extends UIComponentBase
+ *     implements ValueHolder {
+ *
+ *     private ValueHolderSupport support = new ValueHolderSupport();
+ *
+ *     public Converter getConverter() {
+ *       return (support.getConverter());
+ *     }
+ *
+ *     public void setConverter(Converter converter) {
+ *       support.setConverter(converter);
+ *     }
+ *
+ *     ... and so on ...
+ *
+ * </pre>
+ *
+ * <p>{@link ValueHolderSupport} implements StateHolder, and you will want
+ * to make sure that the <code>support</code> instance variable gets saved
+ * and restored as part of the state of your component class.</p>
+ */
+
+public class ValueHolderSupport implements StateHolder, ValueHolder {
+
+
+    // ------------------------------------------------------------ Constructors
+
+
+    /**
+     * <p>Construct a new {@link ValueHolderSupport} instance not associated
+     * with any {@link UIComponent}.</p>
+     */
+    public ValueHolderSupport() {
+
+        this(null);
+
+    }
+
+
+    /**
+     * <p>Construct a new {@link ValueHolderSupport} instance associated with
+     * the specified {@link UIComponent}.</p>
+     *
+     * @param component {@link UIComponent} with which we are associated
+     */
+    public ValueHolderSupport(UIComponent component) {
+
+        this.component = component;
+
+    }
+
+
+    // ------------------------------------------------------ Instance Variables
+
+
+    private UIComponent component = null;
+    private Converter converter = null;
+    private Object value = null;
+    private String valueRef = null;
+
+
+    // -------------------------------------------------------------- Properties
+
+
+
+    public Converter getConverter() {
+
+        return (this.converter);
+
+    }
+
+
+    public void setConverter(Converter converter) {
+
+        this.converter = converter;
+
+    }
+
+
+    public Object getValue() {
+
+        Repeater repeater = RepeaterSupport.findParentRepeater(component);
+        if (repeater != null) {
+            if (repeater.getRowIndex() > 0) {
+                return (repeater.getChildValue(component));
+            } else {
+                return (this.value);
+            }
+        } else {
+            return (this.value);
+        }
+
+    }
+
+
+    public void setValue(Object value) {
+
+        Repeater repeater = RepeaterSupport.findParentRepeater(component);
+        if (repeater != null) {
+            if (repeater.getRowIndex() > 0) {
+                repeater.setChildValue(component, value);
+            } else {
+                this.value = value;
+            }
+        } else {
+            this.value = value;
+        }
+
+    }
+
+
+    public String getValueRef() {
+
+        return (this.valueRef);
+
+    }
+
+
+    public void setValueRef(String valueRef) {
+
+        this.valueRef = valueRef;
+
+    }
+
+
+    // ----------------------------------------------------- ValueHolder Methods
+
+
+    public Object currentValue(FacesContext context) {
+
+        if (context == null) {
+            throw new NullPointerException();
+        }
+        Object value = getValue();
+        if (value != null) {
+            return (value);
+        }
+        String valueRef = getValueRef();
+        if (valueRef != null) {
+            Application application = context.getApplication();
+            ValueBinding binding = application.getValueBinding(valueRef);
+            return (binding.getValue(context));
+        }
+        return (null);
+
+    }
+
+
+    // ----------------------------------------------------- StateHolder Methods
+
+
+    private boolean transientFlag = false;
+
+
+    public boolean isTransient() {
+
+        return (this.transientFlag);
+
+    }
+
+
+    public void setTransient(boolean transientFlag) {
+
+        this.transientFlag = transientFlag;
+
+    }
+
+
+    public Object saveState(FacesContext context) {
+
+        // NOTE:  The associated component is not stored as part of the state,
+        // because it will be a different object instance on restoration
+
+        Object values[] = new Object[3];
+        List[] converterList = new List[1];
+        List theConverter = new ArrayList(1);
+        theConverter.add(converter);
+        converterList[0] = theConverter;
+        values[0] =
+            context.getApplication().getViewHandler().getStateManager().
+            getAttachedObjectState(context, component,
+                                   "converter", converterList);
+        values[1] = value;
+        values[2] = valueRef;
+        return (values);
+
+    }
+
+
+    public void restoreState(FacesContext context, Object state)
+        throws IOException {
+
+        // PENDING(craigmc) - The "component" ivar is still not restored!!!
+
+        Object values[] = (Object[]) state;
+        List[] converterList = (List[])
+            context.getApplication().getViewHandler().getStateManager().
+            restoreAttachedObjectState(context, values[0], null, component);
+        // PENDING(craigmcc) - it shouldn't be this hard to restore converters
+	if (converterList != null) {
+            List theConverter = converterList[0];
+            if ((theConverter != null) && (theConverter.size() > 0)) {
+                converter = (Converter) theConverter.get(0);
+            }
+	}
+        value = values[1];
+        valueRef = (String) values[2];
+
+    }
+
+
+
+}
