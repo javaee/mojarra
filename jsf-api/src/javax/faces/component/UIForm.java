8ea4f999d32a5001add844c460916060ba2084ef
rlubke <rlubke@java.net>
2008-04-02 05:52:49 +0000

    [project @ 2003-09-19 19:55:34 by eburns] Scarab: JSFA76, JSFA104 RI Impact:
      FormRenderer.encode() must render a hidden field with the name being
      the clientId of the UIForm component.  The value is inconsequential,
      but may need to be non-null.  Not sure.  Whatever you need to do to
      make the below logic in decode() work.
    
      FormRenderer.decode() must check
      RequestParameterMap.containsKey(clientId).  If so, it must call
      UIForm.setSubmitted(true).  Because we rendered a hidden field in
      encode, this will only happen to be the case if this is the form that
      was submitted.
    
    M src/javax/faces/component/UIForm.java
    
    - new property "submitted"
    
    +     * <p>If this <code>UIForm</code> instance is experiencing a submit
    +     * during this request processing lifecycle, this method must be
    +     * called, passing <code>true</code>, during the {@link
    +     * UIComponent#decode} for this <code>UIForm</code> instance.</p>
    +     *
    +     * <p>The standard implementation delegates the responsibility for
    +     * calling this method to the {@link javax.faces.render.Renderer}
    +     * for this <code>UIForm</code> instance.  The standard form
    +     * renderer includes a hidden field in the form, named by the
    +     * <code>clientId</code> of this <code>UIForm</code> instance.  The
    +     * renderer checks for the presence of a request parameter named the
    +     * by the <code>clientId</code> of this <code>UIForm</code>
    +     * instance.  If present, it calls <code>setSubmitted(true)</code>.
    +     * If not present, it may call <code>setSubmitted(false)</code>.</p>
    +     *
    +     * <p>The value of a <code>UIForm</code>'s submitted property must
    +     * not be saved as part of its state.</p>
    
    - override of processDecodes() to make sure the form is decoded before
      its children.
    
    - overrides of processValidators(), and processUpdates() to make sure
      they do nothing if the form isn't the submitted form.
    
    M src/javax/faces/component/base/UIFormBase.java
    
    - implementation of interface changes.
    
    Original author: eburns
    Date: 2003-09-19 19:55:35+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@1916 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/javax/faces/component/UIForm.java
+++ jsf-api/src/javax/faces/component/UIForm.java
@@ -1,5 +1,5 @@
 /*
- * $Id: UIForm.java,v 1.27 2003/07/28 22:18:42 eburns Exp $
+ * $Id: UIForm.java,v 1.28 2003/09/19 19:55:34 eburns Exp $
  */
 
 /*
@@ -9,6 +9,8 @@
 
 package javax.faces.component;
 
+import javax.faces.context.FacesContext;
+import java.io.IOException;
 
 /**
  * <p><strong>UIForm</strong> is a {@link UIComponent} that represents an
@@ -22,6 +24,63 @@ package javax.faces.component;
  */
 
 public interface UIForm extends UIComponent, NamingContainer {
+
+    /**
+     * <p>Returns the current value of the <code>submitted</code>
+     * property.  The default value is <code>false</code>.  See {@link
+     * #setSubmitted} for details.</p>
+     *
+     */
+
+    public boolean isSubmitted();
+
+    /**
+     * <p>If this <code>UIForm</code> instance is experiencing a submit
+     * during this request processing lifecycle, this method must be
+     * called, passing <code>true</code>, during the {@link
+     * UIComponent#decode} for this <code>UIForm</code> instance.</p>
+     *
+     * <p>The standard implementation delegates the responsibility for
+     * calling this method to the {@link javax.faces.render.Renderer}
+     * for this <code>UIForm</code> instance.  The standard form
+     * renderer includes a hidden field in the form, named by the
+     * <code>clientId</code> of this <code>UIForm</code> instance.  The
+     * renderer checks for the presence of a request parameter named the
+     * by the <code>clientId</code> of this <code>UIForm</code>
+     * instance.  If present, it calls <code>setSubmitted(true)</code>.
+     * If not present, it may call <code>setSubmitted(false)</code>.</p>
+     *
+     * <p>The value of a <code>UIForm</code>'s submitted property must
+     * not be saved as part of its state.</p>
+     */
+
+    public void setSubmitted(boolean submitted);
+
+    /**
+     * <p>Override {@link UIComponent#processDecodes} to ensure that the
+     * form is decoded <strong>before</strong> its children.  This is
+     * necessary to allow the <code>submitted</code> property to be
+     * correctly set.</p>
+     *
+     */
+
+    public void processDecodes(FacesContext context) throws IOException;
+
+    /**
+     * <p>Override {@link UIComponent#processValidators} to ensure that
+     * the children of this <code>UIForm</code> instance are only
+     * processed if {@link #isSubmitted} returns <code>true</code>.</p>
+     */
+
+    public void processValidators(FacesContext context);
+
+    /**
+     * <p>Override {@link UIComponent#processUpdates} to ensure that the
+     * children of this <code>UIForm</code> instance are only processed
+     * if {@link #isSubmitted} returns <code>true</code>.</p>
+     */
+
+    public void processUpdates(FacesContext context);
 
 
 }
--- jsf-api/src/javax/faces/component/base/UIFormBase.java
+++ jsf-api/src/javax/faces/component/base/UIFormBase.java
@@ -1,5 +1,5 @@
 /*
- * $Id: UIFormBase.java,v 1.3 2003/07/28 22:18:46 eburns Exp $
+ * $Id: UIFormBase.java,v 1.4 2003/09/19 19:55:35 eburns Exp $
  */
 
 /*
@@ -9,8 +9,13 @@
 
 package javax.faces.component.base;
 
+import java.io.IOException;
+import java.util.Iterator;
 
 import javax.faces.component.UIForm;
+import javax.faces.component.UIInput;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
 
 
 /**
@@ -35,6 +40,102 @@ public class UIFormBase extends UINamingContainerBase implements UIForm {
         setRendererType("Form");
 
     }
+
+    // ------------------------------------------------ Properties
+
+    /**
+     * <p>See {@link UIForm#isSubmitted}</p>
+     */
+    
+    private boolean submitted;
+
+    public boolean isSubmitted() {
+	return submitted;
+    }
+
+    public void setSubmitted(boolean submitted) {
+	this.submitted = submitted;
+    }
+
+    // ------------------------------------------------ UIComponent overrides
+
+    public void processDecodes(FacesContext context) throws IOException {
+
+        if (context == null) {
+            throw new NullPointerException();
+        }
+	
+        // Process this component itself
+        decode(context);
+
+        // Process all facets and children of this component
+        Iterator kids = getFacetsAndChildren();
+        while (kids.hasNext()) {
+            UIComponent kid = (UIComponent) kids.next();
+            kid.processDecodes(context);
+        }
+	
+    }
+
+    public void processValidators(FacesContext context) {
+        if (context == null) {
+            throw new NullPointerException();
+        }
+
+	if (!isSubmitted()) {
+	    return;
+	}
+
+        // Process all the facets and children of this component
+        Iterator kids = getFacetsAndChildren();
+        while (kids.hasNext()) {
+            UIComponent kid = (UIComponent) kids.next();
+            kid.processValidators(context);
+        }
+
+	// Validate this component itself
+	if (this instanceof UIInput) {
+	    // PENDING(craigmcc): shouldn't this be in UIInputBase
+	    ((UIInput) this).validate(context);
+	}
+
+	// Advance to Render Response if this component is not valid
+        if ((this instanceof UIInput) &&
+            !((UIInput) this).isValid()) {
+	    // PENDING(craigmcc): shouldn't this be in UIInputBase
+            context.renderResponse();
+        }
+    }
+
+    public void processUpdates(FacesContext context) {
+        if (context == null) {
+            throw new NullPointerException();
+        }
+
+	if (!isSubmitted()) {
+	    return;
+	}
+
+        // Process all facets and children of this component
+        Iterator kids = getFacetsAndChildren();
+        while (kids.hasNext()) {
+            UIComponent kid = (UIComponent) kids.next();
+            kid.processUpdates(context);
+        }
+
+        // Process this component itself
+        if (this instanceof UIInput) {
+	    // PENDING(craigmcc): shouldn't this be in UIInputBase
+            ((UIInput) this).updateModel(context);
+            if (!((UIInput) this).isValid()) {
+                context.renderResponse();
+            }
+        }
+    }
+
+
+
+
 
 
 }
