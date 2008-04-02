01fdc9824bcca706dd22f2cd482eb8a3065278b7
rlubke <rlubke@java.net>
2008-04-02 05:35:15 +0000

    [project @ 2003-09-12 13:21:19 by eburns] Spec javadoc changes for UISelectMany.
    Original author: eburns
    Date: 2003-09-12 13:21:19+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@1862 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/javax/faces/component/UISelectMany.java
+++ jsf-api/src/javax/faces/component/UISelectMany.java
@@ -1,5 +1,5 @@
 /*
- * $Id: UISelectMany.java,v 1.29 2003/08/28 21:08:52 craigmcc Exp $
+ * $Id: UISelectMany.java,v 1.30 2003/09/12 13:21:19 eburns Exp $
  */
 
 /*
@@ -27,6 +27,48 @@ import javax.faces.context.FacesContext;
  * <p>By default, the <code>rendererType</code> property must be set to
  * "<code>Listbox</code>".  This value can be changed by calling the
  * <code>setRendererType()</code> method.</p>
+ *
+ * <p>The {@link javax.faces.render.Renderer} for this component must
+ * perform the following logic on <code>decode()</code>:</p>
+ *
+ * <ul>
+ *
+ * <p>Obtain the values array from the request.  Obtain the {@link
+ * javax.faces.convert.Converter} using the following algorithm:</p>
+ *
+ * <ul> 
+ *
+ * <p>If the component has an attached <code>Converter</code>, use
+ * it.</p>
+ *
+ * <p>If not, look at the <code>valueRef</code>.  If there is a
+ * <code>valueRef</code>, look at it's type.  <strong>The
+ * <code>valueRef</code> for a <code>UISelectMany</code> component must
+ * point to something that is an array.</strong> If the type is an array
+ * type, use {@link
+ * javax.faces.application.Application#createConverter(java.lang.Class)}
+ * passing the <code>Class</code> instance for the element type of the
+ * array.</p>
+ *
+ * <p>If for any reason a <code>Converter</code> cannot be found, add a
+ * conversion error message to the {@link
+ * javax.faces.context.FacesContext}, set the local value to be the
+ * values array from the request, set the component's <code>valid</code>
+ * state to <code>false</code>, and return.</p>
+ *
+ * </ul>
+ *
+ * <p>Use the <code>Converter</code> to convert each element in the
+ * values array from the request to the proper type.  If the component
+ * has a <code>valueRef</code>, create an array of the expected type to
+ * hold the converted values.  If the component does not have a
+ * <code>valueRef</code> create an array of <code>Object</code>.  Store
+ * the created array as the local value of the component, set the
+ * components <code>valid</code> state to <code>true</code> and
+ * return.</p>
+ *
+ * </ul>
+ *
  */
 
 public interface UISelectMany extends UIInput {
