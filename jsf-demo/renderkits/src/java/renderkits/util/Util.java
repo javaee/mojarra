31d86dd49037497bb816a62bfcbf3d2af7ce4440
rlubke <rlubke@java.net>
2008-04-02 17:30:30 +0000

    [project @ 2005-06-21 14:32:29 by rogerk] added responsewriter "writeXXX" methods
    Original author: rogerk
    Date: 2005-06-21 14:32:29+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@3243 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-demo/renderkits/src/java/renderkits/util/Util.java
+++ jsf-demo/renderkits/src/java/renderkits/util/Util.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2005 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
 package renderkits.util;
 
 import java.io.IOException;
