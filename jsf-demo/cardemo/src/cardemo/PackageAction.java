884e8b08b01686e7d4fea24417f29b1f21ca2c9e
rlubke <rlubke@java.net>
2008-04-02 01:08:36 +0000

    [project @ 2003-01-31 01:14:03 by rkitain] new action listener implementation for package selection
    Original author: rkitain
    Date: 2003-01-31 01:14:03+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@1035 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ jsf-demo/cardemo/src/cardemo/PackageAction.java
@@ -0,0 +1,239 @@
+/*
+ * $Id: PackageAction.java,v 1.1 2003/01/31 01:14:03 rkitain Exp $
+ */
+
+/*
+ * Copyright 2002 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+// PackageAction.java
+
+package cardemo;
+
+import com.sun.faces.util.Util;
+import javax.faces.component.SelectItem;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseId;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.ActionListener;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ *
+ *  <B>PackageAction</B> is a class ...
+ *
+ * <B>Lifetime And Scope</B> <P>
+ *
+ * @version $Id: PackageAction.java,v 1.1 2003/01/31 01:14:03 rkitain Exp $
+ * 
+ * @see	Blah
+ * @see	Bloo
+ *
+ */
+
+public class PackageAction implements ActionListener {
+//
+// Protected Constants
+//
+
+//
+// Class Variables
+//
+
+//
+// Instance Variables
+//
+
+// Attribute Instance Variables
+
+// Relationship Instance Variables
+
+//
+// Constructors and Initializers    
+//
+
+    public PackageAction() {
+    }
+
+//
+// Class methods
+//
+
+//
+// General Methods
+//
+
+    
+    // This listener will process events after the phase specified.
+    
+    public PhaseId getPhaseId() {
+        return PhaseId.ANY_PHASE;
+    }
+
+    public void processAction(ActionEvent event) {
+        System.out.println("PackageAction.processAction : actionCommand : "+
+            event.getActionCommand());
+        String actionCommand = event.getActionCommand();
+
+        if (actionCommand.equals("standard")) {
+            processStandard(event);
+        } else if (actionCommand.equals("performance")) {
+            processPerformance(event);
+        } else if (actionCommand.equals("deluxe")) {
+            processDeluxe(event);
+        }
+    }
+
+    // helper method to set UI values for "standard" package selection
+
+    private void processStandard(ActionEvent event) {
+        UIComponent component = event.getComponent();
+        int i = 0;
+        UIComponent foundComponent = null;
+
+//PENDING(rogerk) application data should be read from config file..
+ 
+        FacesContext context = FacesContext.getCurrentInstance();
+        String[] engines = {"V4", "V6"};
+        ArrayList engineOption = new ArrayList(engines.length);
+        for (i=0; i<engines.length; i++) {
+            engineOption.add(new SelectItem(
+                engines[i], engines[i], engines[i]));
+        }
+        context.setModelValue("CurrentOptionServer.engineOption", engineOption);
+
+//PENDING(rogerk)get locale specific string
+
+        String[] suspensions = {"regular"};
+        ArrayList suspensionOption = new ArrayList();
+        suspensionOption.add(new SelectItem(suspensions[0],
+            suspensions[0], suspensions[0]));
+        context.setModelValue("CurrentOptionServer.suspensionOption",
+            suspensionOption);
+
+        foundComponent = component.findComponent("securitySystem");
+        foundComponent.setAttribute("disabled", "true");
+
+        foundComponent = component.findComponent("gps");
+        foundComponent.setAttribute("disabled", "true");
+
+        foundComponent = component.findComponent("towPackage");
+        foundComponent.setAttribute("disabled", "true");
+
+//PENDING(rogerk)get locale specific string for button labels
+
+        foundComponent = component.findComponent("standard");
+        foundComponent.setAttribute("label", "Standard *");
+
+        foundComponent = component.findComponent("performance");
+        foundComponent.setAttribute("label", "Performance");
+
+        foundComponent = component.findComponent("deluxe");
+        foundComponent.setAttribute("label", "Deluxe");
+        
+        context.renderResponse();
+    }
+
+    // helper method to set UI values for "performance" package selection
+
+    private void processPerformance(ActionEvent event) {
+        UIComponent component = event.getComponent();
+        UIComponent foundComponent = null;
+
+//PENDING(rogerk) application data should be read from config file..
+
+        FacesContext context = FacesContext.getCurrentInstance();
+        String[] engines = {"V8"};
+        ArrayList engineOption = new ArrayList();
+        engineOption.add(new SelectItem(engines[0], engines[0], engines[0]));
+        context.setModelValue("CurrentOptionServer.engineOption", engineOption);
+
+//PENDING(rogerk)get locale specific string
+
+        String[] suspensions = {"performance"};
+        ArrayList suspensionOption = new ArrayList();
+        suspensionOption.add(new SelectItem(suspensions[0],
+            suspensions[0], suspensions[0]));
+        context.setModelValue("CurrentOptionServer.suspensionOption",
+            suspensionOption);
+
+        foundComponent = component.findComponent("securitySystem");
+        foundComponent.setAttribute("disabled", "true");
+
+        foundComponent = component.findComponent("gps");
+        foundComponent.setAttribute("disabled", "true");
+
+        foundComponent = component.findComponent("towPackage");
+        foundComponent.setAttribute("disabled", "false");
+
+//PENDING(rogerk)get locale specific string for button labels
+
+        foundComponent = component.findComponent("standard");
+        foundComponent.setAttribute("label", "Standard");
+
+        foundComponent = component.findComponent("performance");
+        foundComponent.setAttribute("label", "Performance *");
+
+        foundComponent = component.findComponent("deluxe");
+        foundComponent.setAttribute("label", "Deluxe");
+
+        context.renderResponse();
+    }
+
+    // helper method to set UI values for "deluxe" package selection
+
+    private void processDeluxe(ActionEvent event) {
+        UIComponent component = event.getComponent();
+        UIComponent foundComponent = null;
+        int i = 0;
+
+        FacesContext context = FacesContext.getCurrentInstance();
+
+//PENDING(rogerk) application data should be read from config file..
+
+        String[] engines = {"V4", "V6", "V8"};
+        ArrayList engineOption = new ArrayList(engines.length);
+        for (i=0; i<engines.length; i++) {
+            engineOption.add(new SelectItem(
+                engines[i], engines[i], engines[i]));
+        }
+        context.setModelValue("CurrentOptionServer.engineOption", engineOption);
+
+//PENDING(rogerk)get locale specific string
+
+        String[] suspensions = {"performance"};
+        ArrayList suspensionOption = new ArrayList();
+        suspensionOption.add(new SelectItem(suspensions[0],
+            suspensions[0], suspensions[0]));
+        context.setModelValue("CurrentOptionServer.suspensionOption",
+            suspensionOption);
+
+        foundComponent = component.findComponent("securitySystem");
+        foundComponent.setAttribute("disabled", "false");
+
+        foundComponent = component.findComponent("gps");
+        foundComponent.setAttribute("disabled", "false");
+
+        foundComponent = component.findComponent("towPackage");
+        foundComponent.setAttribute("disabled", "false");
+
+//PENDING(rogerk)get locale specific string for button labels
+
+        foundComponent = component.findComponent("standard");
+        foundComponent.setAttribute("label", "Standard");
+
+        foundComponent = component.findComponent("performance");
+        foundComponent.setAttribute("label", "Performance");
+
+        foundComponent = component.findComponent("deluxe");
+        foundComponent.setAttribute("label", "Deluxe *");
+
+        context.renderResponse();
+    }
+
+} // end of class PackageAction
