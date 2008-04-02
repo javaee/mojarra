6fd0515731edc76ad7dcc83ca2cbb0e7cc1a7372
rlubke <rlubke@java.net>
2008-04-02 09:00:13 +0000

    [project @ 2003-11-09 16:52:19 by eburns] [JSFA80] Changes for "CLOSED" version of "displaying errors" thread.
    Original author: eburns
    Date: 2003-11-09 16:52:21+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@2343 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/javax/faces/component/UIMessages.java
+++ jsf-api/src/javax/faces/component/UIMessages.java
@@ -1,5 +1,5 @@
 /*
- * $Id: UIMessages.java,v 1.3 2003/11/07 18:55:31 craigmcc Exp $
+ * $Id: UIMessages.java,v 1.4 2003/11/09 16:52:21 eburns Exp $
  */
 
 /*
@@ -42,12 +42,45 @@ public class UIMessages extends UIComponentBase {
     // ------------------------------------------------------ Instance Variables
 
 
+    private String forVal = null;
     private boolean globalOnly = true;
     private boolean globalOnlySet = false;
-
+    private boolean showDetail = false;
+    private boolean showDetailSet = false;
+    private boolean showSummary = true;
+    private boolean showSummarySet = false;
 
     // -------------------------------------------------------------- Properties
 
+    /**
+     * <p>Return the client identifier of the component for which
+     * this component represents associated message(s) (if any).</p>
+     */
+    public String getFor() {
+
+	if (this.forVal != null) {
+	    return (this.forVal);
+	}
+	ValueBinding vb = getValueBinding("for");
+	if (vb != null) {
+	    return ((String) vb.getValue(getFacesContext()));
+	} else {
+	    return (null);
+	}
+
+    }
+
+    /**
+     * <p>Set the client identifier of the component for which
+     * this component represents associated message(s) (if any).</p>
+     *
+     * @param newFor The new client id
+     */
+    public void setFor(String newFor) {
+
+	forVal = newFor;
+
+    }
 
     /**
      * <p>Return the flag indicating whether only global messages (that is,
@@ -82,15 +115,88 @@ public class UIMessages extends UIComponentBase {
 
     }
 
+    /**
+     * <p>Return the flag indicating whether the <code>detail</code> property
+     * of the associated message(s) should be displayed.</p>
+     */
+    public boolean isShowDetail() {
+
+	if (this.showDetailSet){
+	    return (this.showDetail);
+	}
+	ValueBinding vb = getValueBinding("showDetail");
+	if (vb != null) {
+	    Boolean value = (Boolean) vb.getValue(getFacesContext());
+	    return (value.booleanValue());
+	} else {
+	    return (this.showDetail);
+	}
+
+    }
+
+
+    /**
+     * <p>Set the flag indicating whether the <code>detail</code> property
+     * of the associated message(s) should be displayed.</p>
+     *
+     * @param showDetail The new flag
+     */
+    public void setShowDetail(boolean showDetail) {
+
+	this.showDetail = showDetail;
+	this.showDetailSet = true;
+
+    }
+
+
+    /**
+     * <p>Return the flag indicating whether the <code>summary</code> property
+     * of the associated message(s) should be displayed.</p>
+     */
+    public boolean isShowSummary() {
+
+	if (this.showSummarySet) {
+	    return (this.showSummary);
+	}
+	ValueBinding vb = getValueBinding("showSummary");
+	if (vb != null) {
+	    Boolean value = (Boolean) vb.getValue(getFacesContext());
+	    return (value.booleanValue());
+	} else {
+	    return (this.showSummary);
+	}
+
+    }
+
+
+    /**
+     * <p>Set the flag indicating whether the <code>summary</code> property
+     * of the associated message(s) should be displayed.</p>
+     *
+     * @param showSummary The new flag value
+     */
+    public void setShowSummary(boolean showSummary) {
+
+	this.showSummary = showSummary;
+	this.showSummarySet = true;
+
+    }
+
 
     // ----------------------------------------------------- StateHolder Methods
 
 
     public Object saveState(FacesContext context) {
 
-        Object values[] = new Object[2];
+        Object values[] = new Object[8];
         values[0] = super.saveState(context);
         values[1] = this.globalOnly ? Boolean.TRUE : Boolean.FALSE;
+        values[2] = this.globalOnlySet ? Boolean.TRUE : Boolean.FALSE;
+        values[3] = this.showDetail ? Boolean.TRUE : Boolean.FALSE;
+        values[4] = this.showDetailSet ? Boolean.TRUE : Boolean.FALSE;
+        values[5] = this.showSummary ? Boolean.TRUE : Boolean.FALSE;
+        values[6] = this.showSummarySet ? Boolean.TRUE : Boolean.FALSE;
+
         return (values);
 
     }
@@ -101,6 +207,11 @@ public class UIMessages extends UIComponentBase {
         Object values[] = (Object[]) state;
         super.restoreState(context, values[0]);
         globalOnly = ((Boolean) values[1]).booleanValue();
+        globalOnlySet = ((Boolean) values[2]).booleanValue();
+        showDetail = ((Boolean) values[3]).booleanValue();
+        showDetailSet = ((Boolean) values[4]).booleanValue();
+        showSummary = ((Boolean) values[5]).booleanValue();
+        showSummarySet = ((Boolean) values[6]).booleanValue();
 
     }
 
