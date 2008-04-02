c8bef6417fbefe3415c0cd59f95b567f979dd257
rlubke <rlubke@java.net>
2008-04-02 12:19:19 +0000

    [project @ 2004-05-12 18:46:27 by ofung] updated copyright text to 2004 - Omar.
    Original author: ofung
    Date: 2004-05-12 18:47:39+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@2849 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-demo/components/src/components/model/ChartItem.java
+++ jsf-demo/components/src/components/model/ChartItem.java
@@ -1,26 +1,26 @@
 /*
- * $Id: ChartItem.java,v 1.3 2004/04/06 16:54:19 rkitain Exp $
+ * $Id: ChartItem.java,v 1.4 2004/05/12 18:46:45 ofung Exp $
  */
 
 /*
  * Copyright 2004 Sun Microsystems, Inc. All Rights Reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or
  * without modification, are permitted provided that the following
  * conditions are met:
- *
+ * 
  * - Redistributions of source code must retain the above copyright
  *   notice, this list of conditions and the following disclaimer.
- *
+ * 
  * - Redistribution in binary form must reproduce the above
  *   copyright notice, this list of conditions and the following
  *   disclaimer in the documentation and/or other materials
  *   provided with the distribution.
- *
+ *    
  * Neither the name of Sun Microsystems, Inc. or the names of
  * contributors may be used to endorse or promote products derived
  * from this software without specific prior written permission.
- *
+ *  
  * This software is provided "AS IS," without a warranty of any
  * kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND
  * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
@@ -34,7 +34,7 @@
  * CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF
  * THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS
  * BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
- *
+ *  
  * You acknowledge that this software is not designed, licensed or
  * intended for use in the design, construction, operation or
  * maintenance of any nuclear facility.
--- jsf-demo/components/src/components/renderkit/ChartServlet.java
+++ jsf-demo/components/src/components/renderkit/ChartServlet.java
@@ -1,26 +1,26 @@
 /*
- * $Id: ChartServlet.java,v 1.8 2004/04/24 22:18:45 jvisvanathan Exp $
+ * $Id: ChartServlet.java,v 1.9 2004/05/12 18:46:46 ofung Exp $
  */
 
 /*
  * Copyright 2004 Sun Microsystems, Inc. All Rights Reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or
  * without modification, are permitted provided that the following
  * conditions are met:
- *
+ * 
  * - Redistributions of source code must retain the above copyright
  *   notice, this list of conditions and the following disclaimer.
- *
+ * 
  * - Redistribution in binary form must reproduce the above
  *   copyright notice, this list of conditions and the following
  *   disclaimer in the documentation and/or other materials
  *   provided with the distribution.
- *
+ *    
  * Neither the name of Sun Microsystems, Inc. or the names of
  * contributors may be used to endorse or promote products derived
  * from this software without specific prior written permission.
- *
+ *  
  * This software is provided "AS IS," without a warranty of any
  * kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND
  * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY,
@@ -34,7 +34,7 @@
  * CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF
  * THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS
  * BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
- *
+ *  
  * You acknowledge that this software is not designed, licensed or
  * intended for use in the design, construction, operation or
  * maintenance of any nuclear facility.
