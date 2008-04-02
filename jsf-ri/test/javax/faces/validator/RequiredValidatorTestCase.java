93e25b099e2c1a01df4c3925d1839ea565ced1b4
rlubke <rlubke@java.net>
2008-04-02 00:46:11 +0000

    [project @ 2002-12-24 16:50:35 by eburns] Add test for RequiredValidator bugfix.
    Original author: eburns
    Date: 2002-12-24 16:50:36+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@961 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ jsf-ri/test/javax/faces/validator/RequiredValidatorTestCase.java
@@ -0,0 +1,111 @@
+/*
+ * $Id: RequiredValidatorTestCase.java,v 1.1 2002/12/24 16:50:36 eburns Exp $
+ */
+
+/*
+ * Copyright 2002 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package javax.faces.validator;
+
+import com.sun.faces.ServletFacesTestCase;
+
+import java.io.IOException;
+import java.util.Iterator;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.component.UIInput;
+import javax.faces.component.UIComponent;
+import junit.framework.TestCase;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+
+
+public class RequiredValidatorTestCase extends ServletFacesTestCase {
+
+
+    // ----------------------------------------------------- Instance Variables
+
+
+    /**
+     * The component to be tested for each test.
+     */
+    protected UIComponent component = null;
+    protected Validator validator = null;
+
+
+    // ---------------------------------------------------------- Constructors
+
+
+    /**
+     * Construct a new instance of this test case.
+     *
+     * @param name Name of the test case
+     */
+    public RequiredValidatorTestCase(String name) {
+        super(name);
+    }
+
+
+    // -------------------------------------------------- Overall Test Methods
+
+
+    /**
+     * Set up instance variables required by this test case.
+     */
+    public void setUp() {
+	super.setUp();
+
+        component = new UIInput();
+	validator = new RequiredValidator();
+
+    }
+
+
+    /**
+     * Return the tests included in this test suite.
+     */
+    public static Test suite() {
+
+        return (new TestSuite(RequiredValidatorTestCase.class));
+
+    }
+
+    /**
+     * Tear down instance variables required by this test case.
+     */
+    public void tearDown() {
+	super.tearDown();
+        component = null;
+	validator = null;
+
+    }
+
+    // ------------------------------------------------ Individual Test Methods
+    public void testValidatorNull() {
+	component.addValidator(validator);
+	component.setValue(null);
+	validator.validate(getFacesContext(), component);
+	Iterator messages = getFacesContext().getMessages();
+	assertTrue(messages.hasNext());
+    }
+
+    public void testValidatorZeroLength() {
+	component.addValidator(validator);
+	component.setValue(new String(""));
+	validator.validate(getFacesContext(), component);
+	Iterator messages = getFacesContext().getMessages();
+	assertTrue(messages.hasNext());
+    }
+
+    public void testValidatorValid() {
+	component.addValidator(validator);
+	component.setValue("hello");
+	validator.validate(getFacesContext(), component);
+	Iterator messages = getFacesContext().getMessages();
+	assertTrue(!messages.hasNext());
+    }
+
+}
