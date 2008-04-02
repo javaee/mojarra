cb582dc47f5c093371c7d4e9e28b15fc1ea26f9f
rlubke <rlubke@java.net>
2008-04-02 02:10:59 +0000

    [project @ 2003-03-28 04:39:21 by eburns] Has the content from TestFacesContextImpl_EL
    Original author: eburns
    Date: 2003-03-28 04:39:21+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@1230 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/test/com/sun/faces/el/TestValueBindingImpl.java
+++ jsf-ri/test/com/sun/faces/el/TestValueBindingImpl.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestValueBindingImpl.java,v 1.1 2003/03/24 19:45:38 eburns Exp $
+ * $Id: TestValueBindingImpl.java,v 1.2 2003/03/28 04:39:21 eburns Exp $
  */
 
 /*
@@ -22,13 +22,17 @@ import com.sun.faces.TestBean.Inner2Bean;
 
 import org.apache.cactus.WebRequest;
 
+import javax.servlet.http.Cookie;
+
+import java.util.Enumeration;
+
 /**
  *
  *  <B>TestValueBindingImpl</B> is a class ...
  *
  * <B>Lifetime And Scope</B> <P>
  *
- * @version $Id: TestValueBindingImpl.java,v 1.1 2003/03/24 19:45:38 eburns Exp $
+ * @version $Id: TestValueBindingImpl.java,v 1.2 2003/03/28 04:39:21 eburns Exp $
  * 
  * @see	Blah
  * @see	Bloo
@@ -71,22 +75,26 @@ public class TestValueBindingImpl extends ServletFacesTestCase
 // General Methods
 //
 
-public void beginEL(WebRequest theRequest) 
-{
-    theRequest.addHeader("ELHeader", "ELHeader");
-    theRequest.addHeader("multiheader", "1");
-    theRequest.addHeader("multiheader", "2");
-    theRequest.addParameter("ELParam", "ELParam");
-    theRequest.addParameter("multiparam", "one");
-    theRequest.addParameter("multiparam", "two");
-    theRequest.addCookie("cookie", "monster");
-}
+    public void populateRequest(WebRequest theRequest) {
+	theRequest.addHeader("ELHeader", "ELHeader");
+	theRequest.addHeader("multiheader", "1");
+	theRequest.addHeader("multiheader", "2");
+	theRequest.addParameter("ELParam", "ELParam");
+	theRequest.addParameter("multiparam", "one");
+	theRequest.addParameter("multiparam", "two");
+	theRequest.addCookie("cookie", "monster");
+    }
+	
+    public void beginELGet(WebRequest theRequest) {
+	populateRequest(theRequest);
+    }
 
-    public void testEl() {
+    public void testELGet() {
 	TestBean testBean = new TestBean();
 	InnerBean newInner, oldInner = new InnerBean();
 	testBean.setInner(oldInner);
-	ValueBindingImpl valueBinding = new ValueBindingImpl(new VariableResolverImpl(),
+	ValueBindingImpl valueBinding = new ValueBindingImpl(getFacesContext(),
+							     new VariableResolverImpl(),
 							     new PropertyResolverImpl());
 	Object result = null;
 
@@ -100,8 +108,152 @@ public void beginEL(WebRequest theRequest)
 								   testBean);
 	result = valueBinding.getValue(getFacesContext());
 	assertTrue(result == oldInner);
+
+	valueBinding.setRef("TestBean[\"inner\"]");
+	result = valueBinding.getValue(getFacesContext());
+	assertTrue(result == oldInner);
+	
+	valueBinding.setRef("TestBean[\"inner\"].customers[1]");
+	result = valueBinding.getValue(getFacesContext());
+	assertTrue(result instanceof String);
+	assertTrue(result.equals("Jerry"));
+	
+	// try out the implicit objects
+	valueBinding.setRef("sessionScope.TestBean.inner");
+	result = valueBinding.getValue(getFacesContext());
+	assertTrue(result == oldInner);
+
+	valueBinding.setRef("header.ELHeader");
+	result = valueBinding.getValue(getFacesContext());
+	assertTrue(getFacesContext().getExternalContext().getRequest() == 
+		   request);
+	assertTrue(request.getHeader("ELHeader").equals("ELHeader"));
+	assertTrue(result.equals("ELHeader"));
+	
+	valueBinding.setRef("param.ELParam");
+	result = valueBinding.getValue(getFacesContext());
+	assertTrue(result.equals("ELParam"));
+	
+	String multiparam[] = null;
+	valueBinding.setRef("paramValues.multiparam");
+	multiparam = (String[])
+	    valueBinding.getValue(getFacesContext());
+	assertTrue(null != multiparam);
+	assertTrue(2 == multiparam.length);
+	assertTrue(multiparam[0].equals("one"));
+	assertTrue(multiparam[1].equals("two"));
+
+	valueBinding.setRef("headerValues.multiheader");
+	Enumeration multiHeader = (Enumeration) 
+	    valueBinding.getValue(getFacesContext());
+	assertTrue(null != multiHeader);
+	int elements = 0;
+	while (multiHeader.hasMoreElements()) {
+	    elements++;
+	    // PENDING(edburns): due to an apparent bug in cactus, multiple
+	    // calls to WebRequest.addHeader() still result in all the values
+	    // being concatenated together into a comma separated String.
+	    String element = (String)multiHeader.nextElement();
+	    assertTrue(element.equals("1,2"));
+	}
+	assertTrue(1 == elements);
+	
+	valueBinding.setRef("initParam.saveStateInClient");
+	result = valueBinding.getValue(getFacesContext());
+	assertTrue(null != result);
+	assertTrue(result.equals("false") || result.equals("true"));
+	
+	valueBinding.setRef("cookie.cookie");
+	result = valueBinding.getValue(getFacesContext());
+	assertTrue(null != result);
+	assertTrue(result instanceof Cookie);
+	assertTrue(((Cookie) result).getValue().equals("monster"));
 	
     }
 
+    public void beginELSet(WebRequest theRequest) {
+	populateRequest(theRequest);
+    }
+
+    public void testELSet() {
+	TestBean testBean = new TestBean();
+	InnerBean newInner, oldInner = new InnerBean();
+	testBean.setInner(oldInner);
+	ValueBindingImpl valueBinding = new ValueBindingImpl(getFacesContext(),
+							     new VariableResolverImpl(),
+							     new PropertyResolverImpl());
+	Object result = null;
+
+	//
+	// Set tests
+	//
+
+	newInner = new InnerBean();
+	valueBinding.setRef("TestBean.inner");
+	valueBinding.setValue(getFacesContext(), newInner);
+	result = valueBinding.getValue(getFacesContext());
+	assertTrue(result == newInner);
+	assertTrue(oldInner != newInner);
+	
+	oldInner = newInner;
+	newInner = new InnerBean();
+	valueBinding.setRef("TestBean[\"inner\"]");
+	valueBinding.setValue(getFacesContext(), newInner);
+	result = valueBinding.getValue(getFacesContext());
+	assertTrue(result == newInner);
+	assertTrue(oldInner != newInner);
+
+	String 
+	    oldCustomer0 = null,
+	    oldCustomer1 = null,
+	    customer0 = null,
+	    customer1 = null;
+	
+	valueBinding.setRef("TestBean[\"inner\"].customers[0]");
+	oldCustomer0 = customer0 = (String) valueBinding.getValue(getFacesContext());
+	valueBinding.setRef("TestBean[\"inner\"].customers[1]");
+	oldCustomer1 = customer1 = (String) valueBinding.getValue(getFacesContext());
+	
+	valueBinding.setRef("TestBean[\"inner\"].customers[0]");
+	valueBinding.setValue(getFacesContext(), "Jerry");
+	valueBinding.setRef("TestBean[\"inner\"].customers[1]");
+	valueBinding.setValue(getFacesContext(), "Mickey");
+
+	valueBinding.setRef("TestBean[\"inner\"].customers[0]");
+	customer0 = (String) valueBinding.getValue(getFacesContext());
+	valueBinding.setRef("TestBean[\"inner\"].customers[1]");
+	customer1 = (String) valueBinding.getValue(getFacesContext());
+	assertTrue(customer0.equals("Jerry"));
+	assertTrue(customer1.equals("Mickey"));
+
+	valueBinding.setRef("TestBean[\"inner\"].customers[0]");
+	assertTrue(valueBinding.getValue(getFacesContext()) != oldCustomer0);
+	valueBinding.setRef("TestBean[\"inner\"].customers[1]");
+	assertTrue(valueBinding.getValue(getFacesContext()) != oldCustomer1);
+	
+	// put in a map to the customers Collection
+	Inner2Bean inner2 = new Inner2Bean();
+	assertTrue(null == inner2.getNicknames().get("foo"));
+	valueBinding.setRef("TestBean[\"inner\"].customers[2]");
+	valueBinding.setValue(getFacesContext(), inner2);
+	valueBinding.setRef("TestBean[\"inner\"].customers[2]");
+	assertTrue(valueBinding.getValue(getFacesContext()) == inner2);    
+
+	valueBinding.setRef("TestBean[\"inner\"].customers[2].nicknames.foo");
+	valueBinding.setValue(getFacesContext(), "bar");
+	assertTrue(((String) inner2.getNicknames().get("foo")).equals("bar"));
+	
+	// PENDING(edburns): test set with implicit objects.
+	
+    }
+
+    public void testReadOnly() {
+	// PENDING(edburns): implement read only tests
+    }
+
+    public void testType() {
+	// PENDING(edburns): implement type tests
+    }
+    
 
 } // end of class TestValueBindingImpl
