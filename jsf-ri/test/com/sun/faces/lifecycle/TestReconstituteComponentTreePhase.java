14e22cc6cc31b37e998023a277f57f84f308666b
rlubke <rlubke@java.net>
2008-04-02 03:08:27 +0000

    [project @ 2003-05-14 20:00:09 by rkitain] This change bundle fixes Bug: 4863135 : ActionListeners on Facets Don;t Get Registered...
    M test/com/sun/faces/lifecycle/TestReconstituteComponentTreePhase.java
       - new test method "testRegisteredListeners"
    
    Original author: rkitain
    Date: 2003-05-14 20:00:09+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@1423 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/test/com/sun/faces/lifecycle/TestReconstituteComponentTreePhase.java
+++ jsf-ri/test/com/sun/faces/lifecycle/TestReconstituteComponentTreePhase.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TestReconstituteComponentTreePhase.java,v 1.3 2003/03/28 18:34:08 horwat Exp $
+ * $Id: TestReconstituteComponentTreePhase.java,v 1.4 2003/05/14 20:00:09 rkitain Exp $
  */
 
 /*
@@ -11,29 +11,35 @@
 
 package com.sun.faces.lifecycle;
 
-import org.apache.cactus.WebRequest;
+import com.sun.faces.RIConstants;
+import com.sun.faces.context.FacesContextImpl;
+import com.sun.faces.lifecycle.Phase;
+import com.sun.faces.ServletFacesTestCase;
+import com.sun.faces.tree.SimpleTreeImpl;
 
-import org.mozilla.util.Assert;
-import org.mozilla.util.ParameterCheck;
+import java.util.List;
+import java.util.Locale;
 
 import javax.faces.FacesException;
 import javax.faces.FactoryFinder;
 import javax.faces.context.FacesContext;
 import javax.faces.lifecycle.Lifecycle;
+import javax.faces.component.UICommand;
 import javax.faces.component.UIComponent;
-import javax.faces.component.UIInput;
 import javax.faces.component.UIForm;
+import javax.faces.component.UIInput;
 import javax.faces.component.UINamingContainer;
-import com.sun.faces.tree.SimpleTreeImpl;
+import javax.faces.component.UIPanel;
+import javax.faces.event.PhaseId;
 import javax.faces.tree.TreeFactory;
 import javax.faces.tree.Tree;
-import com.sun.faces.RIConstants;
-import com.sun.faces.context.FacesContextImpl;
-import com.sun.faces.lifecycle.Phase;
-import com.sun.faces.ServletFacesTestCase;
 import javax.servlet.http.HttpSession;
 import javax.faces.render.RenderKitFactory;
-import java.util.Locale;
+
+import org.apache.cactus.WebRequest;
+
+import org.mozilla.util.Assert;
+import org.mozilla.util.ParameterCheck;
 
 /**
  *
@@ -41,7 +47,7 @@ import java.util.Locale;
  *
  * <B>Lifetime And Scope</B> <P>
  *
- * @version $Id: TestReconstituteComponentTreePhase.java,v 1.3 2003/03/28 18:34:08 horwat Exp $
+ * @version $Id: TestReconstituteComponentTreePhase.java,v 1.4 2003/05/14 20:00:09 rkitain Exp $
  * 
  * @see	Blah
  * @see	Bloo
@@ -181,7 +187,105 @@ public void testReconstituteRequestSubmit()
     assertTrue(basicForm == root.findComponent("basicForm")); 
 }
 
+/**
+ * This method will test the <code>registerActionListeners</code> method.
+ * It will first create a simple tree consisting of a couple of <code>UICommand</code>
+ * components added to a facet;  Then the <code>ReconstituteComponentTree.execute</code>
+ * method is run;  And finally, an assertion is done to ensure that default action
+ * listeners have been registered on the <code>UICommand</code> components;
+ */
+public void testRegisterListeners() { 
+    Tree requestTree = null;
+    // precreate tree and set it in session and make sure the tree is
+    // restored from session.
+   
+    UIComponent root = new UINamingContainer() {
+        public String getComponentType() { return "root"; }
+    };
+    
+    UIForm basicForm = new UIForm();
+    basicForm.setComponentId("basicForm");
+    root.addChild(basicForm);
+    UIPanel panel = new UIPanel();
+    basicForm.addChild(panel);
+    UIPanel commandPanel = new UIPanel();
+    commandPanel.setComponentId("commandPanel");
+    TestCommand command1 = new TestCommand();
+    TestCommand command2 = new TestCommand();
+    commandPanel.addChild(command1);
+    commandPanel.addChild(command2);
+    panel.addFacet("commandPanel", commandPanel);
 
+    requestTree = new SimpleTreeImpl(getFacesContext(), root, 
+            TEST_URI);
+    assertTrue(requestTree != null);
 
+    HttpSession session = (HttpSession) 
+        getFacesContext().getExternalContext().getSession(false);
+    session.setAttribute(RIConstants.FACES_TREE, requestTree);
+
+    Phase reconstituteTree = new ReconstituteComponentTreePhase();
+
+    try {
+	reconstituteTree.execute(getFacesContext());
+    }
+    catch (Throwable e) {
+	assertTrue(false);
+    }
+    assertTrue(!((FacesContextImpl)getFacesContext()).getRenderResponse() &&
+        !((FacesContextImpl)getFacesContext()).getResponseComplete());
+
+    assertTrue(command1.getDefaultListenerCount() > 0); 
+    assertTrue(command2.getDefaultListenerCount() > 0); 
+
+    // Now test with no facets... Listeners should still be registered on UICommand
+    // components....
+    // 
+    root = new UINamingContainer() {
+        public String getComponentType() { return "root"; }
+    };
+    basicForm = new UIForm();
+    basicForm.setComponentId("basicForm");
+    root.addChild(basicForm);
+    command1 = new TestCommand();
+    command2 = new TestCommand();
+    basicForm.addChild(command1);
+    basicForm.addChild(command2);
+    requestTree = new SimpleTreeImpl(getFacesContext(), root, 
+            TEST_URI);
+    assertTrue(requestTree != null);
+
+    session = (HttpSession) 
+        getFacesContext().getExternalContext().getSession(false);
+    session.setAttribute(RIConstants.FACES_TREE, requestTree);
+
+    reconstituteTree = new ReconstituteComponentTreePhase();
+
+    try {
+	reconstituteTree.execute(getFacesContext());
+    }
+    catch (Throwable e) {
+	assertTrue(false);
+    }   
+    assertTrue(!((FacesContextImpl)getFacesContext()).getRenderResponse() &&
+        !((FacesContextImpl)getFacesContext()).getResponseComplete());
+
+    assertTrue(command1.getDefaultListenerCount() > 0); 
+    assertTrue(command2.getDefaultListenerCount() > 0); 
+}
+
+public static class TestCommand extends UICommand {
+    public int getDefaultListenerCount() {
+	List list = null;
+	int ordinal = PhaseId.INVOKE_APPLICATION.getOrdinal();
+	if (listeners != null) {
+            list = listeners[ordinal]; 
+            return list.size();
+	} else {
+            return 0;
+	}
+    }
+}
 
 } // end of class TestReconstituteComponentTreePhase
+
