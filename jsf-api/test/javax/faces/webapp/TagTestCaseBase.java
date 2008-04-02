a52542b715006547f41e9d35574e695195f77beb
rlubke <rlubke@java.net>
2008-04-02 08:40:12 +0000

    [project @ 2003-11-04 04:07:59 by eburns] [JSFA80] key/bundle: Part I: api infrastructure
    This change-bundle adds the ability to say
    
    <f:loadBundle baseName="com.foo.MyBundle" var="messages"/>
    
    or
    
    <f:loadBundle baseName="com.foo.ImagesBundle" var="images"/>
    
    The next part is pending resolution of the much vaunted VBL issue.
    
    SECTION: API Changes
    
    A jsf-api/src/javax/faces/webapp/LoadBundleTag.java
    
    - The simple action to suck values out of a ResourceBundle, stuff them
      in a HashMap, and put the map in the request scope under the specified
      key.
    
    M jsf-api/test/javax/faces/Messages.properties
    
    - test messages
    
    A jsf-api/test/javax/faces/webapp/LoadBundleTagTestCase.java
    
    - simple test case
    
    A jsf-api/test/javax/faces/webapp/TagTestCaseBase.java
    
    - Refactor common functionality up to base class
    
    M jsf-api/test/javax/faces/webapp/UIComponentTagTestCase.java
    
    - remove duplicate functionality
    
    SECTION: RI Changes
    
    M jsf-ri/conf/share/jsf_core.tld
    
    - add a tag for LoadBundleTag
    
    Original author: eburns
    Date: 2003-11-04 04:08:03+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@2297 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ jsf-api/test/javax/faces/webapp/TagTestCaseBase.java
@@ -0,0 +1,163 @@
+/*
+ * $Id: TagTestCaseBase.java,v 1.1 2003/11/04 04:08:01 eburns Exp $
+ */
+
+/*
+ * Copyright 2003 Sun Microsystems, Inc. All rights reserved.
+ * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package javax.faces.webapp;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import javax.faces.FactoryFinder;
+import javax.faces.application.Application;
+import javax.faces.application.ApplicationFactory;
+import javax.faces.component.NamingContainer;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import javax.faces.event.FacesEvent;
+import javax.faces.render.RenderKit;
+import javax.faces.render.RenderKitFactory;
+import javax.faces.validator.Validator;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.Tag;
+import junit.framework.TestCase;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import javax.faces.mock.MockApplication;
+import javax.faces.mock.MockExternalContext;
+import javax.faces.mock.MockFacesContext;
+import javax.faces.mock.MockHttpServletRequest;
+import javax.faces.mock.MockHttpServletResponse;
+import javax.faces.mock.MockHttpSession;
+import javax.faces.mock.MockJspWriter;
+import javax.faces.mock.MockLifecycle;
+import javax.faces.mock.MockPageContext;
+import javax.faces.mock.MockRenderKit;
+import javax.faces.mock.MockRenderKitFactory;
+import javax.faces.mock.MockServlet;
+import javax.faces.mock.MockServletConfig;
+import javax.faces.mock.MockServletContext;
+
+
+/**
+ * <p>Base unit tests for all UIComponentTag classes.</p>
+ */
+
+public class TagTestCaseBase extends TestCase {
+
+
+    // ----------------------------------------------------- Instance Variables
+
+
+    protected MockApplication         application = null;
+    protected MockServletConfig       config = null;
+    protected MockExternalContext     externalContext = null;
+    protected MockFacesContext        facesContext = null;
+    protected MockLifecycle           lifecycle = null;
+    protected MockPageContext         pageContext = null;
+    protected MockHttpServletRequest  request = null;
+    protected MockHttpServletResponse response = null;
+    protected MockServlet             servlet = null;
+    protected MockServletContext      servletContext = null;
+    protected MockHttpSession         session = null;
+
+    protected Tag                     root = null;
+
+
+    // ---------------------------------------------------------- Constructors
+
+
+    /**
+     * Construct a new instance of this test case.
+     *
+     * @param name Name of the test case
+     */
+    public TagTestCaseBase(String name) {
+
+        super(name);
+
+    }
+
+
+    // -------------------------------------------------- Overall Test Methods
+
+
+    /**
+     * Set up instance variables required by this test case.
+     */
+    public void setUp() throws Exception {
+
+        // Set up Servlet API Objects
+        servletContext = new MockServletContext();
+        config = new MockServletConfig(servletContext);
+        servlet = new MockServlet(config);
+        session = new MockHttpSession();
+        request = new MockHttpServletRequest(session);
+        response = new MockHttpServletResponse();
+
+        // Set up JSP API Objects
+        pageContext = new MockPageContext();
+        pageContext.initialize(servlet, request, response, null,
+                               true, 1024, true);
+
+        // Set up Faces API Objects
+	FactoryFinder.setFactory(FactoryFinder.APPLICATION_FACTORY,
+				 "javax.faces.mock.MockApplicationFactory");
+	FactoryFinder.setFactory(FactoryFinder.RENDER_KIT_FACTORY,
+				 "javax.faces.mock.MockRenderKitFactory");
+        externalContext =
+            new MockExternalContext(servletContext, request, response);
+        lifecycle = new MockLifecycle();
+        facesContext = new MockFacesContext(externalContext, lifecycle);
+	UIViewRoot root = new UIViewRoot();
+	root.setViewId("/root");
+        facesContext.setViewRoot(root);
+        ApplicationFactory applicationFactory = (ApplicationFactory)
+            FactoryFinder.getFactory(FactoryFinder.APPLICATION_FACTORY);
+        application = (MockApplication) applicationFactory.getApplication();
+        facesContext.setApplication(application);
+        RenderKitFactory renderKitFactory = (RenderKitFactory)
+            FactoryFinder.getFactory(FactoryFinder.RENDER_KIT_FACTORY);
+        RenderKit renderKit = new MockRenderKit();
+        try {
+            renderKitFactory.addRenderKit(RenderKitFactory.DEFAULT_RENDER_KIT,
+                                          renderKit);
+        } catch (IllegalArgumentException e) {
+            ;
+        }
+
+    }
+
+    /**
+     * Tear down instance variables required by this test case.
+     */
+    public void tearDown() {
+
+        application = null;
+        config = null;
+        externalContext = null;
+        facesContext = null;
+        lifecycle = null;
+        pageContext = null;
+        request = null;
+        response = null;
+        servlet = null;
+        servletContext = null;
+        session = null;
+
+        root = null;
+
+    }
+
+}
--- jsf-api/test/javax/faces/webapp/UIComponentTagTestCase.java
+++ jsf-api/test/javax/faces/webapp/UIComponentTagTestCase.java
@@ -1,5 +1,5 @@
 /*
- * $Id: UIComponentTagTestCase.java,v 1.15 2003/10/21 04:04:02 eburns Exp $
+ * $Id: UIComponentTagTestCase.java,v 1.16 2003/11/04 04:08:02 eburns Exp $
  */
 
 /*
@@ -54,25 +54,11 @@ import javax.faces.mock.MockServletContext;
  * <p>Base unit tests for all UIComponentTag classes.</p>
  */
 
-public class UIComponentTagTestCase extends TestCase {
+public class UIComponentTagTestCase extends TagTestCaseBase {
 
 
     // ----------------------------------------------------- Instance Variables
 
-
-    protected MockApplication         application = null;
-    protected MockServletConfig       config = null;
-    protected MockExternalContext     externalContext = null;
-    protected MockFacesContext        facesContext = null;
-    protected MockLifecycle           lifecycle = null;
-    protected MockPageContext         pageContext = null;
-    protected MockHttpServletRequest  request = null;
-    protected MockHttpServletResponse response = null;
-    protected MockServlet             servlet = null;
-    protected MockServletContext      servletContext = null;
-    protected MockHttpSession         session = null;
-
-    protected Tag                     root = null;
     protected Map                     tags = new HashMap();
 
 
@@ -95,53 +81,6 @@ public class UIComponentTagTestCase extends TestCase {
 
 
     /**
-     * Set up instance variables required by this test case.
-     */
-    public void setUp() throws Exception {
-
-        // Set up Servlet API Objects
-        servletContext = new MockServletContext();
-        config = new MockServletConfig(servletContext);
-        servlet = new MockServlet(config);
-        session = new MockHttpSession();
-        request = new MockHttpServletRequest(session);
-        response = new MockHttpServletResponse();
-
-        // Set up JSP API Objects
-        pageContext = new MockPageContext();
-        pageContext.initialize(servlet, request, response, null,
-                               true, 1024, true);
-
-        // Set up Faces API Objects
-	FactoryFinder.setFactory(FactoryFinder.APPLICATION_FACTORY,
-				 "javax.faces.mock.MockApplicationFactory");
-	FactoryFinder.setFactory(FactoryFinder.RENDER_KIT_FACTORY,
-				 "javax.faces.mock.MockRenderKitFactory");
-        externalContext =
-            new MockExternalContext(servletContext, request, response);
-        lifecycle = new MockLifecycle();
-        facesContext = new MockFacesContext(externalContext, lifecycle);
-	UIViewRoot root = new UIViewRoot();
-	root.setViewId("/root");
-        facesContext.setViewRoot(root);
-        ApplicationFactory applicationFactory = (ApplicationFactory)
-            FactoryFinder.getFactory(FactoryFinder.APPLICATION_FACTORY);
-        application = (MockApplication) applicationFactory.getApplication();
-        facesContext.setApplication(application);
-        RenderKitFactory renderKitFactory = (RenderKitFactory)
-            FactoryFinder.getFactory(FactoryFinder.RENDER_KIT_FACTORY);
-        RenderKit renderKit = new MockRenderKit();
-        try {
-            renderKitFactory.addRenderKit(RenderKitFactory.DEFAULT_RENDER_KIT,
-                                          renderKit);
-        } catch (IllegalArgumentException e) {
-            ;
-        }
-
-    }
-
-
-    /**
      * Return the tests included in this test suite.
      */
     public static Test suite() {
@@ -154,21 +93,8 @@ public class UIComponentTagTestCase extends TestCase {
      * Tear down instance variables required by this test case.
      */
     public void tearDown() {
-
-        application = null;
-        config = null;
-        externalContext = null;
-        facesContext = null;
-        lifecycle = null;
-        pageContext = null;
-        request = null;
-        response = null;
-        servlet = null;
-        servletContext = null;
-        session = null;
-
-        root = null;
         tags.clear();
+	super.tearDown();
 
     }
 
