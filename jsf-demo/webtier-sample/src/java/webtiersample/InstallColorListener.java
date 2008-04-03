c8e92a81fae031bb5d325b42347e9551aa12f9e3
rlubke <rlubke@java.net>
2008-04-03 03:30:30 +0000

    [project @ 2008-03-21 00:38:15 by rlubke] Updated license headers.
    Original author: rlubke
    Date: 2008-03-21 00:38:21+00:00
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@4707 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-demo/webtier-sample/src/java/webtiersample/InstallColorListener.java
+++ jsf-demo/webtier-sample/src/java/webtiersample/InstallColorListener.java
@@ -2,6 +2,8 @@
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
  * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ * Portions Copyright Apache Software Foundation.
  * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -34,21 +36,7 @@
  * holder.
  */
 
-/*
-* Copyright 2004 The Apache Software Foundation
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+
 package webtiersample;
 
 import javax.servlet.ServletContext;
