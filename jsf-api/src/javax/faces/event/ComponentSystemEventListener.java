11c9500632410eceac15af4b3e0add76c6bf6e28
rlubke <rlubke@java.net>
2008-06-06 00:54:55 +0000

    Correct typo.
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@4879 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/javax/faces/event/ComponentSystemEventListener.java
+++ jsf-api/src/javax/faces/event/ComponentSystemEventListener.java
@@ -10,7 +10,7 @@ package javax.faces.event;
  *  <p class="changed_added_2_0">Implementors of this class do not need
  *  an <code>isListenerForSource()</code> method because they are only
  *  installed on specific component instances, therefore the
- *  <code>isListenerForSource()</code> merthod is implicit.  Also, the 
+ *  <code>isListenerForSource()</code> method is implicit.  Also, the 
  * {@link #processEvent} method on this interface takes a 
  * {@link ComponentSystemEvent} because the event will always be associated with
  * a {@link javax.faces.component.UIComponent} instance.</p>
