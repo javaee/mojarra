d0868b0341c395eb1107ef5a455799a64038b53f
rogerk <rogerk@java.net>
2008-07-10 17:57:26 +0000

    merge from trunk
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/AJAX_293_BRANCH@5029 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/com/sun/faces/application/resource/ResourceInfo.java
+++ jsf-ri/src/com/sun/faces/application/resource/ResourceInfo.java
@@ -203,7 +203,6 @@ public class ResourceInfo {
                   .getApplicationMap().get("javax.servlet.context.tempdir");
             if (servletTmpDir == null || !servletTmpDir.isDirectory()) {
                 if (LOGGER.isLoggable(Level.FINE)) {
-                    // PENDING i18n
                     LOGGER.log(Level.FINE,
                                "File ({0}) referenced by javax.servlet.context.tempdir attribute is null, or was is not a directory.  Compression for {1} will be unavailable.",
                                new Object[]{((servletTmpDir == null)
@@ -216,14 +215,24 @@ public class ResourceInfo {
                 String tPath = ((path.charAt(0) == '/') ? path : '/' + path);
                 File newDir = new File(servletTmpDir, COMPRESSED_CONTENT_DIRECTORY
                                                       + tPath);
-                try {
-                    newDir.mkdirs();
-                    compressedPath = newDir.getCanonicalPath();
-                } catch (Exception e) {
-                    LOGGER.log(Level.SEVERE,
-                               e.toString(),
-                               e);
-                    compressable = false;
+                if (!newDir.exists()) {
+                    try {
+                        if (newDir.mkdirs()) {
+                            compressedPath = newDir.getCanonicalPath();
+                        } else {
+                            compressable = false;
+                            if (LOGGER.isLoggable(Level.WARNING)) {
+                                LOGGER.log(Level.WARNING,
+                                           "jsf.application.resource.unable_to_create_compression_directory",
+                                           newDir.getCanonicalPath());
+                            }
+                        }
+                    } catch (Exception e) {
+                        LOGGER.log(Level.SEVERE,
+                                   e.toString(),
+                                   e);
+                        compressable = false;
+                    }
                 }
             }
         }
