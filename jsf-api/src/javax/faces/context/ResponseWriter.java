549bd8915576a9a9ff642b3a936c47cff1e252d0
rogerk <rogerk@java.net>
2008-08-07 00:01:32 +0000

    revert to head
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/AJAX_293_BRANCH@5127 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/javax/faces/context/ResponseWriter.java
+++ jsf-api/src/javax/faces/context/ResponseWriter.java
@@ -58,7 +58,6 @@ import javax.faces.component.UIComponent;
 
 public abstract class ResponseWriter extends Writer {
 
-    private boolean enableWriter = true;
 
     /**
      * <p>Return the content type (such as "text/html") for this {@link
@@ -289,24 +288,5 @@ public abstract class ResponseWriter extends Writer {
      */
     public abstract ResponseWriter cloneWithWriter(Writer writer);
 
-    /**
-     * <p class="changed_added_2_0">Enable or disable the writing capabilities
-     * of this <code>Writer</code>.  The <code>Writer</code> is enabled by default.
-     * This method may be used to temporarily ensure that no output be written
-     * to the response.</p>
-     */
-    public void enableWriter(boolean enable) {
-        enableWriter = enable;
-    }
-
-    /**
-     * <p class="changed_added_2_0">Returns <code>true</code> if this <code>Writer</code>
-     * is enabled for writing, <code>false</code> otherwise.
-     * This method may be used to temporarily ensure that no output be written
-     * to the response.</p>
-     */
-    public boolean isWriterEnabled() {
-        return enableWriter;
-    }
 
 }
