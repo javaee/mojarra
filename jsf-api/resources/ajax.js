481ceb74b64fe46d1ff87d763c571b76390628af
rogerk <rogerk@java.net>
2008-08-11 21:36:58 +0000

    more javadocs for ajaxRequest/ajaxResponse functions
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/AJAX_293_BRANCH@5154 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/resources/ajax.js
+++ jsf-api/resources/ajax.js
@@ -95,20 +95,69 @@ javax.faces.Ajax.viewState = function(form) {
 
 /**
  * <p><span class="changed_added_2_0">Send</span> an Ajax request
- * to the server.
+ * to the server.  This function must:
+ * <ul>
+ * <li>Capture the element that triggered this Ajax request
+ * (from the <code>element</code> argument, also known as the
+ * <code>source</code> element.</li>
+ * <li>Determine the <code>source</code> element's <code>form</code>
+ * element.</li>
+ * <li>Get the <code>form</code> view state by calling 
+ * <code>javax.faces.Ajax.viewState</code> passing the 
+ * <code>form</code> element as the argument.</li> 
+ * <li>Determine additional arguments from the <code>options</code>
+ * argument.</li>
+ * <li>Set up post data arguments for the Ajax request.  The following
+ * name/value pairs must be passed as post data:
+ * <ul>
+ * <li>The name and value of the <code>source</code> element that 
+ * triggered this request;</li>
+ * <li><code>javax.faces.partial.ajax</code> with the value 
+ * <code>true</code></li>
+ * </ul>
+ * </li>
+ * <li>Optionally, the following arguments can be sent as post
+ * data: 
+ * <ul>
+ * <li><code>javax.faces.partial.execute</code> with the value
+ * <code>true</code>.</li> 
+ * <li><code>javax.faces.partial.render</code> with the value
+ * <code>true</code>.</li> 
+ * <li>basic <code>event</code> information:
+ * <ul>
+ * <li><code>target</code> - the ID of the element that triggered the event.</li>
+ * <li><code>captured</code> - the ID of the element that captured the event.</li>
+ * <li><code>type</code> - the type of event (ex: onkeypress)</li>
+ * </ul>
+ * </li>
+ * <li>mouse and keyboard <code>event</code> information:
+ * <ul>
+ * <li><code>alt</code> - <code>true</code> if ALT key was pressed.</li>
+ * <li><code>ctrl</code> - <code>true</code> if CTRL key was pressed.</li>
+ * <li><code>shift</code> - <code>true</code> if SHIFT key was pressed. </li>
+ * <li><code>meta</code> - <code>true</code> if META key was pressed. </li>
+ * <li><code>right</code> - <code>true</code> if right mouse button  
+ * was pressed. </li>
+ * <li><code>left</code> - <code>true</code> if left mouse button  
+ * was pressed. </li>
+ * <li><code>keycode</code> - the key code. 
+ * </ul>
+ * </li>
+ * </ul>
+ * </li>
+ * </ul>  
  *
- * @param event The DOM event that triggered this Ajax request.
+ * @param element The DOM element that triggered this Ajax request.
  * @param options The set of available options that can be sent as
  * request parameters to control client and/or server side 
- * request processing. 
+ * request processing. This argument is optional. 
+ * @param event The DOM event that triggered this Ajax request.  This
+ * argument is optional.
  */
-javax.faces.Ajax.ajaxRequest = function(event, options) {
+javax.faces.Ajax.ajaxRequest = function(element, options, event) {
 
-    // Make sure we have the event that triggered this Ajax request..
-    event = event || window.event || null;
-
-    // Determine the element that triggered this Ajax request..
-    var source = event.target || event.srcElement || null;
+    // Capture the element that triggered this Ajax request.
+    var source = element;
 
     var utils = new javax.faces.Ajax.Utils();
     var form = utils.getForm(source); 
@@ -118,9 +167,13 @@ javax.faces.Ajax.ajaxRequest = function(event, options) {
     var args = new Object();
     if (options.execute) {
         args["javax.faces.partial.execute"] = utils.toArray(options.execute,',').join(','); 
+        options.execute = null;
+        delete options.execute;
     }
     if (options.render) {
         args["javax.faces.partial.render"] = utils.toArray(options.render,',').join(','); 
+        options.render = null;
+        delete options.render;
     }
 
     utils.extend(args, options);
@@ -143,7 +196,28 @@ javax.faces.Ajax.ajaxRequest = function(event, options) {
 
 /**
  * <p><span class="changed_added_2_0">Receive</span> an Ajax response
- * from the server.
+ * from the server.  This function must evaluate the markup returned 
+ * in the <code>responseXML</code> object and update the <code>DOM</code>
+ * as follows:
+ * <ul>
+ * <li>Determine if the entire <code>DOM</code> should be replaced, or
+ * if only specified sections (known as partial rendering) should be
+ * updated.  The entire <code>DOM</code>must be replaced if a 
+ * <code>render</code> element identifier is 
+ * <code>javax.faces.viewRoot</code>.</li> 
+ * <li>If partial <code>DOM</code> update is required, replace the 
+ * <code>DOM</code> markup whose identifier matches the corresponding
+ * <code>render</code> identifier.</li>
+ * <li>Capture the view state sent in the response and insert it into
+ * the <code>DOM</code> as a <code>hidden input</code> field with the
+ * identifier <code>javax.faces.viewState</code>.  Look for all the
+ * <code>form</code> elements in the <code>DOM</code>, and for each
+ * <code>form</code> element, determine if a <code>javax.faces.viewState</code>
+ * field exists.  If it does, replace it with the view state from the 
+ * response.  If it does not exist, create a <code>hidden input</code>
+ * field with the identifier <code>javax.faces.viewState</code> and 
+ * insert it as a child element of the <code>form</code> elements.</li> 
+ * </ul>
  *
  * @param request The <code>XMLHttpRequest</code> instance that 
  * contains the status code and response message from the server.
