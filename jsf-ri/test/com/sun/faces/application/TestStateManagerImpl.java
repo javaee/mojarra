12fca4d4b74ea7c4f22b41cbba4fbbc66f2bcfdb
rlubke <rlubke@java.net>
2008-09-05 19:03:39 +0000

    - Added tests for StateManager (and by association ResponseStateManager) getViewState() methods.     Validates getViewState() for both client and server.
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@5345 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/test/com/sun/faces/application/TestStateManagerImpl.java
+++ jsf-ri/test/com/sun/faces/application/TestStateManagerImpl.java
@@ -44,6 +44,8 @@ package com.sun.faces.application;
 import java.io.StringWriter;
 import java.util.Locale;
 import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIGraphic;
@@ -51,11 +53,20 @@ import javax.faces.component.UIInput;
 import javax.faces.component.UIOutput;
 import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import javax.faces.application.StateManager;
+import javax.faces.render.RenderKit;
+import javax.faces.render.RenderKitFactory;
 
 import com.sun.faces.cactus.ServletFacesTestCase;
+import com.sun.faces.cactus.TestingUtil;
 import com.sun.faces.renderkit.ServerSideStateHelper;
+import com.sun.faces.renderkit.RenderKitImpl;
 import com.sun.faces.renderkit.html_basic.HtmlResponseWriter;
 import com.sun.faces.util.Util;
+import com.sun.faces.config.WebConfiguration;
+import static com.sun.faces.config.WebConfiguration.WebContextInitParameter.StateSavingMethod;
+import com.sun.faces.context.FacesContextImpl;
 import org.apache.cactus.WebRequest;
 
 
@@ -233,6 +244,88 @@ public class TestStateManagerImpl extends ServletFacesTestCase {
         ctx.getApplication().setStateManager(wrapper);
         
         // construct a view
+        initView(ctx);
+
+        UIViewRoot root = ctx.getViewRoot();
+        root.getAttributes().put("checkThisValue", "checkThisValue");
+        getFacesContext().setResponseWriter(new HtmlResponseWriter(new StringWriter(), "text/html", "UTF-8"));
+        Object viewState = wrapper.saveView(getFacesContext());
+        wrapper.writeState(getFacesContext(), viewState);
+        
+        // See that the Logical View and Actual View maps are correctly created
+        Map sessionMap = ctx.getExternalContext().getSessionMap();
+        assertTrue(sessionMap.containsKey(ServerSideStateHelper.LOGICAL_VIEW_MAP));
+        assertTrue(((Map)sessionMap.get(ServerSideStateHelper.LOGICAL_VIEW_MAP)).containsKey("j_id1"));
+
+        UIViewRoot newRoot = wrapper.restoreView(ctx, "test", "HTML_BASIC");
+        assertNotNull(newRoot);
+        assertEquals(root.getAttributes().get("checkThisValue"),
+                     newRoot.getAttributes().get("checkThisValue"));
+        
+    }
+
+
+    public void testGetViewStateServer() {
+
+        // this exercise ResponseStateManager.getViewState() as well
+        FacesContext ctx = getFacesContext();
+
+        initView(ctx);
+
+        String control = "j_id1:j_id2";
+        String result = ctx.getApplication().getStateManager().getViewState(ctx);
+
+        assertEquals(control, result);
+
+    }
+
+    public void testGetViewStateClient() throws Exception {
+
+        // this exercise ResponseStateManager.getViewState() as well
+        FacesContext ctx = getFacesContext();
+        WebConfiguration webConfig =
+              WebConfiguration.getInstance(ctx.getExternalContext());
+        webConfig.overrideContextInitParameter(StateSavingMethod,
+                                               StateManager.STATE_SAVING_METHOD_CLIENT);
+
+        // recreate the RenderKit so the change is picked up.
+        RenderKit rk = new RenderKitImpl();
+        TestingUtil.setPrivateField("lastRk",
+                                    FacesContextImpl.class,
+                                    ctx,
+                                    rk);
+        TestingUtil.setPrivateField("lastRkId",
+                                    FacesContextImpl.class,
+                                    ctx,
+                                    RenderKitFactory.HTML_BASIC_RENDER_KIT);
+
+        initView(ctx);
+
+        StringWriter capture = new StringWriter();
+        ResponseWriter writer = new HtmlResponseWriter(capture,
+                                                       "text/html",
+                                                       "UTF-8");
+        ctx.setResponseWriter(writer);
+        StateManager manager = ctx.getApplication().getStateManager();
+        Object state = ctx.getApplication().getStateManager().saveView(ctx);
+        manager.writeState(ctx, state);
+        String rawResult = capture.toString();
+        Pattern p = Pattern.compile("\\bvalue=\"(.+)\"");
+        Matcher m = p.matcher(rawResult);
+        assertTrue(m.find());
+        String control = m.group(1);
+        String result = ctx.getApplication().getStateManager().getViewState(ctx);
+
+        assertEquals(control, result);
+
+    }
+
+
+    // --------------------------------------------------------- Private Methods
+
+
+    private void initView(FacesContext ctx) {
+
         UIViewRoot root = Util.getViewHandler(getFacesContext()).createView(ctx, null);
         root.setViewId("/test");
         root.setId("root");
@@ -257,21 +350,7 @@ public class TestStateManagerImpl extends ServletFacesTestCase {
         root.getChildren().add(comp3);
 
         ctx.setViewRoot(root);
-        root.getAttributes().put("checkThisValue", "checkThisValue");
-        getFacesContext().setResponseWriter(new HtmlResponseWriter(new StringWriter(), "text/html", "UTF-8"));
-        Object viewState = wrapper.saveView(getFacesContext());
-        wrapper.writeState(getFacesContext(), viewState);
-        
-        // See that the Logical View and Actual View maps are correctly created
-        Map sessionMap = ctx.getExternalContext().getSessionMap();
-        assertTrue(sessionMap.containsKey(ServerSideStateHelper.LOGICAL_VIEW_MAP));
-        assertTrue(((Map)sessionMap.get(ServerSideStateHelper.LOGICAL_VIEW_MAP)).containsKey("j_id1"));
 
-        UIViewRoot newRoot = wrapper.restoreView(ctx, "test", "HTML_BASIC");
-        assertNotNull(newRoot);
-        assertEquals(root.getAttributes().get("checkThisValue"),
-                     newRoot.getAttributes().get("checkThisValue"));
-        
     }
 
 
