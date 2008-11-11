adad6660ae19047fafced6c9a8c699724d207600
rogerk <rogerk@java.net>
2008-11-11 15:11:11 +0000

    simple demo illustrating declarative ajax
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/AJAX_PFD_BRANCH@5775 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ jsf-demo/declarative-ajax/src/main/java/declarativeajax/Bean.java
@@ -0,0 +1,191 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package declarativeajax;
+
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIForm;
+import javax.faces.component.UIInput;
+import javax.faces.component.UIOutput;
+import javax.faces.component.UIPanel;
+import javax.faces.component.UISelectMany;
+import javax.faces.component.html.HtmlInputText;
+import javax.faces.component.html.HtmlSelectOneMenu;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.ValueChangeEvent;
+import javax.faces.model.SelectItem;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * <p>This bean has the methods that are used to illustrate
+ *  the <code>JSF 2.0 declarative ajax</code>.  * <p/>
+ *
+ */
+
+public class Bean {
+    
+    private static final Logger LOGGER = Logger.getLogger("declarativeajax");
+    
+    //
+    // Relationship Instance Variables
+    //
+
+    private Map<String,String> stateMap = null;
+
+    private List stateOptions = null;
+    
+    //
+    // Constructors
+    //
+    
+    public Bean() {
+        stateMap = new HashMap<String,String>();
+        stateMap.put("CA", "California");
+        stateMap.put("CT", "Connecticut");
+        stateMap.put("MA", "Massachusetts");
+        stateMap.put("NJ", "New Jersey");
+        stateMap.put("NY", "New York");
+
+        stateOptions = new ArrayList();
+        stateOptions.add(new SelectItem("CA","CA","CA"));
+        stateOptions.add(new SelectItem("CT","CT","CT"));
+        stateOptions.add(new SelectItem("MA","MA","MA"));
+        stateOptions.add(new SelectItem("NJ","NJ","NJ"));
+        stateOptions.add(new SelectItem("NY","NY","NY"));
+
+    }
+
+    public Collection getStateOptions() {
+        return stateOptions;
+    }
+
+
+    public void setStateOptions(Collection newOptions) {
+        stateOptions = new ArrayList(newOptions);
+    }
+
+    public void beanAction(ActionEvent event) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        UIForm form = (UIForm)context.getViewRoot().findComponent("form1");
+        UIInput name = (UIInput)form.findComponent("name");
+        name.setValue("");
+        UIInput address = (UIInput)form.findComponent("address");
+        address.setValue("");
+        UIInput city = (UIInput)form.findComponent("city");
+        city.setValue("");
+        UIInput zip = (UIInput)form.findComponent("zip");
+        zip.setValue("");
+    }
+
+    public void displayState(ValueChangeEvent event) {
+        String state = (String)((HtmlSelectOneMenu)event.getComponent()).getValue();
+        FacesContext context = FacesContext.getCurrentInstance();
+        UIForm form = (UIForm)context.getViewRoot().findComponent("form1");
+        UIOutput output = (UIOutput)form.findComponent("stateout");
+        output.setValue((String)stateMap.get(state));
+    }
+
+    public void displayText(ValueChangeEvent event) {
+        String text = (String)((UIInput)event.getComponent()).getValue();
+        String label = (String)((UIInput)event.getComponent()).getAttributes().get("label"); 
+        FacesContext context = FacesContext.getCurrentInstance();
+        UIForm form = (UIForm)context.getViewRoot().findComponent("form1");
+        UIOutput output = null;
+        if ((label!=null)&&label.equals("Name")) {
+            output = (UIOutput)form.findComponent("nameout");
+            output.setValue("You entered: "+text);
+        } else if ((label!=null)&&label.equals("Address")) {
+            output = (UIOutput)form.findComponent("addressout");
+            output.setValue("You entered: "+text);
+        } else if ((label!=null)&&label.equals("City")) {
+            output = (UIOutput)form.findComponent("cityout");
+            output.setValue("You entered: "+text);
+        } else if ((label!=null)&&label.equals("Zip")) {
+            output = (UIOutput)form.findComponent("zipout");
+            output.setValue("You entered: "+text);
+        }
+    }
+
+    public void displayRadio(ValueChangeEvent event) {
+        String text = (String)((UIInput)event.getComponent()).getValue();
+        FacesContext context = FacesContext.getCurrentInstance();
+        UIForm form = (UIForm)context.getViewRoot().findComponent("form1");
+        UIOutput output = null;
+        if (text.equals("Java")) {
+            output = (UIOutput)form.findComponent("softwareLang");
+            output.setValue("Great choice!");
+        } else if (text.equals("C#")) {
+            output = (UIOutput)form.findComponent("softwareLang");
+            output.setValue("Pretty similar to Java..");
+        } else if (text.equals("C++")) {
+            output = (UIOutput)form.findComponent("softwareLang");
+            output.setValue("Great lower level OO language..");
+        } else if (text.equals("C")) {
+            output = (UIOutput)form.findComponent("softwareLang");
+            output.setValue("When speed and device programming matter..");
+        }
+    }
+
+    public void displayList(ValueChangeEvent event) {
+        String text = (String)((UIInput)event.getComponent()).getValue();
+        FacesContext context = FacesContext.getCurrentInstance();
+        UIForm form = (UIForm)context.getViewRoot().findComponent("form1");
+        UIOutput output = null;
+        if (text.equals("10")) {
+            output = (UIOutput)form.findComponent("out1");
+            output.setValue("Really?");
+        } else if (text.equals("8")) {
+            output = (UIOutput)form.findComponent("out1");
+            output.setValue("Are you sure?");
+        } else if (text.equals("all")) {
+            output = (UIOutput)form.findComponent("out1");
+            output.setValue("Correct!");
+        }
+    }
+        
+}
