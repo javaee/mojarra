fc2e7cb8d1744d0d3db5efec32190397f95f1185
driscoll <driscoll@java.net>
2008-11-14 01:12:17 +0000

    Demo cleanup for switchlist, prior to blogging.
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@5819 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-demo/ajax-switchlist/src/example/ListHolder.java
+++ jsf-demo/ajax-switchlist/src/example/ListHolder.java
@@ -14,25 +14,35 @@ import java.util.List;
 public class ListHolder {
 
     private Map<String, String> items1 = new LinkedHashMap<String, String>();
-
     private Map<String, String> items2 = new LinkedHashMap<String, String>();
-
-    {
-        items1.put("one", "one");
-        items1.put("two", "two");
-        items1.put("three", "three");
-        items1.put("four", "four");
-    }
-
-    {
-        items2.put("five", "five");
-        items2.put("six", "six");
-        items2.put("seven", "seven");
-        items2.put("eight", "eight");
-    }
-
     private String[] list1 = null;
     private String[] list2 = null;
+
+    {   items1.put("one", "one");
+        items1.put("two", "two");
+        items1.put("three", "three");
+        items1.put("four", "four");     }
+
+    {   items2.put("five", "five");
+        items2.put("six", "six");
+        items2.put("seven", "seven");
+        items2.put("eight", "eight");   }
+
+    public void move1to2(ActionEvent ae) {
+        if (list1 != null && list1.length > 0) {
+            for (String item : list1 ) {
+                items2.put(item, items1.remove(item));
+            }
+        }
+    }
+
+    public void move2to1(ActionEvent ae) {
+        if (list2 != null && list2.length > 0) {
+            for (String item : list2 ) {
+                items1.put(item, items2.remove(item));
+            }
+        }
+    }
 
     public String[] getList1() {
         return list1;
@@ -58,22 +68,6 @@ public class ListHolder {
         return items2;
     }
 
-    public void move1to2(ActionEvent ae) {
-        if (list1 != null && list1.length > 0) {
-            for (String item : list1 ) {
-                items2.put(item, items1.remove(item));
-            }
-        }
-    }
-
-    public void move2to1(ActionEvent ae) {
-        if (list2 != null && list2.length > 0) {
-            for (String item : list2 ) {
-                items1.put(item, items2.remove(item));
-            }
-        }
-    }
-
     public String getList1Cat() {
         return concat(list1);
     }
@@ -81,7 +75,7 @@ public class ListHolder {
     public String getList2Cat() {
         return concat(list2);
     }
-    
+
     private String concat(String[] arr) {
         String ret = "";
         if (arr == null) return ret;
@@ -90,5 +84,4 @@ public class ListHolder {
         }
         return ret;
     }
-
 }
