ace1342807d3e439a87e4ac3850cc84aa5e348a2
rlubke <rlubke@java.net>
2008-12-22 19:38:08 +0000

    - minor changes to NavigationCase   * Since NavigationCase is immutable, removed the copy constructor,     clone() implementation and the Clonable marker interface.   * added docs to the NavigationCase constructor   * implemented equals() and hashCode()
     - removed unused import in NavigationConfigProcessor
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@6163 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/com/sun/faces/config/processor/NavigationConfigProcessor.java
+++ jsf-ri/src/com/sun/faces/config/processor/NavigationConfigProcessor.java
@@ -53,8 +53,8 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.text.MessageFormat;
-import javax.faces.application.NavigationCase;
 
+ 
 /**
  * <p>
  *  This <code>ConfigProcessor</code> handles all elements defined under
