13bda564fe459825080eb6941505fca456325710
edburns <edburns@java.net>
2009-02-17 20:58:50 +0000

    519-pageParams_sample_app
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/PAGE_PARAMS_20090217@6494 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ jsf-demo/news/src/java/news/NewsReader.java
@@ -0,0 +1,74 @@
+package news;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.model.ManagedBean;
+import javax.faces.model.ManagedProperty;
+import javax.faces.model.RequestScoped;
+
+/**
+ * @author Dan Allen
+ */
+@RequestScoped @ManagedBean
+public class NewsReader {
+
+    private FacesContext facesContext;
+
+    @ManagedProperty("#{newsIndex}")
+    private NewsIndex newsIndex;
+
+    private List<NewsStory> stories;
+
+    private NewsStory selectedStory;
+
+    private Long selectedStoryId;
+
+    @PostConstruct
+    public void postConstruct() {
+        facesContext = FacesContext.getCurrentInstance();
+        stories = new ArrayList<NewsStory>(newsIndex.getEntries().values());
+    }
+
+    public void loadStory() {
+        if (!facesContext.getValidationFailed()) {
+            NewsStory story = newsIndex.getStory(selectedStoryId);
+            if (story != null) {
+                selectedStory = story;
+                return;
+            }
+
+            facesContext.addMessage(null, new FacesMessage("The headline you requested does not exist."));
+        }
+
+        //facesContext.getFlash().setKeepMessages(true); // only needed if navigation case is a redirect
+        facesContext.getApplication().getNavigationHandler().handleNavigation(facesContext, null, "home");
+        // we would like the following instead
+        // facesContext.fireNavigation("home");
+    }
+
+    public List<NewsStory> getStories() {
+        return stories;
+    }
+
+    public NewsStory getSelectedStory() {
+        return selectedStory;
+    }
+
+    public Long getSelectedStoryId() {
+        return selectedStoryId;
+    }
+
+    public void setSelectedStoryId(Long storyId) {
+        this.selectedStoryId = storyId;
+    }
+
+    // Injected Properties
+
+    public void setNewsIndex(NewsIndex newsIndex) {
+        this.newsIndex = newsIndex;
+    }
+
+}
