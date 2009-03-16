0c81b1cb021287d4c0dc334ba6e8403955b68a04
aschwart <aschwart@java.net>
2009-03-16 22:13:21 +0000

    Enable behavior partial state saving.  Changes include:
    - BehaviorBase now implements PartialStateHolder
    - BehaviorBase in addition to the PartialStateHolder methods, Behavior base now exposes a protected clearInitialState() method.  This is called in repsonse to modifications in order to force the behavior into full state saving mode.
    - BehaviorBase calls clearInitialState() in response to listener adds/removes.
    - AjaxBehavior calls clearInitialState() in response to property changes
    
    Note - that I also call clearInitialState() in both BehaviorBase.restoreState() and AjaxBehavior.restoreState() in the event that we had state to restore.  My assumption is that if we had state to restore, that means that we previously performed full state saving, which I think means that we want to perform full state saving again the next time we save state.  So, by clearing the initialState flag in restore, we propagate the initialState="false" value across saves/restores.
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/STATE_SAVING_20081211@6838 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/javax/faces/component/behavior/AjaxBehavior.java
+++ jsf-api/src/javax/faces/component/behavior/AjaxBehavior.java
@@ -138,6 +138,7 @@ public class AjaxBehavior extends ClientBehaviorBase {
 
         this.onerror = onerror;
 
+        clearInitialState();
     }
 
     /**
@@ -167,6 +168,7 @@ public class AjaxBehavior extends ClientBehaviorBase {
 
         this.onevent = onevent;
 
+        clearInitialState();
    }
 
     /**
@@ -202,6 +204,7 @@ public class AjaxBehavior extends ClientBehaviorBase {
 
         this.execute = copyToList(execute);
 
+        clearInitialState();
     }
 
     /**
@@ -238,6 +241,7 @@ public class AjaxBehavior extends ClientBehaviorBase {
         // See setExecute() for comment on the lack of a defensive copy.
         this.execute = copyToList(execute);
 
+        clearInitialState();
     }
 
     /**
@@ -261,6 +265,7 @@ public class AjaxBehavior extends ClientBehaviorBase {
 
         this.disabled = disabled;
 
+        clearInitialState();
     }
 
     /**
@@ -285,6 +290,7 @@ public class AjaxBehavior extends ClientBehaviorBase {
 
         this.immediate = immediate;
 
+        clearInitialState();
     }
 
     /**
@@ -368,6 +374,7 @@ public class AjaxBehavior extends ClientBehaviorBase {
             }
         }
 
+        clearInitialState();
     }
 
     /**
@@ -405,16 +412,28 @@ public class AjaxBehavior extends ClientBehaviorBase {
     @Override
     public Object saveState(FacesContext context) {
 
-        Object[] values = new Object[8];
+        Object[] values;
+
+        Object superState = super.saveState(context);
+
+        if (initialStateMarked()) {
+            if (superState == null) {
+                values = null;
+            } else {
+                values = new Object[] { superState };
+            }
+        } else {
+            values = new Object[8];
       
-        values[0] = super.saveState(context);
-        values[1] = onerror;
-        values[2] = onevent;
-        values[3] = disabled;
-        values[4] = immediate;
-        values[5] = saveList(execute);
-        values[6] = saveList(render);
-        values[7] = saveBindings(context, bindings);
+            values[0] = superState;
+            values[1] = onerror;
+            values[2] = onevent;
+            values[3] = disabled;
+            values[4] = immediate;
+            values[5] = saveList(execute);
+            values[6] = saveList(render);
+            values[7] = saveBindings(context, bindings);
+        }
 
         return values;
     }
@@ -422,16 +441,24 @@ public class AjaxBehavior extends ClientBehaviorBase {
     @Override
     public void restoreState(FacesContext context, Object state) {
 
-        Object[] values = (Object[]) state;
-        super.restoreState(context, values[0]);
+        if (state != null) {
 
-        onerror = (String)values[1];
-        onevent = (String)values[2];
-        disabled = (Boolean)values[3];
-        immediate = (Boolean)values[4];
-        execute = restoreList(EXECUTE, values[5]);
-        render = restoreList(RENDER, values[6]);
-        bindings = restoreBindings(context, values[7]);
+            Object[] values = (Object[]) state;
+            super.restoreState(context, values[0]);
+
+            if (values.length != 1) {
+                onerror = (String)values[1];
+                onevent = (String)values[2];
+                disabled = (Boolean)values[3];
+                immediate = (Boolean)values[4];
+                execute = restoreList(EXECUTE, values[5]);
+                render = restoreList(RENDER, values[6]);
+                bindings = restoreBindings(context, values[7]);
+
+                // If we saved state last time, save state again next time.
+                clearInitialState();
+            }
+        }
     }
 
 
