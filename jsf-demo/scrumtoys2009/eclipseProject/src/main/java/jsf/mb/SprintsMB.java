a204e33b77ed707e95b780427571e4bf0cd2d387
yarahornos <yarahornos@java.net>
2009-04-24 17:16:02 +0000

    Update view layer, css and js
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@7135 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-demo/scrumtoys2009/eclipseProject/src/main/java/jsf/mb/SprintsMB.java
+++ jsf-demo/scrumtoys2009/eclipseProject/src/main/java/jsf/mb/SprintsMB.java
@@ -16,65 +16,97 @@ import entities.Sprint;
  * @ since 2008
  *
  */
-@ManagedBean(name="sprintsMB")
-@ViewScoped
+@ManagedBean(name = "sprintsMB")
+
 public class SprintsMB extends AppCrudMB<SprintDAO, SprintService, Sprint> {
 
-	private static final long serialVersionUID = -1094408292080760267L;
+    private static final long serialVersionUID = -1094408292080760267L;
+    private boolean showAddNewStoryForm = false;
 
-	@Override
-	public String getEntitiesLabel() {
-		return "sprints";
-	}
+    @Override
+    public String getEntitiesLabel() {
+        return "sprints";
+    }
 
-	@Override
-	public String getEntityLabel() {
-		return "sprint";
-	}
+    @Override
+    public String getEntityLabel() {
+        return "sprint";
+    }
 
-	@Override
-	public Sprint getNewEntity() {
-		return new Sprint();
-	}
+    @Override
+    public Sprint getNewEntity() {
+        return new Sprint();
+    }
 
-	@Override
-	public SprintService getService() {
-		return new SprintService();
-	}
-	
-	private enum OUTCOME {
-		STORIES("stories"), DASHBOARD("dashboard");
-		private String value;
+    @Override
+    public SprintService getService() {
+        return new SprintService();
+    }
 
-		OUTCOME(String $value) {
-			this.value = $value;
-		}
+    /**
+     * @return the showAddNewStoryForm
+     */
+    public boolean isShowAddNewStoryForm() {
+        System.out.println("isShowAddNewStoryForm - showAddNewStoryForm =" + showAddNewStoryForm);
+        return showAddNewStoryForm;
+    }
 
-		public String getValue() {
-			return this.value;
-		}
-	}	
+    /**
+     * @param showAddNewStoryForm the showAddNewStoryForm to set
+     */
+    public void setShowAddNewStoryForm(boolean showAddNewStoryForm) {
+        System.out.println("setShowAddNewStoryForm - showAddNewStoryForm =" + showAddNewStoryForm);
+        this.showAddNewStoryForm = showAddNewStoryForm;
+        System.out.println("setShowAddNewStoryForm - showAddNewStoryForm =" + showAddNewStoryForm);
 
-	public String showStories(){
-		Sprint selectedSprint = (Sprint) this.getDm().getRowData();
-		FacesUtil.getInstance().putRequestAttr("selectedSprint", selectedSprint);
-		return OUTCOME.STORIES.getValue();
-	}
+    }
 
-	public String showDashboard(){
-		Sprint selectedSprint = (Sprint) this.getDm().getRowData();
-		try {
-			selectedSprint = new SprintDAOMemory().findAllById(selectedSprint);
-		} catch (DAOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		FacesUtil.getInstance().putRequestAttr("selectedSprint", selectedSprint);
-		return OUTCOME.DASHBOARD.getValue();
-	}
-	
-	@Override
-	public boolean preSave() {
-		return true;
-	}
+    public void showForm() {
+        System.out.println("showForm - showAddNewStoryForm =" + showAddNewStoryForm);
+        this.setShowAddNewStoryForm(true);
+        System.out.println("showForm - showAddNewStoryForm =" + showAddNewStoryForm);
+    }
+
+    public void hideForm() {
+        System.out.println("hideForm - showAddNewStoryForm =" + showAddNewStoryForm);
+        this.setShowAddNewStoryForm(false);
+        System.out.println("hideForm - showAddNewStoryForm =" + showAddNewStoryForm);
+    }
+
+    private enum OUTCOME {
+
+        STORIES("stories"), DASHBOARD("dashboard");
+        private String value;
+
+        OUTCOME(String $value) {
+            this.value = $value;
+        }
+
+        public String getValue() {
+            return this.value;
+        }
+    }
+
+    public String showStories() {
+        Sprint selectedSprint = (Sprint) this.getDm().getRowData();
+        FacesUtil.getInstance().putRequestAttr("selectedSprint", selectedSprint);
+        return OUTCOME.STORIES.getValue();
+    }
+
+    public String showDashboard() {
+        Sprint selectedSprint = (Sprint) this.getDm().getRowData();
+        try {
+            selectedSprint = new SprintDAOMemory().findAllById(selectedSprint);
+        } catch (DAOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        FacesUtil.getInstance().putRequestAttr("selectedSprint", selectedSprint);
+        return OUTCOME.DASHBOARD.getValue();
+    }
+
+    @Override
+    public boolean preSave() {
+        return true;
+    }
 }
