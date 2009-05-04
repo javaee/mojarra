9259ef4c63f45399192593acfb6fa5b8aadb8f85
rlubke <rlubke@java.net>
2009-05-04 16:05:04 +0000

    Add RELEASE_PENDING
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@7270 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/javax/faces/event/PreDestroyViewMapEvent.java
+++ jsf-api/src/javax/faces/event/PreDestroyViewMapEvent.java
@@ -12,7 +12,16 @@ import javax.faces.component.UIViewRoot;
  * <p class="changed_added_2_0">This event must be published by a call
  * to {@link javax.faces.application.Application#publishEvent} when the
  * <code>clear</code> method is called on the map returned from {@link
- * UIViewRoot#getViewMap}.  This must happen when a call is made to
+ * UIViewRoot#getViewMap}.
+ *
+ * RELEASE_PENDING (edburns) The javadocs for FacesContext.setViewRoot()
+ *  already cover this.  I don't think we need to duplicate it here.
+ *  Additionally, we need to make sure the navigation section of the
+ *  spec document clearly states that the view map is cleared when
+ *  navigation to a view ID different from the current view ID regardless
+ *  of the navigation being a forward or a redirect.
+ * 
+ * This must happen when a call is made to
  * {@link javax.faces.context.FacesContext#setViewRoot} and the argument
  * UIViewRoot is different from the current, non-<code>null</code>
  * UIViewRoot.  See {@link javax.faces.context.FacesContext#setViewRoot}
