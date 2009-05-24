db767ea5edc27d309d451638f56d560a6b8304a4
yarahornos <yarahornos@java.net>
2009-05-24 18:26:40 +0000

    .
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@7369 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-demo/scrumtoys2009/eclipseProject/src/main/java/jsf2/demo/scrum/web/controller/DashboardManager.java
+++ jsf-demo/scrumtoys2009/eclipseProject/src/main/java/jsf2/demo/scrum/web/controller/DashboardManager.java
@@ -1,84 +1,117 @@
 package jsf2.demo.scrum.web.controller;
 
 import java.io.Serializable;
-import java.util.Collections;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.ViewScoped;
-import javax.faces.event.AbortProcessingException;
-import javax.faces.event.SystemEvent;
-import javax.faces.event.SystemEventListener;
+import javax.faces.bean.SessionScoped;
 import javax.faces.model.DataModel;
 import javax.faces.model.ListDataModel;
 import jsf2.demo.scrum.model.entities.Sprint;
 import jsf2.demo.scrum.model.entities.Story;
-import jsf2.demo.scrum.web.event.CurrentSprintChangeEvent;
+import jsf2.demo.scrum.model.entities.Task;
 
-/**
- *
- * @author Dr. Spock (spock at dev.java.net)
- */
+
 @ManagedBean(name = "dashboardManager")
-@ViewScoped
+@SessionScoped
 public class DashboardManager extends AbstractManager implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    private DataModel<Story> stories;
-    private List<Story> storyList;
-    @ManagedProperty("#{sprintManager.currentSprint}")
-    private Sprint sprint;
-    private SystemEventListener sprintChangeListener;
 
-    @PostConstruct
-    public void construct() {
-        sprintChangeListener = new SprintChangeListener();
-        subscribeToEvent(CurrentSprintChangeEvent.class, sprintChangeListener);
-        init();
-    }
+    @ManagedProperty("#{taskManager}")
+    private TaskManager taskManager;
+    @ManagedProperty("#{sprintManager}")
+    private SprintManager sprintManager;
+    @ManagedProperty("#{storyManager}")
+    private StoryManager storyManager;
+    
+    
+    private ListDataModel<Task> toDoTasks;
+    private ListDataModel<Task> workingTasks;
+    private ListDataModel<Task> doneTasks;        
 
-    @PreDestroy
-    public void destroy() {
-        unsubscribeFromEvent(CurrentSprintChangeEvent.class, sprintChangeListener);
-    }
-
-    public void init() {
-        if (sprint != null) {
-            storyList = new LinkedList<Story>(sprint.getStories());
-        } else {
-            storyList = Collections.emptyList();
-        }
-        stories = new ListDataModel<Story>(storyList);
-    }
+   
 
     public Sprint getSprint() {
-        return sprint;
+        return getSprintManager().getCurrentSprint();
     }
 
     public void setSprint(Sprint sprint) {
-        this.sprint = sprint;
+        this.getSprintManager().setCurrentSprint(sprint);
     }
 
     public DataModel<Story> getStories() {
-        return stories;
+        return storyManager.getStories();
     }
+
 
     public void setStories(DataModel<Story> stories) {
-        this.stories = stories;
+        storyManager.setStories(stories);
     }
 
-    private class SprintChangeListener implements SystemEventListener, Serializable {
-
-        public void processEvent(SystemEvent event) throws AbortProcessingException {
-            sprint = (Sprint) event.getSource();
-            init();
+    public ListDataModel getToDoTasks (){
+        List toDoTasksList = new ArrayList();       
+        for (Story story: storyManager.getStoryList()){
+            toDoTasksList.addAll(story.getTodoTasks());
         }
-
-        public boolean isListenerForSource(Object source) {
-            return (source instanceof Sprint);
-        }
+        toDoTasks = new ListDataModel(toDoTasksList);
+        return toDoTasks;
     }
+
+    public ListDataModel getWorkingTasks (){
+        List workingTasksList = new ArrayList();
+        for (Story story: storyManager.getStoryList()){
+            workingTasksList.addAll(story.getWorkingTasks());
+        }
+        workingTasks = new ListDataModel(workingTasksList);
+        return workingTasks;
+    }
+
+       public ListDataModel getDoneTasks (){
+        List doneTasksList = new ArrayList();        
+        for (Story story: storyManager.getStoryList()){
+            doneTasksList.addAll(story.getDoneTasks());
+        }
+        doneTasks = new ListDataModel(doneTasksList);
+        return doneTasks;
+    }
+    public String editToDoTask() {
+        taskManager.setCurrentTask(toDoTasks.getRowData());
+        return "/task/edit";
+    }
+
+    public String editDoneTask() {
+        taskManager.setCurrentTask(doneTasks.getRowData());
+       return "/task/edit";
+    }
+
+    public String editWorkingTask() {
+        taskManager.setCurrentTask(workingTasks.getRowData());
+       return "/task/edit";
+    }
+    public TaskManager getTaskManager() {
+        return taskManager;
+    }
+
+    public void setTaskManager(TaskManager taskManager) {
+        this.taskManager = taskManager;
+    }
+
+    public SprintManager getSprintManager() {
+        return sprintManager;
+    }
+
+    public void setSprintManager(SprintManager sprintManager) {
+        this.sprintManager = sprintManager;
+    }
+
+    public StoryManager getStoryManager() {
+        return storyManager;
+    }
+
+    public void setStoryManager(StoryManager storyManager) {
+        this.storyManager = storyManager;
+    }
+    
 }
