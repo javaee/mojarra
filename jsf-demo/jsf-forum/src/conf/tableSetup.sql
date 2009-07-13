43b7adfc14a431be9e34a82a364e3dd8d77ce12f
driscoll <driscoll@java.net>
2009-07-13 17:20:13 +0000

    Add autogeneration of indexes.
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@7536 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-demo/jsf-forum/src/conf/tableSetup.sql
+++ jsf-demo/jsf-forum/src/conf/tableSetup.sql
@@ -5,22 +5,25 @@ drop table threads;
 drop table topics;
 
 create table topics (
-    topicID INT NOT NULL,
+    topicID INT NOT NULL
+            PRIMARY KEY GENERATED ALWAYS AS IDENTITY
+                (START WITH 1, INCREMENT BY 1),
     title VARCHAR(40) NOT NULL,
-    subject VARCHAR(160),
-    PRIMARY KEY (topicID));
+    subject VARCHAR(160));
 
 create table threads (
-    threadID INT NOT NULL,
+    threadID INT NOT NULL
+            PRIMARY KEY GENERATED ALWAYS AS IDENTITY
+                (START WITH 1, INCREMENT BY 1),
     topicID INT NOT NULL,
     title VARCHAR(40) NOT NULL,
-    PRIMARY KEY (threadID),
     FOREIGN KEY (topicID) REFERENCES topics);
 
 create table messages (
-    messageID INT NOT NULL,
+    messageID INT NOT NULL
+            PRIMARY KEY GENERATED ALWAYS AS IDENTITY
+                (START WITH 1, INCREMENT BY 1),
     threadID INT NOT NULL,
     subject VARCHAR(80),
     text VARCHAR(10000),
-    PRIMARY KEY (messageID),
     FOREIGN KEY (threadID) REFERENCES threads);
