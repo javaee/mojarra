d29c1a9d9b0ff64f364ba6631f7e5bd365263240
driscoll <driscoll@java.net>
2009-08-05 20:20:52 +0000

    Revert: Fix for Bug 1194 - allow the "media" attribute be specified for outputStylesheet tag.
    This will instead be applied to Mojarra 2.1.
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@7639 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/com/sun/faces/renderkit/html_basic/StylesheetRenderer.java
+++ jsf-ri/src/com/sun/faces/renderkit/html_basic/StylesheetRenderer.java
@@ -78,8 +78,6 @@ public class StylesheetRenderer extends ScriptStyleBaseRenderer {
         String name = (String) attributes.get("name");
         String library = (String) attributes.get("library");
         String key = name + library;
-
-        String media = (String) attributes.get("media");
         
         if (null == name) {
             return;
@@ -103,9 +101,6 @@ public class StylesheetRenderer extends ScriptStyleBaseRenderer {
                                   ? resource.getRequestPath()
                                   : "RES_NOT_FOUND"),
                               "href");
-        if (media != null) {
-            writer.writeAttribute("media", media, "media");
-        }
         writer.endElement("link");
 
     }
