0b71059d80af8e36de24194ba8c2ec1b928ee577
edburns <edburns@java.net>
2009-08-10 16:49:31 +0000

    M       date.js M       validations.js
    - add link to OSR: <https://opensourcereview.central.sun.com/app?action=ViewReq&review_type=Expedited&traq_num=12626>
    
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@7656 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-demo/scrumtoys2009/eclipseProject/src/main/webapp/resources/js/date.js
+++ jsf-demo/scrumtoys2009/eclipseProject/src/main/webapp/resources/js/date.js
@@ -3,7 +3,8 @@
  * This file has been removed pending approval of an Open Source Request
  * (OSR).  The following information describes the file that should be
  * restored once the OSR has been approved.
- *
+
+ * OSR: https://opensourcereview.central.sun.com/app?action=ViewReq&review_type=Expedited&traq_num=12626
  * source: http://www.javascripttoolbox.com/libsource.php/date/source/date.js
  * version: 1.02
 
--- jsf-demo/scrumtoys2009/eclipseProject/src/main/webapp/resources/js/validations.js
+++ jsf-demo/scrumtoys2009/eclipseProject/src/main/webapp/resources/js/validations.js
@@ -3,7 +3,8 @@
  * This file has been removed pending approval of an Open Source Request
  * (OSR).  The following information describes the file that should be
  * restored once the OSR has been approved.
- *
+
+ * OSR: https://opensourcereview.central.sun.com/app?action=ViewReq&review_type=Expedited&traq_num=12626
  * source: http://www.mattkruse.com/javascript/validations/validations.js
  * version: December 29, 2003
 
