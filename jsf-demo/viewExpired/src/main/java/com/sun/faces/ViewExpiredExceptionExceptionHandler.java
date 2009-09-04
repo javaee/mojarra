bd13ebae08c2b04c04a6fe825997bdb33af9a0b5
edburns <edburns@java.net>
2009-09-04 16:32:57 +0000

    ViewExpired demo and test
    SECTION: Modified Files
    ----------------------------
    M       nbproject/project.xml
    M       jsf-demo/build.xml
    M       jsf-demo/build-tests.xml
    
    
    A       jsf-demo/viewExpired
    A       jsf-demo/viewExpired/profiles.xml
    A       jsf-demo/viewExpired/src
    A       jsf-demo/viewExpired/src/main
    A       jsf-demo/viewExpired/src/main/java
    A       jsf-demo/viewExpired/src/main/java/com
    A       jsf-demo/viewExpired/src/main/java/com/sun
    A       jsf-demo/viewExpired/src/main/java/com/sun/faces
    A       jsf-demo/viewExpired/src/main/java/com/sun/faces/ViewExpiredExceptionExceptionHandlerFactory.java
    A       jsf-demo/viewExpired/src/main/java/com/sun/faces/NullRestoreViewViewHandler.java
    A       jsf-demo/viewExpired/src/main/java/com/sun/faces/ViewExpiredExceptionExceptionHandler.java
    A       jsf-demo/viewExpired/src/main/webapp
    A       jsf-demo/viewExpired/src/main/webapp/main.xhtml
    A       jsf-demo/viewExpired/src/main/webapp/viewExpired.xhtml
    A       jsf-demo/viewExpired/src/main/webapp/WEB-INF
    A       jsf-demo/viewExpired/src/main/webapp/WEB-INF/sun-web.xml
    A       jsf-demo/viewExpired/src/main/webapp/WEB-INF/faces-config.xml
    A       jsf-demo/viewExpired/src/main/webapp/WEB-INF/web.xml
    A       jsf-demo/viewExpired/pom.xml
    A       jsf-demo/viewExpired/build.xml
    A       jsf-demo/test/tests/com/sun/faces/demotest/viewExpired
    A       jsf-demo/test/tests/com/sun/faces/demotest/viewExpired/TestViewExpiredExceptionExceptionHandler.java
    
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@7819 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ jsf-demo/viewExpired/src/main/java/com/sun/faces/ViewExpiredExceptionExceptionHandler.java
@@ -0,0 +1,108 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+
+package com.sun.faces;
+
+import java.util.Iterator;
+import java.util.Map;
+import javax.faces.FacesException;
+import javax.faces.application.NavigationHandler;
+import javax.faces.application.ViewExpiredException;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.ExceptionHandler;
+import javax.faces.context.ExceptionHandlerWrapper;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ExceptionQueuedEvent;
+import javax.faces.event.ExceptionQueuedEventContext;
+
+public class ViewExpiredExceptionExceptionHandler extends ExceptionHandlerWrapper {
+
+    private ExceptionHandler wrapped;
+
+    public ViewExpiredExceptionExceptionHandler(ExceptionHandler wrapped) {
+        this.wrapped = wrapped;
+    }
+
+    @Override
+    public ExceptionHandler getWrapped() {
+        return this.wrapped;
+    }
+
+    @Override
+    public void handle() throws FacesException {
+        for (Iterator<ExceptionQueuedEvent> i = getUnhandledExceptionQueuedEvents().iterator(); i.hasNext();) {
+            ExceptionQueuedEvent event = i.next();
+            ExceptionQueuedEventContext context = (ExceptionQueuedEventContext) event.getSource();
+            Throwable t = context.getException();
+            if (t instanceof ViewExpiredException) {
+                ViewExpiredException vee = (ViewExpiredException) t;
+                FacesContext fc = FacesContext.getCurrentInstance();
+                Map<String, Object> requestMap = fc.getExternalContext().getRequestMap();
+                NavigationHandler nav =
+                        fc.getApplication().getNavigationHandler();
+                this.thisMethodIsOnlyNecessaryBecauseOfTheWayWeForceTheViewExpiredException(fc);
+                try {
+                    // Push some useful stuff to the request scope for
+                    // use in the page
+                    requestMap.put("currentViewId", vee.getViewId());
+
+                    nav.handleNavigation(fc, null, "viewExpired");
+                    fc.renderResponse();
+
+                } finally {
+                    i.remove();
+                }
+            } 
+        }
+        // At this point, the queue will not contain any ViewExpiredEvents.
+        // Therefore, let the parent handle them.
+        getWrapped().handle();
+
+    }
+
+    
+
+    private void thisMethodIsOnlyNecessaryBecauseOfTheWayWeForceTheViewExpiredException(FacesContext context) {
+        UIViewRoot root = context.getApplication().getViewHandler().createView(context, "/main.xhtml");
+        context.setViewRoot(root);
+
+
+    }
+
+
+
+}
