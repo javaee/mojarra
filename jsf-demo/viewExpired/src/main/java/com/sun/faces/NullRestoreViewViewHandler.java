b41d382683d39b1ec8bf7e5b38d1caef5e1ed476
driscoll <driscoll@java.net>
2009-10-15 00:07:56 +0000

    Cleanup View Expired demo
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@7984 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-demo/viewExpired/src/main/java/com/sun/faces/NullRestoreViewViewHandler.java
+++ jsf-demo/viewExpired/src/main/java/com/sun/faces/NullRestoreViewViewHandler.java
@@ -1,11 +1,7 @@
 /*
- * $Id: RestoreViewPhase.java,v 1.53.8.3 2008/04/12 16:32:30 edburns Exp $
- */
-
-/*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
