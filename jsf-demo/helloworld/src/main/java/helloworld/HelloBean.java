302e16148876950f9b3ff31088edf74a9c661f88
driscoll <driscoll@java.net>
2009-10-15 21:57:46 +0000

    Rework helloFaclet to be helloworld demo.
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@8017 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-demo/helloworld/src/main/java/helloworld/HelloBean.java
+++ jsf-demo/helloworld/src/main/java/helloworld/HelloBean.java
@@ -34,16 +34,18 @@
  * holder.
  */
  
-package helloFacelet;
+package helloworld;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
 
+@ManagedBean
+@SessionScoped
 public class HelloBean implements Serializable {
 
-    private String name;
-    
-   public HelloBean() {}
-    
+   private String name;
+
    public String getName() { return name;}
    
    public void setName(String name) { this.name = name; }
