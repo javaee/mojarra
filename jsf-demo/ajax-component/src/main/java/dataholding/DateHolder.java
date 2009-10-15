a8fbb1b84154ee388cb98070ade6718922812f00
driscoll <driscoll@java.net>
2009-10-15 21:08:31 +0000

    Update copyright year in demo directory...
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@8010 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-demo/ajax-component/src/main/java/dataholding/DateHolder.java
+++ jsf-demo/ajax-component/src/main/java/dataholding/DateHolder.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -43,6 +43,9 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean(name="date")
 @SessionScoped
 public class DateHolder implements Serializable {
+
+    private static final long serialVersionUID = 6499154494910177344L;
+
 
     Date date1;
     Date date2;
