1db26429156f2069fdcc9a7e1ff6f4ff5b7e9df5
rlubke <rlubke@java.net>
2009-10-16 18:03:58 +0000

    UC2 updates.
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@8057 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- updatecenter2/conf/jsf_proto.py
+++ updatecenter2/conf/jsf_proto.py
@@ -37,17 +37,16 @@
 #
 pkg = {
     "name"          : "glassfish-jsf",
-    "version"       : "2.0.0,0-17",
+    "version"       : "2.0.0,0-23",
     "depends"       : {
                        "pkg:/glassfish-jsf@2.0" : {"type" : "incorporate"},
                        "pkg:/glassfish-common" : { "type" : "require" }
                       },
     "attributes"    : { 
-                        "pkg.summary" : "JSF 2.0 - Project Mojarra 2.0.0 RC",
+                        "pkg.summary" : "JSF 2.0 - Project Mojarra 2.0.0 FCS",
                         "pkg.description" : "Updated Mojarra 2.0.0 implementation.  \
 Current version supports JSF 2.0.  This version replaces the default version \
-of Mojarra that ships with Glassfish.  This software is release candidtate quality, please \
-see release notes.",
+of Mojarra that ships with Glassfish.",
                         "info.classification" : "Application Servers"
                       },
 
