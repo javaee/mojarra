800e03541020adacd5b964d0854d34730513dd02
rlubke <rlubke@java.net>
2009-11-04 19:04:58 +0000

    Remove unused import.
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@8128 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/com/sun/faces/context/PartialViewContextImpl.java
+++ jsf-ri/src/com/sun/faces/context/PartialViewContextImpl.java
@@ -58,7 +58,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.EnumSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Arrays;
