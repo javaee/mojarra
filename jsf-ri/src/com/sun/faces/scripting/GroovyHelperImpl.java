b555f90e77e1ac94847c99f41b4f121ec83966de
rlubke <rlubke@java.net>
2009-12-03 23:07:57 +0000

    Fix for issue 1428.
    Patch by guyv 'at' dev.java.net.
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@8221 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/com/sun/faces/scripting/GroovyHelperImpl.java
+++ jsf-ri/src/com/sun/faces/scripting/GroovyHelperImpl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -227,7 +227,7 @@ class GroovyHelperImpl extends GroovyHelper {
                 try {
                     c = gse.loadScriptByName(name);
                 } catch (Exception e) {
-                    throw new RuntimeException(e);
+                	throw new ClassNotFoundException(name, e);
                 }
             }
             if (c == null) {
@@ -235,8 +235,6 @@ class GroovyHelperImpl extends GroovyHelper {
             }
             return c;
         }
-
-
 
     }
 
