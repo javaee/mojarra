f80c01ecb24a49b3ade5423d40be388fc1130c98
driscoll <driscoll@java.net>
2010-01-08 00:39:49 +0000

    Remove cvs id tags, update copyright
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@8252 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/main/java/javax/faces/application/Application.java
+++ jsf-api/src/main/java/javax/faces/application/Application.java
@@ -1,11 +1,7 @@
 /*
- * $Id: Application.java,v 1.59.2.10 2008/04/12 16:32:29 edburns Exp $
- */
-
-/*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2010 Sun Microsystems, Inc. All rights reserved.
  * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
