2060f67ba0cfe2bf11c4c62df754899cbc75cd88
driscoll <driscoll@java.net>
2010-01-08 01:00:35 +0000

    Remove cvs id tags, update copyright
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@8253 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/test/java/javax/faces/component/UICommandTestCase.java
+++ jsf-api/src/test/java/javax/faces/component/UICommandTestCase.java
@@ -1,11 +1,7 @@
 /*
- * $Id: UICommandTestCase.java,v 1.34 2007/08/24 19:03:07 rlubke Exp $
- */
-
-/*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2010 Sun Microsystems, Inc. All rights reserved.
  * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
--- jsf-api/src/test/java/javax/faces/component/UISelectManyTestCase.java
+++ jsf-api/src/test/java/javax/faces/component/UISelectManyTestCase.java
@@ -1,11 +1,7 @@
 /*
- * $Id: UISelectManyTestCase.java,v 1.32 2008/01/08 22:29:24 rlubke Exp $
- */
-
-/*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2010 Sun Microsystems, Inc. All rights reserved.
  * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
