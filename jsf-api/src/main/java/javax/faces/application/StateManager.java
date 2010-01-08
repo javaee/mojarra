ef5f260bbd4ccb47b8cd70fbc07a34222786e456
driscoll <driscoll@java.net>
2010-01-08 01:18:49 +0000

    Remove cvs id tags, update copyright
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@8254 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/main/java/javax/faces/application/StateManager.java
+++ jsf-api/src/main/java/javax/faces/application/StateManager.java
@@ -1,11 +1,7 @@
 /*
- * $Id: StateManager.java,v 1.42 2008/01/24 18:52:34 edburns Exp $
- */
-
-/*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2010 Sun Microsystems, Inc. All rights reserved.
  * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
--- jsf-api/src/main/java/javax/faces/application/StateManagerWrapper.java
+++ jsf-api/src/main/java/javax/faces/application/StateManagerWrapper.java
@@ -1,11 +1,7 @@
 /*
- * $Id: StateManagerWrapper.java,v 1.8 2007/11/02 16:17:18 rlubke Exp $
- */
-
-/*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2010 Sun Microsystems, Inc. All rights reserved.
  * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
