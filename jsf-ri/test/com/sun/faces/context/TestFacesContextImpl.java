e95e58330f3cb36e0ee722c0f75ed34548e77734
rogerk <rogerk@java.net>
2010-07-12 13:01:59 +0000

    Spec Issue 607
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@8472 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/test/com/sun/faces/context/TestFacesContextImpl.java
+++ jsf-ri/test/com/sun/faces/context/TestFacesContextImpl.java
@@ -590,6 +590,12 @@ public class TestFacesContextImpl extends ServletFacesTestCase {
         assertTrue(null != fc.getApplication());
     }
 
+    public void testIsReleased() {
+        FacesContext context = getFacesContext();
+        assertTrue(!context.isReleased());
+        context.release();
+        assertTrue(context.isReleased());
+    }
 
     public void testRelease() {
         System.out.println("Testing release method");
