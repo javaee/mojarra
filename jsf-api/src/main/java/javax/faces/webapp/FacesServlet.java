f865613ee4266f758a879d7ce0a8bc8e6b757b31
edburns <edburns@java.net>
2010-07-14 22:09:55 +0000

    Declare the required behavior to enable no web.xml deployment https://javaserverfaces-spec-public.dev.java.net/issues/show_bug.cgi?id=863
    SECTION: Modified Files
    ----------------------------
    M       jsf-api/src/main/java/javax/faces/webapp/FacesServlet.java
    M       jsf-api/src/main/java/javax/faces/webapp/package.html
    
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@8491 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/main/java/javax/faces/webapp/FacesServlet.java
+++ jsf-api/src/main/java/javax/faces/webapp/FacesServlet.java
@@ -60,10 +60,34 @@ import javax.servlet.http.HttpServletResponse;
 
 
 /**
- * <p><strong class="changed_modified_2_0
- * changed_modified_2_0_rev_a">FacesServlet</strong> is a servlet that
- * manages the request processing lifecycle for web applications that
- * are utilizing JavaServer Faces to construct the user interface.</p>
+ * <p><strong class="changed_modified_2_0 changed_modified_2_0_rev_a
+ * changed_modified_2_1">FacesServlet</strong> is a servlet that manages
+ * the request processing lifecycle for web applications that are
+ * utilizing JavaServer Faces to construct the user interface.</p>
+ *
+ * <div class="changed_added_2_1">
+ *
+ * <p>The runtime must support the following additional behavior when
+ * running in a Servlet 3.0 (and beyond) container.  The runtime must
+ * provide an implementation of {@link
+ * javax.servlet.ServletContainerInitializer} whose
+ * <code>onStartup()</code> method takes following actions.</p>
+
+ * <p>Adds mappings <em>/faces</em>, <em>*.jsf</em>, and
+ * <em>*.faces</em> for the <code>FacesServlet</code> (if it hasn't
+ * already been mapped) if the following conditions are met:</p>
+ *
+ * <ul>
+ *    <li>
+ *       The <code>Set</code> of classes passed to this initializer is not
+ *       empty, or
+ *    </li>
+ *    <li>
+ *       /WEB-INF/faces-config.xml exists.
+ *    </li>
+ * </ul>
+
+ * </div>
  */
 
 public final class FacesServlet implements Servlet {
