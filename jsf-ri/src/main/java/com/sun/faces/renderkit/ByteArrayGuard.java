8085c56afba5d1c45089368276ae873de4e37918
rogerk <rogerk@java.net>
2010-08-23 21:23:01 +0000

    Issue:  CR 6943065 Client Side State Security Compromised
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@8565 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/renderkit/ByteArrayGuard.java
+++ jsf-ri/src/main/java/com/sun/faces/renderkit/ByteArrayGuard.java
@@ -37,15 +37,16 @@
 package com.sun.faces.renderkit;
 
 import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.Mac;
 import javax.crypto.NullCipher;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.DESedeKeySpec;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.faces.FacesException;
 
 import java.security.Key;
-import java.security.MessageDigest;
 import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -56,7 +57,7 @@ import com.sun.faces.util.FacesLogger;
  * decryption <code>Ciphers</code> to <code>ResponseStateManager</code>
  * implementations wishing to provide encryption support.</p>
  * 
- * <p>The algorithm used to encrypt byte array is 3DES with CBC.</p>
+ * <p>The algorithm used to encrypt byte array is AES with CBC.</p>
  *  
  * <p>Original author Inderjeet Singh, J2EE Blue Prints Team. Modified to suit JSF
  * needs.</p> 
@@ -66,156 +67,159 @@ public final class ByteArrayGuard {
 
      // Log instance for this class
     private static final Logger LOGGER = FacesLogger.RENDERKIT.getLogger();
-    private static final int IV_LENGTH = 8;        
-    private static final int KEY_LENGTH = 24;
-      
-    private static Cipher NULL_CIPHER = new NullCipher();               
+
+    private static final int MAC_LENGTH = 32;
+    private static final int KEY_LENGTH = 128;
+    private static final int IV_LENGTH = 16;
+
+    private static final String KEY_ALGORITHM = "AES";
+    private static final String CIPHER_CODE = "AES/CBC/PKCS5Padding";
+    private static final String MAC_CODE = "HmacSHA256";
     
-    private Cipher decryptCipher = NULL_CIPHER;
-    private Cipher encryptCipher = NULL_CIPHER;  
+    private Mac encryptMac;
+    private Mac decryptMac;
+
+    private SecretKey sk;
 
     // ------------------------------------------------------------ Constructors
 
+    public ByteArrayGuard() {
 
-    /**
-     * Constructs a new <code>ByteArrayGuard</code> using the specified
-     * <code>keyLength</code>, <code>macLength</code>, <code>ivLength</code>.    
-     * @param password the password to seed the encryption 
-     */
-    public ByteArrayGuard(String password) {
-       
-        if (password != null) {
-            if (LOGGER.isLoggable(Level.FINE)) {
-                LOGGER.log(Level.FINE,
-                           "Client state saving encryption enabled.");
+        try {
+            setupKeyAndMac();
+        } catch (Exception e) {
+            if (LOGGER.isLoggable(Level.SEVERE)) { 
+                LOGGER.log(Level.SEVERE,
+                           "Unexpected exception initializing encryption."
+                           + "  No encryption will be performed.",
+                           e);
             }
-            byte[] passwordKey = convertPasswordToKey(password.getBytes());           
-            try {
-                SecureRandom prng = SecureRandom.getInstance("SHA1PRNG");
-                SecretKeyFactory keygen = SecretKeyFactory.getInstance("DESede");
-                encryptCipher =
-                      getBlockCipherForEncryption(keygen, prng, passwordKey);
-                byte[] iVector = encryptCipher.getIV();
-                decryptCipher =
-                      getBlockCipherForDecryption(keygen, 
-                                                  prng, 
-                                                  passwordKey,
-                                                  iVector);
-            } catch (Exception e) {
-                if (LOGGER.isLoggable(Level.SEVERE)) {
-                    LOGGER.log(Level.SEVERE,
-                               "Unexpected exception initializing encryption."
-                               + "  No encryption will be performed.",
-                               e);
-                }               
-                               
-                encryptCipher = NULL_CIPHER;
-                decryptCipher = NULL_CIPHER;              
-               
-            }
-        }       
+            System.err.println("ERROR: Initializing Ciphers");
+        }
     }
-
 
     // ---------------------------------------------------------- Public Methods    
 
 
-    public Cipher getEncryptionCipher() {
-        return encryptCipher;
+    /**
+     * This method:
+     *    Encrypts bytes using a cipher.  
+     *    Generates MAC for intialization vector of the cipher
+     *    Generates MAC for encrypted data
+     *    Returns a byte array consisting of the following concatenated together:
+     *       |MAC for cnrypted Data | MAC for Init Vector | Encrypted Data |
+     * @param bytes The byte array to be encrypted.
+     * @return the encrypted byte array.
+     */
+    public byte[] encrypt(byte[] bytes) {
+        byte[] securedata = null;
+        try {
+            // Generate IV
+            SecureRandom rand = new SecureRandom();
+            byte[] iv = new byte[16];
+            rand.nextBytes(iv);
+            IvParameterSpec ivspec = new IvParameterSpec(iv);
+            Cipher encryptCipher = Cipher.getInstance(CIPHER_CODE);
+            encryptCipher.init(Cipher.ENCRYPT_MODE, sk, ivspec);
+            encryptMac.update(iv);
+            // encrypt the plaintext
+            byte[] encdata = encryptCipher.doFinal(bytes);
+            byte[] macBytes = encryptMac.doFinal(encdata);
+            byte[] tmp = concatBytes(macBytes, iv);
+            securedata = concatBytes(tmp, encdata);
+        } catch (Exception e) {
+            if (LOGGER.isLoggable(Level.SEVERE)) {
+                LOGGER.log(Level.SEVERE,
+                           "Unexpected exception initializing encryption."
+                           + "  No encryption will be performed.",
+                           e);
+            }
+            return null;
+        }
+        return securedata;
     }
-    
-    public Cipher getDecryptionCipher() {
-        return decryptCipher;
-    }
-    
-
-    // --------------------------------------------------------- Private Methods
-    
-
 
     /**
-     * This method converts the specified password into a key in a
-     * deterministic manner. The key is then usable for creating ciphers
-     * and MACs.
-     * 
-     * @param password plain text password
-     * 
-     * @return a byte array containing a key based on the specified
-     * password. The length of the returned byte array is KEY_LENGTH.
+     * This method decrypts the provided byte array.
+     * The decryption is only performed if the regenerated MAC
+     * is the same as the MAC for the received value.
+     * @param bytes Encrypted byte array to be decrypted.
+     * @return Decrypted byte array.
      */
-    private byte[] convertPasswordToKey(byte[] password) {
+    public byte[] decrypt(byte[] bytes) {
+        try {
+            // Extract MAC
+            byte[] macBytes = new byte[MAC_LENGTH];
+            System.arraycopy(bytes, 0, macBytes, 0, macBytes.length);
+
+            // Extract IV
+            byte[] iv = new byte[IV_LENGTH];
+            System.arraycopy(bytes, macBytes.length, iv, 0, iv.length);
+
+            // Extract encrypted data
+            byte[] encdata = new byte[bytes.length - macBytes.length - iv.length];
+            System.arraycopy(bytes, macBytes.length + iv.length, encdata, 0, encdata.length);
+
+            IvParameterSpec ivspec = new IvParameterSpec(iv);
+            Cipher decryptCipher = Cipher.getInstance(CIPHER_CODE);
+            decryptCipher.init(Cipher.DECRYPT_MODE, sk, ivspec);
+
+            // verify MAC by regenerating it and comparing it with the received value
+            decryptMac.update(iv);
+            decryptMac.update(encdata);
+            byte[] macBytesCalculated = decryptMac.doFinal();
+            if (Arrays.equals(macBytes, macBytesCalculated)) {
+                // continue only if the MAC was valid
+                // System.out.println("Valid MAC found!");
+                byte[] plaindata = decryptCipher.doFinal(encdata);
+                return plaindata;
+            } else {
+                System.err.println("ERROR: MAC did not verify!");
+                return null;
+            }
+        } catch (Exception e) {
+            System.err.println("ERROR: Decrypting:"+e.getCause());
+            return null; // Signal to JSF runtime
+        }
+    }
+
+    // --------------------------------------------------------- Private Methods
+
+    /**
+     * Generates secret key.
+     * Initializes MAC(s).
+     */
+    private void setupKeyAndMac() {
 
         try {
-            MessageDigest md = MessageDigest.getInstance("SHA");
-            byte[] seed = md.digest(password);
+            KeyGenerator kg = KeyGenerator.getInstance(KEY_ALGORITHM);
+            kg.init(KEY_LENGTH);   // 256 if you're using the Unlimited Policy Files
+            sk = kg.generateKey(); 
 
-            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
-            random.setSeed(seed);
+            encryptMac = Mac.getInstance(MAC_CODE);
+            decryptMac = Mac.getInstance(MAC_CODE);
+            encryptMac.init(sk);
+            decryptMac.init(sk);
 
-            byte[] rawkey = new byte[KEY_LENGTH];
-            random.nextBytes(rawkey);
-            return rawkey;
         } catch (Exception e) {
             throw new FacesException(e);
         }
-
     }
 
-
-    /** 
-     * Obtain a <code>Cipher</code> for decrypting data.
-     * @param keyGen the key factory
-     * @param random secure random
-     * @param rawKey must be 24 bytes in length.
-     * @param iv initialization vector  @return a 3DES block cipher to be used for decryption based on the
-     * specified key
-     * @return an initialized <code>Cipher</code> for decryption
+    /**
+     * This method concatenates two byte arrays
+     * @return a byte array of array1||array2
+     * @param array1 first byte array to be concatenated
+     * @param array2 second byte array to be concatenated
      */
-    private Cipher getBlockCipherForDecryption(SecretKeyFactory keyGen,
-                                               SecureRandom random,
-                                               byte[] rawKey,
-                                               byte[] iv) {
-
-        try {           
-            DESedeKeySpec keyspec = new DESedeKeySpec(rawKey);
-            Key key = keyGen.generateSecret(keyspec);
-            Cipher cipher = Cipher.getInstance("DESede/CBC/PKCS5Padding");
-            IvParameterSpec ivspec = new IvParameterSpec(iv);
-            cipher.init(Cipher.DECRYPT_MODE, key, ivspec, random);
-            return cipher;
-        } catch (Exception e) {
+    private static byte[] concatBytes(byte[] array1, byte[] array2) {
+        byte[] cBytes = new byte[array1.length + array2.length];
+        try {
+            System.arraycopy(array1, 0, cBytes, 0, array1.length);
+            System.arraycopy(array2, 0, cBytes, array1.length, array2.length);
+        } catch(Exception e) {
             throw new FacesException(e);
         }
-
-    }
-
-
-    /** 
-     * Obtain a <code>Cipher</code> for encrypting data.
-     * @param keyGen the key factory
-     * @param random secure random
-     * @param rawKey must be 24 bytes in length.  @return a 3DES block cipher to be used for encryption based on the
-     * specified key
-     * @return an initialized <code>Cipher</code> for decryption
-     */
-    private Cipher getBlockCipherForEncryption(SecretKeyFactory keyGen,
-                                               SecureRandom random,
-                                               byte[] rawKey) {
-
-        try {           
-            DESedeKeySpec keyspec = new DESedeKeySpec(rawKey);
-            Key key = keyGen.generateSecret(keyspec);
-            Cipher cipher = Cipher.getInstance("DESede/CBC/PKCS5Padding");
-            byte[] iv = new byte[IV_LENGTH];
-            random.nextBytes(iv);
-            IvParameterSpec ivspec = new IvParameterSpec(iv);
-            cipher.init(Cipher.ENCRYPT_MODE, key, ivspec, random);
-            return cipher;
-        } catch (Exception e) {
-            throw new FacesException(e);
-        }
-
-    }
-
-    
+        return cBytes;
+    }    
 }
