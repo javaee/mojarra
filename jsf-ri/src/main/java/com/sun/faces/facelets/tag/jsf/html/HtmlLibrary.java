8b27673087ed14bcbc783b0c0d563ab2b84be7d2
edburns <edburns@java.net>
2010-12-15 23:12:37 +0000

    Revert fix for http://java.net/jira/browse/JAVASERVERFACES-1691
    SECTION: Modified Files
    ----------------------------
    M       jsf-api/doc/standard-html-renderkit-base.xml
    M       jsf-api/doc/standard-html-renderkit.xml
    M       jsf-ri/src/main/java/com/sun/faces/facelets/tag/jsf/html/HtmlLibrary.java
    
    This causes a signature violation.
    
    Ed
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@8760 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/facelets/tag/jsf/html/HtmlLibrary.java
+++ jsf-ri/src/main/java/com/sun/faces/facelets/tag/jsf/html/HtmlLibrary.java
@@ -70,7 +70,7 @@ public final class HtmlLibrary extends AbstractHtmlLibrary {
     public HtmlLibrary() {
         super(Namespace);
 
-        this.addHtmlComponent("body", "javax.faces.HtmlOutputBody",
+        this.addHtmlComponent("body", "javax.faces.Output",
                 "javax.faces.Body");
 
         this.addHtmlComponent("button", "javax.faces.HtmlOutcomeTargetButton",
@@ -93,7 +93,7 @@ public final class HtmlLibrary extends AbstractHtmlLibrary {
         this.addHtmlComponent("graphicImage", "javax.faces.HtmlGraphicImage",
                 "javax.faces.Image");
 
-        this.addHtmlComponent("head", "javax.faces.HtmlOutputHead",
+        this.addHtmlComponent("head", "javax.faces.Output",
                 "javax.faces.Head");
 
         this.addHtmlComponent("html", "javax.faces.Output",
