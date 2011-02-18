aae80f838403398ced796175473c484f8af93618
rogerk <rogerk@java.net>
2011-02-18 18:57:26 +0000

    version : 2.1.1,0-1
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_1X_ROLLING@8860 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- updatecenter2/conf/jsf_proto.py
+++ updatecenter2/conf/jsf_proto.py
@@ -42,7 +42,7 @@
 
 pkg = {
     "name"          : "glassfish-jsf",
-    "version"       : "2.1.0,0-11",
+    "version"       : "2.1.1,0-1",
     "depends"       : {
                        "pkg:/glassfish-jsf@2.1" : {"type" : "incorporate"},
                        "pkg:/glassfish-common" : { "type" : "require" }
