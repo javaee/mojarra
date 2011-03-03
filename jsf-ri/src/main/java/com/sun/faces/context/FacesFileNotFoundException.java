7409ad444c410384f2513998bb1f818eeaab85f6
rogerk <rogerk@java.net>
2011-03-03 13:35:59 +0000

    << http://java.net/jira/browse/JAVASERVERFACES-1970 >>
    SECTION: Modified Files
    ----------------------------
    M       jsf-ri/src/main/java/com/sun/faces/context/FacesFileNotFoundException.java
    ---- Constructor with String arg needs to call super.
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@8882 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/context/FacesFileNotFoundException.java
+++ jsf-ri/src/main/java/com/sun/faces/context/FacesFileNotFoundException.java
@@ -60,6 +60,7 @@ public class FacesFileNotFoundException extends FileNotFoundException {
     private static final long serialVersionUID = 7593137790944497673L;
 
     public FacesFileNotFoundException(String s) {
+        super(s);
     }
 
     public FacesFileNotFoundException() {
