90efc37a2d69b24068e3adb6a86285d70c20c99e
ioss <ioss@java.net>
2011-05-23 17:57:06 +0000

    Fix for http://java.net/jira/browse/JAVASERVERFACES-2074 (if actionId already contains a '?' use '&' for the following parameter instead)
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@9077 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/facelets/tag/ui/UIDebug.java
+++ jsf-ri/src/main/java/com/sun/faces/facelets/tag/ui/UIDebug.java
@@ -128,7 +128,7 @@ public final class UIDebug extends UIComponentBase {
         sb.append("function faceletsDebug(URL) { day = new Date(); id = day.getTime(); eval(\"page\" + id + \" = window.open(URL, '\" + id + \"', 'toolbar=0,scrollbars=1,location=0,statusbar=0,menubar=0,resizable=1,width=800,height=600,left = 240,top = 212');\"); };");
         sb.append("var faceletsOrigKeyup = document.onkeyup; document.onkeyup = function(e) { if (window.event) e = window.event; if (String.fromCharCode(e.keyCode) == '" + this.getHotkey() + "' & e.shiftKey & e.ctrlKey) faceletsDebug('");
         sb.append(actionId);
-        sb.append('?');
+        sb.append(actionId.indexOf('?')==-1 ? '?' : '&');
         sb.append(KEY);
         sb.append('=');
         sb.append(writeDebugOutput(faces, this));
