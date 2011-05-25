1bbf2032b8e4455fef18c4cb3ed4ba3adf4f679f
rogerk <rogerk@java.net>
2011-05-25 17:31:34 +0000

    << http://java.net/jira/browse/JAVASERVERFACES-1988 >>
    Patch provided.  Essentially, the patch removes UIComponent as an instance variable
    and uses it locally in the createComponent method.
     
    SECTION: Modified Files
    ----------------------------
    M       jsf-ri/src/main/java/com/sun/faces/facelets/tag/jsf/ComponentTagHandlerDelegateImpl.java
    ---- Remove the setting of UIComponent instance variable. 
    M       jsf-ri/src/main/java/com/sun/faces/facelets/tag/jsf/CompositeComponentTagHandler.java
    ---- Relocate UIComponent cc (instance variable) to createComponent method for thread safety. 
    
    
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@9098 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/facelets/tag/jsf/CompositeComponentTagHandler.java
+++ jsf-ri/src/main/java/com/sun/faces/facelets/tag/jsf/CompositeComponentTagHandler.java
@@ -122,7 +122,6 @@ public class CompositeComponentTagHandler extends ComponentHandler implements Cr
 
     private static final Logger LOGGER = FacesLogger.TAGLIB.getLogger();
     private Resource ccResource;
-    private UIComponent cc;
     private TagAttribute binding;
 
 
@@ -144,6 +143,7 @@ public class CompositeComponentTagHandler extends ComponentHandler implements Cr
     public UIComponent createComponent(FaceletContext ctx) {
         
         FacesContext context = ctx.getFacesContext();
+        UIComponent cc;
         // we have to handle the binding here, as Application doesn't
         // expose a method to do so with Resource.
         if (binding != null) {
@@ -205,11 +205,7 @@ public class CompositeComponentTagHandler extends ComponentHandler implements Cr
         }
 
     }
-
-    public void setCompositeComponent(UIComponent cc) {
-        if (this.cc == null)
-            this.cc = cc;
-    }
+    
 
     /**
      * Specialized implementation to prevent caching of the MetaRuleset when
@@ -245,6 +241,9 @@ public class CompositeComponentTagHandler extends ComponentHandler implements Cr
     protected MetaRuleset createMetaRuleset(Class type) {
 
         Util.notNull("type", type);
+        FacesContext context = FacesContext.getCurrentInstance();
+        FaceletContext faceletContext = (FaceletContext) context.getAttributes().get(FaceletContext.FACELET_CONTEXT_KEY);
+        UIComponent cc = createComponent(faceletContext);
         MetaRuleset m = new CompositeComponentMetaRuleset(getTag(), type, (BeanInfo) cc.getAttributes().get(UIComponent.BEANINFO_KEY));
 
         // ignore standard component attributes
