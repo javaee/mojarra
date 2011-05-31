2903b5191bb120a8bcbd2edbd190edb2959963ed
rogerk <rogerk@java.net>
2011-05-31 18:54:26 +0000

    << http://java.net/jira/browse/JAVASERVERFACES-2055 >>
    SECTION: Modified Files
    ----------------------------
    M       jsf-api/src/main/resources/jsf.js
    ---- Table related markup inserted after a specified element using:
    ----    partialWriter.startInsertAfter(id);
    ---- is not processed correctly via the doInsert function.
    M       jsf-ri/systest/src/com/sun/faces/ajax/AjaxInsertDeleteTestCase.java
    M       jsf-ri/systest/src/com/sun/faces/systest/model/ajax/InsertDeleteBean.java
    M       jsf-ri/systest/web/ajax/ajaxInsertDelete.xhtml
    ---- Modified test case to include the insertion/deletion of markup within a table.
    
    
    
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_0X_ROLLING@9116 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/systest/src/com/sun/faces/ajax/AjaxInsertDeleteTestCase.java
+++ jsf-ri/systest/src/com/sun/faces/ajax/AjaxInsertDeleteTestCase.java
@@ -1,27 +1,31 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
+ * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
  * and Distribution License("CDDL") (collectively, the "License").  You
- * may not use this file except in compliance with the License. You can obtain
- * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
- * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
  *
  * When distributing the software, include this License Header Notice in each
- * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
- * Sun designates this particular file as subject to the "Classpath" exception
- * as provided by Sun in the GPL Version 2 section of the License file that
- * accompanied this code.  If applicable, add the following below the License
- * Header, with the fields enclosed by brackets [] replaced by your own
- * identifying information: "Portions Copyrighted [year]
- * [name of copyright owner]"
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
  *
  * Contributor(s):
- *
  * If you wish your version of this file to be governed by only the CDDL or
  * only the GPL Version 2, indicate your decision by adding "[Contributor]
  * elects to include this software in this distribution under the [CDDL or GPL
@@ -36,7 +40,7 @@
 
 package com.sun.faces.ajax;
 
-import com.sun.faces.htmlunit.AbstractTestCase;
+import com.sun.faces.htmlunit.HtmlUnitFacesTestCase;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 import com.gargoylesoftware.htmlunit.html.HtmlHorizontalRule;
@@ -44,7 +48,7 @@ import com.gargoylesoftware.htmlunit.html.HtmlHeading2;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-public class AjaxInsertDeleteTestCase extends AbstractTestCase {
+public class AjaxInsertDeleteTestCase extends HtmlUnitFacesTestCase {
 
     public AjaxInsertDeleteTestCase(String name) {
         super(name);
@@ -91,6 +95,7 @@ public class AjaxInsertDeleteTestCase extends AbstractTestCase {
         HtmlHeading2 beforeHeading = getBeforeHeading(page);
         assertNotNull(beforeHeading);
         assertTrue(beforeHeading.getNextSibling() instanceof HtmlHorizontalRule);
+        assertNotNull(page.getElementById("trbefore"));
 
         HtmlSubmitInput afterButton = getAfterButton(page);
         assertNotNull(afterButton);
@@ -99,6 +104,7 @@ public class AjaxInsertDeleteTestCase extends AbstractTestCase {
         HtmlHeading2 afterHeading = getAfterHeading(page);
         assertNotNull(afterHeading);
         assertTrue(afterHeading.getPreviousSibling() instanceof HtmlHorizontalRule);
+        assertNotNull(page.getElementById("trafter"));
 
         HtmlSubmitInput removeBefore = getRemoveBeforeButton(page);
         assertNotNull(removeBefore);
@@ -107,9 +113,13 @@ public class AjaxInsertDeleteTestCase extends AbstractTestCase {
         assertNull(getBeforeHeading(page));
         assertNotNull(getAfterHeading(page));
 
+        assertNull(page.getElementById("trbefore"));
+
         HtmlSubmitInput removeAfter = getRemoveAfterButton(page);
         assertNotNull(removeAfter);
         page = removeAfter.click();
+
+        assertNull(page.getElementById("trafter"));
 
         assertNull(getBeforeHeading(page));
         assertNull(getAfterHeading(page));
