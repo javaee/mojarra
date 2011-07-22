01a2f1296089147ec9e4d06a694903a25b2c3635
edburns <edburns@java.net>
2011-07-22 19:55:30 +0000

    Further tweaks and optimizations http://java.net/jira/browse/JAVASERVERFACES_SPEC_PUBLIC-745
    SECTION: Modified Files
    ----------------------------
    M       jsf-ri/src/main/java/com/sun/faces/facelets/tag/composite/AttributeHandler.java
    M       userIntefaceComponentModel.fw
    
    - new text in 3.6.2.1 Composite Component Metadata Specification
    
        Any additional attributes on <composite:attribute/ > are exposed as
        attributes accessible from the getValue() and attributeNames() methods
        on PropertyDescriptor. The return type from getValue() must be a
        ValueExpression with the exception of the getValue("type"). The return
        type from getValue("type") must be Class. If the value specified for
        the type attribute of <cc:attribute/> cannot be converted to an actual
        Class, a TagAttributeException must be thrown, including the Tag and
        TagAttribute instances in the constructor.
    
      Yes, this is a backwards incompatible change, but because the usecase
      is so specific, and the performance benefit so substantial, it was
      judged to be worth the cost.
    
    M       jsf-ri/src/main/java/com/sun/faces/facelets/tag/jsf/CompositeComponentTagHandler.java
    
    - Fix callsite of getValue("type");
    
    M       jsf-ri/src/main/java/com/sun/faces/el/CompositeComponentAttributesELResolver.java
    M       valueReferences.fm
    
    - New spec for getType().  
    
      If base is not null and is an instance of the composite component
      attributes Map, if there is a value in the map for this property, get
      the value and consult its type. If the type is a string, follow the
      steps below to inspect the composite component metadata to discover
      the type. If the type is not a string, call setPropertyResolved(true),
      and return the getClass() of the value. If there is no value in the
      Map, check the top level component's ValueExpression collection for an
      expression under the name given by the property argument to
      getType(). If the expression exists, call getType() on the
      expression. After calling getType(), if the "propertyResolved"
      property of the ELContext is true, take no further action and return
      the result. Otherwise, if the "propertyResolved" property of the
      ELContext is false, and the property argument to getType() is not null
      and not empty, look in the metadata for the composite component to
      find a value to return. For example, if the metedata for the attribute
      was <cc:attribute name="i" type="java.lang.Integer" /> the composite
      component metadata would allow the discovery of the value of the
      "type" attribute. See Section 3.6.2.1 "Composite Component
      Metadata". If this yields a result, call setPropertyResolved(true) on
      the ELContext, and return the result. Otherwise, return null.
    
    M       jsf-test/JAVASERVERFACES_SPEC_PUBLIC-745/i_spec_745_war/src/main/java/com/sun/faces/regression/i_spec_745_war/JavaTopLevelComponent.java
    M       jsf-test/JAVASERVERFACES_SPEC_PUBLIC-745/i_spec_745_war/src/main/java/com/sun/faces/regression/i_spec_745_war/UserBean.java
    A       jsf-test/JAVASERVERFACES_SPEC_PUBLIC-745/i_spec_745_war/src/main/webapp/i_spec_745_4.xhtml
    A  +    jsf-test/JAVASERVERFACES_SPEC_PUBLIC-745/i_spec_745_war/src/main/webapp/resources/ezcomp/i_spec_745_cc_3.xhtml
    A       jsf-test/JAVASERVERFACES_SPEC_PUBLIC-745/i_spec_745_war/src/main/webapp/resources/ezcomp/i_spec_745_cc_4.xhtml
    A  +    jsf-test/JAVASERVERFACES_SPEC_PUBLIC-745/i_spec_745_war/src/main/webapp/i_spec_745_3.xhtml
    M       jsf-test/JAVASERVERFACES_SPEC_PUBLIC-745/build.xml
    
    - Test content
    
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@9212 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-test/JAVASERVERFACES_SPEC_PUBLIC-745/i_spec_745_war/src/main/java/com/sun/faces/regression/i_spec_745_war/UserBean.java
+++ jsf-test/JAVASERVERFACES_SPEC_PUBLIC-745/i_spec_745_war/src/main/java/com/sun/faces/regression/i_spec_745_war/UserBean.java
@@ -25,7 +25,15 @@ public class UserBean {
         return items1;    
     }
     
-    
+    private Double doubleValue = Double.MAX_VALUE;
+
+    public Double getDoubleValue() {
+        return doubleValue;
+    }
+
+    public void setDoubleValue(Double doubleValue) {
+        this.doubleValue = doubleValue;
+    }
     
 }
 
