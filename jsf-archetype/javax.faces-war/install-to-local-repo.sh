029d9de872d26e4a147fa84628ed882bc93bfff4
edburns <edburns@java.net>
2011-09-01 16:49:16 +0000

    updates to archetype to fix issue uncovered by Rogers test for JAVASERVERFACES-1845
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@9337 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ jsf-archetype/javax.faces-war/install-to-local-repo.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+cd create-from-project
+
+mvn clean archetype:create-from-project
+
+patch -p0 < ../apply_to_pom.patch 
+
+find target \( -name UserBean.java -or -name web.xml \) -exec perl -pi.bak -e "s/.[\{]groupId[\}]/javax.faces/g" {} \; -print
+
+find . -name "*~" -exec rm -f {} \; -print; find . -name ".*~" -exec rm -f {} \; -print; find . -name ".*.bak" -exec rm -f {} \; -print; find . -name "*backup*" -exec rm -f {} \; -print; find . -name "*.bak" -exec rm -f {} \; -print;
+
+cd target/generated-sources/archetype
+
+mvn install
+
+NOW=`date '+%Y%m%d-%H%M'`
+
+echo "Execute these steps to test the just-installed archetype: mkdir /tmp/$NOW; cd /tmp/$NOW; mvn archetype:generate -DarchetypeCatalog=local"
+
+echo "Manually inspect the created project in /tmp/$NOW"
+
+
