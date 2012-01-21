1fd082ad9715e5c2ba675245a1efe3fd6b2b0814
rogerk <rogerk@java.net>
2012-01-21 17:35:04 +0000

    JAVASERVERFACES-2286 - Remove erroneous page get
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_1X_ROLLING@9597 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-test/admingui/admingui/src/main/java/com/sun/faces/regression/admingui/AdminGuiTestCase.java
+++ jsf-test/admingui/admingui/src/main/java/com/sun/faces/regression/admingui/AdminGuiTestCase.java
@@ -84,8 +84,6 @@ public class AdminGuiTestCase extends HtmlUnitFacesTestCase {
         client.setThrowExceptionOnFailingStatusCode(false);
         CookieManager cm = client.getCookieManager();
         
-        page = getPage("/common/index.jsf");
-
         cm.clearCookies();
         try {
             page = getPage("/common/index.jsf?bare=true");
