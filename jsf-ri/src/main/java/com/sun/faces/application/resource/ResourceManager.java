8763a7035305928bf08b465f27e67a29d27621c6
edburns <edburns@java.net>
2012-01-26 20:19:55 +0000

    Conform to new text in spec section 2.6.1.4 regarding Resource Library Localization http://java.net/jira/browse/JAVASERVERFACES_SPEC_PUBLIC-1065
    SECTION: Modified Files
    ----------------------------
    M       jsf-ri/src/main/java/com/sun/faces/application/resource/ResourceManager.java
    
    - Conform to new text in spec section 2.6.1.4 regarding Resource Library Localization.
    
      When trying to get the locale for the Resource Library, if there is a
      UIViewRoot, ask it for the locale. If not, do the same thing as we
      used to do.
    
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@9612 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/application/resource/ResourceManager.java
+++ jsf-ri/src/main/java/com/sun/faces/application/resource/ResourceManager.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -483,8 +483,14 @@ public class ResourceManager {
         String localePrefix = null;
         String appBundleName = context.getApplication().getMessageBundle();
         if (null != appBundleName) {
-            Locale locale = 
-                  context.getApplication().getViewHandler().calculateLocale(context);
+        	
+            Locale locale = null;
+            if (context.getViewRoot() != null) {
+                locale = context.getViewRoot().getLocale();
+            } else {
+                locale = context.getApplication().getViewHandler().calculateLocale(context);
+            }
+            
                 try {
                     ResourceBundle appBundle =
                           ResourceBundle.getBundle(appBundleName,
