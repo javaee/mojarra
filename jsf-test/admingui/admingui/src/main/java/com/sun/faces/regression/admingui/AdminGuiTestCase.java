c6bc7a00c78d2cf7962649683f4b73e63a368bdc
rogerk <rogerk@java.net>
2012-01-31 19:35:35 +0000

    << ADD DESCRIPTION HERE http://java.net/jira/browse/JAVASERVERFACES-2319 >>
    SECTION: Modified Files
    ----------------------------
    M       jsf-test/admingui/admingui/src/main/java/com/sun/faces/regression/admingui/AdminGuiTestCase.java
    ---- Access system property (set in build.xml) whose value is directory path of war file that 
    ---- will be uploaded for deployment.
    M       jsf-test/admingui/build.xml
    ---- Set system property whose value is the directory path of war file that will be uploaded
    ---- for deployment.
    
    
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_1X_ROLLING@9624 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-test/admingui/admingui/src/main/java/com/sun/faces/regression/admingui/AdminGuiTestCase.java
+++ jsf-test/admingui/admingui/src/main/java/com/sun/faces/regression/admingui/AdminGuiTestCase.java
@@ -101,7 +101,8 @@ public class AdminGuiTestCase extends HtmlUnitFacesTestCase {
         
         // Set the war path
         HtmlTextInput textInput = (HtmlTextInput) page.getElementById("form:sheet1:section1:prop1:dirPath");
-        String dirPathValue = System.getProperty("user.dir") + "/jsf-test/admingui/admingui_test_war/target/admingui_test_war.war";
+        String dirPathValue = System.getProperty("warfile");;
+        System.err.println("DIRPATH:"+dirPathValue);
         textInput.setValueAttribute(dirPathValue);
         
         // Set the appType
