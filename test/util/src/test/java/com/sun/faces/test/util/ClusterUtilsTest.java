8cf9ece37a7c7343af5dfcffe7fce4ff97231eef
mriem <mriem@java.net>
2012-02-20 14:25:31 +0000

    Added utility project to help in testing
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_1X_ROLLING@9716 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ test/util/src/test/java/com/sun/faces/test/util/ClusterUtilsTest.java
@@ -0,0 +1,21 @@
+package com.sun.faces.test.util;
+
+import com.sun.faces.test.util.ClusterUtils;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ClusterUtilsTest {
+    
+    /**
+     * Test getBaseUrls method.
+     */
+    @Test
+    public void testGetBaseUrls() {
+        System.setProperty("integration.url", "dummy");
+        assertEquals(1, ClusterUtils.getBaseUrls().length);
+        System.setProperty("integration.url2", "dummy2");
+        assertEquals(2, ClusterUtils.getBaseUrls().length);
+        System.setProperty("integration.url2", "");
+        assertEquals(1, ClusterUtils.getBaseUrls().length);
+    }
+}
