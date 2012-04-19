47db74e4b94d7b8259a4d607375654e484805f7e
rogerk <rogerk@java.net>
2012-04-19 14:15:38 +0000

    << http://java.net/jira/browse/JAVASERVERFACES-2386 >>
    Problem Summary:
    If a facelets page has a disabled h:link :
        <h:link id="link1" value="Link Page" outcome="hlink" disabled="true"/>
    The disabled attribute will get rendered on the span element which is not valid.
    
    
    SECTION: Modified Files
    ----------------------------
    M       jsf-ri/src/main/java/com/sun/faces/renderkit/html_basic/OutcomeTargetRenderer.java
    ---- renderPassThruAttributes method: pass in an optional List of atttribute names that
    ---- identify the attributes that will be excluded from rendering.
    M       jsf-ri/src/main/java/com/sun/faces/renderkit/html_basic/OutcomeTargetLinkRenderer.java
    ---- renderPassThruAttributes method: pass in an optional List of atttribute names that
    ---- identify the attributes that will be excluded from rendering.  In the case of this
    ---- component, we are excluding disabled if the link is to be renderd as disabled.
    M       jsf-ri/src/main/java/com/sun/faces/renderkit/html_basic/OutcomeTargetButtonRenderer.java
    ---- Don't specify an attribute exclusion list when calling renderPassThruAttributes
    M       jsf-ri/src/main/java/com/sun/faces/renderkit/RenderKitUtils.java
    ---- Call through to the renderXHTMLStyleBooleanAttributes method that takes an attribute
    ---- exclusion List.
    
    A       test/agnostic/renderKit/basic/src/test/java/com/sun/faces/test/agnostic/renderKit/basic/Issue2386IT.java
    A       test/agnostic/renderKit/basic/src/main/webapp/hlink.xhtml
    ---- New test module.  This test has a facelets page with a disabled h:link.
    ---- The test verifies the rendered page does not have disabled attribute on the
    ---- rendered span.
    
    
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@9868 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/renderkit/html_basic/OutcomeTargetButtonRenderer.java
+++ jsf-ri/src/main/java/com/sun/faces/renderkit/html_basic/OutcomeTargetButtonRenderer.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -109,7 +109,7 @@ public class OutcomeTargetButtonRenderer extends OutcomeTargetRenderer {
             writer.writeAttribute("class", styleClass, "styleClass");
         }
 
-        renderPassThruAttributes(context, writer, component, ATTRIBUTES);
+        renderPassThruAttributes(context, writer, component, ATTRIBUTES, null);
 
         writer.endElement("input");
 
