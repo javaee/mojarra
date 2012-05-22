5b93ca83cf652b40982648944a1c26e8a2e57214
mriem <mriem@java.net>
2012-05-22 18:43:16 +0000

    Fixes http://java.net/jira/browse/JAVASERVERFACES-1414, r=rogerk, added tests to verify 1414 has been fixed
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_1X_ROLLING@10040 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ test/agnostic/dynamic/src/main/java/com/sun/faces/test/agnostic/dynamic/Issue1414Bean.java
@@ -0,0 +1,31 @@
+package com.sun.faces.test.agnostic.dynamic;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.event.ActionEvent;
+
+@ManagedBean(name = "issue1414Bean")
+@SessionScoped
+public class Issue1414Bean implements Serializable {
+
+    public void temporaryMoveComponent(ActionEvent ae) {
+
+        System.out.println("Temporarily moving a component");
+        UIComponent button = ae.getComponent();
+        UIComponent movefrom = button.findComponent("movefrom");
+        UIComponent moveto = button.findComponent("moveto");
+
+        if (movefrom.getChildren().isEmpty()) {
+            UIComponent moveme = moveto.getChildren().get(0);
+            moveto.getChildren().remove(moveme);
+            movefrom.getChildren().add(moveme);
+        } else {
+            UIComponent moveme = movefrom.getChildren().get(0);
+            movefrom.getChildren().remove(moveme);
+            moveto.getChildren().add(moveme);
+        }
+    }
+}
