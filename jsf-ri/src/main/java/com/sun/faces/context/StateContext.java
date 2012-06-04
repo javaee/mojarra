6256fd65e9a29bb4437e84b85f43f7de96334d80
mriem <mriem@java.net>
2012-06-04 14:29:10 +0000

    Fixed JavaDoc issue
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_1X_ROLLING@10074 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/context/StateContext.java
+++ jsf-ri/src/main/java/com/sun/faces/context/StateContext.java
@@ -181,10 +181,11 @@ public class StateContext {
 
     /**
      * Toggles the current modification tracking status.
+     * 
      * @param trackMods if <code>true</code> and the listener installed by
-     *  {@link #startTrackViewModifications()} is present, then view modifications
-     *  will be tracked.  If <code>false</code>, then modification events
-     *  will be ignored.
+     * {@link #startTrackViewModifications(javax.faces.context.FacesContext, javax.faces.component.UIViewRoot) 
+     * is present, then view modifications will be tracked.  If 
+     * <code>false</code>, then modification events will be ignored.
      */
     public void setTrackViewModifications(boolean trackMods) {
 
