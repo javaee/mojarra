44ae5423a01ed95d69516b84e1e9efb0ef11d70f
mriem <mriem@java.net>
2012-06-28 17:58:48 +0000

    Added tests for issue #2332 showing it is working properly
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_1X_ROLLING@10156 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ test/agnostic/flash/src/main/java/com/sun/faces/test/agnostic/flash/Issue2332Bean.java
@@ -0,0 +1,23 @@
+package com.sun.faces.test.agnostic.flash;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.context.Flash;
+import javax.faces.event.ActionEvent;
+
+@RequestScoped
+@ManagedBean(name = "issue2332Bean")
+public class Issue2332Bean {
+
+    private Flash flash = FacesContext.getCurrentInstance().getExternalContext().getFlash();
+
+    public void test(ActionEvent ae) {
+        flash.put("flashContent", "== Flash value LINK1 ==");
+    }
+
+    public String test2() {
+        flash.put("flashContent", "== Flash value LINK2 ==");
+        return "page2.xhtml";
+    }
+}
