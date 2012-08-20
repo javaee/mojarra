4f4b151501501b5d04d35bcd90011cda47de3261
mriem <mriem@java.net>
2012-08-20 20:57:02 +0000

    Make sure we fail early
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_1X_ROLLING@10476 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/bin/test-glassfish-default.bat
+++ test/bin/test-glassfish-default.bat
@@ -1,2 +1,2 @@
 call %~dp0\test-glassfish-specific.bat Production true server
-if not "%ERRORLEVEL%" == "0" exit /b
+if not "%ERRORLEVEL%" == "0" exit
--- test/bin/test-glassfish-specific.bat
+++ test/bin/test-glassfish-specific.bat
@@ -4,26 +4,26 @@
 @echo *
 @echo *************************************************************************
 
-call mvn -N -Pintegration-glassfish-cargo-prepare validate
-if not "%ERRORLEVEL%" == "0" exit /b
+call mvn -N -Pintegration-glassfish-prepare validate
+if not "%ERRORLEVEL%" == "0" exit
 
 call mvn -Dwebapp.projectStage=%1 -Dwebapp.partialStateSaving=%2 -Dwebapp.stateSavingMethod=%3 clean install
-if not "%ERRORLEVEL%" == "0" exit /b
+if not "%ERRORLEVEL%" == "0" exit
 
 call mvn -N -Pintegration-glassfish-cargo -Dwebapp.projectStage=%1 -Dwebapp.partialStateSaving=%2 -Dwebapp.stateSavingMethod=%3 cargo:start
-if not "%ERRORLEVEL%" == "0" exit /b
+if not "%ERRORLEVEL%" == "0" exit 
 
 call mvn -Pintegration-glassfish-cargo -Dwebapp.projectStage=%1 -Dwebapp.partialStateSaving=%2 -Dwebapp.stateSavingMethod=%3 cargo:redeploy
 if not "%ERRORLEVEL%" == "0" (
     call mvn -N -Pintegration-glassfish-cargo -Dwebapp.projectStage=%1 -Dwebapp.partialStateSaving=%2 -Dwebapp.stateSavingMethod=%3 cargo:stop 
-    exit /b
+    exit
 )
 
 call mvn -Pintegration-failsafe -Dwebapp.projectStage=%1 -Dwebapp.partialStateSaving=%2 -Dwebapp.stateSavingMethod=%3 verify 
 if not "%ERRORLEVEL%" == "0" (
     call mvn -N -Pintegration-glassfish-cargo -Dwebapp.projectStage=%1 -Dwebapp.partialStateSaving=%2 -Dwebapp.stateSavingMethod=%3 cargo:stop 
-    exit /b
+    exit
 )
 
 call mvn -N -Pintegration-glassfish-cargo -Dwebapp.projectStage=%1 -Dwebapp.partialStateSaving=%2 -Dwebapp.stateSavingMethod=%3 cargo:stop
-if not "%ERRORLEVEL%" == "0" exit /b
+if not "%ERRORLEVEL%" == "0" exit 
