9298ad76e4de4fae10af2da1aa4a45db0514b898
mriem <mriem@java.net>
2012-09-05 12:49:29 +0000

    Removed unused import (fixed 1 PMD issue)
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@10548 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/main/java/javax/faces/validator/Validator.java
+++ jsf-api/src/main/java/javax/faces/validator/Validator.java
@@ -41,7 +41,6 @@
 package javax.faces.validator;
 
 
-import javax.faces.component.StateHolder;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import java.util.EventListener;
