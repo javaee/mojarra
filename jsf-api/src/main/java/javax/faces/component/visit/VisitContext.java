fab04795ce4cb31eb7d71e74c2e3b6f1b8217b7c
mriem <mriem@java.net>
2012-09-06 13:42:52 +0000

    Removed unused imports (fixed 2 PMD issue)
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_1X_ROLLING@10566 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/main/java/javax/faces/component/visit/VisitContext.java
+++ jsf-api/src/main/java/javax/faces/component/visit/VisitContext.java
@@ -41,9 +41,7 @@
 package javax.faces.component.visit;
 
 import java.util.AbstractCollection;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.Set;
 
