f7f106d793bf2afa785eaab6b0263be6b89b0b41
mriem <mriem@java.net>
2012-09-21 13:49:05 +0000

    Fixed PMD issue (removed unused import)
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@10684 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/renderkit/html_basic/MenuRenderer.java
+++ jsf-ri/src/main/java/com/sun/faces/renderkit/html_basic/MenuRenderer.java
@@ -56,8 +56,6 @@ import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.HashSet;
