fc35b924d4d4f84f52cfb29f2005762435344dbc
mriem <mriem@java.net>
2012-09-21 13:49:20 +0000

    Fixed 2 PMD issues (removed unused imports)
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_1X_ROLLING@10685 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/renderkit/html_basic/MenuRenderer.java
+++ jsf-ri/src/main/java/com/sun/faces/renderkit/html_basic/MenuRenderer.java
@@ -57,7 +57,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.HashSet;
