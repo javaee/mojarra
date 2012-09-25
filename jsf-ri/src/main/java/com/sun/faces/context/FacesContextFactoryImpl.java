0ffb2cf8f63c7cfce38e7b41dc161bbb235001ea
mriem <mriem@java.net>
2012-09-25 16:02:30 +0000

    Fixed 4 PMD issues (removed unused imports)
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_1X_ROLLING@10724 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/context/FacesContextFactoryImpl.java
+++ jsf-ri/src/main/java/com/sun/faces/context/FacesContextFactoryImpl.java
@@ -42,8 +42,6 @@ package com.sun.faces.context;
 
 
 import com.sun.faces.util.Util;
-import com.sun.faces.util.MessageUtils;
-
 import javax.faces.FacesException;
 import javax.faces.FactoryFinder;
 import javax.faces.context.ExceptionHandlerFactory;
@@ -51,9 +49,6 @@ import javax.faces.context.ExternalContextFactory;
 import javax.faces.context.FacesContext;
 import javax.faces.context.FacesContextFactory;
 import javax.faces.lifecycle.Lifecycle;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 
 public class FacesContextFactoryImpl extends FacesContextFactory {
 
