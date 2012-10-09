9e2cda191e296317350dee1e20e173dad946af11
mriem <mriem@java.net>
2012-10-09 17:31:18 +0000

    Fixed http://java.net/jira/browse/JAVASERVERFACES-2531, r=rogerk, Migrate test for JAVASERVERFACES-1943 to new harness
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@10819 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ test/agnostic/composite/src/test/java/com/sun/faces/test/agnostic/composite/Issue1943IT.java
@@ -0,0 +1,91 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package com.sun.faces.test.agnostic.composite;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlOption;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSelect;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import java.util.List;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Issue1943IT {
+
+    private String webUrl;
+    private WebClient webClient;
+
+    @Before
+    public void setUp() {
+        webUrl = System.getProperty("integration.url");
+        webClient = new WebClient();
+    }
+
+    @After
+    public void tearDown() {
+        webClient.closeAllWindows();
+    }
+
+    @Test
+    public void testIssue1943() throws Exception {
+        HtmlPage page = webClient.getPage(webUrl + "faces/issue1943.xhtml");
+
+        List<HtmlSelect> selectList = page.getDocumentElement().getHtmlElementsByTagName("select");
+        HtmlSelect select = selectList.get(0);
+        List<HtmlOption> options = select.getOptions();
+        select.setSelectedAttribute(options.get(0), true);
+        List<HtmlSubmitInput> buttonList = page.getDocumentElement().getHtmlElementsByTagName("input");
+        HtmlSubmitInput submitButton = buttonList.get(1);
+        page = (HtmlPage) submitButton.click();
+        assertTrue(page.asText().contains("valueChange Called"));
+
+        page = webClient.getPage(webUrl + "faces/issue1943-action.xhtml");
+        buttonList = page.getDocumentElement().getHtmlElementsByTagName("input");
+        HtmlSubmitInput submitButton1 = buttonList.get(1);
+        page = (HtmlPage) submitButton1.click();
+        assertTrue(page.asText().contains("removeGroup Called"));
+        HtmlSubmitInput submitButton2 = buttonList.get(2);
+        page = (HtmlPage) submitButton2.click();
+        assertTrue(!page.asText().contains("removeGroup Called"));
+    }
+}
