e9af3cdae7163a44d1e9634d132b578ffc573cd4
rogerk <rogerk@java.net>
2012-10-16 20:17:46 +0000

    << http://java.net/jira/browse/JAVASERVERFACES-2543 >>
    SECTION: Modified Files
    ----------------------------
    M       jsf-ri/src/main/java/com/sun/faces/context/ExternalContextImpl.java
    ---- Ensure NullPointerException is thrown in getResource method if path argument is null.
    
    M       test/agnostic/context/regular/src/test/java/com/sun/faces/test/agnostic/context/regular/ExternalContextIT.java
    M       test/agnostic/context/regular/src/main/java/com/sun/faces/test/agnostic/context/regular/ExternalContextBean.java
    A       test/agnostic/context/regular/src/main/webapp/issue2543.xhtml
    ---- Test Case.
    
    
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_1X_ROLLING@10868 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/agnostic/context/regular/src/main/java/com/sun/faces/test/agnostic/context/regular/ExternalContextBean.java
+++ test/agnostic/context/regular/src/main/java/com/sun/faces/test/agnostic/context/regular/ExternalContextBean.java
@@ -151,5 +151,21 @@ public class ExternalContextBean implements Serializable {
 
         return "FAILED";
     }
+    public String getResourceNPE() {
+        try {
+            FacesContext currentContext = FacesContext.getCurrentInstance();
+            ExternalContextImpl externalContext =
+                    new ExternalContextImpl(
+                    (ServletContext) currentContext.getExternalContext().getContext(),
+                    (HttpServletRequest) currentContext.getExternalContext().getRequest(),
+                    (HttpServletResponse) currentContext.getExternalContext().getResponse());
+
+            externalContext.getResource(null);
+        } catch (NullPointerException exception) {
+            return "PASSED";
+        }
+
+        return "FAILED";
+    }
 
 }
