55cc063a947aaaef38bd8e7eb5e3dec4335898da
rogerk <rogerk@java.net>
2012-11-28 14:09:15 +0000

    << http://java.net/jira/browse/JAVASERVERFACES-2179 >>
    SECTION: Modified Files
    ----------------------------
    M       jsf-ri/src/main/java/com/sun/faces/context/PartialViewContextImpl.java
    ---- For partial components processing re-throw exceptions as FacesException(s) so
    ---- exception handlers can pick it up.
    M       test/agnostic/ajax/src/main/webapp/WEB-INF/web.xml
    ---- added taglib.xml reference
    
    
    A       test/agnostic/ajax/src/test/java/com/sun/faces/test/agnostic/ajax/Issue2179IT.java
    A       test/agnostic/ajax/src/main/java/com/sun/faces/test/agnostic/ajax/Issue2179Component1.java
    A       test/agnostic/ajax/src/main/java/com/sun/faces/test/agnostic/ajax/Issue2179Component2.java
    A       test/agnostic/ajax/src/main/webapp/WEB-INF/taglib.xml
    A       test/agnostic/ajax/src/main/webapp/issue2179-page1.xhtml
    A       test/agnostic/ajax/src/main/webapp/issue2179-page2.xhtml
    ---- Test case consisting of two custom components that override decode/encode to
    ---- pruposely throw exceptions.
    
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_1X_ROLLING@11044 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/context/PartialViewContextImpl.java
+++ jsf-ri/src/main/java/com/sun/faces/context/PartialViewContextImpl.java
@@ -40,6 +40,7 @@
 
 package com.sun.faces.context;
 
+import javax.faces.FacesException;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIViewRoot;
 import javax.faces.component.visit.VisitCallback;
@@ -250,12 +251,13 @@ import com.sun.faces.util.Util;
 
             try {
                 processComponents(viewRoot, phaseId, executeIds, ctx);
-            } catch (Exception e) {
+            } catch (IOException e) {
                 if (LOGGER.isLoggable(Level.INFO)) {
                     LOGGER.log(Level.INFO,
                            e.toString(),
                            e);
                 }
+                throw new FacesException(e);
             }
 
             // If we have just finished APPLY_REQUEST_VALUES phase, install the
@@ -509,27 +511,12 @@ import com.sun.faces.util.Util;
                 } else if (curPhase == PhaseId.UPDATE_MODEL_VALUES) {
                     comp.processUpdates(ctx);
                 } else if (curPhase == PhaseId.RENDER_RESPONSE) {
-
                     PartialResponseWriter writer = ctx.getPartialViewContext().getPartialResponseWriter();
-
                     writer.startUpdate(comp.getClientId(ctx));
-                    try {
-                        // do the default behavior...
-                        comp.encodeAll(ctx);
-                    }
-                    catch (Exception ce) {
-                        if (LOGGER.isLoggable(Level.SEVERE)) {
-                            LOGGER.severe(ce.toString());
-                        }
-                        if (LOGGER.isLoggable(Level.FINE)) {
-                            LOGGER.log(Level.FINE,
-                            ce.toString(),
-                            ce);
-                        }
-                    }
+                    // do the default behavior...
+                    comp.encodeAll(ctx);          
                     writer.endUpdate();
-                }
-                else {
+                } else {
                     throw new IllegalStateException("I18N: Unexpected " +
                                                     "PhaseId passed to " +
                                               " PhaseAwareContextCallback: " +
@@ -537,7 +524,15 @@ import com.sun.faces.util.Util;
                 }
             }
             catch (IOException ex) {
-                ex.printStackTrace();
+                if (LOGGER.isLoggable(Level.SEVERE)) {
+                    LOGGER.severe(ex.toString());
+                }
+                if (LOGGER.isLoggable(Level.FINE)) {
+                    LOGGER.log(Level.FINE,
+                    ex.toString(),
+                    ex);
+                }
+                throw new FacesException(ex);
             }
 
             // Once we visit a component, there is no need to visit
