b5b58f7302012f4c9a217dea46145cb44c6e132b
mriem <mriem@java.net>
2012-12-19 17:21:06 +0000

    Fixed PMD issues, add logging to empty catch blocks
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@11255 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/renderkit/ServerSideStateHelper.java
+++ jsf-ri/src/main/java/com/sun/faces/renderkit/ServerSideStateHelper.java
@@ -344,7 +344,9 @@ public class ServerSideStateHelper extends StateHelper {
             try {
                 value = Integer.valueOf(defaultValue);
             } catch (NumberFormatException ne) {
-                // won't occur
+                if (LOGGER.isLoggable(Level.FINEST)) {
+                    LOGGER.log(Level.FINEST, "Unable to convert number", ne);
+                }
             }
         }
 
@@ -378,7 +380,11 @@ public class ServerSideStateHelper extends StateHelper {
                 if (oas != null) {
                     try {
                         oas.close();
-                    } catch (IOException ignored) { }
+                    } catch (IOException ioe) { 
+                        if (LOGGER.isLoggable(Level.FINEST)) {
+                            LOGGER.log(Level.FINEST, "Closing stream", ioe);
+                        }
+                    }
                 }
             }
             return baos.toByteArray();
@@ -412,7 +418,11 @@ public class ServerSideStateHelper extends StateHelper {
                 if (ois != null) {
                     try {
                         ois.close();
-                    } catch (IOException ignored) { }
+                    } catch (IOException ioe) { 
+                        if (LOGGER.isLoggable(Level.FINEST)) {
+                            LOGGER.log(Level.FINEST, "Closing stream", ioe);
+                        }
+                    }
                 }
             }
         } else {
