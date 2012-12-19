faa42cb00ef2ea8b9664cfc277835f20bb870b9f
mriem <mriem@java.net>
2012-12-19 17:01:22 +0000

    Fixed PMD issues, add logging to empty catch blocks
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@11252 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/facelets/util/DevTools.java
+++ jsf-ri/src/main/java/com/sun/faces/facelets/util/DevTools.java
@@ -75,6 +75,8 @@ import java.io.*;
 import java.lang.reflect.Method;
 import java.text.DateFormat;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * <p>
@@ -89,6 +91,8 @@ import java.util.*;
  *
  */
 public final class DevTools {
+
+    private static final Logger LOGGER = Logger.getLogger(DevTools.class.getPackage().getName());
     
     private final static String TS = "&lt;";
     
@@ -208,7 +212,9 @@ public final class DevTools {
         try {
             flash = ctx.getFlash();
         } catch (UnsupportedOperationException uoe) {
-            // ignore
+            if (LOGGER.isLoggable(Level.FINEST)) {
+                LOGGER.log(Level.FINEST, "Flash not supported", uoe);
+            }
         }
         if (flash != null) {
             writeVariables(writer, flash, "Flash Attributes");
@@ -395,7 +401,9 @@ public final class DevTools {
                             writer.write("\"");
                         }
                     } catch (Exception e) {
-                        // do nothing
+                        if (LOGGER.isLoggable(Level.FINEST)) {
+                            LOGGER.log(Level.FINEST, "Error writing out attribute", e);
+                        }
                     }
                 }
             }
@@ -407,7 +415,9 @@ public final class DevTools {
                 writer.write("\"");
             }
         } catch (Exception e) {
-            // do nothing
+            if (LOGGER.isLoggable(Level.FINEST)) {
+                LOGGER.log(Level.FINEST, "Error writing out attributes", e);
+            }
         }
 
     }
