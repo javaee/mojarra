17c7ab3a9048f1260a2736dea6326023f14e3d2a
edburns <edburns@java.net>
2012-12-20 22:17:00 +0000

    http://java.net/jira/browse/JAVASERVERFACES-2584
    Use the lazy strategy for choosing which exceptionHandler to use
    
    
    SECTION: Modified Files
    ----------------------------
    M       jsf-ri/src/main/java/com/sun/faces/context/ExceptionHandlerFactoryImpl.java
    
    - Make getExceptionHandler() return a wrapper that does the ajax/no-ajax
      switching lazily.
    
    A         jsf-ri/src/main/java/com/sun/faces/context/AjaxNoAjaxExceptionHandler.java
    
    - Do the ajax/no-ajax switching in getWrapped().
    
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@11294 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/context/AjaxNoAjaxExceptionHandler.java
+++ jsf-ri/src/main/java/com/sun/faces/context/AjaxNoAjaxExceptionHandler.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
- *
+ * 
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
  * and Distribution License("CDDL") (collectively, the "License").  You
@@ -11,20 +11,20 @@
  * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
  * or packager/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
- *
+ * 
  * When distributing the software, include this License Header Notice in each
  * file and include the License file at packager/legal/LICENSE.txt.
- *
+ * 
  * GPL Classpath Exception:
  * Oracle designates this particular file as subject to the "Classpath"
  * exception as provided by Oracle in the GPL Version 2 section of the License
  * file that accompanied this code.
- *
+ * 
  * Modifications:
  * If applicable, add the following below the License Header, with the fields
  * enclosed by brackets [] replaced by your own identifying information:
  * "Portions Copyright [year] [name of copyright owner]"
- *
+ * 
  * Contributor(s):
  * If you wish your version of this file to be governed by only the CDDL or
  * only the GPL Version 2, indicate your decision by adding "[Contributor]
@@ -36,57 +36,34 @@
  * and therefore, elected the GPL Version 2 license, then the option applies
  * only if the new code is made subject to such option by the copyright
  * holder.
- */
 
+ */
 package com.sun.faces.context;
 
 import javax.faces.context.ExceptionHandler;
-import javax.faces.context.ExceptionHandlerFactory;
+import javax.faces.context.ExceptionHandlerWrapper;
 import javax.faces.context.FacesContext;
 
-import com.sun.faces.application.ApplicationAssociate;
-
-/**
- * Default ExceptionHandlerFactory implementation.
- */
-public class ExceptionHandlerFactoryImpl extends ExceptionHandlerFactory {
-
-    private ApplicationAssociate associate;
-
-    // ------------------------------------ Methods from ExceptionHandlerFactory
-
-    public ExceptionHandlerFactoryImpl() {
-    }
-
+public class AjaxNoAjaxExceptionHandler extends ExceptionHandlerWrapper {
     
-    /**
-     * @see javax.faces.context.ExceptionHandlerFactory#getExceptionHandler()
-     */
-    public ExceptionHandler getExceptionHandler() {
+    private AjaxExceptionHandlerImpl ajaxExceptionHandlerImpl;
+    private ExceptionHandlerImpl exceptionHandlerImpl;
+
+    public AjaxNoAjaxExceptionHandler(AjaxExceptionHandlerImpl ajaxExceptionHandlerImpl, ExceptionHandlerImpl exceptionHandlerImpl) {
+        this.ajaxExceptionHandlerImpl = ajaxExceptionHandlerImpl;
+        this.exceptionHandlerImpl = exceptionHandlerImpl;
+    }
+
+    @Override
+    public ExceptionHandler getWrapped() {
         FacesContext fc = FacesContext.getCurrentInstance();
-        ApplicationAssociate myAssociate = getAssociate(fc);
-
-        ExceptionHandler result = new AjaxNoAjaxExceptionHandler(
-                new AjaxExceptionHandlerImpl(new ExceptionHandlerImpl(Boolean.TRUE)),
-                new ExceptionHandlerImpl(((myAssociate != null) ? myAssociate.isErrorPagePresent() : Boolean.TRUE)));
-        return result;
-
-    }
-
-
-    // --------------------------------------------------------- Private Methods
-
-
-    private ApplicationAssociate getAssociate(FacesContext ctx) {
-
-        if (associate == null) {
-            associate = ApplicationAssociate.getCurrentInstance();
-            if (associate == null) {
-                associate = ApplicationAssociate.getInstance(ctx.getExternalContext());
-            }
+        if (null != fc && fc.getPartialViewContext().isAjaxRequest()) {
+            return ajaxExceptionHandlerImpl;
         }
-        return associate;
-
+        return exceptionHandlerImpl;
+        
     }
-
+    
+    
+    
 }
