be0109143292e45f5c5b211c893e6301ce7206d1
rogerk <rogerk@java.net>
2012-12-21 13:34:00 +0000

    << http://java.net/jira/browse/JAVASERVERFACES-2629 >>
    SECTION: Modified Files
    ----------------------------
    M       jsf-ri/src/main/java/com/sun/faces/component/PassthroughElement.java
    ---- Add missing attributes (like onclick) and accessor methods
    ---- (like getOnclick, setOnclick).
    
    
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@11296 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/component/PassthroughElement.java
+++ jsf-ri/src/main/java/com/sun/faces/component/PassthroughElement.java
@@ -76,6 +76,18 @@ public class PassthroughElement extends javax.faces.component.UIPanel
 
 
     protected enum PropertyKeys {
+        onclick,
+        ondblclick,
+        onkeydown,
+        onkeypress,
+        onkeyup,
+        onmousedown,
+        onmousemove,
+        onmouseout,
+        onmouseover,
+        onmouseup,
+        style,
+        styleClass,
 ;
         String toString;
         PropertyKeys(String toString) { this.toString = toString; }
@@ -83,6 +95,228 @@ public class PassthroughElement extends javax.faces.component.UIPanel
         public String toString() {
             return ((toString != null) ? toString : super.toString());
         }
+    }
+
+    /**
+     * <p>Return the value of the <code>onclick</code> property.</p>
+     * <p>Contents: Javascript code executed when a pointer button is
+     * clicked over this element.
+     */
+    public java.lang.String getOnclick() {
+        return (java.lang.String) getStateHelper().eval(PropertyKeys.onclick);
+
+    }
+
+    /**
+     * <p>Set the value of the <code>onclick</code> property.</p>
+     */
+    public void setOnclick(java.lang.String onclick) {
+        getStateHelper().put(PropertyKeys.onclick, onclick);
+        handleAttribute("onclick", onclick);
+    }
+
+
+    /**
+     * <p>Return the value of the <code>ondblclick</code> property.</p>
+     * <p>Contents: Javascript code executed when a pointer button is
+     * double clicked over this element.
+     */
+    public java.lang.String getOndblclick() {
+        return (java.lang.String) getStateHelper().eval(PropertyKeys.ondblclick);
+
+    }
+
+    /**
+     * <p>Set the value of the <code>ondblclick</code> property.</p>
+     */
+    public void setOndblclick(java.lang.String ondblclick) {
+        getStateHelper().put(PropertyKeys.ondblclick, ondblclick);
+        handleAttribute("ondblclick", ondblclick);
+    }
+
+
+    /**
+     * <p>Return the value of the <code>onkeydown</code> property.</p>
+     * <p>Contents: Javascript code executed when a key is
+     * pressed down over this element.
+     */
+    public java.lang.String getOnkeydown() {
+        return (java.lang.String) getStateHelper().eval(PropertyKeys.onkeydown);
+
+    }
+
+    /**
+     * <p>Set the value of the <code>onkeydown</code> property.</p>
+     */
+    public void setOnkeydown(java.lang.String onkeydown) {
+        getStateHelper().put(PropertyKeys.onkeydown, onkeydown);
+        handleAttribute("onkeydown", onkeydown);
+    }
+
+    /**
+     * <p>Return the value of the <code>onkeypress</code> property.</p>
+     * <p>Contents: Javascript code executed when a key is
+     * pressed and released over this element.
+     */
+    public java.lang.String getOnkeypress() {
+        return (java.lang.String) getStateHelper().eval(PropertyKeys.onkeypress);
+
+    }
+
+    /**
+     * <p>Set the value of the <code>onkeypress</code> property.</p>
+     */
+    public void setOnkeypress(java.lang.String onkeypress) {
+        getStateHelper().put(PropertyKeys.onkeypress, onkeypress);
+        handleAttribute("onkeypress", onkeypress);
+    }
+
+
+    /**
+     * <p>Return the value of the <code>onkeyup</code> property.</p>
+     * <p>Contents: Javascript code executed when a key is
+     * released over this element.
+     */
+    public java.lang.String getOnkeyup() {
+        return (java.lang.String) getStateHelper().eval(PropertyKeys.onkeyup);
+
+    }
+
+    /**
+     * <p>Set the value of the <code>onkeyup</code> property.</p>
+     */
+    public void setOnkeyup(java.lang.String onkeyup) {
+        getStateHelper().put(PropertyKeys.onkeyup, onkeyup);
+        handleAttribute("onkeyup", onkeyup);
+    }
+
+
+    /**
+     * <p>Return the value of the <code>onmousedown</code> property.</p>
+     * <p>Contents: Javascript code executed when a pointer button is
+     * pressed down over this element.
+     */
+    public java.lang.String getOnmousedown() {
+        return (java.lang.String) getStateHelper().eval(PropertyKeys.onmousedown);
+
+    }
+
+    /**
+     * <p>Set the value of the <code>onmousedown</code> property.</p>
+     */
+    public void setOnmousedown(java.lang.String onmousedown) {
+        getStateHelper().put(PropertyKeys.onmousedown, onmousedown);
+        handleAttribute("onmousedown", onmousedown);
+    }
+
+    /**
+     * <p>Return the value of the <code>onmousemove</code> property.</p>
+     * <p>Contents: Javascript code executed when a pointer button is
+     * moved within this element.
+     */
+    public java.lang.String getOnmousemove() {
+        return (java.lang.String) getStateHelper().eval(PropertyKeys.onmousemove);
+
+    }
+
+    /**
+     * <p>Set the value of the <code>onmousemove</code> property.</p>
+     */
+    public void setOnmousemove(java.lang.String onmousemove) {
+        getStateHelper().put(PropertyKeys.onmousemove, onmousemove);
+        handleAttribute("onmousemove", onmousemove);
+    }
+
+
+    /**
+     * <p>Return the value of the <code>onmouseout</code> property.</p>
+     * <p>Contents: Javascript code executed when a pointer button is
+     * moved away from this element.
+     */
+    public java.lang.String getOnmouseout() {
+        return (java.lang.String) getStateHelper().eval(PropertyKeys.onmouseout);
+
+    }
+
+    /**
+     * <p>Set the value of the <code>onmouseout</code> property.</p>
+     */
+    public void setOnmouseout(java.lang.String onmouseout) {
+        getStateHelper().put(PropertyKeys.onmouseout, onmouseout);
+        handleAttribute("onmouseout", onmouseout);
+    }
+
+    /**
+     * <p>Return the value of the <code>onmouseover</code> property.</p>
+     * <p>Contents: Javascript code executed when a pointer button is
+     * moved onto this element.
+     */
+    public java.lang.String getOnmouseover() {
+        return (java.lang.String) getStateHelper().eval(PropertyKeys.onmouseover);
+
+    }
+
+    /**
+     * <p>Set the value of the <code>onmouseover</code> property.</p>
+     */
+    public void setOnmouseover(java.lang.String onmouseover) {
+        getStateHelper().put(PropertyKeys.onmouseover, onmouseover);
+        handleAttribute("onmouseover", onmouseover);
+    }
+
+
+    /**
+     * <p>Return the value of the <code>onmouseup</code> property.</p>
+     * <p>Contents: Javascript code executed when a pointer button is
+     * released over this element.
+     */
+    public java.lang.String getOnmouseup() {
+        return (java.lang.String) getStateHelper().eval(PropertyKeys.onmouseup);
+
+    }
+
+    /**
+     * <p>Set the value of the <code>onmouseup</code> property.</p>
+     */
+    public void setOnmouseup(java.lang.String onmouseup) {
+        getStateHelper().put(PropertyKeys.onmouseup, onmouseup);
+        handleAttribute("onmouseup", onmouseup);
+    }
+
+
+    /**
+     * <p>Return the value of the <code>style</code> property.</p>
+     * <p>Contents: CSS style(s) to be applied when this component is rendered.
+     */
+    public java.lang.String getStyle() {
+        return (java.lang.String) getStateHelper().eval(PropertyKeys.style);
+
+    }
+
+    /**
+     * <p>Set the value of the <code>style</code> property.</p>
+     */
+    public void setStyle(java.lang.String style) {
+        getStateHelper().put(PropertyKeys.style, style);
+        handleAttribute("style", style);
+    }
+
+    /**
+     * <p>Return the value of the <code>styleClass</code> property.</p>
+     * <p>Contents: Space-separated list of CSS style class(es) to be applied when
+     * this element is rendered.  This value must be passed through
+     * as the "class" property on generated markup.
+     */
+    public java.lang.String getStyleClass() {
+        return (java.lang.String) getStateHelper().eval(PropertyKeys.styleClass);
+
+    }
+
+    /**
+     * <p>Set the value of the <code>styleClass</code> property.</p>
+     */
+    public void setStyleClass(java.lang.String styleClass) {
+        getStateHelper().put(PropertyKeys.styleClass, styleClass);
     }
 
     private static final Collection<String> EVENT_NAMES = Collections.unmodifiableCollection(
