fc0057b3edb9511b3999d2a685bab8b3a33bc095
rogerk <rogerk@java.net>
2013-01-28 15:07:20 +0000

    << http://java.net/jira/browse/JAVASERVERFACES-2607>>
    SECTION: Modified Files
    ----------------------------
    M       test/agnostic/renderKit/passthrough/pom.xml
    
    A       test/agnostic/renderKit/passthrough/src/test/java/com/sun/faces/test/agnostic/renderKit/passthrough/Issue2607IT.java
    A       test/agnostic/renderKit/passthrough/src/main/webapp/meta.xhtml
    A       test/agnostic/renderKit/passthrough/src/main/webapp/base.xhtml
    A       test/agnostic/renderKit/passthrough/src/main/webapp/command.xhtml
    
    
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@11471 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ test/agnostic/renderKit/passthrough/src/test/java/com/sun/faces/test/agnostic/renderKit/passthrough/Issue2607IT.java
@@ -0,0 +1,123 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package com.sun.faces.test.agnostic.renderKit.passthrough;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlFieldSet;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import org.junit.After;
+import org.junit.Before;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class Issue2607IT {
+    
+    /**
+     * Stores the web URL.
+     */
+    private String webUrl;
+    /**
+     * Stores the web client.
+     */
+    private WebClient webClient;
+
+    /**
+     * Setup before testing.
+     */
+    @Before
+    public void setUp() {
+        webUrl = System.getProperty("integration.url");
+        webClient = new WebClient();
+        webClient.setJavaScriptEnabled(true);
+        webClient.setJavaScriptTimeout(60000);
+    }
+
+    /**
+     * Tear down after testing.
+     */
+    @After
+    public void tearDown() {
+        webClient.closeAllWindows();
+    }
+
+    // This tests the following markup:
+    // <base jsf:id="base1" href="http://foobar.com" target="_blank" />
+    //
+    @Test
+    public void testBase() throws Exception {
+        HtmlPage page = webClient.getPage(webUrl + "/faces/base.xhtml");
+        HtmlElement base = page.getElementById("base1");
+        String xml = base.asXml();
+        assertTrue(xml.contains("<base"));
+        assertTrue(xml.contains("id=\"" + "base1" + "\""));
+        assertTrue(xml.contains("href=\"" + "http://foobar.com" + "\""));
+        assertTrue(xml.contains("target=\"" + "_blank" + "\""));
+    } 
+
+    // This tests the following markup:
+    // <command jsf:id="command1" onclick="doThat()"/>
+    //
+    @Test
+    public void testCommand() throws Exception {
+        HtmlPage page = webClient.getPage(webUrl + "/faces/command.xhtml");
+        HtmlElement command = page.getElementById("command1");
+        String xml = command.asXml();
+        assertTrue(xml.contains("<command"));
+        assertTrue(xml.contains("id=\"" + "command1" + "\""));
+        assertTrue(xml.contains("onclick=\"" + "doThat()" + "\""));
+    } 
+
+    // This tests the following markup:
+    // <meta jsf:id="meta1" charset="UTF-8"/>
+    //
+    @Test
+    public void testMeta() throws Exception {
+        HtmlPage page = webClient.getPage(webUrl + "/faces/meta.xhtml");
+        HtmlElement meta = page.getElementById("meta1");
+        String xml = meta.asXml();
+        assertTrue(xml.contains("<meta"));
+        assertTrue(xml.contains("id=\"" + "meta1" + "\""));
+        assertTrue(xml.contains("charset=\"" + "UTF-8" + "\""));
+    } 
+
+}
