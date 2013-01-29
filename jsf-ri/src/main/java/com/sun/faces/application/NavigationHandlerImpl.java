c85c0eb631d72d6c586dfd492e3e83108e43f11f
mriem <mriem@java.net>
2013-01-29 23:13:09 +0000

    Fixed PMD issue
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@11485 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/application/NavigationHandlerImpl.java
+++ jsf-ri/src/main/java/com/sun/faces/application/NavigationHandlerImpl.java
@@ -63,7 +63,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import javax.el.ELContext;
 import javax.el.MethodExpression;
 import javax.el.ValueExpression;
-import javax.faces.application.Application;
 import javax.faces.application.ConfigurableNavigationHandler;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIViewAction;
