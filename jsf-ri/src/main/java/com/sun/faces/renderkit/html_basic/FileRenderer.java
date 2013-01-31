aa5b3a915e68d55d8a6e4e3d780fe46141e12d8a
rogerk <rogerk@java.net>
2013-01-31 14:01:22 +0000

    Remove unused import
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@11517 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/renderkit/html_basic/FileRenderer.java
+++ jsf-ri/src/main/java/com/sun/faces/renderkit/html_basic/FileRenderer.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -45,7 +45,6 @@ package com.sun.faces.renderkit.html_basic;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Map;
-import java.util.logging.Level;
 
 import javax.faces.FacesException;
 import javax.faces.component.UIComponent;
