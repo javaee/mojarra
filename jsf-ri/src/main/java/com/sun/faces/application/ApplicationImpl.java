4429b0cce495f9209e1e42216a4421a900cb53fc
rogerk <rogerk@java.net>
2013-02-01 13:50:31 +0000

    Remove unused import
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@11542 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/application/ApplicationImpl.java
+++ jsf-ri/src/main/java/com/sun/faces/application/ApplicationImpl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -98,7 +98,6 @@ import com.sun.faces.el.ELUtils;
 import com.sun.faces.el.FacesCompositeELResolver;
 import com.sun.faces.el.PropertyResolverImpl;
 import com.sun.faces.el.VariableResolverImpl;
-import com.sun.faces.facelets.impl.DefaultFaceletFactory;
 import com.sun.faces.spi.InjectionProvider;
 import com.sun.faces.util.Cache;
 import com.sun.faces.util.Cache.Factory;
