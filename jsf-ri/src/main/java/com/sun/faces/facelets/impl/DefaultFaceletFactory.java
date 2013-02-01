63fb42c05c25f8d2cd880b5e444e409947a8483b
rogerk <rogerk@java.net>
2013-02-01 13:54:08 +0000

    removed unsed imports
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@11544 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/facelets/impl/DefaultFaceletFactory.java
+++ jsf-ri/src/main/java/com/sun/faces/facelets/impl/DefaultFaceletFactory.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -90,12 +90,10 @@ import java.util.concurrent.ConcurrentMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
-import javax.el.ELException;
 import javax.faces.FacesException;
 import javax.faces.application.Application;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.faces.view.facelets.FaceletException;
 
 
 /**
