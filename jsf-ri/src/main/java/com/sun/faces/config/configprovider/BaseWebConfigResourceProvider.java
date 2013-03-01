e59460517a8515a3475579feeeb929eaf40fa0b1
mriem <mriem@java.net>
2013-03-01 15:16:43 +0000

    Fixes http://java.net/jira/browse/JAVASERVERFACES-2765, r=mriem, another fix for Windows directory path issues, thanks Neil!
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@11680 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/config/configprovider/BaseWebConfigResourceProvider.java
+++ jsf-ri/src/main/java/com/sun/faces/config/configprovider/BaseWebConfigResourceProvider.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -119,7 +119,9 @@ public abstract class BaseWebConfigResourceProvider implements
         try {
             URL url = context.getResource(path);
             if (null != url) {
-                result = new URI(url.toExternalForm());
+                String urlString = url.toExternalForm();
+                urlString = urlString.replaceAll(" ", "%20");
+                result = new URI(urlString);            
             }
         } catch (MalformedURLException mue) {
             throw new FacesException(mue);
