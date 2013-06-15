3fa2584027d429780e9d57f91e80fb4d23b3783c
rogerk <rogerk@java.net>
2013-06-15 14:41:38 +0000

    << http://java.net/jira/browse/JAVASERVERFACES-2899 >>
    Refer to https://java.net/jira/browse/JAVASERVERFACES-2717 for more details about the 
    original issue.
    
    SECTION: Modified Files
    ----------------------------
    M       test/agnostic/facelets/ui/src/test/java/com/sun/faces/test/agnostic/facelets/ui/Issue2717IT.java
    M       test/agnostic/facelets/ui/src/main/java/com/sun/faces/test/agnostic/facelets/ui/RepeatResetNullBean.java
    M       test/agnostic/facelets/ui/src/main/webapp/repeatResetNull.xhtml
    
    
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_1X_ROLLING@12003 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/agnostic/facelets/ui/src/test/java/com/sun/faces/test/agnostic/facelets/ui/Issue2717IT.java
+++ test/agnostic/facelets/ui/src/test/java/com/sun/faces/test/agnostic/facelets/ui/Issue2717IT.java
@@ -40,7 +40,6 @@
 package com.sun.faces.test.agnostic.facelets.ui;
 
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
@@ -66,19 +65,18 @@ public class Issue2717IT {
         webClient.closeAllWindows();
     }
 
-    @Ignore
     @Test
     public void testIssue2717() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "faces/repeatResetNull.xhtml");
         assertTrue(page.asText().contains("myString")); 
-        assertTrue(page.asText().contains("isnull? false"));
+        assertTrue(page.asText().contains("isnull Or Empty ? false"));
         HtmlTextInput input = (HtmlTextInput)page.getHtmlElementById("repeat:0:input1");
         page = (HtmlPage)input.setValueAttribute("");
         assertTrue(!page.asText().contains("myString")); 
         HtmlSubmitInput button = (HtmlSubmitInput) page.getHtmlElementById("submit");
         page = button.click();
         webClient.waitForBackgroundJavaScript(120000);
-        assertTrue(page.asText().contains("isnull? true"));
+        assertTrue(page.asText().contains("isnull Or Empty ? true"));
     }
 }
 
