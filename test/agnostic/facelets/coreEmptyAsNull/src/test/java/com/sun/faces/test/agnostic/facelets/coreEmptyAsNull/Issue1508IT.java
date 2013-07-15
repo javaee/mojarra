39c6afa5a353d805d91c464fc80f3aaba082e02c
mriem <mriem@java.net>
2013-07-15 14:21:28 +0000

    Force this test to be excluded on Tomcat since the included EL parser requires a System property for this test to work, which would cause problems with other tests. See http://tomcat.apache.org/tomcat-7.0-doc/config/systemprops.html and look for COERCE_TO_ZERO
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_2X_ROLLING@12102 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/agnostic/facelets/coreEmptyAsNull/src/test/java/com/sun/faces/test/agnostic/facelets/coreEmptyAsNull/Issue1508IT.java
+++ test/agnostic/facelets/coreEmptyAsNull/src/test/java/com/sun/faces/test/agnostic/facelets/coreEmptyAsNull/Issue1508IT.java
@@ -68,7 +68,11 @@ public class Issue1508IT {
     /**
      * Test to validate empty fields.
      * 
-     * Note: this test is excluded on Tomcat.
+     * Note: this test is excluded on Tomcat because the included EL parser 
+     * requires a System property for this test to work, which would cause 
+     * problems with other tests. See 
+     * http://tomcat.apache.org/tomcat-7.0-doc/config/systemprops.html and 
+     * look for COERCE_TO_ZERO
      */
     @Test
     public void testValidateEmptyFields() throws Exception {
