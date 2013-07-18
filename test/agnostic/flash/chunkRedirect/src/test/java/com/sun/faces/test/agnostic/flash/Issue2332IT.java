19667e0c9b4ce38eaee839aabd729d5fefa0c156
mriem <mriem@java.net>
2013-07-18 20:24:29 +0000

    Fix fallout from upgrade to HtmlUnit 2.12
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_2X_ROLLING@12171 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/agnostic/flash/chunkRedirect/src/test/java/com/sun/faces/test/agnostic/flash/Issue2332IT.java
+++ test/agnostic/flash/chunkRedirect/src/test/java/com/sun/faces/test/agnostic/flash/Issue2332IT.java
@@ -43,10 +43,10 @@ package com.sun.faces.test.agnostic.flash;
 
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
-import com.gargoylesoftware.htmlunit.html.HtmlLink;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Integration tests for issue #2332
@@ -72,7 +72,7 @@ public class Issue2332IT {
     @Test
     public void testFlashChunkingLink1() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "faces/index.xhtml");
-        HtmlElement link = page.getElementById("form:link1");
+        HtmlElement link = page.getHtmlElementById("form:link1");
         assertNotNull("Unable to find form:link1 element", link);
         page = link.click();
         webClient.waitForBackgroundJavaScript(60000);
@@ -82,7 +82,7 @@ public class Issue2332IT {
     @Test
     public void testFlashChunkingLink2() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "faces/index.xhtml");
-        HtmlElement link = page.getElementById("form:link2");
+        HtmlElement link = page.getHtmlElementById("form:link2");
         assertNotNull("Unable to find form:link2 element", link);
         page = link.click();
         assertTrue(page.getBody().asText().indexOf("== Flash value LINK2 ==") != -1);
