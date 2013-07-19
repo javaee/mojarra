d0a14851130fa1ee44f1cc37c3e4440db41c228d
mriem <mriem@java.net>
2013-07-19 19:41:18 +0000

    Fixes https://java.net/jira/browse/JAVASERVERFACES-2952, Fix package names on test-cluster-webprofile-flow-basic_faces_flow_call project
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_2X_ROLLING@12213 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/cluster/web-profile/flow/basic_faces_flow_call/src/main/java/com/sun/faces/test/cluster/webprofile/flow/basic_faces_flow_call/FlowA.java
+++ test/cluster/web-profile/flow/basic_faces_flow_call/src/main/java/com/sun/faces/test/cluster/webprofile/flow/basic_faces_flow_call/FlowA.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
+ * 
  * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
- *
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
  * and Distribution License("CDDL") (collectively, the "License").  You
@@ -11,20 +11,20 @@
  * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
  * or packager/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
- *
+ * 
  * When distributing the software, include this License Header Notice in each
  * file and include the License file at packager/legal/LICENSE.txt.
- *
+ * 
  * GPL Classpath Exception:
  * Oracle designates this particular file as subject to the "Classpath"
  * exception as provided by Oracle in the GPL Version 2 section of the License
  * file that accompanied this code.
- *
+ * 
  * Modifications:
  * If applicable, add the following below the License Header, with the fields
  * enclosed by brackets [] replaced by your own identifying information:
  * "Portions Copyright [year] [name of copyright owner]"
- *
+ * 
  * Contributor(s):
  * If you wish your version of this file to be governed by only the CDDL or
  * only the GPL Version 2, indicate your decision by adding "[Contributor]
@@ -36,23 +36,37 @@
  * and therefore, elected the GPL Version 2 license, then the option applies
  * only if the new code is made subject to such option by the copyright
  * holder.
+
  */
-package com.sun.faces.test.webprofile.flow.basic;
+package com.sun.faces.test.cluster.webprofile.flow.basic_faces_flow_call;
 
-import javax.faces.flow.FlowScoped;
 import java.io.Serializable;
+import javax.enterprise.inject.Produces;
+import javax.faces.flow.Flow;
+import javax.faces.flow.builder.FlowBuilder;
+import javax.faces.flow.builder.FlowDefinition;
+import javax.faces.flow.builder.FlowBuilderParameter;
 
-import javax.inject.Named;
 
-@Named
-@FlowScoped(definingDocumentId="unique", value="flow-a")
-public class Flow_a_Bean implements Serializable {
+public class FlowA implements Serializable {
+    
+    private static final long serialVersionUID = -7623501087369765218L;
 
-   public String getName() {
-       return "Flow_a_Bean";
-   }
-   
-   public String getReturnValue() {
-       return "/return1";
-   }
+    public FlowA() {
+    }
+    
+    @Produces @FlowDefinition
+    public Flow buildMyFlow(@FlowBuilderParameter FlowBuilder flowBuilder) {
+        String flowId = "flow-a";
+        flowBuilder.id("unique", flowId);
+        flowBuilder.returnNode("taskFlowReturn1").
+                fromOutcome("#{flow_a_Bean.returnValue}");
+        flowBuilder.inboundParameter("param1FromFlowB", "#{flowScope.param1Value}");
+        flowBuilder.inboundParameter("param2FromFlowB", "#{flowScope.param2Value}");
+        flowBuilder.flowCallNode("callB").flowReference("", "flow-b").
+                outboundParameter("param1FromFlowA", "param1Value").
+                outboundParameter("param2FromFlowA", "param2Value");
+        
+        return flowBuilder.getFlow();
+    }
 }
--- test/cluster/web-profile/flow/basic_faces_flow_call/src/test/java/com/sun/faces/test/cluster/webprofile/flow/basic_faces_flow_call/FlowACallsFlowBIT.java
+++ test/cluster/web-profile/flow/basic_faces_flow_call/src/test/java/com/sun/faces/test/cluster/webprofile/flow/basic_faces_flow_call/FlowACallsFlowBIT.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-package com.sun.faces.test.webprofile.flow.basic_faces_flow_call;
+package com.sun.faces.test.cluster.webprofile.flow.basic_faces_flow_call;
 
 import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import org.junit.After;
