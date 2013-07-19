ccddbf06b814d91bb5d5902ba51dc5e0135af834
mriem <mriem@java.net>
2013-07-19 20:02:26 +0000

    Fixes https://java.net/jira/browse/JAVASERVERFACES-2953, Fix package names on test-javaee6-resource-cacheLastMod project
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_2X_ROLLING@12215 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/javaee6/resource/cacheLastMod/war-test/src/test/java/com/sun/faces/test/javaee6/resource/cacheLastMod/wartest/Issue2895IT.java
+++ test/javaee6/resource/cacheLastMod/war-test/src/test/java/com/sun/faces/test/javaee6/resource/cacheLastMod/wartest/Issue2895IT.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-package com.sun.faces.test.agnostic.resource.cache.wartest;
+package com.sun.faces.test.javaee6.resource.cacheLastMod.wartest;
 
 import com.gargoylesoftware.htmlunit.CookieManager;
 import org.junit.After;
