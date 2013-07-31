6d73418e065baaac342a0d570beddafa096952df
mriem <mriem@java.net>
2013-07-31 17:25:59 +0000

    Fixes https://java.net/jira/browse/JAVASERVERFACES-2971, fixes unit test failure
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_2X_ROLLING@12340 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/unit/src/test/java/javax/faces/component/UIInputTest.java
+++ test/unit/src/test/java/javax/faces/component/UIInputTest.java
@@ -47,7 +47,6 @@ import org.easymock.EasyMock;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.easymock.EasyMock.*;
-import org.junit.Ignore;
 
 /**
  * @author Manfred Riem (manfred.riem@oracle.com)
@@ -142,7 +141,6 @@ public class UIInputTest {
     }
 
     @Test
-    @Ignore
     public void testRestoreState5() {
         FacesContext context = EasyMock.createMock(FacesContext.class);
         UIInput input = new UIInput();
@@ -160,7 +158,7 @@ public class UIInputTest {
         assertTrue(!l2.initialStateMarked());
         Object state = input.saveState(context);
         assertTrue(state instanceof Object[]);
-        Object[] validatorState = (Object[]) ((Object[]) state)[3];
+        Object[] validatorState = (Object[]) ((Object[]) state)[1];
         assertNotNull(validatorState);
         assertNull(validatorState[0]);
         assertNotNull(validatorState[1]);
@@ -191,7 +189,7 @@ public class UIInputTest {
         state = input.saveState(context);
         assertNotNull(validatorState);
         assertTrue(state instanceof Object[]);
-        validatorState = (Object[]) ((Object[]) state)[3];
+        validatorState = (Object[]) ((Object[]) state)[1];
         assertNotNull(validatorState);
         assertTrue(validatorState.length == 3);
         assertNotNull(validatorState[0]);
