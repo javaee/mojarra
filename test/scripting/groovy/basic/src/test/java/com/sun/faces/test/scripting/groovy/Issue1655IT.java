2d824e3fde020bde0cc2a09157907f0ef3a6e5c9
edburns <edburns@java.net>
2013-08-26 12:55:34 +0000

    http://java.net/jira/browse/JAVASERVERFACES-3005 Re-enable Groovy support
    Backport from 2.2.x.  558 tests run successfully.
    
    Ever since the 2012-08-12 commit for JAVASERVERFACES-2491 Groovy support
    has been broken and the automated tests that asserted its functionality
    have been commented out.  I was never content with this resolution.
    
    This commit re-activates Groovy support without invalifating the fix for
    JAVASERVERFACES-2491.
    
    Ran the agnostic and web-profile tests with
    
    -Dwebapp.projectStage=Development -Dwebapp.stateSavingMethod=server -Dwebapp.partialStateSaving=false
    
    Ran the new test as well.
    
    SECTION: Modified Files
    ----------------------------
    M       jsf-ri/src/main/java/com/sun/faces/scripting/groovy/GroovyHelperImpl.java
    
    - Make some things in here public.
    
    M       jsf-ri/src/main/java/com/sun/faces/util/Util.java
    
    - In loadClass() use the newly publicized things to cause the
      performance penalty of Groovy support to only be paid if Groovy is actually enabled.
    
    A       test/scripting
    A       test/scripting/groovy
    A       test/scripting/groovy/basic
    A       test/scripting/groovy/basic/src
    A       test/scripting/groovy/basic/src/main
    A       test/scripting/groovy/basic/src/main/java
    A       test/scripting/groovy/basic/src/main/java/com
    A       test/scripting/groovy/basic/src/main/java/com/sun
    A       test/scripting/groovy/basic/src/main/java/com/sun/faces
    A       test/scripting/groovy/basic/src/main/java/com/sun/faces/test
    A       test/scripting/groovy/basic/src/main/java/com/sun/faces/test/scripting
    A       test/scripting/groovy/basic/src/main/java/com/sun/faces/test/scripting/groovy
    A  +    test/scripting/groovy/basic/src/main/java/com/sun/faces/test/scripting/groovy/Name.java
    A       test/scripting/groovy/basic/src/main/webapp
    A       test/scripting/groovy/basic/src/main/webapp/WEB-INF
    A  +    test/scripting/groovy/basic/src/main/webapp/WEB-INF/faces-config.xml
    A  +    test/scripting/groovy/basic/src/main/webapp/WEB-INF/groovy-taglib.xml
    A       test/scripting/groovy/basic/src/main/webapp/WEB-INF/groovy
    A       test/scripting/groovy/basic/src/main/webapp/WEB-INF/groovy/hello
    A  +    test/scripting/groovy/basic/src/main/webapp/WEB-INF/groovy/hello/MessageRenderer.groovy
    A  +    test/scripting/groovy/basic/src/main/webapp/WEB-INF/groovy/hello/AgeComponent.groovy
    A  +    test/scripting/groovy/basic/src/main/webapp/WEB-INF/groovy/hello/MessageOutput.groovy
    A  +    test/scripting/groovy/basic/src/main/webapp/WEB-INF/groovy/hello/NameValidator.groovy
    A  +    test/scripting/groovy/basic/src/main/webapp/WEB-INF/groovy/hello/NameConverter.groovy
    A  +    test/scripting/groovy/basic/src/main/webapp/WEB-INF/groovy/hello/HelloBean.groovy
    A  +    test/scripting/groovy/basic/src/main/webapp/hello.xhtml
    A  +    test/scripting/groovy/basic/src/main/webapp/submit.xhtml
    
    - Migrated test from old test harness to new area in new harness.  This
      test will not be run until
      <https://java.net/jira/browse/JAVASERVERFACES-3006> is fixed.
    
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_1X_ROLLING@12449 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ test/scripting/groovy/basic/src/test/java/com/sun/faces/test/scripting/groovy/Issue1655IT.java
@@ -0,0 +1,139 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package com.sun.faces.test.scripting.groovy;
+
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class Issue1655IT {
+    /**
+     * Stores the web URL.
+     */
+    private String webUrl;
+    /**
+     * Stores the web client.
+     */
+    private WebClient webClient;
+
+    /**
+     * Setup before testing.
+     * 
+     * @throws Exception when a serious error occurs.
+     */
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    /**
+     * Cleanup after testing.
+     * 
+     * @throws Exception when a serious error occurs.
+     */
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    /**
+     * Setup before testing.
+     */
+    @Before
+    public void setUp() {
+        webUrl = System.getProperty("integration.url");
+        webClient = new WebClient();
+    }
+
+    /**
+     * Tear down after testing.
+     */
+    @After
+    public void tearDown() {
+        webClient.closeAllWindows();
+    }
+
+    @Test
+    public void testBasicAppFunctionality() throws Exception {
+        HtmlPage page = webClient.getPage(webUrl);
+
+        HtmlTextInput inputText = (HtmlTextInput) page.getElementById("form:name");
+        String val = "" + System.currentTimeMillis();
+        inputText.setValueAttribute(val);
+
+        HtmlSubmitInput button = (HtmlSubmitInput) page.getElementById("form:command");
+        page = (HtmlPage) button.click();
+        String pageAsText = page.asText();
+        assertTrue(pageAsText.contains("Hello " + val));
+        assertTrue(pageAsText.contains("Happy Birthday"));
+
+        String pageText = page.asXml();
+        assertTrue(pageText.contains("<input type=\"hidden\" name=\"javax.faces.ViewState\" id="));
+    }
+    
+    @Test
+    public void testBasicAppFunctionalityNegative() throws Exception {
+        HtmlPage page = webClient.getPage(webUrl);
+        
+        HtmlTextInput inputText = (HtmlTextInput) page.getElementById("form:name");
+        String val = "" + System.currentTimeMillis();
+        inputText.setValueAttribute(val);
+        inputText = (HtmlTextInput) page.getElementById("form:age");
+        inputText.setValueAttribute("-12");
+
+        HtmlSubmitInput button = (HtmlSubmitInput) page.getElementById("form:command");
+        page = (HtmlPage) button.click();
+        String pageAsText = page.asText();
+        assertFalse(pageAsText.contains("Hello " + val));
+        assertFalse(pageAsText.contains("Happy Birthday"));
+        assertTrue(pageAsText.contains("please enter a valid age between 0 and 65"));
+
+        String pageText = page.asXml();
+        assertTrue(pageText.contains("<input type=\"hidden\" name=\"javax.faces.ViewState\" id="));
+        
+    }
+    
+}
