8260bee75fa48e813e30522e2fa0f4938c0d91c3
mriem <mriem@java.net>
2013-09-12 18:03:23 +0000

    Fixes https://java.net/jira/browse/JAVASERVERFACES-3006, moving groovy to its own directory and added groovy patch profile.
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_2X_ROLLING@12529 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ test/groovy/basic/src/main/webapp/WEB-INF/groovy/hello/NameConverter.groovy
@@ -0,0 +1,29 @@
+package hello
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import com.sun.faces.test.groovy.basic.Name;
+
+public class NameConverter implements Converter {
+
+       private String message = "04:48";
+
+
+    public NameConverter() {
+        System.out.println("NameConverter instantiated " + message);
+    }
+    public Object getAsObject(FacesContext context, UIComponent component,
+            String value) {
+      Name name = new Name(value);
+      return name.getFirst() + " " + name.getLast();
+
+    }
+
+    public String getAsString(FacesContext context, UIComponent component,
+                              Object value) {
+      return value.toString();
+    }
+
+  
+}
