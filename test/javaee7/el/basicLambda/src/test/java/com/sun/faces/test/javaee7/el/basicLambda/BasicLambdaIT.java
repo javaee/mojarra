e6b1a58b8c01f1a2164d9cea2889200ef8863e62
edburns <edburns@java.net>
2013-09-16 12:16:10 +0000

    http://java.net/jira/browse/JAVASERVERFACES-3028
    With JSR-341 and JSF you can do crazy stuff like this:
    
            <h:form prependId="false">
                <p><h:inputText id="index" value="#{requestScope.i}" /></p>
                
                <!--
                #{xx = (null != requestScope.i) ? requestScope.i : 0}
                -->
                
                <p id="output"><h:outputText value="#{[1,2,3,4,5,6,7,8].stream().
                                                      filter(i->i%2 == 0).map(i->i*10).toArray()[xx]}" /></p>
                <h:commandButton value="submit" />
            </h:form>
    
    
    SECTION: Modified Files
    ----------------------------
    M       test/javaee7/beanValidation/pom.xml
    M       test/javaee7/beanValidation/methodValidation/pom.xml
    A       test/javaee7/el
    A       test/javaee7/el/basicLambda
    A       test/javaee7/el/basicLambda/nbactions.xml
    A       test/javaee7/el/basicLambda/src
    A       test/javaee7/el/basicLambda/src/test
    A       test/javaee7/el/basicLambda/src/test/java
    A       test/javaee7/el/basicLambda/src/test/java/com
    A       test/javaee7/el/basicLambda/src/test/java/com/sun
    A       test/javaee7/el/basicLambda/src/test/java/com/sun/faces
    A       test/javaee7/el/basicLambda/src/test/java/com/sun/faces/test
    A       test/javaee7/el/basicLambda/src/test/java/com/sun/faces/test/javaee7
    A       test/javaee7/el/basicLambda/src/test/java/com/sun/faces/test/javaee7/el
    A       test/javaee7/el/basicLambda/src/test/java/com/sun/faces/test/javaee7/el/basicLambda
    A       test/javaee7/el/basicLambda/src/test/java/com/sun/faces/test/javaee7/el/basicLambda/BasicLambdaIT.java
    A       test/javaee7/el/basicLambda/src/main
    A       test/javaee7/el/basicLambda/src/main/java
    A       test/javaee7/el/basicLambda/src/main/java/com
    A       test/javaee7/el/basicLambda/src/main/java/com/sun
    A       test/javaee7/el/basicLambda/src/main/java/com/sun/faces
    A       test/javaee7/el/basicLambda/src/main/java/com/sun/faces/test
    A       test/javaee7/el/basicLambda/src/main/java/com/sun/faces/test/javaee7
    A       test/javaee7/el/basicLambda/src/main/java/com/sun/faces/test/javaee7/el
    A       test/javaee7/el/basicLambda/src/main/java/com/sun/faces/test/javaee7/el/basicLambda
    A       test/javaee7/el/basicLambda/src/main/java/com/sun/faces/test/javaee7/el/basicLambda/BookTableBacking.java
    A       test/javaee7/el/basicLambda/src/main/java/com/sun/faces/test/javaee7/el/basicLambda/Library.java
    A       test/javaee7/el/basicLambda/src/main/java/com/sun/faces/test/javaee7/el/basicLambda/Book.java
    A       test/javaee7/el/basicLambda/src/main/resources
    A       test/javaee7/el/basicLambda/src/main/webapp
    A       test/javaee7/el/basicLambda/src/main/webapp/index.xhtml
    A       test/javaee7/el/basicLambda/src/main/webapp/WEB-INF
    A       test/javaee7/el/basicLambda/src/main/webapp/WEB-INF/web.xml
    A       test/javaee7/el/basicLambda/src/main/webapp/WEB-INF/glassfish-web.xml
    A       test/javaee7/el/basicLambda/src/main/webapp/bookTable.xhtml
    A       test/javaee7/el/basicLambda/pom.xml
    A       test/javaee7/el/pom.xml
    M       test/javaee7/pom.xml
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_2X_ROLLING@12532 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ test/javaee7/el/basicLambda/src/test/java/com/sun/faces/test/javaee7/el/basicLambda/BasicLambdaIT.java
@@ -0,0 +1,138 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package com.sun.faces.test.javaee7.el.basicLambda;
+
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class BasicLambdaIT {
+    /**
+     * Stores the web URL.
+     */
+    private String webUrl;
+    /**
+     * Stores the web client.
+     */
+    private WebClient webClient;
+
+    /**
+     * Setup before testing.
+     * 
+     * @throws Exception when a serious error occurs.
+     */
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    /**
+     * Cleanup after testing.
+     * 
+     * @throws Exception when a serious error occurs.
+     */
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    /**
+     * Setup before testing.
+     */
+    @Before
+    public void setUp() {
+        webUrl = System.getProperty("integration.url");
+        webClient = new WebClient();
+    }
+
+    /**
+     * Tear down after testing.
+     */
+    @After
+    public void tearDown() {
+        webClient.closeAllWindows();
+    }
+
+    @Test
+    public void testIndex() throws Exception {
+        HtmlPage page = webClient.getPage(webUrl);
+        
+        HtmlElement out = page.getElementById("output");
+
+        assertEquals("20", out.asText());
+        
+        HtmlTextInput input = (HtmlTextInput) page.getElementById("input");
+        input.setValueAttribute("1");
+        HtmlSubmitInput button = (HtmlSubmitInput) page.getElementById("button");
+        page = button.click();
+        
+        out = page.getElementById("output");
+        assertEquals("40", out.asText());
+
+        input = (HtmlTextInput) page.getElementById("input");
+        input.setValueAttribute("2");
+        button = (HtmlSubmitInput) page.getElementById("button");
+        page = button.click();
+        
+        out = page.getElementById("output");
+        assertEquals("60", out.asText());
+                
+    }
+    
+    @Test 
+    public void testBookTable() throws Exception {
+        HtmlPage page = webClient.getPage(webUrl + "faces/bookTable.xhtml");
+        
+        assertTrue(page.asText().contains("At Swim Two Birds"));
+        assertTrue(page.asText().contains("The Third Policeman"));
+        
+        HtmlElement out = page.getElementById("output2");
+
+        assertEquals("The Picture of Dorian Gray", out.asText());
+        
+    }
+}
