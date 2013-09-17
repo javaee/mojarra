aa824a1b9d45bc56931cb30d213170163bc572c0
edburns <edburns@java.net>
2013-09-17 13:35:46 +0000

    SECTION: Modified Files ---------------------------- M       test/groovy/basic/src/test/java/com/sun/faces/test/groovy/basic/Issue1655IT.java M       test/groovy/weldAware/src/test/java/com/sun/faces/test/groovy/weldAware/Issue3005IT.java
    - Print diagnostic information.
    
    Ed
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_2X_ROLLING@12548 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/groovy/basic/src/test/java/com/sun/faces/test/groovy/basic/Issue1655IT.java
+++ test/groovy/basic/src/test/java/com/sun/faces/test/groovy/basic/Issue1655IT.java
@@ -129,6 +129,7 @@ public class Issue1655IT {
         String pageAsText = page.asXml();
         assertFalse(pageAsText.contains("Hello " + val));
         assertFalse(pageAsText.contains("Happy Birthday"));
+        System.out.println("debug: edburns: " + pageAsText);
         assertTrue(pageAsText.matches("(?s).*please.*enter.*a.*valid.*age.*between.*0.*and.*65.*"));
 
         String pageText = page.asXml();
