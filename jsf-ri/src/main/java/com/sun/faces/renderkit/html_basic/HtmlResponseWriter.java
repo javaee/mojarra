28ffdcac952298302f8e138ab44ea947dc0b61b7
edburns <edburns@java.net>
2014-01-27 21:43:15 +0000

    http://java.net/jira/browse/JAVASERVERFACES-3150
    Backport to 2.1.x at reporter's kind request.
    
    
    SECTION: Modified Files
    ----------------------------
    M       jsf-ri/src/main/java/com/sun/faces/renderkit/html_basic/HtmlResponseWriter.java
    
    - in endElement(String), force dontEscape to false after writing the
      script, element, if any.
    
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_1X_ROLLING@12798 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/renderkit/html_basic/HtmlResponseWriter.java
+++ jsf-ri/src/main/java/com/sun/faces/renderkit/html_basic/HtmlResponseWriter.java
@@ -543,6 +543,9 @@ public class HtmlResponseWriter extends ResponseWriter {
         }
         isScript = false;
         isStyle = false;
+
+        dontEscape = false;
+
         if ("cdata".equalsIgnoreCase(name)) {
             endCDATA();
             return;
