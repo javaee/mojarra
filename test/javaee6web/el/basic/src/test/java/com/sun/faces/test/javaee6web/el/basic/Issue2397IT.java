a2abec1ec270dd9d29de67dd5aecebc47dfd7c31
mriem <mriem@java.net>
2014-01-31 17:09:07 +0000

    Fix package name to reflect new location
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_2X_ROLLING@12813 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/javaee6web/el/basic/src/test/java/com/sun/faces/test/javaee6web/el/basic/Issue2397IT.java
+++ test/javaee6web/el/basic/src/test/java/com/sun/faces/test/javaee6web/el/basic/Issue2397IT.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-package com.sun.faces.test.webprofile.el.basic;
+package com.sun.faces.test.javaee6web.el.basic;
 
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
