1903f15fbe83e8f697f6293d4230fdc91f259762
edburns <edburns@java.net>
2014-02-05 14:56:43 +0000

    http://java.net/jira/browse/JAVASERVERFACES-3031
    SECTION: Modified Files
    ----------------------------
    M       test/agnostic/ajax-namespace/src/main/java/com/sun/faces/test/agnostic/ajax_namespace/UserBean.java
    D       test/agnostic/ajax-namespace/src/main/webapp/WEB-INF/beans.xml
    
    - convert from CDI to JSF managed bean
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_1X_ROLLING@12830 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/agnostic/ajax-namespace/src/main/java/com/sun/faces/test/agnostic/ajax_namespace/UserBean.java
+++ test/agnostic/ajax-namespace/src/main/java/com/sun/faces/test/agnostic/ajax_namespace/UserBean.java
@@ -4,14 +4,14 @@ import java.io.Serializable;
 import java.util.Date;
 
 
-import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.ValidatorException;
-import javax.inject.Named;
 
-@Named
+@ManagedBean
 @SessionScoped
 public class UserBean implements Serializable {
     
