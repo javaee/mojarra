c24220e329dee91e87b23369c05598a1d23a415c
mriem <mriem@java.net>
2014-04-03 17:05:19 +0000

    Fixes https://java.net/jira/browse/JAVASERVERFACES-3221, backport issue #3215 to 2.1 branch
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_1X_ROLLING@13084 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/facelets/component/UIRepeat.java
+++ jsf-ri/src/main/java/com/sun/faces/facelets/component/UIRepeat.java
@@ -876,7 +876,7 @@ public class UIRepeat extends UINamingContainer {
         }
 
         public void populate(EditableValueHolder evh) {
-            this.value = evh.getValue();
+            this.value = evh.getLocalValue();
             this.valid = evh.isValid();
             this.submittedValue = evh.getSubmittedValue();
             this.localValueSet = evh.isLocalValueSet();
