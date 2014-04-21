6c88c0474d73ff02a41a84f32195ecfd955ee70a
mriem <mriem@java.net>
2014-04-21 19:31:59 +0000

    Remove unused import
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_2X_ROLLING@13142 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/renderkit/html_basic/TableRenderer.java
+++ jsf-ri/src/main/java/com/sun/faces/renderkit/html_basic/TableRenderer.java
@@ -47,7 +47,6 @@ import com.sun.faces.util.Util;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
