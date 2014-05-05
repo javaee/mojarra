2c47a6588d1d51c001c407daab36bfd8f51895e3
mriem <mriem@java.net>
2014-05-05 19:11:35 +0000

    Ignoring while waiting for resolution on WLS
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_2X_ROLLING@13203 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/servlet30/facelets/html/src/test/java/com/sun/faces/test/servlet30/facelets/html/Issue2923IT.java
+++ test/servlet30/facelets/html/src/test/java/com/sun/faces/test/servlet30/facelets/html/Issue2923IT.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2014 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -49,6 +49,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 @RunWith(JsfTestRunner.class)
@@ -70,6 +71,7 @@ public class Issue2923IT {
     
     @JsfTest(JsfVersion.JSF_2_2_2)
     @Test
+    @Ignore
     public void testInputFileRequired() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "faces/inputFileRequired.xhtml");
         HtmlElement button = page.getHtmlElementById("form:button");
