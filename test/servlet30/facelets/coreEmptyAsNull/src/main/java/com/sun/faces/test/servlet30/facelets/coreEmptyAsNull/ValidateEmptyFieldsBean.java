706bab6a2a73cfafde652796462c075089beb846
mriem <mriem@java.net>
2014-05-28 20:16:26 +0000

    Fixes https://java.net/jira/browse/JAVASERVERFACES-3294, migrate test/agnostic/facelets/coreEmptyAsNull to test/servlet30/facelets/coreEmptyAsNull
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_2X_ROLLING@13291 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/servlet30/facelets/coreEmptyAsNull/src/main/java/com/sun/faces/test/servlet30/facelets/coreEmptyAsNull/ValidateEmptyFieldsBean.java
+++ test/servlet30/facelets/coreEmptyAsNull/src/main/java/com/sun/faces/test/servlet30/facelets/coreEmptyAsNull/ValidateEmptyFieldsBean.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-package com.sun.faces.test.agnostic.facelets.coreEmptyAsNull;
+package com.sun.faces.test.servlet30.facelets.coreEmptyAsNull;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
--- test/servlet30/facelets/coreEmptyAsNull/src/test/java/com/sun/faces/test/servlet30/facelets/coreEmptyAsNull/Issue1508IT.java
+++ test/servlet30/facelets/coreEmptyAsNull/src/test/java/com/sun/faces/test/servlet30/facelets/coreEmptyAsNull/Issue1508IT.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-package com.sun.faces.test.agnostic.facelets.coreEmptyAsNull;
+package com.sun.faces.test.servlet30.facelets.coreEmptyAsNull;
 
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
