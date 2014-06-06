b3729dfb64b73c17cc6c25c151e9564993d741d8
mriem <mriem@java.net>
2014-06-06 15:30:56 +0000

    Exclude this test and put it on the table for 2.2.8
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_2X_ROLLING@13344 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/servlet30/component/src/test/java/com/sun/faces/test/servlet30/component/Issue3319IT.java
+++ test/servlet30/component/src/test/java/com/sun/faces/test/servlet30/component/Issue3319IT.java
@@ -42,12 +42,19 @@ package com.sun.faces.test.servlet30.component;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import static com.sun.faces.test.junit.JsfServerExclude.GLASSFISH_4_0;
+import static com.sun.faces.test.junit.JsfServerExclude.GLASSFISH_4_0_1;
+import com.sun.faces.test.junit.JsfTest;
+import com.sun.faces.test.junit.JsfTestRunner;
+import static com.sun.faces.test.junit.JsfVersion.JSF_2_2_0;
 import org.junit.After;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import org.junit.runner.RunWith;
 
-public class Spec997IT {
+@RunWith(JsfTestRunner.class)
+public class Issue3319IT {
 
     private String webUrl;
     private WebClient webClient;
@@ -63,6 +70,7 @@ public class Spec997IT {
         webClient.closeAllWindows();
     }
 
+    @JsfTest(value=JSF_2_2_0, excludes = {GLASSFISH_4_0, GLASSFISH_4_0_1})
     @Test
     public void testEventListener() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "faces/eventListener.xhtml");        
