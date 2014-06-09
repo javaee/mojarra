44b534cdd5344db0053c27dd9c309e0914e80bed
mriem <mriem@java.net>
2014-06-09 13:22:29 +0000

    Exclude for Weblogic 12.1.3
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_2X_ROLLING@13356 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/servlet30/component/src/test/java/com/sun/faces/test/servlet30/component/Issue3319IT.java
+++ test/servlet30/component/src/test/java/com/sun/faces/test/servlet30/component/Issue3319IT.java
@@ -46,6 +46,7 @@ import static com.sun.faces.test.junit.JsfServerExclude.GLASSFISH_3_1_2_2;
 import static com.sun.faces.test.junit.JsfServerExclude.GLASSFISH_4_0;
 import static com.sun.faces.test.junit.JsfServerExclude.GLASSFISH_4_0_1;
 import static com.sun.faces.test.junit.JsfServerExclude.TOMCAT_7_0_35;
+import static com.sun.faces.test.junit.JsfServerExclude.WEBLOGIC_12_1_3;
 import com.sun.faces.test.junit.JsfTest;
 import com.sun.faces.test.junit.JsfTestRunner;
 import static com.sun.faces.test.junit.JsfVersion.JSF_2_2_0;
@@ -73,7 +74,7 @@ public class Issue3319IT {
     }
 
     @JsfTest(value = JSF_2_2_0, excludes = {GLASSFISH_3_1_2_2, GLASSFISH_4_0,
-        GLASSFISH_4_0_1, TOMCAT_7_0_35})
+        GLASSFISH_4_0_1, TOMCAT_7_0_35, WEBLOGIC_12_1_3})
     @Test
     public void testEventListener() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "faces/eventListener.xhtml");
