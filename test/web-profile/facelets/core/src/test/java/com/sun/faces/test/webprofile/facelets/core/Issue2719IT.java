7fb5be295a5184a1d4830687ab21e3338c45d75b
mriem <mriem@java.net>
2014-07-29 20:19:58 +0000

    Make sure each of these tests correctly qualify which version they are fixed in.
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_1X_ROLLING@13471 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/web-profile/facelets/core/src/test/java/com/sun/faces/test/webprofile/facelets/core/Issue2719IT.java
+++ test/web-profile/facelets/core/src/test/java/com/sun/faces/test/webprofile/facelets/core/Issue2719IT.java
@@ -41,14 +41,16 @@ package com.sun.faces.test.webprofile.facelets.core;
 
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.util.NameValuePair;
-import java.util.List;
+import com.sun.faces.test.junit.JsfTest;
+import com.sun.faces.test.junit.JsfTestRunner;
+import com.sun.faces.test.junit.JsfVersion;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.Ignore;
+import org.junit.runner.RunWith;
 
+@RunWith(JsfTestRunner.class)
 public class Issue2719IT {
 
     private String webUrl;
@@ -65,6 +67,7 @@ public class Issue2719IT {
         webClient.closeAllWindows();
     }
 
+    @JsfTest(JsfVersion.JSF_2_1_22)
     @Test
     public void testPreRenderViewListenerRegisteredOnlyOnce() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "faces/eventPreRenderView.xhtml");
