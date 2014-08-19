d2dcae079d530346a1d600d445e168397559c58c
mriem <mriem@java.net>
2014-08-19 14:52:24 +0000

    Fixes https://java.net/jira/browse/JAVASERVERFACES-3371, Implement JAVASERVERFACES_SPEC_PUBLIC-1268
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@13543 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/main/java/javax/faces/view/ViewScoped.java
+++ jsf-api/src/main/java/javax/faces/view/ViewScoped.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2014 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -115,7 +115,7 @@ import javax.enterprise.context.NormalScope;
 
  * @since 2.2
  */
-@NormalScope
+@NormalScope(passivating = true)
 @Inherited
 @Documented
 @Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD})
