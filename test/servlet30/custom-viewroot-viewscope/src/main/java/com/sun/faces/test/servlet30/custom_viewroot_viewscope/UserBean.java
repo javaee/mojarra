e79be0e9228668a34f39c6496bafba1e714d1d74
edburns <edburns@java.net>
2014-08-21 18:33:38 +0000

    http://java.net/jira/browse/JAVASERVERFACES-3375
    SECTION: Modified Files
    ----------------------------
    M       test/servlet30/pom.xml
    A       test/servlet30/custom-viewroot-viewscope
    A       test/servlet30/custom-viewroot-viewscope/src
    A       test/servlet30/custom-viewroot-viewscope/src/test
    A       test/servlet30/custom-viewroot-viewscope/src/test/java
    A       test/servlet30/custom-viewroot-viewscope/src/test/java/com
    A       test/servlet30/custom-viewroot-viewscope/src/test/java/com/sun
    A       test/servlet30/custom-viewroot-viewscope/src/test/java/com/sun/faces
    A       test/servlet30/custom-viewroot-viewscope/src/test/java/com/sun/faces/test
    A       test/servlet30/custom-viewroot-viewscope/src/test/java/com/sun/faces/test/agnostic
    A       test/servlet30/custom-viewroot-viewscope/src/test/java/com/sun/faces/test/agnostic/ajax_namespace
    A       test/servlet30/custom-viewroot-viewscope/src/test/java/com/sun/faces/test/agnostic/ajax_namespace/Issue3375IT.java
    A       test/servlet30/custom-viewroot-viewscope/src/main
    A       test/servlet30/custom-viewroot-viewscope/src/main/java
    A       test/servlet30/custom-viewroot-viewscope/src/main/java/com
    A       test/servlet30/custom-viewroot-viewscope/src/main/java/com/sun
    A       test/servlet30/custom-viewroot-viewscope/src/main/java/com/sun/faces
    A       test/servlet30/custom-viewroot-viewscope/src/main/java/com/sun/faces/test
    A       test/servlet30/custom-viewroot-viewscope/src/main/java/com/sun/faces/test/servlet30
    A       test/servlet30/custom-viewroot-viewscope/src/main/java/com/sun/faces/test/servlet30/custom_viewroot_viewscope
    A       test/servlet30/custom-viewroot-viewscope/src/main/java/com/sun/faces/test/servlet30/custom_viewroot_viewscope/NamingContainerViewRoot.java
    A       test/servlet30/custom-viewroot-viewscope/src/main/java/com/sun/faces/test/servlet30/custom_viewroot_viewscope/ApplicationFactoryImpl.java
    A       test/servlet30/custom-viewroot-viewscope/src/main/java/com/sun/faces/test/servlet30/custom_viewroot_viewscope/UserBean.java
    A       test/servlet30/custom-viewroot-viewscope/src/main/java/com/sun/faces/test/servlet30/custom_viewroot_viewscope/ApplicationImpl.java
    A       test/servlet30/custom-viewroot-viewscope/src/main/resources
    A       test/servlet30/custom-viewroot-viewscope/src/main/webapp
    A       test/servlet30/custom-viewroot-viewscope/src/main/webapp/index.xhtml
    A       test/servlet30/custom-viewroot-viewscope/src/main/webapp/WEB-INF
    A       test/servlet30/custom-viewroot-viewscope/src/main/webapp/WEB-INF/web.xml
    A       test/servlet30/custom-viewroot-viewscope/src/main/webapp/WEB-INF/glassfish-web.xml
    A       test/servlet30/custom-viewroot-viewscope/src/main/webapp/WEB-INF/faces-config.xml
    A       test/servlet30/custom-viewroot-viewscope/pom.xml
    A       test/servlet30/custom-viewroot-viewscope/nbactions.xml
    
    - New testcase
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/2.2.8@13551 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ test/servlet30/custom-viewroot-viewscope/src/main/java/com/sun/faces/test/servlet30/custom_viewroot_viewscope/UserBean.java
@@ -0,0 +1,91 @@
+package com.sun.faces.test.servlet30.custom_viewroot_viewscope;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+
+public class UserBean implements Serializable {
+    private static final long serialVersionUID = 8969834712163408855L;
+    
+    protected String firstName = "Duke";
+    protected String lastName = "Java";
+    protected Date dob;
+    protected String sex = "Unknown";
+    protected String email;
+    protected String serviceLevel = "medium";
+    
+    public UserBean() {}
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getServiceLevel() {
+        return serviceLevel;
+    }
+
+    public void setServiceLevel(String serviceLevel) {
+        this.serviceLevel = serviceLevel;
+    }
+    
+    public void validateEmail(FacesContext context, UIComponent toValidate,
+            Object value) throws ValidatorException {
+        String emailStr = (String) value;
+        if (-1 == emailStr.indexOf("@")) {
+            FacesMessage message = new FacesMessage("Invalid email address");
+            throw new ValidatorException(message);
+        }
+    }
+
+    public String addConfirmedUser() {
+        // This method would call a database or other service and add the 
+        // confirmed user information.
+        // For now, we just place an informative message in request scope
+        FacesMessage doneMessage = 
+                new FacesMessage("Successfully added new user");
+        FacesContext.getCurrentInstance().addMessage(null, doneMessage);
+        return "done";
+    }
+}
+
