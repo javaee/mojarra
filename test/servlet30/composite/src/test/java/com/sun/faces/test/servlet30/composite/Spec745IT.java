31863401b81ea0a61cd881443a3b49acafeab71d
mriem <mriem@java.net>
2014-08-27 17:15:37 +0000

    Fixes https://java.net/jira/browse/JAVASERVERFACES-3392, Backport JAVASERVERFACES-3391 to 2.2.x branch
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_2X_ROLLING@13598 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/servlet30/composite/src/test/java/com/sun/faces/test/servlet30/composite/Spec745IT.java
+++ test/servlet30/composite/src/test/java/com/sun/faces/test/servlet30/composite/Spec745IT.java
@@ -45,7 +45,6 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.After;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class Spec745IT {
@@ -67,126 +66,138 @@ public class Spec745IT {
     @Test
     public void testType1() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "faces/attribute/attributeType.xhtml?test=untypedXunset");
-        assertTrue(page.asXml().contains("type of @untypedXunset: Object"));       
+        assertTrue(page.asXml().contains("type of @untypedXunset: Object"));
         HtmlElement button = (HtmlElement) page.getElementById("form:submit");
         page = button.click();
-        assertTrue(page.asXml().contains("type of @untypedXunset: Object"));       
+        assertTrue(page.asXml().contains("type of @untypedXunset: Object"));
     }
 
     @Test
     public void testType2() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "faces/attribute/attributeType.xhtml?test=untypedXsetByApi");
-        assertTrue(page.asXml().contains("type of @untypedXsetByApi: Object"));       
+        assertTrue(page.asXml().contains("type of @untypedXsetByApi: Object"));
         HtmlElement button = (HtmlElement) page.getElementById("form:submit");
         page = button.click();
-        assertTrue(page.asXml().contains("type of @untypedXsetByApi: Object"));       
+        assertTrue(page.asXml().contains("type of @untypedXsetByApi: Object"));
     }
 
     @Test
     public void testType3() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "faces/attribute/attributeType.xhtml?test=untypedXliteral");
-        assertTrue(page.asXml().contains("type of @untypedXliteral: Object"));       
+        assertTrue(page.asXml().contains("type of @untypedXliteral: Object"));
         HtmlElement button = (HtmlElement) page.getElementById("form:submit");
         page = button.click();
-        assertTrue(page.asXml().contains("type of @untypedXliteral: Object"));       
+        assertTrue(page.asXml().contains("type of @untypedXliteral: Object"));
     }
 
     @Test
     public void testType4() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "faces/attribute/attributeType.xhtml?test=untypedXwideEL");
-        assertTrue(page.asXml().contains("type of @untypedXwideEL: Animal"));       
+        assertTrue(page.asXml().contains("type of @untypedXwideEL: Animal"));
         HtmlElement button = (HtmlElement) page.getElementById("form:submit");
         page = button.click();
-        assertTrue(page.asXml().contains("type of @untypedXwideEL: Animal"));       
+        assertTrue(page.asXml().contains("type of @untypedXwideEL: Animal"));
     }
 
     @Test
     public void testType5() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "faces/attribute/attributeType.xhtml?test=untypedXmediumEL");
-        assertTrue(page.asXml().contains("type of @untypedXmediumEL: Dog"));       
+        assertTrue(page.asXml().contains("type of @untypedXmediumEL: Dog"));
         HtmlElement button = (HtmlElement) page.getElementById("form:submit");
         page = button.click();
-        assertTrue(page.asXml().contains("type of @untypedXmediumEL: Dog"));       
+        assertTrue(page.asXml().contains("type of @untypedXmediumEL: Dog"));
     }
 
     @Test
     public void testType6() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "faces/attribute/attributeType.xhtml?test=untypedXnarrowEL");
-        assertTrue(page.asXml().contains("type of @untypedXnarrowEL: Wienerdoodle"));       
+        assertTrue(page.asXml().contains("type of @untypedXnarrowEL: Wienerdoodle"));
         HtmlElement button = (HtmlElement) page.getElementById("form:submit");
         page = button.click();
-        assertTrue(page.asXml().contains("type of @untypedXnarrowEL: Wienerdoodle"));       
+        assertTrue(page.asXml().contains("type of @untypedXnarrowEL: Wienerdoodle"));
     }
 
     @Test
     public void testType7() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "faces/attribute/attributeType.xhtml?test=untypedXnullEL");
-        assertTrue(page.asXml().contains("type of @untypedXnullEL: Dog"));       
+        assertTrue(page.asXml().contains("type of @untypedXnullEL: Dog"));
         HtmlElement button = (HtmlElement) page.getElementById("form:submit");
         page = button.click();
-        assertTrue(page.asXml().contains("type of @untypedXnullEL: Dog"));       
+        assertTrue(page.asXml().contains("type of @untypedXnullEL: Dog"));
     }
 
     @Test
     public void testType8() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "faces/attribute/attributeType.xhtml?test=typedXunset");
-        assertTrue(page.asXml().contains("type of @typedXunset: Dog"));       
-        HtmlElement button = (HtmlElement) page.getElementById("form:submit");
-        page = button.click();
-        assertTrue(page.asXml().contains("type of @typedXunset: Dog"));       
+        if (!(page.asXml().contains("Partial State Saving: false")
+                && page.asXml().contains("State Saving Method: client"))) {
+            assertTrue(page.asXml().contains("type of @typedXunset: Dog"));
+            HtmlElement button = (HtmlElement) page.getElementById("form:submit");
+            page = button.click();
+            assertTrue(page.asXml().contains("type of @typedXunset: Dog"));
+        }
     }
 
     @Test
     public void testType9() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "faces/attribute/attributeType.xhtml?test=typedXliteral");
-        assertTrue(page.asXml().contains("type of @typedXliteral: Integer"));       
-        HtmlElement button = (HtmlElement) page.getElementById("form:submit");
-        page = button.click();
-        assertTrue(page.asXml().contains("type of @typedXliteral: Integer"));       
+        if (!(page.asXml().contains("Partial State Saving: false")
+                && page.asXml().contains("State Saving Method: client"))) {
+            assertTrue(page.asXml().contains("type of @typedXliteral: Integer"));
+            HtmlElement button = (HtmlElement) page.getElementById("form:submit");
+            page = button.click();
+            assertTrue(page.asXml().contains("type of @typedXliteral: Integer"));
+        }
     }
 
     @Test
     public void testType10() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "faces/attribute/attributeType.xhtml?test=typedXwideEL");
-        assertTrue(page.asXml().contains("type of @typedXwideEL: Dog"));       
-        HtmlElement button = (HtmlElement) page.getElementById("form:submit");
-        page = button.click();
-        assertTrue(page.asXml().contains("type of @typedXwideEL: Dog"));       
+        if (!(page.asXml().contains("Partial State Saving: false")
+                && page.asXml().contains("State Saving Method: client"))) {
+            assertTrue(page.asXml().contains("type of @typedXwideEL: Dog"));
+            HtmlElement button = (HtmlElement) page.getElementById("form:submit");
+            page = button.click();
+            assertTrue(page.asXml().contains("type of @typedXwideEL: Dog"));
+        }
     }
 
     @Test
     public void testType11() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "faces/attribute/attributeType.xhtml?test=typedXmediumEL");
-        assertTrue(page.asXml().contains("type of @typedXmediumEL: Dog"));       
+        assertTrue(page.asXml().contains("type of @typedXmediumEL: Dog"));
         HtmlElement button = (HtmlElement) page.getElementById("form:submit");
         page = button.click();
-        assertTrue(page.asXml().contains("type of @typedXmediumEL: Dog"));       
+        assertTrue(page.asXml().contains("type of @typedXmediumEL: Dog"));
     }
 
     @Test
     public void testType12() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "faces/attribute/attributeType.xhtml?test=typedXnarrowEL");
-        assertTrue(page.asXml().contains("type of @typedXnarrowEL: Wienerdoodle"));       
+        assertTrue(page.asXml().contains("type of @typedXnarrowEL: Wienerdoodle"));
         HtmlElement button = (HtmlElement) page.getElementById("form:submit");
         page = button.click();
-        assertTrue(page.asXml().contains("type of @typedXnarrowEL: Wienerdoodle"));       
+        assertTrue(page.asXml().contains("type of @typedXnarrowEL: Wienerdoodle"));
     }
 
     @Test
     public void testType13() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "faces/attribute/attributeType.xhtml?test=typedXnullEL");
-        assertTrue(page.asXml().contains("type of @typedXnullEL: Dog"));       
+        assertTrue(page.asXml().contains("type of @typedXnullEL: Dog"));
         HtmlElement button = (HtmlElement) page.getElementById("form:submit");
         page = button.click();
-        assertTrue(page.asXml().contains("type of @typedXnullEL: Dog"));       
+        assertTrue(page.asXml().contains("type of @typedXnullEL: Dog"));
     }
 
     @Test
     public void testType14() throws Exception {
         HtmlPage page = webClient.getPage(webUrl + "faces/attribute/attributeType.xhtml?test=typedXsetByApi");
-        assertTrue(page.asXml().contains("type of @typedXsetByApi: Dog"));       
-        HtmlElement button = (HtmlElement) page.getElementById("form:submit");
-        page = button.click();
-        assertTrue(page.asXml().contains("type of @typedXsetByApi: Dog"));       
+        if (!(page.asXml().contains("Partial State Saving: false")
+                && page.asXml().contains("State Saving Method: client"))) {
+            assertTrue(page.asXml().contains("type of @typedXsetByApi: Dog"));
+            HtmlElement button = (HtmlElement) page.getElementById("form:submit");
+            page = button.click();
+            assertTrue(page.asXml().contains("type of @typedXsetByApi: Dog"));
+        }
     }
 }
