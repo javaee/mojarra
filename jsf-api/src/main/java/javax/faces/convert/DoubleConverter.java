faddd74d6522133011155eb816b5af35a4c8bc3c
mriem <mriem@java.net>
2014-10-20 19:50:49 +0000

    Fix Javadoc issues in DoubleConverter, no spec changes
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@13794 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/main/java/javax/faces/convert/DoubleConverter.java
+++ jsf-api/src/main/java/javax/faces/convert/DoubleConverter.java
@@ -70,7 +70,7 @@ public class DoubleConverter implements Converter {
      * <li><code>{1}</code> replaced by an example value.</li>
      * <li><code>{2}</code> replaced by a <code>String</code> whose value
      * is the label of the input component that produced this message.</li>
-     * </ul></p>
+     * </ul>
      */
     public static final String DOUBLE_ID =
          "javax.faces.converter.DoubleConverter.DOUBLE";
@@ -84,7 +84,7 @@ public class DoubleConverter implements Converter {
      * <li><code>{0}</code> relaced by the unconverted value.</li>
      * <li><code>{1}</code> replaced by a <code>String</code> whose value
      * is the label of the input component that produced this message.</li>
-     * </ul></p>
+     * </ul>
      */
     public static final String STRING_ID =
          "javax.faces.converter.STRING";
