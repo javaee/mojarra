e934f8bb1a91622e78847363ab648d399c8abcd1
mriem <mriem@java.net>
2014-10-20 21:52:40 +0000

    Fix Javadoc issues in PreValidateEvent, no spec changes
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@13831 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/main/java/javax/faces/event/PreValidateEvent.java
+++ jsf-api/src/main/java/javax/faces/event/PreValidateEvent.java
@@ -73,7 +73,7 @@ public class PreValidateEvent extends ComponentSystemEvent {
      * @param component the <code>UIComponent</code> that is about to be
      * validated.
 
-     * @throws <code>IllegalArgumentException</code> if the argument is <code>null</code>.
+     * @throws IllegalArgumentException if the argument is <code>null</code>.
      */
     public PreValidateEvent(UIComponent component) {
         super(component);
