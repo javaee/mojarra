9b75f6ef0ed61632640c1cae742ad130522cf3ff
mriem <mriem@java.net>
2014-10-20 19:33:07 +0000

    Fix Javadoc issues in ByteConverter, no spec changes
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@13790 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/main/java/javax/faces/convert/ByteConverter.java
+++ jsf-api/src/main/java/javax/faces/convert/ByteConverter.java
@@ -70,7 +70,7 @@ public class ByteConverter implements Converter {
      * <li><code>{1}</code> replaced by an example value.</li>
      * <li><code>{2}</code> replaced by a <code>String</code> whose value
      * is the label of the input component that produced this message.</li>
-     * </ul></p>
+     * </ul>
      */
     public static final String BYTE_ID =
          "javax.faces.converter.ByteConverter.BYTE";
@@ -84,7 +84,7 @@ public class ByteConverter implements Converter {
      * <li><code>{0}</code> relaced by the unconverted value.</li>
      * <li><code>{1}</code> replaced by a <code>String</code> whose value
      * is the label of the input component that produced this message.</li>
-     * </ul></p>
+     * </ul>
      */
     public static final String STRING_ID =
          "javax.faces.converter.STRING";
