cc2304e0b757ca4beaaf47e9eb4e2152127d3f6a
mriem <mriem@java.net>
2014-10-20 19:27:14 +0000

    Fix Javadoc issues in BigIntegerConverter, no spec changes
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@13788 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/main/java/javax/faces/convert/BigIntegerConverter.java
+++ jsf-api/src/main/java/javax/faces/convert/BigIntegerConverter.java
@@ -71,7 +71,7 @@ public class BigIntegerConverter implements Converter {
      * <li><code>{1}</code> replaced by an example value.</li>
      * <li><code>{2}</code> replaced by a <code>String</code> whose value
      * is the label of the input component that produced this message.</li>
-     * </ul></p>
+     * </ul>
      */
     public static final String BIGINTEGER_ID =
          "javax.faces.converter.BigIntegerConverter.BIGINTEGER";
@@ -85,7 +85,7 @@ public class BigIntegerConverter implements Converter {
      * <li><code>{0}</code> relaced by the unconverted value.</li>
      * <li><code>{1}</code> replaced by a <code>String</code> whose value
      * is the label of the input component that produced this message.</li>
-     * </ul></p>
+     * </ul>
      */
     public static final String STRING_ID =
          "javax.faces.converter.STRING";
