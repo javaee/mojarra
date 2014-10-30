49815301d64d18d6791c501922292dd07b8d3554
mriem <mriem@java.net>
2014-10-30 17:59:28 +0000

    Remove unused import
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_2X_ROLLING@13945 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/application/view/FaceletPartialStateManagementStrategy.java
+++ jsf-ri/src/main/java/com/sun/faces/application/view/FaceletPartialStateManagementStrategy.java
@@ -49,7 +49,6 @@ import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
