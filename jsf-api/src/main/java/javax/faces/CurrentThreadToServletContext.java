1ee16525e070388886fd8deec285cb91059fb7a9
edburns <edburns@java.net>
2014-11-26 00:11:23 +0000

    http://java.net/jira/browse/JAVASERVERFACES-3542
    SECTION: Modified Files
    ----------------------------
    M       jsf-api/src/main/java/javax/faces/CurrentThreadToServletContext.java
    
    - Take a different approach.  Simplify the search code.
    
      Tested with new harness in two permutations:
    
    ++++++++++++++++++++SECTION: -Dwebapp.projectStage=Development -Dwebapp.stateSavingMethod=server -Dwebapp.partialStateSaving=false
    ++++++++++++++++++++SECTION: -Dwebapp.projectStage=Development -Dwebapp.stateSavingMethod=client -Dwebapp.partialStateSaving=true
    
      No errors or failures.
    
      Also ran through tests from Bug 20064382 on multiple tenants.
    
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@13989 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/main/java/javax/faces/CurrentThreadToServletContext.java
+++ jsf-api/src/main/java/javax/faces/CurrentThreadToServletContext.java
@@ -121,9 +121,9 @@ final class CurrentThreadToServletContext {
                         break;
                     }
                 }
-                // We must create a new FactoryFinderInstance if there was no match
-                // at all found in the applicationMap, or a match was found
-                // and the match is safe to use in this web app
+                // We must create a new FactoryFinderInstance if there was no matchingKey
+                // at all found in the applicationMap, or a matchingKey was found
+                // and the matchingKey is safe to use in this web app
                 createNewFactoryManagerInstance = foundNoMatchInApplicationMap || (null == result && classLoadersMatchButContextsDoNotMatch);
             } else {
                 createNewFactoryManagerInstance = true;
@@ -240,7 +240,7 @@ final class CurrentThreadToServletContext {
             // factoryMap and see if any of them has a classloader
             // equal to our argument cl.
             Set<FactoryManagerCacheKey> keys = factoryMap.keySet();
-            FactoryManagerCacheKey match = null;
+            FactoryManagerCacheKey matchingKey = null;
             
             if (keys.isEmpty()) {
                 this.cl = cl;
@@ -250,36 +250,43 @@ final class CurrentThreadToServletContext {
                 boolean found = false;
                 // For each entry in the factoryMap's keySet...
                 for (FactoryManagerCacheKey currentKey : keys) {
-                    ClassLoader curCL = cl;
-                    // For each ClassLoader in the hierarchy starting 
-                    // with the argument ClassLoader...
-                    while (!found && null != curCL) {
-                        // if the ClassLoader at this level in the hierarchy
-                        // is equal to the argument ClassLoader, consider it a match.
-                        found = curCL.equals(currentKey.cl);
-                        // If it's not a match, try the parent in the ClassLoader
-                        // hierarchy.
-                        if (!found) {
-                            curCL = curCL.getParent();
-                        }
-                    }
-                    // Keep searching for another match to detect an unsupported
-                    // deployment scenario.
-                    if (found) {
-                        if (null != currentKey && null != match) {
+                    assert(null != currentKey.cl);
+                    ClassLoader matchingCL = findMatchConsideringParentClassLoader(cl, currentKey.cl);
+                    // If there is a match...
+                    if (null != matchingCL) {
+                        // If the match was found on a previous iteration...
+                        if (null != matchingKey) {
                             LOGGER.log(Level.WARNING, "Multiple JSF Applications found on same ClassLoader.  Unable to safely determine which FactoryManager instance to use. Defaulting to first match.");
                             break;
                         }
-                        match = currentKey;
-                        this.cl = curCL;
+                        matchingKey = currentKey;
+                        this.cl = matchingCL;
                     }
                 }
-                if (null != match) {
-                    this.marker = match.marker;
-                    this.context = match.context;
+                if (null != matchingKey) {
+                    this.marker = matchingKey.marker;
+                    this.context = matchingKey.context;
                 }
             }
             
+        }
+
+        private ClassLoader findMatchConsideringParentClassLoader(ClassLoader argumentCL, ClassLoader currentKeyCL) {
+            ClassLoader curCL = argumentCL;
+            // For each ClassLoader in the hierarchy starting 
+            // with the argument ClassLoader...
+            while (null != curCL) {
+                // if the ClassLoader at this level in the hierarchy
+                // is equal to the argument ClassLoader, consider it a matchingKey.
+                if (curCL.equals(currentKeyCL)) {
+                    return curCL;
+                } else {
+                    // If it's not a matchingKey, try the parent in the ClassLoader
+                    // hierarchy.
+                    curCL = curCL.getParent();
+                } 
+            }
+            return null;
         }
         
         private void initFromAppMap(ExternalContext extContext, ClassLoader cl) {
