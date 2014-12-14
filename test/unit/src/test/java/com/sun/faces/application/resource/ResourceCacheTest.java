492a1988f28337f522ba6e9148e44c4a04cafc93
cappi <cappi@java.net>
2014-12-14 15:23:49 +0000

    https://java.net/jira/browse/JAVASERVERFACES-3553
    SECTION: Modified Files
    ----------------------------
    M       jsf-ri/src/main/java/com/sun/faces/application/resource/ResourceCache.java
    
    Added package private constructor with check period, to make this class unit test friendly.
    
    SECTION: Added Files
    ----------------------------
    A       test/unit/src/test/java/com/sun/faces/application/resource/ResourceCacheTest.java
    
    This test ensures, the memory leak is gone.
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@14039 761efcf2-d34d-6b61-9145-99dcacc3edf1

---
+++ test/unit/src/test/java/com/sun/faces/application/resource/ResourceCacheTest.java
@@ -0,0 +1,26 @@
+package com.sun.faces.application.resource;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ResourceCacheTest {
+
+    @Test
+    public void noMemoryLeakWithContracts() {
+        ResourceCache cache = new ResourceCache(-1L);
+        ResourceInfo resourceInfo = new ResourceInfo(new ContractInfo("foo"), "bar.gif", null, null);
+        List<String> contracts = new ArrayList<>(Arrays.asList("foo", "baz"));
+        cache.add(resourceInfo, contracts);
+        // now we clear the contracts list, which was used to create the cache entry.
+        contracts.clear();
+        ResourceInfo cachedResourceInfo = cache.get("bar.gif", null, null, Arrays.asList("foo", "baz"));
+        assertThat(cachedResourceInfo, is(resourceInfo));
+    }
+
+}
