85ca12d8d0419b02515dd3b6404633e086e2c02a
mriem <mriem@java.net>
2014-12-23 14:54:28 +0000

    Fixes https://java.net/jira/browse/JAVASERVERFACES-3635, Migrate test/javaee7/cdi/noBeansXml to test/javaee7/cdiNoBeansXml
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@14113 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/javaee7/cdiNoBeansXml/src/test/java/com/sun/faces/test/javaee7/cdinobeansxml/NoBeansXmlIT.java
+++ test/javaee7/cdiNoBeansXml/src/test/java/com/sun/faces/test/javaee7/cdinobeansxml/NoBeansXmlIT.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
- * 
+ *
+ * Copyright (c) 1997-2014 Oracle and/or its affiliates. All rights reserved.
+ *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
  * and Distribution License("CDDL") (collectively, the "License").  You
@@ -11,20 +11,20 @@
  * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
  * or packager/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
- * 
+ *
  * When distributing the software, include this License Header Notice in each
  * file and include the License file at packager/legal/LICENSE.txt.
- * 
+ *
  * GPL Classpath Exception:
  * Oracle designates this particular file as subject to the "Classpath"
  * exception as provided by Oracle in the GPL Version 2 section of the License
  * file that accompanied this code.
- * 
+ *
  * Modifications:
  * If applicable, add the following below the License Header, with the fields
  * enclosed by brackets [] replaced by your own identifying information:
  * "Portions Copyright [year] [name of copyright owner]"
- * 
+ *
  * Contributor(s):
  * If you wish your version of this file to be governed by only the CDDL or
  * only the GPL Version 2, indicate your decision by adding "[Contributor]
@@ -36,28 +36,55 @@
  * and therefore, elected the GPL Version 2 license, then the option applies
  * only if the new code is made subject to such option by the copyright
  * holder.
-
  */
-package com.sun.faces.test.javaee7.cdi.noBeansXml;
+package com.sun.faces.test.javaee7.cdinobeansxml;
 
-import java.io.Serializable;
-import javax.faces.flow.FlowScoped;
-import javax.inject.Named;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+import static com.sun.faces.test.junit.JsfServerExclude.WEBLOGIC_12_1_4;
+import static com.sun.faces.test.junit.JsfServerExclude.WEBLOGIC_12_2_1;
+import com.sun.faces.test.junit.JsfTest;
+import com.sun.faces.test.junit.JsfTestRunner;
+import static com.sun.faces.test.junit.JsfVersion.JSF_2_2_0;
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-@Named
-@FlowScoped("01_simplest")
-public class SimplestFlowBean implements Serializable {
-    
-    private static final long serialVersionUID = -7991411387185942791L;
-    
-    private String name;
+@RunWith(JsfTestRunner.class)
+public class NoBeansXmlIT {
 
-    public String getName() {
-        return name;
+    private String webUrl;
+    private WebClient webClient;
+
+    @Before
+    public void setUp() {
+        webUrl = System.getProperty("integration.url");
+        webClient = new WebClient();
     }
 
-    public void setName(String name) {
-        this.name = name;
+    @After
+    public void tearDown() {
+        webClient.closeAllWindows();
     }
-    
+
+    @JsfTest(value = JSF_2_2_0,
+            excludes = {WEBLOGIC_12_2_1, WEBLOGIC_12_1_4})
+    @Test
+    public void testFlowWithNoBeansXml() throws Exception {
+        HtmlPage page = webClient.getPage(webUrl);
+        HtmlSubmitInput button = (HtmlSubmitInput) page.getElementById("enterFlow");
+        page = button.click();
+        HtmlTextInput input = (HtmlTextInput) page.getElementById("input");
+        String message = "" + System.currentTimeMillis();
+        input.setValueAttribute(message);
+        button = (HtmlSubmitInput) page.getElementById("a");
+        page = button.click();
+        HtmlElement e = (HtmlElement) page.getElementById("value");
+        assertEquals(e.asText(), message);
+    }    
 }
