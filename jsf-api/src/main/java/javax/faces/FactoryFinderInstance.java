443d68cb87d7b301bf63a623ca684d5a22274440
mriem <mriem@java.net>
2014-12-30 21:07:06 +0000

    Fixes https://java.net/jira/browse/JAVASERVERFACES-3663, Merge TypedCollections into Util class
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@14150 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-api/src/main/java/javax/faces/FactoryFinderInstance.java
+++ jsf-api/src/main/java/javax/faces/FactoryFinderInstance.java
@@ -431,7 +431,11 @@ final class FactoryFinderInstance {
         lock.writeLock().lock();
         try {
             if (result instanceof List) {
-                TypedCollections.dynamicallyCastList((List) result, String.class).add(0, implementation);
+                /*
+                 * REVIEW removed usage of TypedCollections as implementation var
+                 *        is already to be a string for sure.
+                 */
+                ((List) result).add(0, implementation);
             }
         } finally {
             lock.writeLock().unlock();
