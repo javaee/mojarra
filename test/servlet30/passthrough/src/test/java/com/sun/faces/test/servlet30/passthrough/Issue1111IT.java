7b512be9b2d0cce6c574dba8df5730defb572afc
mriem <mriem@java.net>
2014-12-31 19:01:59 +0000

    Bump the JavaScript timeout
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@14157 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/servlet30/passthrough/src/test/java/com/sun/faces/test/servlet30/passthrough/Issue1111IT.java
+++ test/servlet30/passthrough/src/test/java/com/sun/faces/test/servlet30/passthrough/Issue1111IT.java
@@ -344,6 +344,7 @@ public class Issue1111IT {
         HtmlTextInput email = (HtmlTextInput)page.getElementById("email");
         email.focus();
 
+        webClient.setJavaScriptTimeout(60000);
         waiter.waitForSuccess();
         
         System.out.println(page.asXml());
