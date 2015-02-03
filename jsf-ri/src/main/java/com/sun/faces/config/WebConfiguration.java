5cd223d5fd8ac2827d4c6b7ac655ed801a62587c
mriem <mriem@java.net>
2015-02-03 19:29:35 +0000

    Fixes https://java.net/jira/browse/JAVASERVERFACES-3750, Default to serialize server state for backwards compatibility
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@14312 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- jsf-ri/src/main/java/com/sun/faces/config/WebConfiguration.java
+++ jsf-ri/src/main/java/com/sun/faces/config/WebConfiguration.java
@@ -1297,7 +1297,7 @@ public class WebConfiguration {
         ),
         SerializeServerState(
               StateManager.SERIALIZE_SERVER_STATE_PARAM_NAME,
-              false
+              true
         ),
         SerializeServerStateDeprecated(
               "com.sun.faces.serializeServerState",
