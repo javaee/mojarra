6ea89955450f196c26958246abff4a4f2af3e521
mriem <mriem@java.net>
2015-02-05 17:47:41 +0000

    Migrating systest to correct package
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@14336 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/servlet30/systest/src/test/java/com/sun/faces/test/servlet30/systest/ViewXmlITCase.java
+++ test/servlet30/systest/src/test/java/com/sun/faces/test/servlet30/systest/ViewXmlITCase.java
@@ -37,25 +37,22 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
+package com.sun.faces.test.servlet30.systest;
 
-package com.sun.faces.systest.view.xml;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 import com.sun.faces.htmlunit.HtmlUnitFacesITCase;
 import junit.framework.Test;
+import static junit.framework.TestCase.assertTrue;
 import junit.framework.TestSuite;
 
-
 /**
- * <p>Test Case for JSP Interoperability.</p>
+ * <p>
+ * Test Case for JSP Interoperability.</p>
  */
-
 public class ViewXmlITCase extends HtmlUnitFacesITCase {
 
-
     // ------------------------------------------------------------ Constructors
-
-
     /**
      * Construct a new instance of this test case.
      *
@@ -65,20 +62,14 @@ public class ViewXmlITCase extends HtmlUnitFacesITCase {
         super(name);
     }
 
-
     // ------------------------------------------------------ Instance Variables
-
-
     // ---------------------------------------------------- Overall Test Methods
-
-
     /**
      * Set up instance variables required by this test case.
      */
     public void setUp() throws Exception {
         super.setUp();
     }
-
 
     /**
      * Return the tests included in this test suite.
@@ -87,7 +78,6 @@ public class ViewXmlITCase extends HtmlUnitFacesITCase {
         return (new TestSuite(ViewXmlITCase.class));
     }
 
-
     /**
      * Tear down instance variables required by this test case.
      */
@@ -95,10 +85,7 @@ public class ViewXmlITCase extends HtmlUnitFacesITCase {
         super.tearDown();
     }
 
-
     // ------------------------------------------------- Individual Test Methods
-
-
     public void testPrefixAndExtensionMapping() throws Exception {
         doTest("/view.xml/templateClientUsingXmlAndXhtml.faces", "foo", "bar");
         doTest("/faces/view.xml/templateClientUsingXmlAndXhtml.view.xml", "baz", "baba");
@@ -109,20 +96,18 @@ public class ViewXmlITCase extends HtmlUnitFacesITCase {
         String text = page.asText();
         assertTrue(text.matches("(?s).*Raw XML View\\s*hello\\s*reload.*"));
 
-
     }
 
     private void doTest(String path, String param1, String param2) throws Exception {
         HtmlPage page = getPage(path + "?param1=" + param1 + "&param2=" + param2);
         String text = page.asText();
-        assertTrue(text.matches("(?s).*Templating and XML views\\s*column1\\s*column2\\s*column3\\s*This is the header text declared in xhtmlTemplate.xhtml. The preceding columns are declared in header.view.xml.\\s*hello\\s*reload\\s*HTML table column 1\\s*HTML table column 2\\s*" + 
-                param1 + "\\s*" + param2 + ".*"));
+        assertTrue(text.matches("(?s).*Templating and XML views\\s*column1\\s*column2\\s*column3\\s*This is the header text declared in xhtmlTemplate.xhtml. The preceding columns are declared in header.view.xml.\\s*hello\\s*reload\\s*HTML table column 1\\s*HTML table column 2\\s*"
+                + param1 + "\\s*" + param2 + ".*"));
         HtmlSubmitInput button = (HtmlSubmitInput) page.getElementById("button");
         page = button.click();
         text = page.asText();
-        assertTrue(text.matches("(?s).*Templating and XML views\\s*column1\\s*column2\\s*column3\\s*This is the header text declared in xhtmlTemplate.xhtml. The preceding columns are declared in header.view.xml.\\s*hello\\s*reload\\s*HTML table column 1\\s*HTML table column 2\\s*" +
-                param1 + "\\s*" + param2 + ".*"));
-        
-    }
+        assertTrue(text.matches("(?s).*Templating and XML views\\s*column1\\s*column2\\s*column3\\s*This is the header text declared in xhtmlTemplate.xhtml. The preceding columns are declared in header.view.xml.\\s*hello\\s*reload\\s*HTML table column 1\\s*HTML table column 2\\s*"
+                + param1 + "\\s*" + param2 + ".*"));
 
+    }
 }
