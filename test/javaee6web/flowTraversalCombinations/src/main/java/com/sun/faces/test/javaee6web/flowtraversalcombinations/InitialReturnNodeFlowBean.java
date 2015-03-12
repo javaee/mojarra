01854f023495fa73269eb4dbd5c8d2d7c6192af0
ren.zhijun.oracle <ren.zhijun.oracle@java.net>
2015-03-12 04:41:28 +0000

    JAVASERVERFACES-3813: [PORT 2.2.8] Navigation from method-call-node to method-call-node does not work
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/2.2.8@14493 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/javaee6web/flowTraversalCombinations/src/main/java/com/sun/faces/test/javaee6web/flowtraversalcombinations/InitialReturnNodeFlowBean.java
+++ test/javaee6web/flowTraversalCombinations/src/main/java/com/sun/faces/test/javaee6web/flowtraversalcombinations/InitialReturnNodeFlowBean.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2015 Oracle and/or its affiliates. All rights reserved.
  * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -36,45 +36,24 @@
  * and therefore, elected the GPL Version 2 license, then the option applies
  * only if the new code is made subject to such option by the copyright
  * holder.
-
  */
-package com.sun.faces.test.webprofile.flow.basic_switch;
+package com.sun.faces.test.javaee6web.flowtraversalcombinations;
 
 import java.io.Serializable;
-import javax.enterprise.inject.Produces;
-import javax.faces.flow.Flow;
-import javax.faces.flow.builder.FlowBuilder;
-import javax.faces.flow.builder.FlowDefinition;
-import javax.faces.flow.builder.FlowBuilderParameter;
+import javax.faces.flow.FlowScoped;
+import javax.inject.Named;
 
+@Named
+@FlowScoped(value = "initial-return-node-flow")
+public class InitialReturnNodeFlowBean implements Serializable {
 
-public class FlowA implements Serializable {
+    private String parentNodeToBeReturned;
     
-    private static final long serialVersionUID = -7623501087369765218L;
-
-    public FlowA() {
+    public String getParentNodeToBeReturned() {
+        return parentNodeToBeReturned;
     }
     
-    @Produces @FlowDefinition
-    public Flow defineFlow(@FlowBuilderParameter FlowBuilder flowBuilder) {
-        String flowId = "flow-a";
-        flowBuilder.id("", flowId);
-        flowBuilder.returnNode("taskFlowReturn1").
-                fromOutcome("#{flow_a_Bean.returnValue}");
-        flowBuilder.switchNode("switchA").defaultOutcome("defaultPage").
-                switchCase().condition("#{flow_a_Bean.switchA_Case01}").fromOutcome("page01").
-                switchCase().condition("#{flow_a_Bean.switchA_Case02}").fromOutcome("page02").
-                switchCase().condition("#{flow_a_Bean.switchA_Case03}").fromOutcome("switchA_result");
-        flowBuilder.switchNode("switchB").defaultOutcome("defaultPage").
-                switchCase().condition("#{flow_a_Bean.switchB_Case01}").fromOutcome("page01").
-                switchCase().condition("#{flow_a_Bean.switchB_Case02}").fromOutcome("switchB_result").
-                switchCase().condition("#{flow_a_Bean.switchB_Case03}").fromOutcome("page03");
-        flowBuilder.switchNode("switchC").defaultOutcome("#{flow_a_Bean.defaultOutcome}").
-                switchCase().condition("#{flow_a_Bean.switchC_Case01}").fromOutcome("page01").
-                switchCase().condition("#{flow_a_Bean.switchC_Case02}").fromOutcome("page02").
-                switchCase().condition("#{flow_a_Bean.switchC_Case03}").fromOutcome("page03");
-        
-        return flowBuilder.getFlow();
+    public void setParentNodeToBeReturned(String node) {
+        parentNodeToBeReturned = node;
     }
-    
 }
