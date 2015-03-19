e3d441f4bbd6af8264c5d7f42d3aef0b617c808a
edburns <edburns@java.net>
2015-03-19 18:53:05 +0000

    http://java.net/jira/browse/JAVASERVERFACES-3842
      r14523 | edburns | 2015-03-19 13:46:26 -0400 (Thu, 19 Mar 2015) | 20 lines
    
    http://java.net/jira/browse/JAVASERVERFACES-3404 fileUploadMultiple
    
    
    SECTION: Modified Files
    ----------------------------
    M       jsf-api/doc/standard-html-renderkit.xml
    M       jsf-api/doc/standard-html-renderkit-base.xml
    
    - Put back the javax.faces.component.html.HtmlInputFile, but mark it
      ignored so it does not get code-generated.
    
    A       jsf-api/src/main/java/javax/faces/component/html/HtmlInputFile.java
    D       jsf-ri/src/main/java/com/sun/faces/component/html/HtmlInputFile.java
    
    - Move this from impl to API, where it should have been already.
    
    M       jsf-ri/src/main/java/com/sun/faces/renderkit/html_basic/TextRenderer.java
    
    - Update import.
    
    
    
    git-svn-id: https://svn.java.net/svn/mojarra~svn/branches/MOJARRA_2_2X_ROLLING@14526 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/web-profile/renderKit/basic/src/test/java/com/sun/faces/test/webprofile/renderKit/basic/Spec802IT.java
+++ test/web-profile/renderKit/basic/src/test/java/com/sun/faces/test/webprofile/renderKit/basic/Spec802IT.java
@@ -125,4 +125,43 @@ public class Spec802IT {
                     "File upload component requires a form with an enctype of multipart/form-data"));
         }
     }
+
+    @JsfTest(value = JsfVersion.JSF_2_2_0, excludes = {JsfServerExclude.WEBLOGIC_12_1_3})
+    @Test
+    public void testFileUploadMultipleTimes() throws Exception {
+        webClient = new WebClient();
+        HtmlPage page = webClient.getPage(webUrl + "faces/uploadMultipleTimes.xhtml");
+
+        String basedir = System.getProperty("basedir");
+        HtmlFileInput fileInput = (HtmlFileInput) page.getElementById("file");
+        fileInput.setValueAttribute(basedir + File.separator + "inputFileSuccess.txt");
+
+        HtmlSubmitInput button = (HtmlSubmitInput) page.getElementById("button");
+
+        page = button.click();
+
+        String pageText = page.getBody().asXml();
+        assertTrue(pageText.matches("(?s).*bytes\\s+sent\\s+=\\s+83.*"));
+        
+        fileInput = (HtmlFileInput) page.getElementById("file");
+        fileInput.setValueAttribute(basedir + File.separator + "inputFileSuccess2.txt");
+
+        button = (HtmlSubmitInput) page.getElementById("button");
+
+        page = button.click();
+
+        pageText = page.getBody().asXml();
+        assertTrue(pageText.matches("(?s).*bytes\\s+sent\\s+=\\s+107.*"));
+        
+        fileInput = (HtmlFileInput) page.getElementById("file");
+        fileInput.setValueAttribute(basedir + File.separator + "inputFileSuccess3.txt");
+
+        button = (HtmlSubmitInput) page.getElementById("button");
+
+        page = button.click();
+
+        pageText = page.getBody().asXml();
+        assertTrue(pageText.matches("(?s).*bytes\\s+sent\\s+=\\s+124.*"));
+        
+    }
 }
