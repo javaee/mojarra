8430040ca2af333ee46e13794700dd5b092d9de0
mriem <mriem@java.net>
2015-03-27 13:38:30 +0000

    Excluding on WLS 12.1.4 / 12.2.1
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@14556 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/servlet30/flash/src/test/java/com/sun/faces/test/servlet30/flash/FlashIT.java
+++ test/servlet30/flash/src/test/java/com/sun/faces/test/servlet30/flash/FlashIT.java
@@ -47,13 +47,16 @@ import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 import static com.sun.faces.test.junit.JsfServerExclude.WEBLOGIC_12_1_4;
 import static com.sun.faces.test.junit.JsfServerExclude.WEBLOGIC_12_2_1;
 import com.sun.faces.test.junit.JsfTest;
+import com.sun.faces.test.junit.JsfTestRunner;
 import static com.sun.faces.test.junit.JsfVersion.JSF_2_2_0_M02;
 import org.junit.After;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JsfTestRunner.class)
 public class FlashIT {
 
     private String webUrl;
