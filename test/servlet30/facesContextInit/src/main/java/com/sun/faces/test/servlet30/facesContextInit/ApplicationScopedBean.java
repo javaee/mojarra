96ade15fc973331ad84fb1cf743473845de9ed8f
mriem <mriem@java.net>
2015-08-18 14:39:08 +0000

    Fixes https://java.net/jira/browse/JAVASERVERFACES-4018, Migrate test/agnostic/context/initFacesContext to test/servlet30/facesContextInit
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@15019 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/servlet30/facesContextInit/src/main/java/com/sun/faces/test/servlet30/facesContextInit/ApplicationScopedBean.java
+++ test/servlet30/facesContextInit/src/main/java/com/sun/faces/test/servlet30/facesContextInit/ApplicationScopedBean.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
- *
+ * 
+ * Copyright (c) 1997-2015 Oracle and/or its affiliates. All rights reserved.
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
  * and Distribution License("CDDL") (collectively, the "License").  You
@@ -11,20 +11,20 @@
  * https://glassfish.java.net/public/CDDL+GPL_1_1.html
  * or packager/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
- *
+ * 
  * When distributing the software, include this License Header Notice in each
  * file and include the License file at packager/legal/LICENSE.txt.
- *
+ * 
  * GPL Classpath Exception:
  * Oracle designates this particular file as subject to the "Classpath"
  * exception as provided by Oracle in the GPL Version 2 section of the License
  * file that accompanied this code.
- *
+ * 
  * Modifications:
  * If applicable, add the following below the License Header, with the fields
  * enclosed by brackets [] replaced by your own identifying information:
  * "Portions Copyright [year] [name of copyright owner]"
- *
+ * 
  * Contributor(s):
  * If you wish your version of this file to be governed by only the CDDL or
  * only the GPL Version 2, indicate your decision by adding "[Contributor]
@@ -36,68 +36,56 @@
  * and therefore, elected the GPL Version 2 license, then the option applies
  * only if the new code is made subject to such option by the copyright
  * holder.
+
  */
-package com.sun.faces.test.agnostic.context;
+package com.sun.faces.test.servlet30.facesContextInit;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import static org.junit.Assert.assertTrue;
+import java.io.Serializable;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.context.Flash;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.PreDestroyApplicationEvent;
+import javax.faces.event.SystemEvent;
+import javax.faces.event.SystemEventListener;
 
-public class AutomatedTestIT {
-    /**
-     * Stores the web URL.
-     */
-    private String webUrl;
-    /**
-     * Stores the web client.
-     */
-    private WebClient webClient;
+@ManagedBean(eager = true)
+@ApplicationScoped
+public class ApplicationScopedBean implements Serializable {
 
-    /**
-     * Setup before testing.
-     * 
-     * @throws Exception when a serious error occurs.
-     */
-    @BeforeClass
-    public static void setUpClass() throws Exception {
+    private static final long serialVersionUID = -7637392264151341963L;
+
+    private String result = "FAILURE";
+
+    public ApplicationScopedBean() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        Flash startupFlash = context.getExternalContext().getFlash();
+        result = null != startupFlash ? "SUCCESS" : "FAILURE";
+
+        context.getApplication().subscribeToEvent(PreDestroyApplicationEvent.class, new SystemEventListener() {
+
+            @Override
+            public void processEvent(SystemEvent event) throws AbortProcessingException {
+                ApplicationScopedBean.this.testPredestroy();
+            }
+
+            @Override
+            public boolean isListenerForSource(Object source) {
+                return true;
+            }
+        });
+
     }
 
-    /**
-     * Cleanup after testing.
-     * 
-     * @throws Exception when a serious error occurs.
-     */
-    @AfterClass
-    public static void tearDownClass() throws Exception {
+    public String getResult() {
+        return result;
     }
 
-    /**
-     * Setup before testing.
-     */
-    @Before
-    public void setUp() {
-        webUrl = System.getProperty("integration.url");
-        webClient = new WebClient();
-    }
+    public void testPredestroy() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        Flash startupFlash = context.getExternalContext().getFlash();
+        result = null != startupFlash ? "SUCCESS" : "FAILURE";
 
-    /**
-     * Tear down after testing.
-     */
-    @After
-    public void tearDown() {
-        webClient.closeAllWindows();
-    }
-
-    @Test
-    public void testStartupTimeFlash() throws Exception {
-        HtmlPage page = webClient.getPage(webUrl);
-
-        assertTrue(page.getBody().asText().indexOf("SUCCESS") != -1);
-        
     }
 }
--- test/servlet30/facesContextInit/src/test/java/com/sun/faces/test/servlet30/facesContextInit/AutomatedTestIT.java
+++ test/servlet30/facesContextInit/src/test/java/com/sun/faces/test/servlet30/facesContextInit/AutomatedTestIT.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
- * 
+ *
+ * Copyright (c) 1997-2015 Oracle and/or its affiliates. All rights reserved.
+ *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
  * and Distribution License("CDDL") (collectively, the "License").  You
@@ -11,20 +11,20 @@
  * https://glassfish.java.net/public/CDDL+GPL_1_1.html
  * or packager/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
- * 
+ *
  * When distributing the software, include this License Header Notice in each
  * file and include the License file at packager/legal/LICENSE.txt.
- * 
+ *
  * GPL Classpath Exception:
  * Oracle designates this particular file as subject to the "Classpath"
  * exception as provided by Oracle in the GPL Version 2 section of the License
  * file that accompanied this code.
- * 
+ *
  * Modifications:
  * If applicable, add the following below the License Header, with the fields
  * enclosed by brackets [] replaced by your own identifying information:
  * "Portions Copyright [year] [name of copyright owner]"
- * 
+ *
  * Contributor(s):
  * If you wish your version of this file to be governed by only the CDDL or
  * only the GPL Version 2, indicate your decision by adding "[Contributor]
@@ -36,61 +36,41 @@
  * and therefore, elected the GPL Version 2 license, then the option applies
  * only if the new code is made subject to such option by the copyright
  * holder.
-
  */
-package com.sun.faces.test.agnostic.context;
+package com.sun.faces.test.servlet30.facesContextInit;
 
-import java.io.Serializable;
-import javax.annotation.PreDestroy;
-import javax.faces.bean.ApplicationScoped;
-import javax.faces.bean.ManagedBean;
-import javax.faces.context.FacesContext;
-import javax.faces.context.Flash;
-import javax.faces.event.AbortProcessingException;
-import javax.faces.event.PreDestroyApplicationEvent;
-import javax.faces.event.SystemEvent;
-import javax.faces.event.SystemEventListener;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import static org.junit.Assert.assertTrue;
 
-@ManagedBean(eager=true)
-@ApplicationScoped
-public class ApplicationScopedBean implements Serializable {
-    
-    private static final long serialVersionUID = -7637392264151341963L;
-    
-    private String result = "FAILURE";
-    
-    public ApplicationScopedBean() {
-        FacesContext context = FacesContext.getCurrentInstance();
-        Flash startupFlash = context.getExternalContext().getFlash();
-        result = null != startupFlash ? "SUCCESS" : "FAILURE";
-        
-        context.getApplication().subscribeToEvent(PreDestroyApplicationEvent.class, new SystemEventListener() {
+public class AutomatedTestIT {
 
-            @Override
-            public void processEvent(SystemEvent event) throws AbortProcessingException {
-                ApplicationScopedBean.this.testPredestroy();
-            }
+    private String webUrl;
+    private WebClient webClient;
 
-            @Override
-            public boolean isListenerForSource(Object source) {
-                return true;
-            }
-        });
-        
+    /**
+     * Setup before testing.
+     */
+    @Before
+    public void setUp() {
+        webUrl = System.getProperty("integration.url");
+        webClient = new WebClient();
     }
 
-    public String getResult() {
-        return result;
+    /**
+     * Tear down after testing.
+     */
+    @After
+    public void tearDown() {
+        webClient.closeAllWindows();
     }
 
-    
-    public void testPredestroy() {
-        FacesContext context = FacesContext.getCurrentInstance();
-        Flash startupFlash = context.getExternalContext().getFlash();
-        result = null != startupFlash ? "SUCCESS" : "FAILURE";
-        
+    @Test
+    public void testStartupTimeFlash() throws Exception {
+        HtmlPage page = webClient.getPage(webUrl);
+        assertTrue(page.getBody().asText().indexOf("SUCCESS") != -1);
     }
-    
-    
-    
 }
