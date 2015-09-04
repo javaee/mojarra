90107ed711ede5fd1d6745641971e3cdfbf9c65d
mriem <mriem@java.net>
2015-09-04 17:35:02 +0000

    Fixes https://java.net/jira/browse/JAVASERVERFACES-4053, Migrate test/agnostic/lifecycle/unserializableServerState_positive to test/servlet30/lifecycleServerStatePositive
    git-svn-id: https://svn.java.net/svn/mojarra~svn/trunk@15128 761efcf2-d34d-6b61-9145-99dcacc3edf1

--- test/servlet30/lifecycleServerStatePositive/src/main/java/com/sun/faces/test/servlet30/lifecycleServerStatePositive/Bean.java
+++ test/servlet30/lifecycleServerStatePositive/src/main/java/com/sun/faces/test/servlet30/lifecycleServerStatePositive/Bean.java
@@ -1,30 +1,30 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
+ * 
  * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
- *
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
  * and Distribution License("CDDL") (collectively, the "License").  You
  * may not use this file except in compliance with the License.  You can
  * obtain a copy of the License at
- * https://glassfish.java.net/public/CDDLGPL_1_1.html
+ * https://glassfish.java.net/public/CDDL+GPL_1_1.html
  * or packager/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
- *
+ * 
  * When distributing the software, include this License Header Notice in each
  * file and include the License file at packager/legal/LICENSE.txt.
- *
+ * 
  * GPL Classpath Exception:
  * Oracle designates this particular file as subject to the "Classpath"
  * exception as provided by Oracle in the GPL Version 2 section of the License
  * file that accompanied this code.
- *
+ * 
  * Modifications:
  * If applicable, add the following below the License Header, with the fields
  * enclosed by brackets [] replaced by your own identifying information:
  * "Portions Copyright [year] [name of copyright owner]"
- *
+ * 
  * Contributor(s):
  * If you wish your version of this file to be governed by only the CDDL or
  * only the GPL Version 2, indicate your decision by adding "[Contributor]
@@ -36,38 +36,36 @@
  * and therefore, elected the GPL Version 2 license, then the option applies
  * only if the new code is made subject to such option by the copyright
  * holder.
+
  */
-package com.sun.faces.test.agnostic.lifeycle.unserializableServerState_positive;
+package com.sun.faces.test.servlet30.lifecycleServerStatePositive;
 
-import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import java.io.IOException;
+import java.io.NotSerializableException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.event.ActionEvent;
 
-import org.junit.*;
-import static org.junit.Assert.*;
-
-public class Issue1127IT {
-
-    private String webUrl;
-    private WebClient webClient;
-
-    @Before
-    public void setUp() {
-        webUrl = System.getProperty("integration.url");
-        webClient = new WebClient();
+@ManagedBean
+@RequestScoped
+public class Bean {
+    
+    public void putNonSerializableDataInState(ActionEvent ae) {
+        ae.getComponent().getAttributes().put("myAttribute", new NotSerializableClass());
     }
-
-    @After
-    public void tearDown() {
-        webClient.closeAllWindows();
+    
+    private static class NotSerializableClass implements Serializable {
+        private void writeObject(ObjectOutputStream out) throws IOException
+        {
+            throw new NotSerializableException("This class is not really Serializable");
+        }
+        private void readObject(ObjectInputStream in) throws IOException
+        {
+            throw new NotSerializableException("This class is not really Serializable");
+        }
     }
-
-    @Test
-    public void testConfigurationEffective() throws Exception {
-        HtmlPage page = webClient.getPage(webUrl);
-        HtmlSubmitInput button = (HtmlSubmitInput) page.getElementById("button");
-        webClient.getOptions().setThrowExceptionOnFailingStatusCode(true);
-        button.click();
-        
-    }
+    
 }
