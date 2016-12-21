ed9b086548dc27aadaf67d70f2bc76e98d211124
Manfred Riem <manfred.riem@oracle.com>
2016-12-21 12:15:46 -0700

    Excluding tests against WLS

--- test/servlet30/systest/src/test/java/com/sun/faces/test/servlet30/systest/ManagedBeanIT.java
+++ test/servlet30/systest/src/test/java/com/sun/faces/test/servlet30/systest/ManagedBeanIT.java
@@ -43,6 +43,7 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import static com.sun.faces.test.junit.JsfServerExclude.WEBLOGIC_12_1_3;
 import static com.sun.faces.test.junit.JsfServerExclude.WEBLOGIC_12_1_4;
+import static com.sun.faces.test.junit.JsfServerExclude.WEBLOGIC_12_2_1;
 import com.sun.faces.test.junit.JsfTest;
 import com.sun.faces.test.junit.JsfTestRunner;
 import static com.sun.faces.test.junit.JsfVersion.JSF_2_2_0;
@@ -96,7 +97,7 @@ public class ManagedBeanIT {
      * 
      * 20140903 - edburns, mriem
      */
-    @JsfTest(value=JSF_2_2_0, excludes = {WEBLOGIC_12_1_3, WEBLOGIC_12_1_4})
+    @JsfTest(value=JSF_2_2_0, excludes = {WEBLOGIC_12_1_3, WEBLOGIC_12_1_4, WEBLOGIC_12_2_1})
     @Test
     public void testManagedBean3() throws Exception {
         webClient.setThrowExceptionOnFailingStatusCode(false);
@@ -112,7 +113,7 @@ public class ManagedBeanIT {
      * 
      * 20140903 - edburns, mriem
      */
-    @JsfTest(value=JSF_2_2_0, excludes = {WEBLOGIC_12_1_3, WEBLOGIC_12_1_4})
+    @JsfTest(value=JSF_2_2_0, excludes = {WEBLOGIC_12_1_3, WEBLOGIC_12_1_4, WEBLOGIC_12_2_1})
     @Test
     public void testManagedBean7() throws Exception {
         webClient.setThrowExceptionOnFailingStatusCode(false);
--- test/servlet30/systest/src/test/java/com/sun/faces/test/servlet30/systest/TLValidatorIT.java
+++ test/servlet30/systest/src/test/java/com/sun/faces/test/servlet30/systest/TLValidatorIT.java
@@ -43,6 +43,7 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import static com.sun.faces.test.junit.JsfServerExclude.WEBLOGIC_12_1_3;
 import static com.sun.faces.test.junit.JsfServerExclude.WEBLOGIC_12_1_4;
+import static com.sun.faces.test.junit.JsfServerExclude.WEBLOGIC_12_2_1;
 import com.sun.faces.test.junit.JsfTest;
 import com.sun.faces.test.junit.JsfTestRunner;
 import static com.sun.faces.test.junit.JsfVersion.JSF_2_2_0;
@@ -140,7 +141,7 @@ public class TLValidatorIT {
      * 
      * 20140903 - edburns, mriem
      */
-    @JsfTest(value=JSF_2_2_0, excludes = {WEBLOGIC_12_1_3, WEBLOGIC_12_1_4})
+    @JsfTest(value=JSF_2_2_0, excludes = {WEBLOGIC_12_1_3, WEBLOGIC_12_1_4, WEBLOGIC_12_2_1})
     @Test
     public void testElValidatorActionRefFail() throws Exception {
         webClient.setThrowExceptionOnFailingStatusCode(false);
@@ -156,7 +157,7 @@ public class TLValidatorIT {
      * 
      * 20140903 - edburns, mriem
      */
-    @JsfTest(value=JSF_2_2_0, excludes = {WEBLOGIC_12_1_3, WEBLOGIC_12_1_4})
+    @JsfTest(value=JSF_2_2_0, excludes = {WEBLOGIC_12_1_3, WEBLOGIC_12_1_4, WEBLOGIC_12_2_1})
     @Test
     public void testElValidatorComponentFail() throws Exception {
         webClient.setThrowExceptionOnFailingStatusCode(false);
