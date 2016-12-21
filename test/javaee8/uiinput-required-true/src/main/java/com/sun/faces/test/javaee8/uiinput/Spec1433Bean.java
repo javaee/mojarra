d881ea58118d1832ad61f5c1b68fc751427937fe
Ed Burns <edward.burns@oracle.com>
2016-12-21 00:27:34 -0500

    JAVASERVERFACES_SPEC_PUBLIC-1433
    	modified:   test/javaee8/pom.xml
    	new file:   test/javaee8/uiinput-required-true/pom.xml
    	renamed:    test/javaee8/uiinput/src/main/java/Spec1433Bean.java -> test/javaee8/uiinput-required-true/src/main/java/com/sun/faces/test/javaee8/uiinput/Spec1433Bean.java
    	new file:   test/javaee8/uiinput-required-true/src/main/webapp/WEB-INF/beans.xml
    	new file:   test/javaee8/uiinput-required-true/src/main/webapp/WEB-INF/faces-config.xml
    	new file:   test/javaee8/uiinput-required-true/src/main/webapp/WEB-INF/glassfish-web.xml
    	new file:   test/javaee8/uiinput-required-true/src/main/webapp/WEB-INF/web.xml
    	renamed:    test/javaee8/uiinput/src/main/webapp/Spec1433.xhtml -> test/javaee8/uiinput-required-true/src/main/webapp/spec1433.xhtml
    	renamed:    test/javaee8/uiinput/src/test/java/com/sun/faces/test/javaee8/uiinput/Spec1433IT.java -> test/javaee8/uiinput-required-true/src/test/java/com/sun/faces/test/javaee8/uiinput/Spec1433IT.java
    	modified:   test/javaee8/uiinput/src/main/webapp/WEB-INF/web.xml
    
    - Moved test to separate web app.

--- test/javaee8/uiinput-required-true/src/main/java/com/sun/faces/test/javaee8/uiinput/Spec1433Bean.java
+++ test/javaee8/uiinput-required-true/src/main/java/com/sun/faces/test/javaee8/uiinput/Spec1433Bean.java
@@ -1,3 +1,5 @@
+package com.sun.faces.test.javaee8.uiinput;
+
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
@@ -8,7 +10,7 @@
  * and Distribution License("CDDL") (collectively, the "License").  You
  * may not use this file except in compliance with the License.  You can
  * obtain a copy of the License at
- * https://glassfish.java.net/public/CDDLGPL_1_1.html
+ * https://glassfish.java.net/public/CDDL+GPL_1_1.html
  * or packager/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
  *
@@ -37,57 +39,20 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-package com.sun.faces.test.javaee8.uiinput;
 
-import static org.junit.Assert.assertTrue;
+import javax.enterprise.inject.Model;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+@Model
+public class Spec1433Bean {
 
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
-import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
-import com.sun.faces.test.junit.JsfTestRunner;
+    private String value;
 
-@RunWith(JsfTestRunner.class)
-public class Spec1433IT {
-
-    private String webUrl;
-    private WebClient webClient;
-
-    @Before
-    public void setUp() {
-        webUrl = System.getProperty("integration.url");
-        webClient = new WebClient();
-        webClient.getOptions().setJavaScriptEnabled(true);
-        webClient.setJavaScriptTimeout(120000);
+    public String getValue() {
+        return value;
     }
 
-    @Test
-    public void testSpec1433() throws Exception {
-
-        HtmlPage page = webClient.getPage(webUrl + "Spec1433.xhtml");
-        HtmlTextInput input = (HtmlTextInput) page.getElementById("input");
-        input.setAttribute("id", "");
-        input.setAttribute("name", "");
-        input.setValueAttribute("non-empty value");
-
-	HtmlSubmitInput button = (HtmlSubmitInput) page.getHtmlElementById("submit");
-
-        page = button.click();
-
-        String output = page.asText();
-
-        assertTrue(output.contains("Spec1433Bean Validator Message"));
-
-    }
-
-    @After
-    public void tearDown() {
-        webClient.close();
+    public void setValue(String value) {
+        this.value = value;
     }
 
 }
--- test/javaee8/uiinput-required-true/src/test/java/com/sun/faces/test/javaee8/uiinput/Spec1433IT.java
+++ test/javaee8/uiinput-required-true/src/test/java/com/sun/faces/test/javaee8/uiinput/Spec1433IT.java
@@ -8,7 +8,7 @@
  * and Distribution License("CDDL") (collectively, the "License").  You
  * may not use this file except in compliance with the License.  You can
  * obtain a copy of the License at
- * https://glassfish.java.net/public/CDDL+GPL_1_1.html
+ * https://glassfish.java.net/public/CDDLGPL_1_1.html
  * or packager/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
  *
@@ -37,20 +37,57 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
+package com.sun.faces.test.javaee8.uiinput;
 
-import javax.enterprise.inject.Model;
+import static org.junit.Assert.assertTrue;
 
-@Model
-public class Spec1433Bean {
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-    private String value;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+import com.sun.faces.test.junit.JsfTestRunner;
 
-    public String getValue() {
-        return value;
+@RunWith(JsfTestRunner.class)
+public class Spec1433IT {
+
+    private String webUrl;
+    private WebClient webClient;
+
+    @Before
+    public void setUp() {
+        webUrl = System.getProperty("integration.url");
+        webClient = new WebClient();
+        webClient.getOptions().setJavaScriptEnabled(true);
+        webClient.setJavaScriptTimeout(120000);
     }
 
-    public void setValue(String value) {
-        this.value = value;
+    @Test
+    public void testSpec1433() throws Exception {
+
+        HtmlPage page = webClient.getPage(webUrl + "spec1433.xhtml");
+        HtmlTextInput input = (HtmlTextInput) page.getElementById("input");
+        input.setAttribute("id", "");
+        input.setAttribute("name", "");
+        input.setValueAttribute("non-empty value");
+
+	HtmlSubmitInput button = (HtmlSubmitInput) page.getHtmlElementById("submit");
+
+        page = button.click();
+
+        String output = page.asText();
+
+        assertTrue(output.contains("Spec1433Bean Validator Message"));
+
+    }
+
+    @After
+    public void tearDown() {
+        webClient.close();
     }
 
 }
