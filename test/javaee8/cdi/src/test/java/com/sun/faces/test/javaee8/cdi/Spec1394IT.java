1649b3c19d3d649489ffc1bed6bfb5552d1a1700
Manfred Riem <manfred.riem@oracle.com>
2016-12-21 12:59:32 -0700

    Excluding tests against WLS

--- test/javaee8/cdi/src/test/java/com/sun/faces/test/javaee8/cdi/Spec1394IT.java
+++ test/javaee8/cdi/src/test/java/com/sun/faces/test/javaee8/cdi/Spec1394IT.java
@@ -50,6 +50,7 @@ import org.junit.runner.RunWith;
 
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import static com.sun.faces.test.junit.JsfServerExclude.WEBLOGIC_12_2_1;
 import com.sun.faces.test.junit.JsfTest;
 import com.sun.faces.test.junit.JsfTestRunner;
 
@@ -75,11 +76,11 @@ public class Spec1394IT {
     }
 
     @Test
-    @JsfTest(value = JSF_2_3_0_M03, excludes = { WEBLOGIC_12_1_4 })
+    @JsfTest(value = JSF_2_3_0_M03, excludes = {WEBLOGIC_12_1_4, WEBLOGIC_12_2_1})
     public void testResourceHandler() throws Exception {
-        
+
         HtmlPage page = webClient.getPage(webUrl + "injectResourceHandler.xhtml");
-        
+
         assertTrue(page.asXml().contains("jsfLibraryExists:true"));
     }
 
--- test/javaee8/cdi/src/test/java/com/sun/faces/test/javaee8/cdi/Spec1418IT.java
+++ test/javaee8/cdi/src/test/java/com/sun/faces/test/javaee8/cdi/Spec1418IT.java
@@ -52,6 +52,7 @@ import org.junit.runner.RunWith;
 
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import static com.sun.faces.test.junit.JsfServerExclude.WEBLOGIC_12_2_1;
 import com.sun.faces.test.junit.JsfTest;
 import com.sun.faces.test.junit.JsfTestRunner;
 
@@ -77,31 +78,31 @@ public class Spec1418IT {
     }
 
     @Test
-    @JsfTest(value = JSF_2_3_0_M06, excludes = { WEBLOGIC_12_1_4 })
+    @JsfTest(value = JSF_2_3_0_M06, excludes = {WEBLOGIC_12_1_4, WEBLOGIC_12_2_1})
     public void testManagedPropertyInteger() throws Exception {
-        
+
         HtmlPage page = webClient.getPage(webUrl + "injectManagedProperty.xhtml");
-        
+
         assertTrue(page.asXml().contains("integer1:42"));
         assertTrue(page.asXml().contains("integer2:99"));
         assertTrue(page.asXml().contains("integer3:123"));
     }
-    
+
     @Test
-    @JsfTest(value = JSF_2_3_0_M06, excludes = { WEBLOGIC_12_1_4 })
+    @JsfTest(value = JSF_2_3_0_M06, excludes = {WEBLOGIC_12_1_4, WEBLOGIC_12_2_1})
     public void testManagedPropertyString() throws Exception {
-        
+
         HtmlPage page = webClient.getPage(webUrl + "injectManagedProperty.xhtml?test=foo");
-        
+
         assertTrue(page.asXml().contains("testParam:foo"));
     }
-    
+
     @Test
-    @JsfTest(value = JSF_2_3_0_M06, excludes = { WEBLOGIC_12_1_4 })
+    @JsfTest(value = JSF_2_3_0_M06, excludes = {WEBLOGIC_12_1_4, WEBLOGIC_12_2_1})
     public void testManagedPropertyGenericMap() throws Exception {
-        
+
         HtmlPage page = webClient.getPage(webUrl + "injectManagedProperty.xhtml");
-        
+
         assertTrue(page.asXml().contains("stringMap:bar"));
         assertTrue(page.asXml().contains("integerMap:10"));
     }
