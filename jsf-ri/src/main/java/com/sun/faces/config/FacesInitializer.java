b702bb361a5b47be1a3e3cfd875ab26b324e3d01
arjan tijms <arjan.tijms@gmail.com>
2016-12-28 20:09:39 +0100

    https://java.net/jira/browse/JAVASERVERFACES_SPEC_PUBLIC-1396
    FacesInitializer now takes into account the following two cases:
    
    * Init of WebSocket for Tyrus when there's an existing Servlet mapping
    but no JSF specific classes
    * Application configured via @FacesConfig

--- jsf-ri/src/main/java/com/sun/faces/config/FacesInitializer.java
+++ jsf-ri/src/main/java/com/sun/faces/config/FacesInitializer.java
@@ -41,43 +41,47 @@
 package com.sun.faces.config;
 
 
-import com.sun.faces.RIConstants;
 import static com.sun.faces.RIConstants.ANNOTATED_CLASSES;
+import static com.sun.faces.RIConstants.FACES_INITIALIZER_MAPPINGS_ADDED;
+import static java.lang.Boolean.TRUE;
+
+import java.net.MalformedURLException;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.Resource;
+import javax.faces.annotation.FacesConfig;
 import javax.faces.application.ResourceDependencies;
 import javax.faces.application.ResourceDependency;
 import javax.faces.bean.ManagedBean;
 import javax.faces.component.FacesComponent;
 import javax.faces.component.UIComponent;
-import javax.faces.convert.FacesConverter;
+import javax.faces.component.behavior.FacesBehavior;
 import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
 import javax.faces.event.ListenerFor;
 import javax.faces.event.ListenersFor;
+import javax.faces.event.NamedEvent;
+import javax.faces.event.PhaseListener;
 import javax.faces.render.FacesBehaviorRenderer;
 import javax.faces.render.Renderer;
 import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
+import javax.faces.view.facelets.FaceletsResourceResolver;
 import javax.faces.webapp.FacesServlet;
 import javax.servlet.ServletContainerInitializer;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 import javax.servlet.annotation.HandlesTypes;
-import java.util.Set;
-import java.util.Map;
-import java.net.MalformedURLException;
-import java.util.HashSet;
-import javax.annotation.Resource;
-import javax.faces.component.behavior.FacesBehavior;
-import javax.faces.event.NamedEvent;
-import javax.faces.event.PhaseListener;
-import javax.faces.view.facelets.FaceletsResourceResolver;
 import javax.websocket.Endpoint;
 import javax.websocket.server.ServerApplicationConfig;
 import javax.websocket.server.ServerContainer;
 import javax.websocket.server.ServerEndpoint;
 
 /**
- * Adds mappings <em>/faces</em>, <em>*.jsf</em>, and <em>*.faces</em> for the
+ * Adds mappings <em>*.xhtml</em>, <em>/faces</em>, <em>*.jsf</em>, and <em>*.faces</em> for the
  * FacesServlet (if it hasn't already been mapped) if the following conditions
  * are met:
  *
@@ -87,7 +91,10 @@ import javax.websocket.server.ServerEndpoint;
  *       empty, or
  *    </li>
  *    <li>
- *       /WEB-INF/faces-config.xml exists.
+ *       /WEB-INF/faces-config.xml exists, or
+ *    </li>
+ *     <li>
+ *       A CDI enabled bean with qualifier FacesConfig can be obtained
  *    </li>
  * </ul>
  */
@@ -100,6 +107,7 @@ import javax.websocket.server.ServerEndpoint;
       FacesBehaviorRenderer.class,
       FacesComponent.class,
       FacesConverter.class,
+      FacesConfig.class, // Should actually be check for enabled bean, but difficult to guarantee, see SERVLET_SPEC-79
       FacesValidator.class,
       ListenerFor.class,
       ListenersFor.class,
@@ -121,26 +129,29 @@ public class FacesInitializer implements ServletContainerInitializer {
 
     private static final String FACES_SERVLET_CLASS = FacesServlet.class.getName();
 
+    
     // -------------------------------- Methods from ServletContainerInitializer
 
-
     @Override
-    public void onStartup(Set<Class<?>> classes, ServletContext servletContext)
-        throws ServletException {
+    public void onStartup(Set<Class<?>> classes, ServletContext servletContext) throws ServletException {
 
         Set<Class<?>> annotatedClasses = new HashSet<Class<?>>();
         if (classes != null) {
             annotatedClasses.addAll(classes);
         }
         servletContext.setAttribute(ANNOTATED_CLASSES, annotatedClasses);
+        
+        boolean appHasSomeJsfContent = appMayHaveSomeJsfContent(classes, servletContext);
+        boolean appHasFacesServlet = getExistingFacesServletRegistration(servletContext) != null;
 
-        if (appMayHaveSomeJsfContent(classes, servletContext)) {
+        if (appHasSomeJsfContent || appHasFacesServlet) {
             InitFacesContext initFacesContext = new InitFacesContext(servletContext);
-            if (null == initFacesContext) {
-                throw new ServletException("Unable to initialize Mojarra");
-            }
             try {
-                handleMappingConcerns(servletContext);
+                if (appHasSomeJsfContent) {
+                    // Only look at mapping concerns if there is JSF content
+                    handleMappingConcerns(servletContext);
+                }
+                // Other concerns also handled if there is an existing Faces Servlet mapping
                 handleWebSocketConcerns(servletContext);
             } finally {
                 // Bug 20458755: The InitFacesContext was not being cleaned up, resulting in
@@ -151,25 +162,42 @@ public class FacesInitializer implements ServletContainerInitializer {
             }
         }
     }
+    
+    
+    // --------------------------------------------------------- Private Methods
+    
+    private boolean appMayHaveSomeJsfContent(Set<Class<?>> classes, ServletContext context) {
 
-    private void handleMappingConcerns(ServletContext servletContext)
-            throws ServletException {
-        Map<String,? extends ServletRegistration> existing = servletContext.getServletRegistrations();
-        for (ServletRegistration registration : existing.values()) {
-            if (FACES_SERVLET_CLASS.equals(registration.getClassName())) {
-                // FacesServlet has already been defined, so we're
-                // not going to add additional mappings;
-                if ( isADFApplication() ) {
-                    //For Bug 21114997 and 21322338
-                    registration.addMapping("*.xhtml", "*.jsf");
-                }
-                return;
-            }
+        if (classes != null && !classes.isEmpty()) {
+            return true;
         }
 
-        ServletRegistration reg =
-                servletContext.addServlet("FacesServlet",
-                        "javax.faces.webapp.FacesServlet");
+        // No JSF specific classes found, check for a WEB-INF/faces-config.xml
+        try {
+            if (context.getResource("/WEB-INF/faces-config.xml") != null) {
+                return true;
+            }
+        } catch (MalformedURLException mue) {
+
+        }
+
+        return false;
+    }
+
+    private void handleMappingConcerns(ServletContext servletContext) throws ServletException {
+        
+        ServletRegistration existingFacesServletRegistration = getExistingFacesServletRegistration(servletContext);
+        if (existingFacesServletRegistration != null) {
+            // FacesServlet has already been defined, so we're
+            // not going to add additional mappings;
+            if (isADFApplication()) {
+                // For Bug 21114997 and 21322338 add additional mappings
+                existingFacesServletRegistration.addMapping("*.xhtml", "*.jsf");
+            }
+            return;
+        }
+
+        ServletRegistration reg = servletContext.addServlet("FacesServlet", "javax.faces.webapp.FacesServlet");
 
         if ("true".equalsIgnoreCase(servletContext.getInitParameter("javax.faces.DISABLE_FACESSERVLET_TO_XHTML")) ) {
             reg.addMapping("/faces/*", "*.jsf", "*.faces");
@@ -177,7 +205,7 @@ public class FacesInitializer implements ServletContainerInitializer {
             reg.addMapping("/faces/*", "*.jsf", "*.faces", "*.xhtml");
         }
 
-        servletContext.setAttribute(RIConstants.FACES_INITIALIZER_MAPPINGS_ADDED, Boolean.TRUE);
+        servletContext.setAttribute(FACES_INITIALIZER_MAPPINGS_ADDED, TRUE);
 
         // The following line is temporary until we can solve an ordering
         // issue in V3.  Right now the JSP container looks for a mapping
@@ -187,6 +215,17 @@ public class FacesInitializer implements ServletContainerInitializer {
         // definition from our TLD nor will it parse cause or JSP TLDs to
         // be parsed.
         servletContext.addListener(com.sun.faces.config.ConfigureListener.class);
+    }
+    
+    private ServletRegistration getExistingFacesServletRegistration(ServletContext servletContext) {
+        Map<String,? extends ServletRegistration> existing = servletContext.getServletRegistrations();
+        for (ServletRegistration registration : existing.values()) {
+            if (FACES_SERVLET_CLASS.equals(registration.getClassName())) {
+                return registration;
+            }
+        }
+        
+        return null;
     }
 
     private void handleWebSocketConcerns(ServletContext ctx) throws ServletException {
@@ -202,17 +241,17 @@ public class FacesInitializer implements ServletContainerInitializer {
 
         ClassLoader cl = ctx.getClassLoader();
 
-        Class tyrusInitializerClass;
+        Class<?> tyrusInitializerClass;
         try {
             tyrusInitializerClass = cl.loadClass("org.glassfish.tyrus.servlet.TyrusServletContainerInitializer");
         } catch (ClassNotFoundException cnfe) {
             // No possibility of WebSocket.
             return;
         }
+        
         try {
-            ServletContainerInitializer tyrusInitializer =
-                    (ServletContainerInitializer) tyrusInitializerClass.newInstance();
-            Class configClass = cl.loadClass("org.glassfish.tyrus.server.TyrusServerConfiguration");
+            ServletContainerInitializer tyrusInitializer = (ServletContainerInitializer) tyrusInitializerClass.newInstance();
+            Class<?> configClass = cl.loadClass("org.glassfish.tyrus.server.TyrusServerConfiguration");
 
             // List of classes must be non empty. TyrusServerConfiguration is ignored,
             // so we add that as class to trigger Tyrus to initialize
@@ -226,14 +265,13 @@ public class FacesInitializer implements ServletContainerInitializer {
 
     }
 
-    // --------------------------------------------------------- Private Methods
     private boolean isADFApplication() {
 
         boolean hasResource = false;
         try {
             ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
-            if (null != contextClassLoader) {
-                hasResource = (contextClassLoader.getResource("oracle/adf/view/rich/context/AdfFacesContext.class")  != null );
+            if (contextClassLoader != null) {
+                hasResource = contextClassLoader.getResource("oracle/adf/view/rich/context/AdfFacesContext.class") != null;
             }
         } catch (Exception e) {
             // Intentionally swallow exception.  This should be logged
@@ -241,24 +279,8 @@ public class FacesInitializer implements ServletContainerInitializer {
             // not be used for this class.  I assume that means Logging, and
             // not Kenny Loggins.
         }
+        
         return hasResource;
     }
 
-    private boolean appMayHaveSomeJsfContent(Set<Class<?>> classes,
-                                        ServletContext context) {
-
-        if (classes != null && !classes.isEmpty()) {
-            return true;
-        }
-
-        // no JSF specific parameters found, check for a WEB-INF/faces-config.xml
-        try {
-            return (context.getResource("/WEB-INF/faces-config.xml") != null);
-        } catch (MalformedURLException mue) {
-
-        }
-
-        return false;
-
-    }
 }
