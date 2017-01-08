24b4d83731548a854945edc629d3e95cff59a545
Thomas Andraschko <tandraschko@apache.org>
2017-01-08 15:39:08 +0100

    https://java.net/jira/browse/JAVASERVERFACES_SPEC_PUBLIC-1238 - review fixes

--- test/unit/src/test/java/javax/faces/component/SearchExpressionHandlerTest.java
+++ test/unit/src/test/java/javax/faces/component/SearchExpressionHandlerTest.java
@@ -959,6 +959,11 @@ public class SearchExpressionHandlerTest extends JUnitFacesTestCaseBase {
         assertTrue(result.contains(innerContainer));
     }
     
+    /**
+     * The SearchExpression API was inspired by PrimeFaces.
+     * This tests only tests, if PFS (PrimeFaces Selectors -> jQuery like selectors; like @(#myId > .myStyle))
+     * can be correctly handled by the API+IMPL as a passthrough expression.
+     */
     public void test_PFS() {
         CompositeSearchKeywordResolver s = (CompositeSearchKeywordResolver) application.getSearchKeywordResolver();
         s.add(new SearchKeywordResolver() {
