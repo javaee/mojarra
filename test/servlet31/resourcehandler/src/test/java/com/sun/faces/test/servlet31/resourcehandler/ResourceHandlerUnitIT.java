68b3db14390d7c50c25e15cedb719df4a125e309
Manfred Riem <manfred.riem@oracle.com>
2017-01-20 08:15:31 -0700

    Make sure test is run using JsfTestRunner

--- test/servlet31/resourcehandler/src/test/java/com/sun/faces/test/servlet31/resourcehandler/ResourceHandlerUnitIT.java
+++ test/servlet31/resourcehandler/src/test/java/com/sun/faces/test/servlet31/resourcehandler/ResourceHandlerUnitIT.java
@@ -51,7 +51,10 @@ import org.junit.Test;
 import com.gargoylesoftware.htmlunit.TextPage;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.sun.faces.test.junit.JsfTest;
+import com.sun.faces.test.junit.JsfTestRunner;
+import org.junit.runner.RunWith;
 
+@RunWith(JsfTestRunner.class)
 public class ResourceHandlerUnitIT {
 
     private String webUrl;
