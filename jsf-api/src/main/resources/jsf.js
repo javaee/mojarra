6bcbf1c87b3e0effeac7bd586153b34a788b8c77
Kyle Stiemann <stiemannkj1@gmail.com>
2018-03-06 10:26:55 -0500

    Fix #4340 Script resources in <body> are never run during Ajax requests

--- jsf-api/src/main/resources/jsf.js
+++ jsf-api/src/main/resources/jsf.js
@@ -815,7 +815,7 @@ if (!((jsf && jsf.specversion && jsf.specversion >= 23000 ) &&
                     // Get scripts from text
                     scripts = stripScripts(src);
                     // Remove scripts from text
-                    src = src.replace(/<script[^>]*type="text\/javascript"*>([\S\s]*?)<\/script>/igm,"");
+                    src = src.replace(/<script[^>]*type="text\/javascript"[^>]*>([\S\s]*?)<\/script>/igm,"");
                     temp.innerHTML = src;
                 }
             }
@@ -1492,7 +1492,7 @@ if (!((jsf && jsf.specversion && jsf.specversion >= 23000 ) &&
                         // Get scripts from text
                         scripts = stripScripts(src);
                         // Remove scripts from text
-                        newsrc = src.replace(/<script[^>]*type="text\/javascript"*>([\S\s]*?)<\/script>/igm, "");
+                        newsrc = src.replace(/<script[^>]*type="text\/javascript"[^>]*>([\S\s]*?)<\/script>/igm, "");
                         elementReplace(getBodyElement(newsrc), docBody);
                         runScripts(scripts);
                     } catch (e) {
@@ -1555,7 +1555,7 @@ if (!((jsf && jsf.specversion && jsf.specversion >= 23000 ) &&
                         // Get the scripts from the text
                         scripts = stripScripts(html);
                         // Remove scripts from text
-                        html = html.replace(/<script[^>]*type="text\/javascript"*>([\S\s]*?)<\/script>/igm,"");
+                        html = html.replace(/<script[^>]*type="text\/javascript"[^>]*>([\S\s]*?)<\/script>/igm,"");
                         parserElement.innerHTML = '<table>' + html + '</table>';
                     }
                     var newElement = parserElement.firstChild;
@@ -1583,7 +1583,7 @@ if (!((jsf && jsf.specversion && jsf.specversion >= 23000 ) &&
                         // Get the scripts from the text
                         scripts = stripScripts(html);
                         // Remove scripts from text
-                        html = html.replace(/<script[^>]*type="text\/javascript"*>([\S\s]*?)<\/script>/igm,"");
+                        html = html.replace(/<script[^>]*type="text\/javascript"[^>]*>([\S\s]*?)<\/script>/igm,"");
                         parserElement.innerHTML = html;
                     }
                     replaceNode(parserElement.firstChild, element);
@@ -1621,7 +1621,7 @@ if (!((jsf && jsf.specversion && jsf.specversion >= 23000 ) &&
                 // Get the scripts from the text
                 scripts = stripScripts(html);
                 // Remove scripts from text
-                html = html.replace(/<script[^>]*type="text\/javascript"*>([\S\s]*?)<\/script>/igm,"");
+                html = html.replace(/<script[^>]*type="text\/javascript"[^>]*>([\S\s]*?)<\/script>/igm,"");
             }
             var tempElement = document.createElement('div');
             var newElement = null;
