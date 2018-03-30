cdad53dca6d86aeec1b634557ed1593c4674ef25
Kyle Stiemann <stiemannkj1@gmail.com>
2018-03-30 14:40:45 -0400

    Fix #4358: The wrong content type of "text/html" is set for execute "@all" Ajax responses

--- impl/src/main/java/com/sun/faces/context/PartialViewContextImpl.java
+++ impl/src/main/java/com/sun/faces/context/PartialViewContextImpl.java
@@ -45,6 +45,8 @@ import static com.sun.faces.renderkit.RenderKitUtils.PredefinedPostbackParameter
 import static com.sun.faces.renderkit.RenderKitUtils.PredefinedPostbackParameter.PARTIAL_RESET_VALUES_PARAM;
 import static javax.faces.FactoryFinder.VISIT_CONTEXT_FACTORY;
 
+import com.sun.faces.RIConstants;
+
 import java.io.IOException;
 import java.io.Writer;
 import java.util.ArrayList;
@@ -313,7 +315,7 @@ import com.sun.faces.util.Util;
                 ctx.setResponseWriter(writer);
 
                 ExternalContext exContext = ctx.getExternalContext();
-                exContext.setResponseContentType("text/xml");
+                exContext.setResponseContentType(RIConstants.TEXT_XML_CONTENT_TYPE);
                 exContext.addResponseHeader("Cache-Control", "no-cache");
                 
 //                String encoding = writer.getCharacterEncoding( );
@@ -572,12 +574,12 @@ import com.sun.faces.util.Util;
             if (viewRoot != null) {
                 responseWriter =
                     ctx.getRenderKit().createResponseWriter(out,
-                    "text/xml", encoding);
+                    RIConstants.TEXT_XML_CONTENT_TYPE, encoding);
             } else {
                 RenderKitFactory factory = (RenderKitFactory)
                     FactoryFinder.getFactory(FactoryFinder.RENDER_KIT_FACTORY);
                 RenderKit renderKit = factory.getRenderKit(ctx, RenderKitFactory.HTML_BASIC_RENDER_KIT);
-                responseWriter = renderKit.createResponseWriter(out, "text/xml", encoding);
+                responseWriter = renderKit.createResponseWriter(out, RIConstants.TEXT_XML_CONTENT_TYPE, encoding);
             }
         }
         if (responseWriter instanceof PartialResponseWriter)  {
@@ -682,7 +684,7 @@ import com.sun.faces.util.Util;
             super(null);
             this.ctx = ctx;
             ExternalContext extCtx = ctx.ctx.getExternalContext();
-            extCtx.setResponseContentType("text/xml");
+            extCtx.setResponseContentType(RIConstants.TEXT_XML_CONTENT_TYPE);
             extCtx.setResponseCharacterEncoding(extCtx.getRequestCharacterEncoding());
             extCtx.setResponseBufferSize(ctx.ctx.getExternalContext().getResponseBufferSize());
         }
