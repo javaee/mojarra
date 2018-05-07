3cf226d3e355e796f9da8074905d4d5edb8034c2
arjantijms <arjan.tijms@gmail.com>
2018-05-07 13:57:01 +0200

    Added accidentally removed notNull checks back

--- impl/src/main/java/com/sun/faces/application/applicationimpl/InstanceFactory.java
+++ impl/src/main/java/com/sun/faces/application/applicationimpl/InstanceFactory.java
@@ -312,6 +312,10 @@ public class InstanceFactory {
     }
     
     public UIComponent createComponent(FacesContext context, String componentType, String rendererType) {
+        
+        notNull(CONTEXT, context);
+        notNull(COMPONENT_TYPE, componentType);
+        
         return createComponentApplyAnnotations(context, componentType, rendererType, true);
     }
     
